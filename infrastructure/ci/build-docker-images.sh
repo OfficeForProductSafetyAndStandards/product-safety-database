2f86d6f98085b4aa12a9d886d4b99d28527011bb
Steve Lorek <steve@stevelorek.com>
2019-10-03 15:07:31 +0100

    Use configurable Docker Hub organisation name

--- infrastructure/ci/build-docker-images.sh
+++ infrastructure/ci/build-docker-images.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
-# Builds Docker images for components that have changes, and then pushes all current images 
+# Builds Docker images for components that have changes, and then pushes all current images
 # tagged with the build ID to DockerHub.
 set -ex
 
 COMPONENTS=(
-    'antivirus'
-    'maintenance'
-    'db'
-    'keycloak'
-    'cosmetics-web'
-    'cosmetics-worker'
     'psd-web'
     'psd-worker'
 )
@@ -26,17 +20,17 @@ CHANGED_COMPONENTS="$(./infrastructure/ci/get-changed-components.sh)"
 for COMPONENT in "${COMPONENTS[@]}"; do
     if [[ $CHANGED_COMPONENTS =~ ((^| )$COMPONENT($| )) ]]; then
         echo "Building component $COMPONENT"
-        docker-compose -f docker-compose.yml -f docker-compose.ci.yml build $COMPONENT
-    elif docker_tag_exists beisopss/$COMPONENT $BRANCH; then
+        DOCKER_ORG=$DOCKER_ORG docker-compose -f docker-compose.yml -f docker-compose.ci.yml build $COMPONENT
+    elif docker_tag_exists $DOCKER_ORG/$COMPONENT $BRANCH; then
         echo "No changes for component $COMPONENT, pulling $BRANCH image"
-        docker pull beisopss/$COMPONENT:$BRANCH
-        docker tag beisopss/$COMPONENT:$BRANCH beisopss/$COMPONENT:$BUILD_ID
+        docker pull $DOCKER_ORG/$COMPONENT:$BRANCH
+        docker tag $DOCKER_ORG/$COMPONENT:$BRANCH $DOCKER_ORG/$COMPONENT:$BUILD_ID
     else
         echo "No changes for component $COMPONENT, but $BRANCH image does not exist in repo"
         echo "Building component $COMPONENT"
-        docker-compose -f docker-compose.yml -f docker-compose.ci.yml build $COMPONENT
+        DOCKER_ORG=$DOCKER_ORG docker-compose -f docker-compose.yml -f docker-compose.ci.yml build $COMPONENT
     fi
 
     echo "Pushing image for component $COMPONENT with tag $BUILD_ID"
-    docker push beisopss/$COMPONENT:$BUILD_ID
+    docker push $DOCKER_ORG/$COMPONENT:$BUILD_ID
 done
