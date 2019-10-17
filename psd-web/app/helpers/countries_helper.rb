3a5bc0b1f9ff56e9fae75eea10b84ebc7bd3de2b
yann marquet <ymarquet@gmail.com>
2019-10-17 15:44:13 +0100

    appease rubcop

--- psd-web/app/helpers/countries_helper.rb
+++ psd-web/app/helpers/countries_helper.rb
@@ -1,5 +1,4 @@
 module CountriesHelper
-
   # JSON is of the form [["Abu Dhabi", "territory:AE-AZ"], ["Afghanistan", "country:AF"]]
   def country_from_code(code)
     country = all_countries.find { |c| c[1] == code }
--- psd-web/app/models/country.rb
+++ psd-web/app/models/country.rb
@@ -1,11 +1,10 @@
 class Country
-
   PATH_TO_COUNTRIES_LIST =
     'node_modules/govuk-country-and-territory-autocomplete/dist/location-autocomplete-canonical-list.json'.freeze
 
   class << self
     def all
-      @@all ||= JSON.parse(File.read(PATH_TO_COUNTRIES_LIST))
+      @all ||= JSON.parse(File.read(PATH_TO_COUNTRIES_LIST))
     end
   end
 end
