c4f7f4f330d4ca226c9703d9e5f28be1c9d7e078
yann marquet <ymarquet@gmail.com>
2019-10-21 17:23:55 +0100

    eager load test

--- infrastructure/ci/test-if-changed.sh
+++ infrastructure/ci/test-if-changed.sh
@@ -8,9 +8,7 @@ docker-compose -f docker-compose.yml -f docker-compose.ci.yml up -d $COMPONENT
 if [[ $(./infrastructure/ci/get-changed-components.sh) =~ ((^| )$COMPONENT($| )) ]]; then
     echo "Testing component $COMPONENT"
     docker-compose -f docker-compose.yml -f docker-compose.ci.yml run --rm start_dependencies
-    docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec $COMPONENT printenv
-    docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec $COMPONENT rails runner "pp ENV"
-    docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec $COMPONENT bin/rake db:create db:schema:load test:all
+    docker-compose -f docker-compose.yml -f docker-compose.ci.yml exec $COMPONENT bin/rails db:create db:schema:load test
 else
     echo 'Testing not required.'
 fi
