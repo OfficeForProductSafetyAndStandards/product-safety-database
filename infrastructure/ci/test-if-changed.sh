1fcfed50a9db7fea928de1efed9bf7b011f5b348
yann marquet <ymarquet@gmail.com>
2019-10-23 14:11:25 +0100

    more debugging

--- infrastructure/ci/test-if-changed.sh
+++ infrastructure/ci/test-if-changed.sh
@@ -3,15 +3,19 @@ set -ex
 
 gem install bundler:2.0.2
 
-docker-compose -f docker-compose.yml up --build -d db keycloak
-
 cd psd-web
 
 cp -r ../shared-web  vendor/
 ls -la vendor/
 ls -la vendor/shared-web
-
+pwd
 bundle install --jobs=3  --retry=3
+
+cd ..
+
+docker-compose -f docker-compose.yml up --build -d db keycloak
+
+cd psd-web
 
 bin/rails db:create db:schema:load test BACKTRACE=1
 
