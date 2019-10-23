c58d054ee53e75ff34661bab5824fe39437af194
yann marquet <ymarquet@gmail.com>
2019-10-23 14:49:01 +0100

    debug index_name creation

--- psd-web/app/models/concerns/indexable.rb
+++ psd-web/app/models/concerns/indexable.rb
@@ -2,6 +2,7 @@ module Indexable
   extend ActiveSupport::Concern
 
   included do
+    pp [ENV.fetch("ES_NAMESPACE", "default_namespace"), Rails.env, self.to_s.gsub('::', '').downcase].join("_")
     self.index_name [ENV.fetch("ES_NAMESPACE", "default_namespace"), Rails.env, self.to_s.gsub('::', '').downcase].join("_")
   end
 end
