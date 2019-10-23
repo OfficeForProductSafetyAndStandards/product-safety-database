8e1612302009efb0942b76e17df864a71d593d64
yann marquet <ymarquet@gmail.com>
2019-10-23 15:34:31 +0100

    debug elastic search

--- infrastructure/ci/test-if-changed.sh
+++ infrastructure/ci/test-if-changed.sh
@@ -5,6 +5,16 @@ export DATABASE_URL=postgres://postgres@localhost:5432/psd_test
 export KEYCLOAK_AUTH_URL=http://localhost:8080/auth
 export ELASTICSEARCH_URL=http://localhost:9200
 
+ps aux
+sudo ls -la /etc/elasticsearch
+
+sudo ln -s /etc/elasticsearch /usr/share/elasticsearch/config
+
+sudo chown -R elasticsearch:elasticsearch /var/lib/elasticsearch
+sudo chown -R elasticsearch:elasticsearch /var/run/elasticsearch
+sudo chown -R elasticsearch:elasticsearch /etc/elasticsearch
+
+
 docker-compose -f docker-compose.yml up --build -d db keycloak
 
 gem install bundler:2.0.2
