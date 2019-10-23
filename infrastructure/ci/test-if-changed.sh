108d72ab891ff8d787f485bec427ed1e51a90b4c
yann marquet <ymarquet@gmail.com>
2019-10-23 13:46:07 +0100

    debug directory

--- infrastructure/ci/test-if-changed.sh
+++ infrastructure/ci/test-if-changed.sh
@@ -8,7 +8,7 @@ docker-compose -f docker-compose.yml up --build -d db keycloak
 cd psd-web
 
 cp -r ../shared-web  vendor/
-
+ls -la vendor/
 bundle install --jobs=3  --retry=3 --deployment
 
 bin/rails db:create db:schema:load test BACKTRACE=1
