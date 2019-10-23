df7a5b9b79b6cc5438ddad005b0546b0dc741201
yann marquet <ymarquet@gmail.com>
2019-10-23 13:17:07 +0100

    debug

--- infrastructure/ci/test-if-changed.sh
+++ infrastructure/ci/test-if-changed.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-docker-compose -f docker-compose.yml up --build -d db keycloak
+ls -la
 
-ln -s shared-web  psd-web/vendor/
+cp -r shared-web  psd-web/vendor/
 
 gem install bundler:2.0.2
+
+docker-compose -f docker-compose.yml up --build -d db keycloak
+
 
 bundle install --jobs=3  --retry=3 --deployment --path ~/bundle-cache
 
