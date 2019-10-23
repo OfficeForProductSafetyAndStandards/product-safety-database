555da28c5d65e85073dde9f52a0d1849c592a70c
yann marquet <ymarquet@gmail.com>
2019-10-23 12:48:26 +0100

    fix bundle install

--- infrastructure/ci/test-if-changed.sh
+++ infrastructure/ci/test-if-changed.sh
@@ -5,9 +5,9 @@ docker-compose -f docker-compose.yml up --build -d db keycloak
 
 cd psd-web
 
-bundle install --jobs 3  --retry 3 --path ~/bundle-cache
-
 gem install bundler:2.0.2
+
+bundle install --jobs=3  --retry=3 --deployment --path ~/bundle-cache
 
 bin/rails db:create db:schema:load test BACKTRACE=1
 
