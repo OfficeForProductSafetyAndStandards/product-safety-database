6d0a864767df6c500eabaae8379a6e5e37e13f65
yann marquet <ymarquet@gmail.com>
2019-10-23 14:10:03 +0100

    debug shared-web

--- infrastructure/ci/test-if-changed.sh
+++ infrastructure/ci/test-if-changed.sh
@@ -9,7 +9,9 @@ cd psd-web
 
 cp -r ../shared-web  vendor/
 ls -la vendor/
-bundle install --jobs=3  --retry=3 --deployment
+ls -la vendor/shared-web
+
+bundle install --jobs=3  --retry=3
 
 bin/rails db:create db:schema:load test BACKTRACE=1
 
