d246d54de9cf76b08e647bc8cece221219c712c9
yann marquet <ymarquet@gmail.com>
2019-10-23 15:54:30 +0100

    manually download elasticsearch

--- infrastructure/ci/test-if-changed.sh
+++ infrastructure/ci/test-if-changed.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
 set -ex
 
+
+cd $HOME
+
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.4.1-linux-x86_64.tar.gz
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.4.1-linux-x86_64.tar.gz.sha512
+shasum -a 512 -c elasticsearch-7.4.1-linux-x86_64.tar.gz.sha512
+tar -xzf elasticsearch-7.4.1-linux-x86_64.tar.gz
+ls -la  elasticsearch-7.4.1/
+
+export ES_HOME=$HOME/elasticsearch-7.4.1/
 export DATABASE_URL=postgres://postgres@localhost:5432/psd_test
 export KEYCLOAK_AUTH_URL=http://localhost:8080/auth
 export ELASTICSEARCH_URL=http://localhost:9200
 
-ps aux
-sudo ls -la /etc/elasticsearch
-
-sudo ln -s /etc/elasticsearch /usr/share/elasticsearch/config
-
-sudo chown -R elasticsearch:elasticsearch /var/lib/elasticsearch
-sudo chown -R elasticsearch:elasticsearch /var/run/elasticsearch
-sudo chown -R elasticsearch:elasticsearch /etc/elasticsearch
-
+cd $HOME/build/UKGovernmentBEIS/beis-opss-psd
 
 docker-compose -f docker-compose.yml up --build -d db keycloak
 
-gem install bundler:2.0.2
-
-cd psd-web
+cd $HOME/build/UKGovernmentBEIS/beis-opss-psd/psd-web
 
 mkdir -p  vendor/shared-web
 
 cp -R ../shared-web  vendor/shared-web
+
+gem install bundler:2.0.2
 
 bundle install --jobs=3  --retry=3
 
