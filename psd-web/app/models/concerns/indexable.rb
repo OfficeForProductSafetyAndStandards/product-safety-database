715bc2b7aca3d2e3776304925ae86d7ddb00b6b2
yann marquet <ymarquet@gmail.com>
2019-10-23 15:22:43 +0100

    print index name compoments

--- psd-web/app/models/concerns/indexable.rb
+++ psd-web/app/models/concerns/indexable.rb
@@ -2,7 +2,7 @@ module Indexable
   extend ActiveSupport::Concern
 
   included do
-    pp [ENV.fetch("ES_NAMESPACE", "default_namespace"), Rails.env, self.to_s.gsub('::', '').downcase].join("_")
+    pp [ENV.fetch("ES_NAMESPACE", "default_namespace"), Rails.env, self.to_s.gsub('::', '').downcase]
     self.index_name [ENV.fetch("ES_NAMESPACE", "default_namespace"), Rails.env, self.to_s.gsub('::', '').downcase].join("_")
   end
 end
