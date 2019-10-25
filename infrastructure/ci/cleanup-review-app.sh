d1990c536c2bc1c882a84ba7c5f464ce9a96b119
Sławosz Sławiński <slawosz@gmail.com>
2019-10-25 16:10:43 +0100

    Remove review app (#40)
    * Remove review app
    
    * Build docker only on PR

---
+++ infrastructure/ci/cleanup-review-app.sh
@@ -0,0 +1,8 @@
+set -x
+# https://developer.github.com/v3/repos/commits/#list-pull-requests-associated-with-commit
+number=`curl -H "Accept: application/vnd.github.groot-preview+json" https://api.github.com/repos/UKGovernmentBEIS/beis-opss-psd/commits/$TRAVIS_COMMIT/pulls | jq '.[0] | .number'`
+
+./infrastructure/ci/install-cf.sh
+cf login -a api.london.cloud.service.gov.uk -u $CF_USERNAME -p $CF_PASSWORD -o 'beis-opss' -s $SPACE
+cf delete -f psd-pr-$number-web
+cf logout
