fd311d776f23d088fafb617648823a8cecdd9ded
yann marquet <ymarquet@gmail.com>
2019-10-25 14:57:17 +0200

    remove extra stuff

--- shared-web/app/helpers/shared/web/application_helper.rb
+++ shared-web/app/helpers/shared/web/application_helper.rb
@@ -1,11 +1,6 @@
 module Shared
   module Web
     module ApplicationHelper
-      include ::Webpacker::Helper
-
-      def current_webpacker_instance
-        Shared::Web.webpacker
-      end
     end
   end
 end
--- shared-web/lib/shared/web.rb
+++ shared-web/lib/shared/web.rb
@@ -11,16 +11,6 @@ require "shared/web/engine"
 
 module Shared
   module Web
-    ROOT_PATH = Pathname.new(File.join(__dir__, "..", ".."))
-
-    class << self
-      def webpacker
-        @webpacker ||= ::Webpacker::Instance.new(
-          root_path: ROOT_PATH,
-          config_path: ROOT_PATH.join("config/webpacker.yml")
-        )
-      end
-    end
-
+    # Your code goes here...
   end
 end
