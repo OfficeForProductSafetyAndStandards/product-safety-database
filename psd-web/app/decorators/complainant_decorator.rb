d6ce7ae19c20bf7dad9d8a148b70583aeff144a0
yann marquet <ymarquet@gmail.com>
2019-10-31 14:37:18 +0000

    complainant summary list with optional contact details

---
+++ psd-web/app/decorators/complainant_decorator.rb
@@ -0,0 +1,22 @@
+class ComplainantDecorator < ApplicationDecorator
+  delegate_all
+
+  def summary_list
+    contact_details = [
+      name, phone_number, email_address, other_details
+    ]
+
+    rows = [
+      { key: { text: "Type" }, value: { text: complainant.complainant_type } },
+    ]
+
+    if contact_details.any?
+      rows << {
+        key: { text: "Contact details" },
+        value: { text: contact_details.join(h.tag.br).html_safe }
+      }
+    end
+
+    h.render "components/govuk_summary_list", rows: rows
+  end
+end
