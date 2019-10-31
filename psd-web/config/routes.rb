6a15d9af1db058ebdc5301b86200de346519f9cb
Steve Lorek <steve@stevelorek.com>
2019-10-31 10:21:58 +0000

    Move application routes from shared-web to main application

--- psd-web/config/routes.rb
+++ psd-web/config/routes.rb
@@ -1,6 +1,28 @@
+require 'sidekiq/web'
+require 'sidekiq/cron/web'
+
+if Rails.env.production?
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(username, ENV["SIDEKIQ_USERNAME"]) &&
+      ActiveSupport::SecurityUtils.secure_compare(password, ENV["SIDEKIQ_PASSWORD"])
+  end
+end
+
 # rubocop:disable Metrics/BlockLength
 Rails.application.routes.draw do
   mount Shared::Web::Engine => "/", as: "shared_engine"
+
+  resource :session, only: %i[new] do
+    member do
+      get :new
+      get :signin
+      get :logout
+    end
+  end
+
+  unless Rails.env.production? && (!ENV["SIDEKIQ_USERNAME"] || !ENV["SIDEKIQ_PASSWORD"])
+    mount Sidekiq::Web => "/sidekiq"
+  end
 
   concern :document_attachable do
     resources :documents, controller: "documents" do
