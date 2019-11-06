889ae40cc6b5f6894168ea03f4de7900b52a016e
Steve Lorek <steve@stevelorek.com>
2019-11-06 18:42:52 +0000

    Rubocop fixes

--- psd-web/app/validators/uuid_validator.rb
+++ psd-web/app/validators/uuid_validator.rb
@@ -1,6 +1,6 @@
 class UuidValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+    unless /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.match?(value)
       record.errors[attribute] << (options[:message] || "is not a valid UUID")
     end
   end
--- psd-web/db/migrate/20191106154207_create_teams_users.rb
+++ psd-web/db/migrate/20191106154207_create_teams_users.rb
@@ -3,6 +3,7 @@ class CreateTeamsUsers < ActiveRecord::Migration[5.2]
     create_table :teams_users do |t|
       t.belongs_to :team, type: :uuid
       t.belongs_to :user, type: :uuid
+      t.timestamps
     end
   end
 end
