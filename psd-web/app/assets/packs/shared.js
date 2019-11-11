e8a69459e1a385008566c9085eb6f302dd95d8f3
Frankie Roberto <frankie@frankieroberto.com>
2019-11-11 12:30:05 +0000

    Update import paths

--- psd-web/app/assets/packs/shared.js
+++ psd-web/app/assets/packs/shared.js
@@ -13,15 +13,15 @@ import '../application/javascripts/date_input';
 
 
 // Images
-import 'govuk-frontend/assets/images/favicon.ico';
-import 'govuk-frontend/assets/images/govuk-mask-icon.svg';
-import 'govuk-frontend/assets/images/govuk-crest-2x.png';
-import 'govuk-frontend/assets/images/govuk-apple-touch-icon-180x180.png';
-import 'govuk-frontend/assets/images/govuk-apple-touch-icon-167x167.png';
-import 'govuk-frontend/assets/images/govuk-apple-touch-icon-152x152.png';
-import 'govuk-frontend/assets/images/govuk-apple-touch-icon.png';
-import 'govuk-frontend/assets/images/govuk-opengraph-image.png';
-import 'govuk-frontend/assets/images/govuk-logotype-crown.png';
+import 'govuk-frontend/govuk/assets/images/favicon.ico';
+import 'govuk-frontend/govuk/assets/images/govuk-mask-icon.svg';
+import 'govuk-frontend/govuk/assets/images/govuk-crest-2x.png';
+import 'govuk-frontend/govuk/assets/images/govuk-apple-touch-icon-180x180.png';
+import 'govuk-frontend/govuk/assets/images/govuk-apple-touch-icon-167x167.png';
+import 'govuk-frontend/govuk/assets/images/govuk-apple-touch-icon-152x152.png';
+import 'govuk-frontend/govuk/assets/images/govuk-apple-touch-icon.png';
+import 'govuk-frontend/govuk/assets/images/govuk-opengraph-image.png';
+import 'govuk-frontend/govuk/assets/images/govuk-logotype-crown.png';
 
 import '../application/images/document_placeholder.png';
 
