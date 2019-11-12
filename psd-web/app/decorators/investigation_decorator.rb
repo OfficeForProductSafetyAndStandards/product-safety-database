d0d16690e17da4431f2983ea456749083c580949
yann marquet <ymarquet@gmail.com>
2019-11-12 17:32:27 +0000

    add optional class for summary list

--- psd-web/app/decorators/investigation_decorator.rb
+++ psd-web/app/decorators/investigation_decorator.rb
@@ -11,7 +11,11 @@ class InvestigationDecorator < ApplicationDecorator
     hazards = h.simple_format([hazard_type, hazard_description].join("\n\n"))
     rows = [
 
-      { key: { text: "Category" }, value: { text: category } },
+      {
+        key: { text: "Category" },
+        value: { text: category },
+        actions: []
+      },
       {
         key: { text: "Product details" },
         value: { text: products_details },
@@ -19,22 +23,20 @@ class InvestigationDecorator < ApplicationDecorator
           { href: h.investigation_products_path(object), visually_hidden_text: "product details", text: "View" }
         ]
       },
-      hazard_type.present? ? { key: { text: "Hazards" }, value: { text: hazards } } : nil,
-      non_compliant_reason.present? ? { key: { text: "Compliance" }, value: { text: h.simple_format(non_compliant_reason) } } : nil,
+      hazard_type.present? ? { key: { text: "Hazards" }, value: { text: hazards }, actions: [] } : nil,
+      non_compliant_reason.present? ? { key: { text: "Compliance" }, value: { text: h.simple_format(non_compliant_reason) }, actions: []  } : nil,
     ]
     rows.compact!
-    h.render "components/govuk_summary_list", rows: rows
+    h.render "components/govuk_summary_list", rows: rows, classes: "govuk-summary-list--no-border"
   end
 
   def investigation_summary_list(include_actions: true, classes: "")
     rows = [
       {
         key: { text: "Status", classes: classes },
-        value: { text: status, classes: classes }
+        value: { text: status, classes: classes },
+        actions: []
       },
-      # TODO: Created by should contain the creator's organisation a bit like in
-      # def investigation_assignee(investigation, classes = "")
-      # TODO: Make this a Date time format to_s(:govuk) =>  strftime("%e %B %Y")
       {
         key: { text: "Created by", classes: classes },
         value: { text: investigation.source.name, classes: classes },
@@ -44,6 +46,9 @@ class InvestigationDecorator < ApplicationDecorator
         key: { text: "Assigned to", classes: classes },
         value: { text: h.investigation_assignee(object, classes) },
       },
+      # TODO: Created by should contain the creator's organisation a bit like in
+      # def investigation_assignee(investigation, classes = "")
+      # TODO: Make this a Date time format to_s(:govuk) =>  strftime("%e %B %Y")
       {
         key: { text: "Date created", classes: classes },
         value: { text: investigation.created_at.beginning_of_month.strftime("%e %B %Y"), classes: classes },
@@ -69,7 +74,28 @@ class InvestigationDecorator < ApplicationDecorator
       ]
     end
 
-    h.render "components/govuk_summary_list", rows: rows
+    h.render "components/govuk_summary_list", rows: rows, classes: "govuk-summary-list--no-border"
+  end
+
+  def source_details_summary_list
+    contact_details = [complainant.name, complainant.phone_number, complainant.email_address, complainant.other_details]
+    contact_details = ["Not provided"] if contact_details.empty?
+    unless complainant.can_be_displayed?
+      contact_details = "Reporter details are restricted because they contain GDPR protected data."
+    end
+
+    rows = [
+      date_received? ? { key: { text: "Received date" }, value: { text: date_received.strftime("%e %B %Y") } } : nil,
+      received_type? ? { key: { text: "Received by" }, value: { text: received_type.upcase_first } } : nil,
+      { key: { text: "Source type" }, value: { text: complainant.complainant_type } },
+      { key: { text: "Contact details" }, value: { text: h.simple_format(contact_details.join("\n\n")) } }
+    ]
+
+    if complainant.can_be_displayed?
+    end
+    rows.compact!
+
+    h.render "components/govuk_summary_list", rows: rows, classes: "govuk-summary-list--no-border"
   end
 
   def source_details_summary_list
