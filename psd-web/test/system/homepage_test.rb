6ade018a1723cda37c80924470fd0a29711fb127
Andy <andy@wireworldmedia.co.uk>
2019-11-13 10:53:05 +0000

    Improve home page tests

--- psd-web/test/system/homepage_test.rb
+++ psd-web/test/system/homepage_test.rb
@@ -3,7 +3,8 @@ require "application_system_test_case"
 class HomepageTest < ApplicationSystemTestCase
   test "not signed in visits / stays on /" do
     visit "/"
-    assert_current_path "/"
+    assert_text "if you think you should have access"
+    refute_selector "a.psd-header__link", text: "BETA"
   end
 
   test "not signed in visits /cases gets redirected to /" do
@@ -26,6 +27,7 @@ class HomepageTest < ApplicationSystemTestCase
     sign_in_as(User.find_by(name: "Test User_one"))
     visit "/cases"
     assert_current_path "/cases"
+    assert_selector "a.psd-header__link", text: "BETA"
     reset_keycloak_and_notify_mocks
   end
 end
