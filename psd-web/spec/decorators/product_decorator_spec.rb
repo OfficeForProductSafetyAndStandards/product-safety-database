1ee388ed74a0e7307948af490b728ce40b64e0a4
yann marquet <ymarquet@gmail.com>
2019-11-15 09:13:29 -0500

    use summarise matcher

--- psd-web/spec/decorators/product_decorator_spec.rb
+++ psd-web/spec/decorators/product_decorator_spec.rb
@@ -9,34 +9,21 @@ RSpec.describe ProductDecorator do
 
   describe "#summary_list" do
     include CountriesHelper
-    let(:summary_list) do
-      Capybara.string(subject.summary_list(include_batch_number: include_batch_number))
-    end
+    let(:summary_list) {
+      subject.summary_list(include_batch_number: include_batch_number)
+    }
 
     context "when including the batch number" do
       let(:include_batch_number) { true }
 
       it "displays the product summary list with the batch number" do
-        expect(summary_list).to have_css("dl dt.govuk-summary-list__key",   text: "Product name")
-        expect(summary_list).to have_css("dl dd.govuk-summary-list__value", text: product.name)
-
-        expect(summary_list).to have_css("dl dt.govuk-summary-list__key",   text: "Barcode / serial number")
-        expect(summary_list).to have_css("dl dd.govuk-summary-list__value", text: product.product_code)
-
-        expect(summary_list).to have_css("dl dt.govuk-summary-list__key",   text: "Batch number")
-        expect(summary_list).to have_css("dl dd.govuk-summary-list__value", text: product.batch_number)
-
-        expect(summary_list).to have_css("dl dt.govuk-summary-list__key",   text: "Category")
-        expect(summary_list).to have_css("dl dd.govuk-summary-list__value", text: product.category)
-
-        expect(summary_list).to have_css("dl dt.govuk-summary-list__key",   text: "Webpage")
-        expect(summary_list).to have_css("dl dd.govuk-summary-list__value", text: product.webpage)
-
-        expect(summary_list).to have_css("dl dt.govuk-summary-list__key",   text: "Country of origin")
-        expect(summary_list).to have_css("dl dd.govuk-summary-list__value", text: country_from_code(product.country_of_origin))
-
-        expect(summary_list).to have_css("dl dt.govuk-summary-list__key",   text: "Description")
-        expect(summary_list).to have_css("dl dd.govuk-summary-list__value", text: product.description)
+        expect(summary_list).to summarise("Product name",            text: product.name)
+        expect(summary_list).to summarise("Barcode / serial number", text: product.product_code)
+        expect(summary_list).to summarise("Batch number",            text: product.batch_number)
+        expect(summary_list).to summarise("Category",                text: product.category)
+        expect(summary_list).to summarise("Webpage",                 text: product.webpage)
+        expect(summary_list).to summarise("Country of origin",       text: country_from_code(product.country_of_origin))
+        expect(summary_list).to summarise("Description", text: product.description)
       end
     end
 
@@ -44,26 +31,13 @@ RSpec.describe ProductDecorator do
       let(:include_batch_number) { false }
 
       it "displays the summary list without the batch number" do
-        expect(summary_list).to have_css("dl dt.govuk-summary-list__key",   text: "Product name")
-        expect(summary_list).to have_css("dl dd.govuk-summary-list__value", text: product.name)
-
-        expect(summary_list).to have_css("dl dt.govuk-summary-list__key",   text: "Barcode / serial number")
-        expect(summary_list).to have_css("dl dd.govuk-summary-list__value", text: product.product_code)
-
-        expect(summary_list).to_not have_css("dl dt.govuk-summary-list__key",   text: "Batch number")
-        expect(summary_list).to_not have_css("dl dd.govuk-summary-list__value", text: product.batch_number)
-
-        expect(summary_list).to have_css("dl dt.govuk-summary-list__key",   text: "Category")
-        expect(summary_list).to have_css("dl dd.govuk-summary-list__value", text: product.category)
-
-        expect(summary_list).to have_css("dl dt.govuk-summary-list__key",   text: "Webpage")
-        expect(summary_list).to have_css("dl dd.govuk-summary-list__value", text: product.webpage)
-
-        expect(summary_list).to have_css("dl dt.govuk-summary-list__key",   text: "Country of origin")
-        expect(summary_list).to have_css("dl dd.govuk-summary-list__value", text: country_from_code(product.country_of_origin))
-
-        expect(summary_list).to have_css("dl dt.govuk-summary-list__key",   text: "Description")
-        expect(summary_list).to have_css("dl dd.govuk-summary-list__value", text: product.description)
+        expect(summary_list).to summarise("Product name",            text: product.name)
+        expect(summary_list).to summarise("Barcode / serial number", text: product.product_code)
+        expect(summary_list).to summarise("Category",                text: product.category)
+        expect(summary_list).to summarise("Webpage",                 text: product.webpage)
+        expect(summary_list).to summarise("Country of origin",       text: country_from_code(product.country_of_origin))
+        expect(summary_list).to summarise("Description",             text: product.description)
+        expect(summary_list).to_not summarise("Batch number",        text: product.batch_number)
       end
     end
   end
