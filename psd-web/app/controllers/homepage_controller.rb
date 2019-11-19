89c083afbff9e77962f3e93df856087b101995f2
Andy <andy@wireworldmedia.co.uk>
2019-11-19 10:15:49 +0000

    Simplify route_user method

--- psd-web/app/controllers/homepage_controller.rb
+++ psd-web/app/controllers/homepage_controller.rb
@@ -14,14 +14,10 @@ class HomepageController < ApplicationController
 private
 
   def route_user
-    if !User.current.has_accepted_declaration
-      redirect_to declaration_index_path(redirect_path: params[:request_path])
-    elsif User.current.is_opss?
-      redirect_to investigations_path
-    elsif User.current.has_viewed_introduction
-      render "non_opss"
-    else
-      redirect_to introduction_overview_path
-    end
+    return redirect_to declaration_index_path(redirect_path: params[:request_path]) if !User.current.has_accepted_declaration
+    return redirect_to investigations_path if User.current.is_opss?
+    return redirect_to introduction_overview_path if !User.current.has_viewed_introduction
+
+    render "non_opss"
   end
 end
