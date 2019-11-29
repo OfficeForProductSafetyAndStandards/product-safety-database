a07efd8d3ce8e78e3cc23233221372b6d838a749
yann marquet <ymarquet@gmail.com>
2019-11-29 14:34:34 +0000

    add sources to ivnestigation decorator specs

--- psd-web/test/system/homepage_test.rb
+++ psd-web/test/system/homepage_test.rb
@@ -7,12 +7,6 @@ class HomepageTest < ApplicationSystemTestCase
     refute_selector "a.psd-header__link", text: "BETA"
   end
 
-  test "not signed in visits /cases gets redirected to /" do
-    visit "/cases"
-    assert_title "Sign in to Product safety database"
-    refute_selector "a.psd-header__link", text: "BETA"
-  end
-
   test "signed in visits / gets redirected to /cases" do
     mock_out_keycloak_and_notify
     sign_in_as(User.find_by(name: "Test User_one"))
