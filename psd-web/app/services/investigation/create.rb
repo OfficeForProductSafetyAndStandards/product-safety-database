3fab192a548c6af89c9eae8d3e164f013f951f6f
yann marquet <ymarquet@gmail.com>
2019-12-02 15:28:18 +0000

    Dont attach if no attachment

--- psd-web/app/services/investigation/create.rb
+++ psd-web/app/services/investigation/create.rb
@@ -1,13 +1,13 @@
 class Investigation < ApplicationRecord
   class Create
 
-    def initialize(attributes, attachment)
+    def initialize(attributes, attachment: nil)
       self.attributes = attributes
       self.attachment = attachment
     end
 
     def call
-      investigation.documents.attach(attachment)
+      investigation.documents.attach(attachment) if attachment
       investigation.save
       investigation
     end
--- psd-web/spec/services/investigation/create_spec.rb
+++ psd-web/spec/services/investigation/create_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
-RSpec.describe Investigation::Create do
+RSpec.describe Investigation::Create, :with_stubbed_elasticsearch do
   let(:complainant_attributes)   { attributes_for(:complainant) }
   let(:investigation_attributes) { attributes_for(:allegation) }
-  let(:attachment)    { fixture_file_upload('files/testImage.png') }
+  let(:attachment)               { fixture_file_upload('files/testImage.png') }
 
   let(:attributes) do
     investigation_attributes.tap do |attrs|
@@ -11,13 +11,30 @@ RSpec.describe Investigation::Create do
     end
   end
 
-  subject { described_class.new(attributes, attachment) }
+  subject { described_class.new(attributes, attachment: attachment) }
 
   describe '#call' do
+    let(:investigation) { subject.call }
+
     it "saves the investigation, complainant and attachments" do
-      expect {
-        expect(subject.call).to be true
-      }.to change { Investigation.where(investigation_attributes).count }.from(0).to(1)
+      expect(investigation).to be_persisted
+
+      expect(Complainant.find_by(complainant_attributes)).to eq(investigation.complainant)
+
+      expect(investigation.documents).to be_attached
+
+      attached_blob = investigation.documents.first.blob
+      expect(attached_blob.filename).to eq(attachment.original_filename)
+    end
+
+    context 'without attachment' do
+      let(:attachment) { nil }
+
+      it "saves the investigation and complainant" do
+        expect(investigation).to be_persisted
+        expect(Complainant.find_by(complainant_attributes)).to eq(investigation.complainant)
+        expect(investigation.documents).to_not be_attached
+      end
     end
   end
 
