ffe7a63a4b32425b377029f7dc8248be7b6b398c
yann marquet <ymarquet@gmail.com>
2019-12-06 17:15:14 +0100

    fix rubocop offenses

--- psd-web/spec/features/home_page_spec.rb
+++ psd-web/spec/features/home_page_spec.rb
@@ -1,11 +1,10 @@
 require "rails_helper"
 
 RSpec.describe "HomePage", :with_keycloak_config, :with_elasticsearch do
-
-  context 'when not signed in' do
+  context "when not signed in" do
     before do
       allow(KeycloakClient.instance).to receive(:user_signed_in?).and_return(false)
-      allow(KeycloakClient.instance).to receive(:login_url).and_return('/login')
+      allow(KeycloakClient.instance).to receive(:login_url).and_return("/login")
     end
 
     it "not signed in visits / stays on /" do
@@ -14,7 +13,7 @@ RSpec.describe "HomePage", :with_keycloak_config, :with_elasticsearch do
       expect(page).to_not have_css "a.psd-header__link", text: "BETA"
     end
   end
-  context 'when signed in' do
+  context "when signed in" do
     let(:user) { create(:user, :opss_user, :activated) }
     before { sign_in(as_user: user) }
 
@@ -25,7 +24,7 @@ RSpec.describe "HomePage", :with_keycloak_config, :with_elasticsearch do
 
     it "signed in visits /cases stays on /cases" do
       visit "/cases"
-      expect(page).to  have_current_path("/cases")
+      expect(page).to have_current_path("/cases")
     end
   end
 end
