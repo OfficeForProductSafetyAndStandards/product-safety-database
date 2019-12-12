1854c44a984734a69d24a2db43f37d05ee60e260
Steve Lorek <steve@stevelorek.com>
2019-12-12 16:06:06 +0000

    Ensure correct keycloak URL is used in fixture

--- psd-web/spec/support/keycloak.rb
+++ psd-web/spec/support/keycloak.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.shared_context "with Keycloak configuration", shared_context: :metadata do
-  let(:openid_config) { file_fixture("keycloak_openid_config.json").read }
+  let(:openid_config) { file_fixture("keycloak_openid_config.json").read.gsub("http://keycloak:8080/auth", ENV.fetch("KEYCLOAK_AUTH_URL")) }
   let(:response_double) { double("OpenID response", code: 200, body: openid_config) }
   before { allow(RestClient).to receive(:get).with("#{ENV.fetch('KEYCLOAK_AUTH_URL')}/realms/opss/.well-known/openid-configuration") { response_double } }
 end
