a33a6da1ad69d1f2893417157f41a01657124d27
Edward Horsford <mail@edwardhorsford.com>
2019-12-13 10:59:56 +0000

    Make labels consistent

--- psd-web/app/decorators/product_decorator.rb
+++ psd-web/app/decorators/product_decorator.rb
@@ -5,10 +5,10 @@ class ProductDecorator < ApplicationDecorator
   def summary_list(include_batch_number: false)
     rows = [
       { key: { text: "Product name" }, value: { text: object.name } },
-      { key: { text: "Barcode / serial number" }, value: { text: product_code } },
-      { key: { text: "Type" }, value: { text: product_type } },
-      include_batch_number ? { key: { text: "Batch number" }, value: { text: batch_number } } : nil,
       { key: { text: "Category" }, value: { text: category } },
+      { key: { text: "Product type" }, value: { text: product_type } },
+      { key: { text: "Barcode or serial number" }, value: { text: product_code } },
+      include_batch_number ? { key: { text: "Batch number" }, value: { text: batch_number } } : nil,
       { key: { text: "Webpage" }, value: { text: object.webpage } },
       { key: { text: "Country of origin" }, value: { text: country_from_code(country_of_origin) } },
       { key: { text: "Description" }, value: { text: description } }
