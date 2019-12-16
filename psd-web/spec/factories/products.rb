bbe448ec2181a31bed44a9da71cf3e2308d4d8cf
Steve Lorek <steve@stevelorek.com>
2019-12-16 18:27:22 +0000

    Replace existing Trading Standards case creation system test with RSpec feature spec to improve reliability and re-usability

--- psd-web/spec/factories/products.rb
+++ psd-web/spec/factories/products.rb
@@ -10,7 +10,7 @@ FactoryBot.define do
       name { "iPhone XS MAX" }
       webpage { "https://www.iphone.webpage" }
       product_type { "phone" }
-      category { "phone category" }
+      category { "Communication and media equipment" }
       description { "iphone description" }
       country_of_origin { "United States" }
       batch_number { "1234" }
@@ -20,14 +20,14 @@ FactoryBot.define do
       product_code { 345 }
       name { "iPhone" }
       product_type { "phone" }
-      category { "phone" }
+      category { "Communication and media equipment" }
     end
 
     factory :product_samsung do
       product_code { 4524 }
       name { "galaxy" }
       product_type { "phone" }
-      category { "phone" }
+      category { "Communication and media equipment" }
       description { "a phone by samsung" }
       country_of_origin { "Republic of Korea" }
     end
