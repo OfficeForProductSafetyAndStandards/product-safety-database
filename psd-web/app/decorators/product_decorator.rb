b5d0696912607ac347fa309e55672fae203f87a9
Ed Horsford <mail@edwardhorsford.com>
2019-12-19 15:03:17 +0000

    Improve product page display and link to add images (#109)
    * Improve product page display
    * Use govuk date formatter
    * add spec for #product_type_and_category_label

--- psd-web/app/decorators/product_decorator.rb
+++ psd-web/app/decorators/product_decorator.rb
@@ -2,13 +2,13 @@ class ProductDecorator < ApplicationDecorator
   delegate_all
   decorates_association :investigations
 
-  def summary_list(include_batch_number: false)
+  def summary_list
     rows = [
       { key: { text: "Product name" }, value: { text: object.name } },
       { key: { text: "Category" }, value: { text: category } },
       { key: { text: "Product type" }, value: { text: product_type } },
       { key: { text: "Barcode or serial number" }, value: { text: product_code } },
-      include_batch_number ? { key: { text: "Batch number" }, value: { text: batch_number } } : nil,
+      { key: { text: "Batch number" }, value: { text: batch_number } },
       { key: { text: "Webpage" }, value: { text: object.webpage } },
       { key: { text: "Country of origin" }, value: { text: country_from_code(country_of_origin) } },
       { key: { text: "Description" }, value: { text: description } }
@@ -19,5 +19,15 @@ class ProductDecorator < ApplicationDecorator
 
   def description
     h.simple_format(object.description)
+  end
+
+  def product_type_and_category_label
+    product_and_category = [product_type.presence, category.presence].compact
+
+    if product_and_category.length > 1
+      "#{product_and_category.first} (#{product_and_category.last.downcase})"
+    else
+      product_and_category.first
+    end
   end
 end
