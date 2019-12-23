a710a88052d5a6a276bad53aba5404c7fc812390
Steve Lorek <steve@stevelorek.com>
2019-12-23 11:27:20 +0000

    Rubocop fixes

--- psd-web/app/helpers/link_helper.rb
+++ psd-web/app/helpers/link_helper.rb
@@ -1,7 +1,7 @@
 module LinkHelper
   def link_with_hidden_text_to(text, hidden_text, url, html_options = {})
     link_to url, html_options do
-      "#{text}<span class=\"govuk-visually-hidden\"> (#{hidden_text})</span>".html_safe
+      render "link_helper/link_with_hidden_text_to", text: text, hidden_text: hidden_text
     end
   end
 end
--- psd-web/spec/helpers/link_helper_spec.rb
+++ psd-web/spec/helpers/link_helper_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe LinkHelper do
     let(:hidden_text) { "hidden text" }
     let(:class_name) { "test" }
 
-    subject { helper.link_with_hidden_text_to(title, hidden_text, url, { class: class_name }) }
+    subject { helper.link_with_hidden_text_to(title, hidden_text, url, class: class_name) }
 
     it "outputs hidden text" do
-      expect(subject).to eq("<a class=\"#{class_name}\" href=\"#{url}\">#{title}<span class=\"govuk-visually-hidden\"> (#{hidden_text})</span></a>")
+      expect(subject).to eq("<a class=\"#{class_name}\" href=\"#{url}\">#{title}<span class=\"govuk-visually-hidden\"> (#{hidden_text})</span>\n</a>")
     end
   end
 end
