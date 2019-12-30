81f0f2b4532c5d099cb91421b156104ef08c85d4
Ed Horsford <mail@edwardhorsford.com>
2019-12-30 17:25:50 +0000

    Cleanup case templates (#98)
    * Clean up display of preview images and documents
    
    * Fix linting errors
    
    * Don't change case of product name
    
    * Cleanup
    
    * Cleanup template markup for case pages
    
    * fix merge conflicts
    
    * Misc template cleanup
    
    * Fix page titles and minor tweaks
    
    * Use team name in title
    
    * Fix test
    
    * Add helper to construct links with hidden text
    
    * Rubocop fixes
    
    * Apply link helper to documents
    
    * Use decorator for title
    
    * Update test to search for text
    
    * Fix linting error
    
    * Again try fixing tests
    
    * Fix tests
    
    * Fix test
    
    * Replace system test with RSpec feature specs
    
    Co-authored-by: Steven Lorek <steve@stevelorek.com>

---
+++ psd-web/spec/features/add_attachment_spec.rb
@@ -0,0 +1,106 @@
+require "rails_helper"
+
+RSpec.feature "Adding an attachment to a case", :with_keycloak_config, :with_stubbed_elasticsearch, :with_stubbed_antivirus, :with_stubbed_mailer do
+  let(:user) { create(:user, :activated) }
+  let(:investigation) { create(:allegation, assignee: user) }
+  let(:file) { Rails.root + "test/fixtures/files/test_result.txt" }
+  let(:title) { Faker::Lorem.sentence }
+  let(:description) { Faker::Lorem.paragraph }
+
+  before do
+    sign_in(as_user: user)
+    visit new_investigation_new_path(investigation) # TODO: rename this route
+  end
+
+  scenario "requires a file" do
+    expect_to_be_on_add_attachment_page
+
+    click_button "Upload"
+
+    expect_to_be_on_add_attachment_page
+    expect(page).to have_error_summary("Enter file")
+  end
+
+  scenario "requires a title for the document" do
+    expect_to_be_on_add_attachment_page
+
+    attach_and_submit_file
+
+    expect_to_be_on_enter_attachment_details_page
+
+    click_button "Save attachment"
+
+    expect_to_be_on_enter_attachment_details_page
+    expect(page).to have_error_summary("Enter title")
+  end
+
+  scenario "uploading a file without completing the add attachment flow does not save the attachment" do
+    expect_to_be_on_add_attachment_page
+
+    attach_and_submit_file
+
+    visit investigation_path(investigation)
+
+    click_link "Attachments"
+
+    expect(page).not_to have_selector("h2")
+  end
+
+  scenario "completing the add attachment flow saves the attachment" do
+    expect_to_be_on_add_attachment_page
+
+    attach_and_submit_file
+
+    expect_to_be_on_enter_attachment_details_page
+
+    fill_and_submit_attachment_details_page
+
+    expect_to_be_on_case_overview_page
+
+    click_link "Attachments (1)"
+
+    expect_case_attachments_page_to_show_entered_information
+
+    click_link "Activity"
+
+    expect_case_activity_page_to_show_entered_information
+  end
+
+  def expect_to_be_on_add_attachment_page
+    expect(page).to have_current_path("/cases/#{investigation.pretty_id}/documents/new/upload")
+    expect(page).to have_selector("h1", text: "Add attachment")
+  end
+
+  def expect_to_be_on_enter_attachment_details_page
+    expect(page).to have_current_path("/cases/#{investigation.pretty_id}/documents/new/metadata")
+    expect(page).to have_selector("h3", text: "Document details")
+  end
+
+  def expect_to_be_on_case_overview_page
+    expect(page).to have_current_path("/cases/#{investigation.pretty_id}")
+    expect(page).to have_selector("h1", text: "Overview")
+  end
+
+  def expect_case_attachments_page_to_show_entered_information
+    expect(page).to have_selector("h2", text: title)
+    expect(page).to have_selector("p", text: description)
+  end
+
+  def expect_case_activity_page_to_show_entered_information
+    expect(page).to have_selector("h1", text: "Activity")
+    item = page.find("h3", text: title).find(:xpath, "..")
+    expect(item).to have_selector("p", text: "Document added")
+    expect(item).to have_selector("p", text: description)
+  end
+
+  def attach_and_submit_file
+    attach_file "document[file][file]", file
+    click_button "Upload"
+  end
+
+  def fill_and_submit_attachment_details_page
+    fill_in "Document title", with: title
+    fill_in "Description", with: description
+    click_button "Save attachment"
+  end
+end
---
+++ psd-web/spec/features/delete_attachment_spec.rb
@@ -0,0 +1,66 @@
+require "rails_helper"
+
+RSpec.feature "Deleting an attachment from a case", :with_keycloak_config, :with_stubbed_elasticsearch, :with_stubbed_antivirus, :with_stubbed_mailer do
+  let(:user) { create(:user, :activated) }
+  let(:investigation) { create(:allegation, :with_document, assignee: user) }
+  let(:document) { investigation.documents.first }
+
+  before { sign_in(as_user: user) }
+
+  scenario "deletes the attachment and creates activity" do
+    visit investigation_attachments_path(investigation)
+
+    expect_to_be_on_attachments_page
+
+    click_link "Remove document"
+
+    expect_to_be_on_remove_attachment_confirmation_page
+    expect_remove_attachment_confirmation_page_to_show_attachment_information
+
+    click_button "Delete attachment"
+
+    expect_to_be_on_case_overview_page
+
+    click_link "Attachments"
+
+    expect_case_attachments_page_not_to_show_deleted_attachment
+
+    click_link "Activity"
+
+    expect_case_activity_page_to_show_deleted_document
+  end
+
+  def expect_to_be_on_attachments_page
+    expect(page).to have_current_path("/cases/#{investigation.pretty_id}/attachments")
+    expect(page).to have_selector("h1", text: "Attachments")
+    expect(page).to have_selector("h2", text: document.title)
+    expect(page).to have_selector("p",  text: document.description)
+  end
+
+  def expect_to_be_on_remove_attachment_confirmation_page
+    expect(page).to have_current_path("/investigation/allegation/#{investigation.pretty_id}/documents/#{document.id}/remove")
+    expect(page).to have_selector("h2", text: "Remove attachment")
+  end
+
+  def expect_to_be_on_case_overview_page
+    expect(page).to have_current_path("/cases/#{investigation.pretty_id}")
+    expect(page).to have_selector("h1", text: "Overview")
+  end
+
+  def expect_remove_attachment_confirmation_page_to_show_attachment_information
+    expect(page.find("th", text: "Title")).to have_sibling("td", text: document.title)
+    expect(page.find("th", text: "Description")).to have_sibling("td", text: document.description)
+    expect(page.find("th", text: "URL")).to have_sibling("td", text: document.filename)
+  end
+
+  def expect_case_attachments_page_not_to_show_deleted_attachment
+    expect(page).not_to have_selector("h2", text: document.title)
+    expect(page).not_to have_selector("p",  text: document.description)
+  end
+
+  def expect_case_activity_page_to_show_deleted_document
+    expect(page).to have_selector("h1", text: "Activity")
+    item = page.find("h3", text: document.title).find(:xpath, "..")
+    expect(item).to have_selector("p", text: "Document deleted")
+  end
+end
