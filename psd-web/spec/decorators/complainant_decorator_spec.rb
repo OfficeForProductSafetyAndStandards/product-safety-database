5e25942efacea6b20af92a7e13be476f621f1712
Steve Lorek <steve@stevelorek.com>
2019-12-30 17:13:53 +0000

    Fix existing tests to allow for an assignee on an investigation factory by default

--- psd-web/spec/decorators/complainant_decorator_spec.rb
+++ psd-web/spec/decorators/complainant_decorator_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe ComplainantDecorator do
+RSpec.describe ComplainantDecorator, :with_keycloak_config do
   let(:complainant) { build(:complainant) }
 
   subject { complainant.decorate }
@@ -8,10 +8,10 @@ RSpec.describe ComplainantDecorator do
   describe "#contact_details" do
     context "with contact details" do
       it "displays the contact details" do
-        expect(subject.contact_details).to match(complainant.name)
-        expect(subject.contact_details).to match(complainant.phone_number)
-        expect(subject.contact_details).to match(complainant.email_address)
-        expect(subject.contact_details).to match(complainant.other_details)
+        expect(subject.contact_details).to match(Regexp.escape(complainant.name))
+        expect(subject.contact_details).to match(Regexp.escape(complainant.phone_number))
+        expect(subject.contact_details).to match(Regexp.escape(complainant.email_address))
+        expect(subject.contact_details).to match(Regexp.escape(complainant.other_details))
       end
     end
 
