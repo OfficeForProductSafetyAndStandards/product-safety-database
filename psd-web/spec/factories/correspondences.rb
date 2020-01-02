679adea6105b3399eebd3c8548773b1228665dcc
Steve Lorek <steve@stevelorek.com>
2020-01-02 14:55:42 +0000

    Fix factories which populate date_attribute properties

--- psd-web/spec/factories/correspondences.rb
+++ psd-web/spec/factories/correspondences.rb
@@ -7,9 +7,13 @@ FactoryBot.define do
     email_subject { Faker::Lorem.sentence }
     email_direction { "from" }
     phone_number { Faker::PhoneNumber.phone_number }
-    correspondence_date_day { Faker::Date.backward(days: 14).day }
-    correspondence_date_month { Faker::Date.backward(days: 14).month }
-    correspondence_date_year { Faker::Date.backward(days: 14).year }
+    correspondence_date_day { correspondence_date.day }
+    correspondence_date_month { correspondence_date.month }
+    correspondence_date_year { correspondence_date.year }
     contact_method { %w[phone email].sample }
+
+    transient do
+      correspondence_date { Faker::Date.backward(days: 14) }
+    end
   end
 end
