9b7d96f75009fc9be28f0b47f5c17c83c9201fb9
Steven Lorek <steve@stevelorek.com>
2020-01-02 15:37:17 +0000

    Fix factories which populate date_attribute properties (#127)

--- psd-web/spec/factories/corrective_actions.rb
+++ psd-web/spec/factories/corrective_actions.rb
@@ -3,15 +3,19 @@ FactoryBot.define do
     investigation
     product
     summary { Faker::Lorem.sentence }
-    date_decided_day { Faker::Date.backward(days: 14).day }
-    date_decided_month { Faker::Date.backward(days: 14).month }
-    date_decided_year { Faker::Date.backward(days: 14).year }
+    date_decided_day { date_decided.day }
+    date_decided_month { date_decided.month }
+    date_decided_year { date_decided.year }
     legislation { Rails.application.config.legislation_constants["legislation"].sample }
     measure_type { CorrectiveAction::MEASURE_TYPES.sample }
     duration { CorrectiveAction::DURATION_TYPES.sample }
     geographic_scope { Rails.application.config.corrective_action_constants["geographic_scope"].sample }
     details { Faker::Lorem.sentence }
     related_file { "No" }
+
+    transient do
+      date_decided { Faker::Date.backward(days: 14) }
+    end
 
     trait :with_file do
       related_file { "Yes" }
