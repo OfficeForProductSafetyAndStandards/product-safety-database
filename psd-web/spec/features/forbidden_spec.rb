a0a14309aa8de3b19a3d4b991a6d81b90b2a1443
Steven Lorek <steve@stevelorek.com>
2020-01-02 15:48:08 +0000

    Fix Keycloak client test isolation (#124)
    * Lazily instantiate Keycloak clients to avoid redundant config requests and reset after each test to improve test isolation
    
    * Fix Keycloak internal/admin clients depending on state in Keycloak::Client

--- psd-web/spec/features/forbidden_spec.rb
+++ psd-web/spec/features/forbidden_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "Access forbidden", :with_keycloak_config do
+RSpec.feature "Access forbidden", :with_stubbed_keycloak_config do
   scenario "Logging in when user does not yet exist and has no groups" do
     sign_in(as_user: build(:user, organisation: nil))
     visit "/cases"
