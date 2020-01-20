1f21d1c975437acd4db07846e6cc09d0ac58e603
yann marquet <ymarquet@gmail.com>
2020-01-20 18:07:33 +0100

    Added tests and refactored Ed's code

--- psd-web/app/decorators/products_decorator.rb
+++ psd-web/app/decorators/products_decorator.rb
@@ -1,10 +1,3 @@
 class ProductsDecorator < Draper::CollectionDecorator
-  PRODUCT_IMAGE_DISPLAY_LIMIT = 6
   delegate :current_page, :total_entries, :total_pages, :per_page, :offset
-
-  def image_list
-    h.tag.ul(class: "govuk-list") do
-      h.render object
-    end
-  end
 end
