4f23d248b6b256288bafe1556b0dd050b8bef69f
yann marquet <ymarquet@gmail.com>
2020-01-21 17:03:31 +0000

    add migration

--- psd-web/db/schema.rb
+++ psd-web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_03_185949) do
+ActiveRecord::Schema.define(version: 2020_01_21_154252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
