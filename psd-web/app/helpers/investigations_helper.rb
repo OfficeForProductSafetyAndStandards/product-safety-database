164106a6586a19f7e23c45c988b304f692d34f35
yann marquet <ymarquet@gmail.com>
2020-01-21 16:35:35 +0000

    remove unsused code

--- psd-web/app/helpers/investigations_helper.rb
+++ psd-web/app/helpers/investigations_helper.rb
@@ -169,7 +169,7 @@ module InvestigationsHelper
     set_default_type_filter
     set_default_assignee_filter
     set_default_creator_filter
-    params.permit(:q, :status_open, :status_closed, :page, :allegation, :enquiry, :project, :assigned_to_me, :assigned_to_someone_else, :assigned_to_someone_else_id, :sort_by, :created_by_me, :created_by_me, :created_by_someone_else, :created_by_someone_else_id, :submitted_from,
+    params.permit(:q, :status_open, :status_closed, :page, :allegation, :enquiry, :project, :assigned_to_me, :assigned_to_someone_else, :assigned_to_someone_else_id, :sort_by, :created_by_me, :created_by_me, :created_by_someone_else, :created_by_someone_else_id,
                   assignee_teams_with_keys.map { |key, _t, _n| key }, creator_teams_with_keys.map { |key, _t, _n| key })
   end
 
