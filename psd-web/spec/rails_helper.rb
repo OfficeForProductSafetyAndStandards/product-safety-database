d8b37d3347f11f8daa77004c1bc6b8266df32787
Ed Horsford <mail@edwardhorsford.com>
2020-01-22 11:50:52 +0000

    Limit products shown on overview page (#129)
    * Limit products shown on overview page
    
    * [WIP] - update product factory to add images
    
    * [WIP]
    
    * try to add images to products
    
    * fix adding images in tests
    
    * Added tests and refactored Ed's code
    
    * tidy up
    
    * tidy up
    
    * add no doc when installing gems
    
    * fix linter error
    
    * fix templates
    
    Co-authored-by: Yann Marquet <100796+StupidCodeFactory@users.noreply.github.com>

--- psd-web/spec/rails_helper.rb
+++ psd-web/spec/rails_helper.rb
@@ -10,7 +10,6 @@ require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -37,7 +36,7 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/test/fixtures"
-
+  config.include Rails.application.routes.url_helpers
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
