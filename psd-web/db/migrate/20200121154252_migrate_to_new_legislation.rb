3d466ad0b0bae0d8768f4f79bcb6a16bfb253ace
yann marquet <ymarquet@gmail.com>
2020-01-22 10:59:03 +0000

    update new legislation

--- psd-web/db/migrate/20200121154252_migrate_to_new_legislation.rb
+++ psd-web/db/migrate/20200121154252_migrate_to_new_legislation.rb
@@ -4,7 +4,6 @@ class MigrateToNewLegislation < ActiveRecord::Migration[5.2]
     "Consumer Protection Act 1987 (Part 1)" => "Consumer Protection Act 1987",
     "Aerosol Dispensers Regulations 2009" => "Aerosol Dispensers Regulations 2009 (Consumer Protection Act 1987)",
     "Cosmetic Products Enforcement Regulations 2013 / EC 1223/2009 Cosmetic Products" => "Cosmetic Products Enforcement Regulations 2013",
-    "Furniture and Furnishings (Fire) (Safety) Regulations 1988" =>  "Furniture and Furnishings (Fire)(Safety) Regulations 1988",
     "Gas Appliances (Enforcement) and Miscellaneous Amendments Regulations 2018 / EU 2016/426 Appliances Burning Gaseous Fuels" => "Gas Appliances (Enforcement) and Miscellaneous Amendments Regulations 2018",
     "Nightwear Safety Regulations 1985" => "Nightwear (Safety) Regulations 1985",
     "Noise Emission in the Environment by Equipment for Use Outdoors Regulations 20011" => "Noise Emission in Environment by Equipment for use Outdoors (Amendment) Regulations 2015",
