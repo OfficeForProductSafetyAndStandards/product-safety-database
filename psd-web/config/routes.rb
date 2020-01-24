9865c1e4c0242d9c826b87da650cc307d9a3440f
Steven Lorek <steve@stevelorek.com>
2020-01-24 15:27:43 +0000

    Fix Sidekiq Web Forbidden errors (#216)

--- psd-web/config/routes.rb
+++ psd-web/config/routes.rb
@@ -1,6 +1,8 @@
 require "sidekiq/web"
 require "sidekiq/cron/web"
 
+Sidekiq::Web.set :session_secret, Rails.application.credentials[:secret_key_base]
+
 if Rails.env.production?
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
     ActiveSupport::SecurityUtils.secure_compare(username, ENV["SIDEKIQ_USERNAME"]) &&
