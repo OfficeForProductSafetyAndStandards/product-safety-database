53003fa46df5539dfaafc0572b8619ab416ee767
Steve Lorek <steve@stevelorek.com>
2020-01-27 14:49:07 +0000

    Remove redundant workaround code

--- psd-web/config/initializers/keycloak.rb
+++ psd-web/config/initializers/keycloak.rb
@@ -8,14 +8,3 @@ Keycloak.keycloak_controller = "sessions"
 Keycloak.realm = "opss"
 # Realm url (only used if the installation file is not present)
 Keycloak.auth_server_url = ""
-
-Rails.application.config.after_initialize do
-  begin
-    KeycloakClient.instance.all_organisations unless Rails.env.test? || Sidekiq.server?
-  rescue StandardError => e
-    # Can be deleted after the following is merged: https://github.com/imagov/keycloak/pull/11
-    # The gem we are using is importing client_id and secret too late in default_call method, causing first request
-    # to fail, and all following ones to work.
-    Rails.logger.error "Failed request to Keycloak: #{e.message}"
-  end
-end
