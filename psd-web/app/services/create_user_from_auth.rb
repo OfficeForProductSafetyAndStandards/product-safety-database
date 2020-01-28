b12bbcb6284c0756565686c4ffbda8cde2f3d6ed
yann marquet <ymarquet@gmail.com>
2020-01-28 12:02:09 +0000

    [WIP] - specs

--- psd-web/app/services/create_user_from_auth.rb
+++ psd-web/app/services/create_user_from_auth.rb
@@ -1,16 +1,44 @@
 class CreateUserFromAuth
-
   def initialize(omniauth_response)
     self.omniauth_response = omniauth_response
   end
 
   def user
     @user ||= begin
-
+      User.find_or_create_by!(id: uuid) do |user|
+        user.email        = email
+        user.name         = name
+        user.organisation = organisation
+        user.teams        = teams
+      end
     end
   end
 
-  private
+private
 
   attr_accessor :omniauth_response
+
+  def teams
+    @teams ||= groups.any? ? Team.where(path: groups) : []
+  end
+
+  def organisation
+    @organisation ||= Organisation.find_by(path: groups) || teams.first&.organisation
+  end
+
+  def groups
+    omniauth_response.dig("extra", "raw_info", "groups")
+  end
+
+  def email
+    omniauth_response.dig("info", "email")
+  end
+
+  def name
+    omniauth_response.dig("info", "name")
+  end
+
+  def uuid
+    omniauth_response["uuid"]
+  end
 end
--- psd-web/spec/services/create_user_from_auth_spec.rb
+++ psd-web/spec/services/create_user_from_auth_spec.rb
@@ -1,9 +1,22 @@
 require "rails_helper"
 
-RSpec.describe CreateUserFromAuth do
+RSpec.shared_examples "user creator" do
+  it "creates the user" do
+    expect {
+      subject.user
+    }.to change {
+      User.where(
+        id:    omniauth_response["uuid"],
+        email: omniauth_response["info"]["email"],
+        name:  omniauth_response["info"]["name"]
+      ).count
+    }.from(0).to(1)
+  end
+end
 
-  let(:organisation) { create :organisation }
-  let(:team)         { create :team, organisation: organisation }
+RSpec.describe CreateUserFromAuth do
+  let!(:organisation) { create :organisation }
+  let!(:team)         { create :team, organisation: organisation }
 
   let(:omniauth_response) do
     {
@@ -11,11 +24,11 @@ RSpec.describe CreateUserFromAuth do
       "uuid" => SecureRandom.uuid,
       "info" => {
         "name" => Faker::Name.name,
-        "email" =>"user@example.com"
+        "email" => "user@example.com"
       },
       "extra" => {
         "raw_info" => {
-          "groups" => [Faker::Quote.matz]
+          "groups" => [group]
         }
       }
     }
@@ -26,22 +39,16 @@ RSpec.describe CreateUserFromAuth do
   describe "#user" do
     context "when the user does not exists" do
       context "when belonging to an existing team" do
-        it "creates the user" do
-          expect {
-            subject.user
-          }.to change {
-            User.where(
-              email: omniauth_response["email"],
-              name: omniauth_response["name"]
-            ).count
-          }.from(0).to(1)
-        end
+        let(:group) { team.path }
+
+        it_behaves_like "user creator"
       end
 
-      context "when belonging to an existing team" do
+      context "when not belonging to an existing team" do
         context "when belonging to an existing organisation" do
-          it "creates the user" do
-          end
+          let(:group) { organisation.path }
+
+          it_behaves_like "user creator"
         end
 
         context "when not belonging to an existing organisation" do
