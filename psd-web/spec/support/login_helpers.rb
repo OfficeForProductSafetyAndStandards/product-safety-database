4a5146efb09fc30fd21da88e216f8c776631ea73
yann marquet <ymarquet@gmail.com>
2020-02-03 15:06:08 +0100

    remove old code

--- psd-web/spec/support/login_helpers.rb
+++ psd-web/spec/support/login_helpers.rb
@@ -2,8 +2,8 @@ module LoginHelpers
   module Features
     def sign_in(as_user: build(:user))
       groups = as_user.teams.flat_map(&:path) << as_user.organisation&.path
+      groups.compact!
 
-      OmniAuth.config.test_mode = true
       OmniAuth.config.mock_auth[:openid_connect] = {
         "provider" => :openid_connect,
         "uid"  => as_user.id,
@@ -22,32 +22,11 @@ module LoginHelpers
       as_user
     end
   end
-
-  def sign_out
-    allow(KeycloakClient.instance).to receive(:user_signed_in?).and_return(false)
-  end
-
-  def keycloak_login_url(additional_params = {})
-    url_params = {
-      response_type: "code",
-      client_id: ENV.fetch("KEYCLOAK_CLIENT_ID")
-    }.merge(additional_params)
-
-    "#{ENV.fetch('KEYCLOAK_AUTH_URL')}/realms/opss/protocol/openid-connect/auth?#{URI.encode_www_form(url_params)}"
-  end
-
-private
-
-  def format_user_for_get_userinfo(user)
-    { id: user.id, email: user.email, name: user.name, groups: ([user.organisation&.path] + user.teams.map(&:path)).compact }
-  end
-
-  def access_token
-    "test"
-  end
 end
 
 RSpec.configure do |config|
-  config.include LoginHelpers
   config.include LoginHelpers::Features, type: :feature
+  config.before do
+    OmniAuth.config.test_mode = true
+  end
 end
