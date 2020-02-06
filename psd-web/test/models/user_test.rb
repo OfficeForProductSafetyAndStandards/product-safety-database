0b35e89bf9c527f48884f2fec3c66e7fa7bdd2eb
Frankie Roberto <frankie@frankieroberto.com>
2020-02-06 10:04:44 +0000

    Fix user model test

--- psd-web/test/models/user_test.rb
+++ psd-web/test/models/user_test.rb
@@ -1,20 +1,10 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  setup do
-    mock_out_keycloak_and_notify
-    @user = User.find_by(name: "Test User_one")
-    @user_four = User.find_by(name: "Test User_four")
-
-    mock_user_as_non_opss(@user)
-    mock_user_as_opss(@user_four)
-  end
 
   test "display name includes user's organisation for non-org-member viewers" do
-    sign_in_as @user_four
-    assert_equal "Test User_one (Organisation 1)", @user.display_name
+    assert_equal "Yann (Southampton Council)", users(:southampton).display_name(other_user: users(:opss))
 
-    sign_in_as @user
-    assert_equal "Test User_four (Office of Product Safety and Standards)", @user_four.display_name
+    assert_equal "Slavosh (Office for Product Safety and Standards)", users(:opss).display_name(other_user: users(:southampton))
   end
 end
