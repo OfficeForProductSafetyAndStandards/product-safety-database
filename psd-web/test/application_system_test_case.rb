e4dd20e2894855c3240a5f3c4fc662f3e8299404
yann marquet <ymarquet@gmail.com>
2020-02-10 18:57:22 +0100

    remove unnessary host and port mappings

--- psd-web/test/application_system_test_case.rb
+++ psd-web/test/application_system_test_case.rb
@@ -2,21 +2,13 @@ require "test_helper"
 require "system_test_helper"
 require_relative "test_helpers/devise"
 
-ENV["HTTP_HOST"] = "localhost"
-ENV["HTTP_PORT"] = "3001"
-
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include SystemTestHelper
   include Capybara::DSL
   include Capybara::Minitest::Assertions
   include TestHelpers::Devise
 
-  Capybara.server_host = ENV["HTTP_HOST"]
-  Capybara.server_port = ENV["HTTP_PORT"]
-  Capybara.app_host = "http://#{ENV['HTTP_HOST']}:#{ENV['HTTP_PORT']}"
-  Capybara.default_host = "http://#{ENV['HTTP_HOST']}:#{ENV['HTTP_PORT']}"
   Capybara.default_max_wait_time = 3
-  Rails.application.routes.default_url_options = { host: ENV["HTTP_HOST"], port: ENV["HTTP_PORT"] }
 
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { args: %w[headless disable-gpu no-sandbox disable-dev-shm-usage] }
 
