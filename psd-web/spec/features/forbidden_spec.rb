9473b364d0d4415434263a83f4992d5210835791
yann marquet <ymarquet@gmail.com>
2020-02-12 17:49:46 +0100

    Fix another page

--- psd-web/spec/features/forbidden_spec.rb
+++ psd-web/spec/features/forbidden_spec.rb
@@ -3,8 +3,7 @@ require "rails_helper"
 RSpec.feature "Access forbidden", :with_stubbed_keycloak_config do
   scenario "Logging in when user does not yet exist and has no groups" do
     sign_in(as_user: build(:user, organisation: nil))
-    expect(page).to have_text("You don’t have permission to see this page")
     visit "/cases"
-    expect(page).to have_link("Sign in to your account", href: user_openid_connect_omniauth_authorize_path)
+    expect(page).to have_text("You don’t have permission to see this page")
   end
 end
