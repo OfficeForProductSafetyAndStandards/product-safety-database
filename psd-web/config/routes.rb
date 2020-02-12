00c5896f54d60862c3f650e54c8693a70ff90b4e
yann marquet <ymarquet@gmail.com>
2020-02-12 17:43:19 +0100

    fix rubocop offense

--- psd-web/config/routes.rb
+++ psd-web/config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   devise_scope :user do
-    get "users/sign_in", to: "sessions#new", :as => :new_user_session
+    get "users/sign_in", to: "sessions#new", as: :new_user_session
 
     resource :session, only: [] do
       get :logout, to: "sessions#destroy"
