381a8419aca4edc50a74025c38de99bd618c23ff
Frankie Roberto <frankie@frankieroberto.com>
2020-02-12 12:41:37 +0000

    rubocop fixes

--- psd-web/app/helpers/password_helper.rb
+++ psd-web/app/helpers/password_helper.rb
@@ -1,10 +1,8 @@
 module PasswordHelper
-
   def password(params = {})
     params["type"] = "password"
     params["autocomplete"] = "tel"
 
     render partial: "components/govuk_input", locals: params
   end
-
 end
--- psd-web/app/helpers/telephone_number_helper.rb
+++ psd-web/app/helpers/telephone_number_helper.rb
@@ -1,10 +1,10 @@
 module TelephoneNumberHelper
-
+  # rubocop:disable Naming/MethodName
   def telephoneNumber(params = {})
     params["type"] = "tel"
     params["autocomplete"] = "tel"
 
     render partial: "components/govuk_input", locals: params
   end
-
+  # rubocop:enable Naming/MethodName
 end
