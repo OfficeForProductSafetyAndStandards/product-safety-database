54f71d8b67c6bc64093ba5997a1f790b770574ae
yann marquet <ymarquet@gmail.com>
2020-02-12 17:29:07 +0100

    ensure we are redirected to keycloack when not authenticated

--- psd-web/config/routes.rb
+++ psd-web/config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   devise_scope :user do
+    get "users/sign_in", to: "sessions#new", :as => :new_user_session
+
     resource :session, only: [] do
       get :logout, to: "sessions#destroy"
     end
