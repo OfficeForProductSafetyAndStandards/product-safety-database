f3dbf41c3316daaab75eb0e3d1b770b502521c65
nasirkhanbeis <47983567+nasirkhanbeis@users.noreply.github.com>
2020-02-13 15:55:47 +0000

    Team page tests (#259)
    * added invite team member tests
    
    * fixed code style
    
    * fixed spacing and typo
    
    * fixed test
    
    * removed commented test
    
    * addressed review comments
    
    * re-worded context and scenario
    
    * updated wording
    
    * fixed false positive
    
    * reverted changes
    
    * removed to_s
    
    * fixed tests after master merge

--- psd-web/spec/features/team_spec.rb
+++ psd-web/spec/features/team_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
-RSpec.feature "Your team page", :with_stubbed_elasticsearch, :with_stubbed_keycloak_config do
+
+RSpec.feature "Your team page", :with_stubbed_keycloak_config, :with_stubbed_mailer, :with_stubbed_elasticsearch do
   let(:team) { create(:team) }
   let(:user) { create(:user, :activated, teams: [team], has_viewed_introduction: true) }
 
@@ -20,13 +21,15 @@ RSpec.feature "Your team page", :with_stubbed_elasticsearch, :with_stubbed_keycl
     expect(page).not_to have_css(".teams--user .teams--user-email:contains(\"#{another_user_another_team.email}\")")
   end
 
+  def resend_link_selector(email)
+    "a[href=\"#{resend_invitation_team_path(team)}?email_address=#{CGI.escape(email)}\"]"
+  end
+
   context "re-sending invitation email" do
     context "as a team admin user" do
       let(:user) { create(:user, :activated, :team_admin, teams: [team], has_viewed_introduction: true) }
 
-      def resend_link_selector(email)
-        "a[href=\"#{resend_invitation_team_path(team)}?email_address=#{CGI.escape(email)}\"]"
-      end
+
 
       scenario "only displays the link for inactive users" do
         expect(page).to have_css(resend_link_selector(another_inactive_user.email))
@@ -34,12 +37,30 @@ RSpec.feature "Your team page", :with_stubbed_elasticsearch, :with_stubbed_keycl
       end
     end
 
-    context "as a normal user" do
-      let(:user) { create(:user, :activated, :psd_user, teams: [team]) }
+    context "as a team admin" do
+      let(:user) { create(:user, :activated, :team_admin, teams: [team], has_viewed_introduction: true) }
 
-      scenario "does not display the link for any users" do
-        expect(page).not_to have_css("a[href*=\"#{resend_invitation_team_path(team)}\"]")
+      scenario "inviting an existing user shows an error message" do
+        click_link "Invite a team member"
+        expect(page).to have_css("h1", text: "Invite a team member")
+        fill_in "new_user_email_address", with: user.email
+        click_button "Send invitation email"
+        expect(page).to have_css(".govuk-error-summary__list", text: "You cannot invite this person to join your team because they are already a member of another team from a different organisation.")
       end
+    end
+  end
+
+  context "as a normal user" do
+    let(:user) { create(:user, :activated, :psd_user, teams: [team], has_viewed_introduction: true) }
+
+    scenario "does not display the resend invite link for any users" do
+      expect(page).to have_css("h1", text: "test organisation")
+      expect(page).not_to have_link(resend_link_selector(another_inactive_user.email))
+    end
+
+    scenario "does not display the invite a team member link" do
+      expect(page).to have_css("h1", text: "test organisation")
+      expect(page).not_to have_button("Invite a team member")
     end
   end
 end
