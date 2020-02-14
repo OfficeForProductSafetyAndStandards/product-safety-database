320f487469b41c44d485534b73306e8ec88628f2
Slawosz Slawinski <slawosz@gmail.com>
2020-02-14 16:20:41 +0000

    Typo

--- psd-web/app/models/keycloak_connector.rb
+++ psd-web/app/models/keycloak_connector.rb
@@ -1,6 +1,3 @@
-require 'base64'
-require 'openssl'
-
 class KeycloakConnector < ApplicationRecord
   KEY_LEN = 64
   CREDENTIALS_QUERY = <<-SQL
@@ -10,7 +7,7 @@ SELECT c.salt, c.value, c.hash_iterations, u.email, c.type FROM credential c INN
   establish_connection :keycloak
 
   def self.copy_keycloak_credentials
-    row = KeycloakConnector.connection.query(CREDENTIALS_QUERY)
+    rows = KeycloakConnector.connection.query(CREDENTIALS_QUERY)
     rows.each do |salt, encrypted_password, iterations, email, credential_type|
       KeycloakCredential.create! do |kc|
         kc.salt = salt
@@ -20,14 +17,6 @@ SELECT c.salt, c.value, c.hash_iterations, u.email, c.type FROM credential c INN
         kc.credential_type = credential_type
       end
     end
-  end
-
-  def encrypt_password(plain_password, salt, iterations)
-    hash = OpenSSL::Digest::SHA256.new
-    value = OpenSSL::KDF.pbkdf2_hmac(plain_password, salt: salt, iterations: iterations,
-                                    length: KEY_LEN, hash: hash)
-    # we want to remove trailing newline
-    Base64.strict_encode64(value).strip
   end
 end
 
