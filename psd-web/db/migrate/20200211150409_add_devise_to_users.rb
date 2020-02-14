ca56d4fe80c73c8ca5d66196a6d00faee364b575
yann marquet <ymarquet@gmail.com>
2020-02-14 14:02:10 +0100

    make migration rollbackable

--- psd-web/db/migrate/20200211150409_add_devise_to_users.rb
+++ psd-web/db/migrate/20200211150409_add_devise_to_users.rb
@@ -2,7 +2,7 @@
 
 class AddDeviseToUsers < ActiveRecord::Migration[5.2]
   # rubocop:disable Rails/BulkChangeTable
-  def self.up
+  def change
     safety_assured do
       change_table :users do |t|
         ## Database authenticatable
@@ -21,33 +21,10 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
         t.datetime :last_sign_in_at
         t.inet     :current_sign_in_ip
         t.inet     :last_sign_in_ip
-
-        ## Confirmable
-        # t.string   :confirmation_token
-        # t.datetime :confirmed_at
-        # t.datetime :confirmation_sent_at
-        # t.string   :unconfirmed_email # Only if using reconfirmable
-
-        ## Lockable
-        # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-        # t.string   :unlock_token # Only if unlock strategy is :email or :both
-        # t.datetime :locked_at
-
-
-        # Uncomment below if timestamps were not included in your original model.
-        # t.timestamps null: false
       end
 
       add_index :users, :reset_password_token, unique: true
-      # add_index :users, :confirmation_token,   unique: true
-      # add_index :users, :unlock_token,         unique: true
     end
   end
   # rubocop:enable Rails/BulkChangeTable
-
-  def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
-  end
 end
