96f3b8a5e614cd2c0a6bdef2d8534dba901ce026
yann marquet <ymarquet@gmail.com>
2020-02-14 12:47:37 +0100

    fix some of the specs

--- psd-web/spec/requests/login_spec.rb
+++ psd-web/spec/requests/login_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Login" do
   context "When not logged in" do
     it "redirects to the login page" do
       expect(get(investigations_path))
-        .to redirect_to(root_path)
+        .to redirect_to(new_user_session_path)
     end
   end
 end
