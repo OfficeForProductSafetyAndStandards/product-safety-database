b339ef5f229ebdd89016722a0f5bc9ac25b684ee
Slawosz Slawinski <slawosz@gmail.com>
2020-02-14 16:03:27 +0000

    Use keycloak database

--- infrastructure/env/get-env-from-vcap.sh
+++ infrastructure/env/get-env-from-vcap.sh
@@ -12,3 +12,5 @@ echo $VCAP_SERVICES | ./env/jq -r '
                 | "\(.key)=\(.value)"
         )
         | @tsv'
+
+echo "KEYCLOAK_DATABASE_URI=$(echo $VCAP_SERVICES | ./env/jq -r '.postgres[] | select(.name == "keycloak-database") .credentials .uri')"
---
+++ psd-web/db/migrate/20200213114958_create_keycloak_credentials.rb
@@ -0,0 +1,13 @@
+class CreateKeycloakCredentials < ActiveRecord::Migration[5.2]
+  def change
+    create_table :keycloak_credentials do |t|
+      t.column :salt, "bytea"
+      t.string :encrypted_password
+      t.integer :hash_iterations
+      t.string :email
+      t.string :credential_type
+
+      t.index :email
+    end
+  end
+end
