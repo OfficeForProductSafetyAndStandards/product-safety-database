e61666e88a6ce9a57c6761c15a25e08acfcd35ec
Frankie Roberto <frankie@frankieroberto.com>
2020-02-17 10:01:16 +0000

    Use PSD_HOST env variable
    This is already defined in staging and production environments, for use by ActionController.

--- psd-web/app/jobs/send_user_invitation_job.rb
+++ psd-web/app/jobs/send_user_invitation_job.rb
@@ -8,7 +8,7 @@ class SendUserInvitationJob < ApplicationJob
     user = User.find(user_id)
     user_inviting = User.find(user_inviting_id)
 
-    invitation_url = create_account_user_url(user.id, invitation: user.invitation_token, host: ENV.fetch("HOST"))
+    invitation_url = create_account_user_url(user.id, invitation: user.invitation_token, host: ENV.fetch("PSD_HOST"))
 
     NotifyMailer.invitation_email(user.email, invitation_url, user_inviting.name).deliver_now
     user.update(has_been_sent_welcome_email: true)
