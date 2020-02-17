767af6f5d45ab9c22904fa6dd6a033312c327ee0
Frankie Roberto <frankie@frankieroberto.com>
2020-02-17 14:57:09 +0000

    removed unused url

--- psd-web/app/jobs/send_user_invitation_job.rb
+++ psd-web/app/jobs/send_user_invitation_job.rb
@@ -8,8 +8,6 @@ class SendUserInvitationJob < ApplicationJob
     user = User.find(user_id)
     user_inviting = User.find(user_inviting_id)
 
-    invitation_url = create_account_user_url(user.id, invitation: user.invitation_token, host: ENV.fetch("PSD_HOST"))
-
     NotifyMailer.invitation_email(user, user_inviting).deliver_now
     user.update!(has_been_sent_welcome_email: true, invited_at: Time.current)
   end
