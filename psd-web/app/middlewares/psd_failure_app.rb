fe3a68d2d371174d4615256abb0a54404fd1b2a1
yann marquet <ymarquet@gmail.com>
2020-02-18 12:10:49 +0000

    fix some spec

--- psd-web/app/middlewares/psd_failure_app.rb
+++
@@ -1,3 +0,0 @@
-class PsdFailureApp < FailureApp::FailureApp
-
-end
--- psd-web/app/services/sign_user_in.rb
+++ psd-web/app/services/sign_user_in.rb
@@ -1,14 +1,14 @@
 class SignUserIn
   include ActiveModel::Model
   include ActiveModel::Attributes
+
   attribute :email
   attribute :password
 
-  validates :email, email: { message: "Enter your email address in the correct format, like name@example.com" }
+  validates :email, email: {
+              message: "Enter your email address in the correct format, like name@example.com",
+              if: ->(sign_in_form) { sign_in_form.email.present? }
+            }
   validates_presence_of :email, message: "Enter your email address"
   validates_presence_of :password, message: "Enter your password"
-
-private
-
-  attr_accessor :email, :password
 end
