47e10a268f49d7889a7c9c2fb78a557812059608
Slawosz Slawinski <slawosz@gmail.com>
2020-02-18 16:34:17 +0000

    Refactor spec

--- psd-web/spec/models/keycloak_credential_spec.rb
+++ psd-web/spec/models/keycloak_credential_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe KeycloakCredential, type: :model do
-  context "authentication" do
+  describe ".authenticate" do
     before do
       create(:keycloak_credential)
     end
@@ -10,25 +10,18 @@ RSpec.describe KeycloakCredential, type: :model do
       "passwordpasswordpasswordpassword"
     end
     let(:email)       { "test@example.org" }
-    let(:wrong_email) { "wrong@example.org" }
 
-    context "when successful" do
-      let(:email)       { "test@example.org" }
-
-      it "returns true" do
-        expect(KeycloakCredential.authenticate(email, password)).to be_truthy
-      end
+    it "returns true when password matches" do
+      expect(KeycloakCredential.authenticate(email, password)).to be_truthy
     end
 
-    context "when unsuccessful" do
-      it "raises NotFound when email not found" do
-        expect { KeycloakCredential.authenticate("notsuch@email.org", password) }
-          .to raise_exception(ActiveRecord::RecordNotFound)
-      end
+    it "raises NotFound when email not found" do
+      expect { KeycloakCredential.authenticate("notsuch@email.org", password) }
+        .to raise_exception(ActiveRecord::RecordNotFound)
+    end
 
-      it "returns false when password does not match" do
-        expect(KeycloakCredential.authenticate(email, password.reverse)).to be_falsy
-      end
+    it "returns false when password does not match" do
+      expect(KeycloakCredential.authenticate(email, password.reverse)).to be_falsy
     end
   end
 end
