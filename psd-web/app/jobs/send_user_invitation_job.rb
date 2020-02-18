72eed08d452c5361740cc8125fd654ff3881dcde
Marc Sardon <pringaohowto@gmail.com>
2020-02-18 16:01:30 +0000

    Remove routes url helpers from user invitation job
    Since the invitation url is now generated in the Mailer, having this
    helper in the job became unnecessary.

--- psd-web/app/jobs/send_user_invitation_job.rb
+++ psd-web/app/jobs/send_user_invitation_job.rb
@@ -1,7 +1,6 @@
 require "notifications/client"
 
 class SendUserInvitationJob < ApplicationJob
-  include Rails.application.routes.url_helpers
   GOV_UK_NOTIFY_TEMPLATE_ID = "22b3799c-aa3d-43e8-899d-3f30307a488f".freeze
 
   def perform(user_id, user_inviting_id)
