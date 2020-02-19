2485fe5c333cede1cdc00bc2c2c3cf353063d247
Slawosz Slawinski <slawosz@gmail.com>
2020-02-19 14:30:27 +0000

    Extract encryption

--- psd-web/app/models/keycloak_credential.rb
+++ psd-web/app/models/keycloak_credential.rb
@@ -1,12 +1,11 @@
 class KeycloakCredential < ApplicationRecord
-  HASH_FUNCTION = OpenSSL::Digest::SHA256.new
-  KEY_LEN       = 64
 
   def self.authenticate(email, plain_password)
     kc = self.find_by!(email: email)
-    hashed_password = OpenSSL::KDF.pbkdf2_hmac(plain_password, salt: kc.salt, iterations: kc.hash_iterations,
-                                    length: KEY_LEN, hash: HASH_FUNCTION)
-    hashed_password64 = Base64.strict_encode64(hashed_password).strip
-    hashed_password64 == kc.encrypted_password
+
+    encryption = Pbkdf2Encryption.new(plain_password, salt: kc.salt, iterations: kc.hash_iterations)
+    hashed_password = encryption.generate_hash
+
+    hashed_password == kc.encrypted_password
   end
 end
