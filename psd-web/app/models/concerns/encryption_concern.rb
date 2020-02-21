44f306ff85fbd48c147dfecc2835bedcf3658d5e
yann marquet <ymarquet@gmail.com>
2020-02-21 14:06:18 +0100

    tidy up

--- psd-web/app/models/concerns/encryption_concern.rb
+++ psd-web/app/models/concerns/encryption_concern.rb
@@ -1,15 +1,16 @@
 module EncryptionConcern
+  extend ActiveSupport::Concern
+  included do
+    has_one :keycloak_credential, foreign_key: :email, inverse_of: :user
+  end
+
   def valid_password?(password)
-    password_digest(password) == keycloak_user.encrypted_password
+    password_digest(password) == keycloak_credential.encrypted_password
   end
 
   def password_digest(password)
-    salt = keycloak_user.salt
+    salt = keycloak_credential.salt
     e = Pbkdf2Encryption.new(password, salt: salt)
     e.generate_hash
-  end
-
-  def keycloak_user
-    KeycloakCredential.find_by!(email: self.email)
   end
 end
