06f7e0ae981454901788d93a45542391207702b9
yann marquet <ymarquet@gmail.com>
2020-02-21 11:45:01 +0100

    [WIP] - fix form: send params in the user[] param

--- psd-web/spec/features/password_reset_spec.rb
+++ psd-web/spec/features/password_reset_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
-RSpec.describe "Password reset management" do
+RSpec.describe "Password reset management", :js do
   let(:user) { create(:user) }
 
   it "allows you to reset your password" do
     visit new_user_session_path
-
     click_link "Forgot your password?"
-    fill_in "Email address", with: user.email
+    fill_in "user[email]", with: user.email
+    save_and_open_page
     click_on "Send email"
 
     expect(page).to have_css("p.govuk-body", text: "Click the link in the email to reset your password.")
