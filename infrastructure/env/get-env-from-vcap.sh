22d90d14cb57d033e2e18f6408d34449f31cf079
Steve Lorek <steve@stevelorek.com>
2020-02-21 11:03:16 +0000

    Revert "Authenticate user using Keycloak DB (#289)"
    This reverts commit c37cc15523ef68d6e734b1153b8a59bfa40bcdfb.

--- infrastructure/env/get-env-from-vcap.sh
+++ infrastructure/env/get-env-from-vcap.sh
@@ -12,5 +12,3 @@ echo $VCAP_SERVICES | ./env/jq -r '
                 | "\(.key)=\(.value)"
         )
         | @tsv'
-
-echo "KEYCLOAK_DATABASE_URI=$(echo $VCAP_SERVICES | ./env/jq -r '.postgres[] | select(.name == "keycloak-database") .credentials .uri')"
