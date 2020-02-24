8b1a66ee013a1e152ec8b8d5475c01d9588a3bae
yann marquet <ymarquet@gmail.com>
2020-02-24 12:44:03 +0100

    re-add spec

--- psd-web/spec/forms/sign_in_form_spec.rb
+++ psd-web/spec/forms/sign_in_form_spec.rb
@@ -7,5 +7,33 @@ RSpec.describe SignInForm do
   subject { described_class.new(email: email, password: password) }
 
   describe "#valid?" do
+    describe "when the email is blank" do
+      let(:email) { "" }
+
+      it "is no valid" do
+        expect(subject).to be_invalid
+        expect(subject.errors.full_messages_for(:email)).to eq(["Enter your email address"])
+      end
+    end
+
+    context "when the email is not blank" do
+      context "when it does not contain an @" do
+        let(:email) { "not_an_email" }
+
+        it "is not valid" do
+          expect(subject).to be_invalid
+          expect(subject.errors.full_messages_for(:email)).to eq(["Enter your email address in the correct format, like name@example.com"])
+        end
+      end
+    end
+
+    describe "when the password is blank" do
+      let(:password) { "" }
+
+      it "is no valid" do
+        expect(subject).to be_invalid
+        expect(subject.errors.full_messages_for(:password)).to eq(["Enter your password"])
+      end
+    end
   end
 end
