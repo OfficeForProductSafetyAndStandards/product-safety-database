115dcf8b427f030583ad72b587a6ed326f93ac4b
yann marquet <ymarquet@gmail.com>
2020-02-24 12:19:45 +0100

    remove decorator code

--- psd-web/app/decorators/user_decorator.rb
+++ psd-web/app/decorators/user_decorator.rb
@@ -15,43 +15,4 @@ class UserDecorator < Draper::Decorator
     h.govukErrorSummary(titleText: "There is a problem", errorList: error_list)
   end
 
-  def email_input
-    options = {
-      id: "email",
-      name: "user[email]",
-      type: "email",
-      classes: "app-!-max-width-two-thirds",
-      label: { text: "Email address" },
-      errorMessage: format_errors_for(errors.full_messages_for(:email)),
-      value: object.email
-    }
-
-    h.render "components/govuk_input", options
-  end
-
-  def password_input
-    options = {
-      id: "password",
-      name: "user[password]",
-      type: "password",
-      classes: "app-!-max-width-two-thirds",
-      label: { text: "Password" },
-      errorMessage: format_errors_for(errors.full_messages_for(:password))
-    }
-
-    h.render "components/govuk_input", options
-  end
-
-private
-
-  def format_errors_for(errors_for_field)
-    return base_errors if object.errors.include?(:base)
-    return             if errors_for_field.empty?
-
-    { text: errors_for_field.to_sentence(last_word_connector: " and ") }
-  end
-
-  def base_errors
-    { text: errors.full_messages_for(:base).to_sentence(last_word_connector: " and ") }
-  end
 end
