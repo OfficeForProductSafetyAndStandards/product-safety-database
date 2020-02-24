f5126ddac31ae22e1e8a54769468b70a4bf5227c
yann marquet <ymarquet@gmail.com>
2020-02-24 17:17:14 +0100

    fix reset password form

--- psd-web/spec/features/password_reset_spec.rb
+++ psd-web/spec/features/password_reset_spec.rb
@@ -1,17 +1,14 @@
 require "rails_helper"
 
-RSpec.describe "Password reset management", :js do
+RSpec.describe "Password reset management" do
   let(:user) { create(:user) }
 
   it "allows you to reset your password" do
     visit new_user_session_path
     click_link "Forgot your password?"
-    fill_in "user[email]", with: user.email
-    save_and_open_page
+    fill_in "Email address", with: user.email
     click_on "Send email"
 
     expect(page).to have_css("p.govuk-body", text: "Click the link in the email to reset your password.")
-
-    pp ActionMailer::Base.deliveries
   end
 end
