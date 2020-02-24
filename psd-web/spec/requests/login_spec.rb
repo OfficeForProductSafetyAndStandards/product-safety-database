5fddc417e39cdf9083cbcc0a0311301799247fc9
Frankie Roberto <frankie@frankieroberto.com>
2020-02-24 12:53:23 +0000

    codestyle

--- psd-web/spec/requests/login_spec.rb
+++ psd-web/spec/requests/login_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe "Login" do
   context "When not logged in" do
-
     # TODO: update once merged with the sign in branch
     it "redirects to the login page"
   end
