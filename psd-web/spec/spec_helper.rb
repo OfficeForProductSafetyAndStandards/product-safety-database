4701de1ba299dcf5f9a4fbb14b25eb04dffecfad
yann marquet <ymarquet@gmail.com>
2020-02-25 17:57:43 +0100

    fix specs

--- psd-web/spec/spec_helper.rb
+++ psd-web/spec/spec_helper.rb
@@ -3,8 +3,6 @@
 require "webmock/rspec"
 
 RSpec.configure do |config|
-  config.include ActiveSupport::Testing::TimeHelpers
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
