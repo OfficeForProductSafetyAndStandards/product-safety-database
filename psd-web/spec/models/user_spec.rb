f1467ffc3ae76cfd47c7b1e4c7c7767971bbd1fb
yann marquet <ymarquet@gmail.com>
2020-02-25 18:53:35 +0100

    tidy up

--- psd-web/spec/models/user_spec.rb
+++ psd-web/spec/models/user_spec.rb
@@ -118,8 +118,8 @@ RSpec.describe User do
     it "includes associations needed for display_name" do
       assignees = described_class.get_assignees.to_a # to_a forces the query execution and load immediately
       expect(-> {
-               assignees.map(&:display_name)
-             }).to not_talk_to_db
+        assignees.map(&:display_name)
+      }).to not_talk_to_db
     end
 
     context "when a user to except is supplied" do
