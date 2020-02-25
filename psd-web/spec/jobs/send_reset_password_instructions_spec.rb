e1e089a708ee5b01d617c2a2512e6540bb918831
yann marquet <ymarquet@gmail.com>
2020-02-25 11:46:00 +0100

    test mailer is sent correctly

--- psd-web/spec/jobs/send_reset_password_instructions_spec.rb
+++ psd-web/spec/jobs/send_reset_password_instructions_spec.rb
@@ -1,14 +1,17 @@
 require "rails_helper"
 
 RSpec.describe SendResetPasswordInstructions do
-  let(:token) { SecureRandom.hex }
-  let(:user) { build(:user) }
+  let(:token)  { SecureRandom.hex }
+  let(:user)   { build(:user) }
+  let(:mailer) { double(NotifyMailer, deliver_now: nil) }
 
   describe "#perform" do
     it "sends the email" do
-      expect(NotifyMailer).to receive(:reset_password_instruction).with(user, token)
+      expect(NotifyMailer)
+        .to receive(:reset_password_instruction)
+              .with(user, token).and_return(mailer)
 
-      SendResetPasswordInstructions
+      described_class.perform_now(user, token)
     end
   end
 end
