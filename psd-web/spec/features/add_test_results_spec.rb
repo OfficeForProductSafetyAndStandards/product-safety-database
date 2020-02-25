231604f92c8c2425ccbf364844e4befe5005c67b
nasirkhanbeis <47983567+nasirkhanbeis@users.noreply.github.com>
2020-02-25 22:29:40 +0000

    Update psd-web/spec/features/add_test_results_spec.rb
    Co-Authored-By: Steven Lorek <steve@stevelorek.com>

--- psd-web/spec/features/add_test_results_spec.rb
+++ psd-web/spec/features/add_test_results_spec.rb
@@ -1,4 +1,5 @@
 require "rails_helper"
+
 RSpec.feature "Adding a test result", :with_stubbed_elasticsearch, :with_stubbed_antivirus, :with_stubbed_mailer, :with_stubbed_keycloak_config do
   let(:user) { create(:user, :activated, has_viewed_introduction: true) }
   let(:investigation) { create(:allegation, products: [create(:product_washing_machine)], assignee: user) }
