1da78839ee68e99513281e3950ce1bba3627ff13
nasirkhanbeis <nasirkhan.beis@gmail.com>
2020-02-25 13:50:17 +0000

    added sign-in edge case tests

--- psd-web/spec/features/sign_in_spec.rb
+++ psd-web/spec/features/sign_in_spec.rb
@@ -42,5 +42,39 @@ RSpec.feature "Signing in", :with_elasticsearch, :with_stubbed_mailer do
         expect(page).to have_css("span#password-error", text: "")
       end
     end
+    context "when email address is not in correct format" do
+      scenario "shows an error message" do
+        visit root_path
+        click_on "Sign in to your account"
+        fill_in "Email address", with: "test.email"
+        fill_in "Password", with: "password"
+        click_on "Continue"
+        expect(page).to have_css(".govuk-error-summary__list", text: "Enter your email address in the correct format, like name@example.com")
+        expect(page).to have_css(".govuk-error-message", text: "Enter your email address in the correct format, like name@example.com")
+      end
+    end
+
+    context "when email and password fields left empty" do
+      scenario "shows error messages" do
+        visit root_path
+        click_on "Sign in to your account"
+        fill_in "Email address", with: " "
+        fill_in "Password", with: " "
+        click_on "Continue"
+        expect(page).to have_css(".govuk-error-message", text: "Enter your email address")
+        expect(page).to have_css(".govuk-error-message", text: "Enter your password")
+      end
+    end
+    context "when password field is left empty" do
+      scenario "shows an error messages" do
+        visit root_path
+        click_on "Sign in to your account"
+        fill_in "Email address", with: user.email
+        fill_in "Password", with: " "
+        click_on "Continue"
+        expect(page).to have_css(".govuk-error-message", text: "Enter your password")
+        expect(page).to have_css(".govuk-error-summary__list", text: "Enter your password")
+      end
+    end
   end
 end
