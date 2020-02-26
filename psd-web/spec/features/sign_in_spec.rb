8a10384ad8aa62c00b6435057cea248c1b0ea2b6
nasirkhanbeis <nasirkhan.beis@gmail.com>
2020-02-26 09:55:17 +0000

    fixed styling-spaces

--- psd-web/spec/features/sign_in_spec.rb
+++ psd-web/spec/features/sign_in_spec.rb
@@ -46,12 +46,12 @@ RSpec.feature "Signing in", :with_elasticsearch, :with_stubbed_mailer do
       scenario "shows an error message" do
         visit root_path
         click_on "Sign in to your account"
-        
+
 
         fill_in "Email address", with: "test.email"
         fill_in "Password", with: "password "
         click_on "Continue"
-        
+
 
         expect(page).to have_css(".govuk-error-summary__list", text: "Enter your email address in the correct format, like name@example.com")
         expect(page).to have_css(".govuk-error-message", text: "Enter your email address in the correct format, like name@example.com")
@@ -68,7 +68,6 @@ RSpec.feature "Signing in", :with_elasticsearch, :with_stubbed_mailer do
         fill_in "Password", with: " "
         click_on "Continue"
 
-
         expect(page).to have_css(".govuk-error-message", text: "Enter your email address")
         expect(page).to have_css(".govuk-error-message", text: "Enter your password")
       end
@@ -79,12 +78,11 @@ RSpec.feature "Signing in", :with_elasticsearch, :with_stubbed_mailer do
       scenario "shows an error messages" do
         visit root_path
         click_on "Sign in to your account"
-        
+
 
         fill_in "Email address", with: user.email
         fill_in "Password", with: " "
         click_on "Continue"
-        
 
         expect(page).to have_css(".govuk-error-message", text: "Enter your password")
         expect(page).to have_css(".govuk-error-summary__list", text: "Enter your password")
