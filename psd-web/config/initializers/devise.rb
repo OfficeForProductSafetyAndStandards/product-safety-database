4d5ed215bb800cf61075158291ac4a072dbd64d5
yann marquet <ymarquet@gmail.com>
2020-02-26 13:26:28 +0000

    wrap devise 2fa config in an if statement
    OTP_SECRET_ENCRYPTION_KEY will no be available at buildpack time

--- psd-web/config/initializers/devise.rb
+++ psd-web/config/initializers/devise.rb
@@ -319,15 +319,17 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
   # Devise two_factor_authentication gem
-  config.max_login_attempts = 3  # Maximum second factor attempts count.
-  config.allowed_otp_drift_seconds = 30  # Allowed TOTP time drift between client and server.
-  config.otp_length = 6  # TOTP code length
-  config.direct_otp_valid_for = 5.minutes  # Time before direct OTP becomes invalid
-  config.direct_otp_length = 6  # Direct OTP code length
-  config.remember_otp_session_for_seconds = 30.days  # Time before browser has to perform 2fA again. Default is 0.
-  config.otp_secret_encryption_key = ENV.fetch('OTP_SECRET_ENCRYPTION_KEY')
-  config.second_factor_resource_id = 'id' # Field or method name used to set value for 2fA remember cookie
-  config.delete_cookie_on_logout = false # Delete cookie when user signs out, to force 2fA again on login
+  if ENV["OTP_SECRET_ENCRYPTION_KEY"]
+    config.max_login_attempts = 3  # Maximum second factor attempts count.
+    config.allowed_otp_drift_seconds = 30  # Allowed TOTP time drift between client and server.
+    config.otp_length = 6  # TOTP code length
+    config.direct_otp_valid_for = 5.minutes  # Time before direct OTP becomes invalid
+    config.direct_otp_length = 6  # Direct OTP code length
+    config.remember_otp_session_for_seconds = 30.days  # Time before browser has to perform 2fA again. Default is 0.
+    config.otp_secret_encryption_key = ENV.fetch("OTP_SECRET_ENCRYPTION_KEY")
+    config.second_factor_resource_id = 'id' # Field or method name used to set value for 2fA remember cookie
+    config.delete_cookie_on_logout = false # Delete cookie when user signs out, to force 2fA again on login
+  end
 end
 
 DeviseController.include NoSecondaryNav
