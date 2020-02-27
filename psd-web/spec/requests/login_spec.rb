87cfb8e0429c4f43de0693281ee2716c663085c2
yann marquet <ymarquet@gmail.com>
2020-02-27 18:13:17 +0100

    [WIP] - try to fix test. the redirect doesnt work

--- psd-web/spec/requests/login_spec.rb
+++ psd-web/spec/requests/login_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe "Login" do
         .to redirect_to(new_user_session_path)
     end
   end
+
+  context "When not logged in", :with_stubbed_elasticsearch do
+    before { sign_in create(:user, :activated, has_viewed_introduction: true) }
+
+    it "does not have a flash messages" do
+      expect(get(new_user_session_path))
+        .to redirect_to(investigations_path)
+      expect(flash).to be_empty
+    end
+  end
 end
