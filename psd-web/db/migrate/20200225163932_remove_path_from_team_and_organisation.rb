132e0bb85aef21e1155d245b79f947247dcf2514
Slawosz Slawinski <slawosz@gmail.com>
2020-02-27 14:23:27 +0000

    Add column type for rubocop

--- psd-web/db/migrate/20200225163932_remove_path_from_team_and_organisation.rb
+++ psd-web/db/migrate/20200225163932_remove_path_from_team_and_organisation.rb
@@ -1,8 +1,8 @@
 class RemovePathFromTeamAndOrganisation < ActiveRecord::Migration[5.2]
   def change
     safety_assured do
-      remove_column :organisations, :path
-      remove_column :teams, :path
+      remove_column :organisations, :path, :string
+      remove_column :teams, :path, :string
     end
   end
 end
