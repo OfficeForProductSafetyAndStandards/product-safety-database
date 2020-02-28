5924098b1a0a21cded47c1149628e3061b0143c7
Steve Lorek <steve@stevelorek.com>
2020-02-28 11:35:52 +0000

    Fixes for RSpec/RepeatedExample cop

--- psd-web/spec/requests/home_page_spec.rb
+++ psd-web/spec/requests/home_page_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "HomePage", :with_elasticsearch do
+RSpec.describe "Home page", :with_elasticsearch do
   context "when not signed in" do
     it "not signed in visits / stays on /" do
       get "/"
@@ -14,11 +14,6 @@ RSpec.describe "HomePage", :with_elasticsearch do
     before { sign_in(user) }
 
     it "signed in visits / gets redirected to /cases" do
-      get "/"
-      expect(response).to redirect_to("/cases")
-    end
-
-    it "signed in visits /cases stays on /cases" do
       get "/"
       expect(response).to redirect_to("/cases")
     end
