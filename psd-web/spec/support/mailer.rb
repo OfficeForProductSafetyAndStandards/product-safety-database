0aa08a2bc5ca81654ea107b5c63a5f1080cab3c6
Steve Lorek <steve@stevelorek.com>
2020-02-28 17:39:46 +0000

    Ignore RSpec/AnyInstance on mailer stub

--- psd-web/spec/support/mailer.rb
+++ psd-web/spec/support/mailer.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.shared_context "with stubbed mailer", shared_context: :metadata do
+  # rubocop:disable RSpec/AnyInstance
   before { allow_any_instance_of(NotifyMailer).to receive(:mail).and_return(true) }
+  # rubocop:enable RSpec/AnyInstance
 end
 
 RSpec.configure do |rspec|
