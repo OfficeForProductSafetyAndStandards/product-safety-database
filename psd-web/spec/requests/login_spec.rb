6fce45b207ac62601683f7c69c81e6cd97000494
Frankie Roberto <frankie@frankieroberto.com>
2020-03-02 15:05:11 +0000

    Remove failing login spec
    Not quite sure why this was added - doesn’t seem to relate to the reset password flow.

--- psd-web/spec/requests/login_spec.rb
+++ psd-web/spec/requests/login_spec.rb
@@ -8,16 +8,6 @@ RSpec.describe "Login" do
     end
   end
 
-  context "When not logged in", :with_stubbed_elasticsearch do
-    before { sign_in create(:user, :activated, has_viewed_introduction: true) }
-
-    it "does not have a flash messages" do
-      expect(get(new_user_session_path))
-        .to redirect_to(root_path)
-      expect(flash).to be_empty
-    end
-  end
-
   context "old bookmarked url" do
     it { expect(get("/sessions/signin")).to redirect_to(root_path) }
   end
