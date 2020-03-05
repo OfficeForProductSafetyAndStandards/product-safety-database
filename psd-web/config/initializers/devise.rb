7d69ce2b55f097077deb1c581ea22b9897a16977
Marc Sardon <pringaohowto@gmail.com>
2020-03-05 15:25:06 +0000

    Move from 2FA defaults to our specific requirements
    Our requirements:
    - Code lenght 5 digits.
    - 2FA session should be valid for 7 days.
    - We allow 10 attempts on 2FA before blocking the user.

--- psd-web/config/initializers/devise.rb
+++ psd-web/config/initializers/devise.rb
@@ -320,12 +320,12 @@ Devise.setup do |config|
 
   # Devise two_factor_authentication gem
   if ENV["OTP_SECRET_ENCRYPTION_KEY"]
-    config.max_login_attempts = 3  # Maximum second factor attempts count.
+    config.max_login_attempts = 10  # Maximum second factor attempts count.
     config.allowed_otp_drift_seconds = 30  # Allowed TOTP time drift between client and server.
-    config.otp_length = 6  # TOTP code length
+    config.otp_length = 5  # TOTP code length
     config.direct_otp_valid_for = 5.minutes  # Time before direct OTP becomes invalid
-    config.direct_otp_length = 6  # Direct OTP code length
-    config.remember_otp_session_for_seconds = 30.days  # Time before browser has to perform 2fA again. Default is 0.
+    config.direct_otp_length = 5  # Direct OTP code length
+    config.remember_otp_session_for_seconds = 7.days  # Time before browser has to perform 2fA again. Default is 0.
     config.otp_secret_encryption_key = ENV.fetch("OTP_SECRET_ENCRYPTION_KEY")
     config.second_factor_resource_id = 'id' # Field or method name used to set value for 2fA remember cookie
     config.delete_cookie_on_logout = false # Delete cookie when user signs out, to force 2fA again on login
