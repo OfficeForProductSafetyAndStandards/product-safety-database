ff4ee629c8b30c338a94cfead6fa63d4b7b7ed52
Frankie Roberto <frankie@frankieroberto.com>
2020-03-06 16:28:15 +0000

    Add basic health check view

---
+++ psd-web/app/controllers/health_controller.rb
@@ -0,0 +1,24 @@
+class HealthController < ApplicationController
+  skip_before_action :authenticate_user!, :authorize_user, :has_accepted_declaration, :has_viewed_introduction
+
+  def show
+    # Check redis services
+    Redis.new(Rails.application.config_for(:redis_session)).info
+    Redis.new(Rails.application.config_for(:redis_store)).info
+
+    # Check database connection
+    ActiveRecord::Migrator.current_version
+
+    # Check Keycloak service is available
+    # TODO: remove once Keycloak is no longer required.
+    KeycloakClient.instance.all_organisations
+
+    # Check Elasticsearch cluster health
+    raise "Elastic search is down" if Elasticsearch::Client.new(Rails.application.config_for(:elasticsearch)).cluster.health[:status] == "red"
+
+    # Check Sidekiq queue length (in time) is within an acceptable limit
+    raise "Sidekiq queue latency is above 30 seconds" if Sidekiq::Queue.new(ENV['SIDEKIQ_QUEUE'] || "psd").latency > 30
+
+    render plain: "OK"
+  end
+end
