b140f2201ebdea2ee8ecb30669e8572dc27b2152
Slawosz Slawinski <slawosz@gmail.com>
2020-03-09 09:54:12 +0000

    New query

--- psd-web/app/models/keycloak_connector.rb
+++ psd-web/app/models/keycloak_connector.rb
@@ -8,17 +8,23 @@ class KeycloakConnector < ApplicationRecord
 
   EMAIL = 1
   FIRST_NAME = 2
-  LAST_NAME = 2
-  USERNAME = 3
-  USER_CREATED_TIMESTAMP = 4
-  SALT = 4
-  CRYPTED_PASSWORD = 4
-  HASH_ITERATIONS = 4
+
+  LAST_NAME = 3
+  USERNAME = 4
+  USER_CREATED_TIMESTAMP = 5
+
+  SALT = 6
+  CRYPTED_PASSWORD = 7
+  HASH_ITERATIONS = 8
+  TYPE = 9
+
   MOBILE_NUMBER = 4
 
+
   FIELDS = <<~SQL.freeze
-    ue.id, ue.email, ue.first_name, ue.last_name, ue.username, ue.created_timestamp,
-    c.salt, c.value, c.hash_iterations, c.user_id,
+    ue.id, ue.email, ue.first_name,
+    ue.last_name, ue.username, ue.created_timestamp,
+    c.salt, c.value, c.hash_iterations, c.user_id, c.type
     ua.user_id, ua.name, ua.value
   SQL
 
@@ -35,13 +41,22 @@ class KeycloakConnector < ApplicationRecord
   def self.copy_keycloak_credentials
     rows = KeycloakConnector.connection.query(CREDENTIALS_QUERY)
     rows.each do |salt, encrypted_password, iterations, email, credential_type|
-      user = User.find_by(email: email)
+      user = User.find_by(email: row[EMAIL])
       next unless user
 
-      user.password_salt = salt
-      user.encrypted_password = encrypted_password
-      user.hash_iterations = iterations
-      user.credential_type = credential_type
+      # user.password_salt = salt
+      # user.encrypted_password = encrypted_password
+      # user.hash_iterations = iterations
+      # user.credential_type = credential_type
+      user.password_salt = row[SALT]
+      user.encrypted_password = row[CRYPTED_PASSWORD]
+      user.hash_iterations = row[HASH_ITERATIONS]
+      user.credential_type = row[TYPE]
+      user.keycloak_first_name = row[FIRST_NAME]
+      user.keycloak_last_name = row[LAST_NAME]
+      user.keycloak_username = row[USERNAME]
+      user.keycloak_created_at = Time.at(row[USER_CREATED_TIMESTAMP])
+      user.mobile_number = Time.at(row[MOBILE_NUMBER])
       user.save!
     end
   end
