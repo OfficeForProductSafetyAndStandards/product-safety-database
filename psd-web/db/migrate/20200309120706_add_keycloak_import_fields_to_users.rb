c7ccb8e160f3d5a7191ccc26b167d3cf2ded568f
Slawosz Slawinski <slawosz@gmail.com>
2020-03-10 15:08:44 +0000

    Modify migration

--- psd-web/db/migrate/20200309120706_add_keycloak_import_fields_to_users.rb
+++ psd-web/db/migrate/20200309120706_add_keycloak_import_fields_to_users.rb
@@ -2,9 +2,6 @@ class AddKeycloakImportFieldsToUsers < ActiveRecord::Migration[5.2]
   def change
     safety_assured do
       change_table(:users, bulk: true) do |t|
-        t.string :keycloak_first_name
-        t.string :keycloak_last_name
-        t.string :keycloak_username
         t.datetime :keycloak_created_at
       end
     end
