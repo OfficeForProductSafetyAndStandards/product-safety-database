c4c2b6af244b54413255a4027bfad95e1cf8d6c1
Marc Sardon <pringaohowto@gmail.com>
2020-03-13 00:00:32 +0000

    Add 2FA form tests
    forgot to commit the new file together with the form object.

---
+++ psd-web/spec/forms/two_factor_authentication_form_spec.rb
@@ -0,0 +1,59 @@
+require "rails_helper"
+
+RSpec.describe TwoFactorAuthenticationForm do
+  subject(:form) { described_class.new(otp_code: otp_code) }
+
+  let(:otp_code) { "test@example.com" }
+
+  describe "#valid?" do
+    before { form.validate }
+
+    context "when the two factor code is blank" do
+      let(:otp_code) { "" }
+
+      it "is not valid" do
+        expect(form).to be_invalid
+      end
+
+      it "populates an error message" do
+        expect(form.errors.full_messages_for(:otp_code)).to eq(["Enter the security code"])
+      end
+    end
+
+    context "when the two factor code has less digits than the required ones" do
+      let(:otp_code) { rand.to_s[2..Devise.otp_length] }
+
+      it "is not valid" do
+        expect(form).to be_invalid
+      end
+
+      it "populates an error message" do
+        expect(form.errors.full_messages_for(:otp_code)).to eq(["You haven't entered enough numbers"])
+      end
+    end
+
+    context "when the two factor code has more digits than the required ones" do
+      let(:otp_code) { rand.to_s[2..Devise.otp_length + 2] }
+
+      it "is not valid" do
+        expect(form).to be_invalid
+      end
+
+      it "populates an error message" do
+        expect(form.errors.full_messages_for(:otp_code)).to eq(["You've entered too many numbers"])
+      end
+    end
+
+    context "when the two factor code has the right number of digits" do
+      let(:otp_code) { rand.to_s[2..Devise.otp_length + 1] }
+
+      it "is valid" do
+        expect(form).to be_valid
+      end
+
+      it "does not contain error messages" do
+        expect(form.errors).to be_empty
+      end
+    end
+  end
+end
