7eaa8814967d9ba4b3433bb197d4c5001529fdf2
Marc Sardon <pringaohowto@gmail.com>
2020-03-13 12:45:32 +0000

    Replace fake notify env API keys with a test stub
    To make clear we're not using real API keys. Is just a testing string
    to initialize the client without format errors on testing environment.

--- psd-web/spec/support/notify.rb
+++ psd-web/spec/support/notify.rb
@@ -2,6 +2,9 @@
 
 RSpec.shared_context "with stubbed notify" do
   before do
+    allow(Rails.application.config)
+      .to receive(:notify_api_key)
+      .and_return("fake_test_key-b1dbb0f5-4651-4af8-9f15-fa8123ff138d-3df264a8-ed9e-381f-b1f3-c3278411adbc")
     stub_request(:post, "https://api.notifications.service.gov.uk/v2/notifications/sms").
       and_return(body: {}.to_json)
   end
