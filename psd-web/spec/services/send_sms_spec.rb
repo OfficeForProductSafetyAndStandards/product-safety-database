258dd902e396478a1e7adc2b1c6f1c30f2ee997f
Marc Sardon <pringaohowto@gmail.com>
2020-03-13 11:59:44 +0000

    Reintroduce fake Notify API for tests
    Because if not the Notify client blows up on initialization as it
    checks the id format.

--- psd-web/spec/services/send_sms_spec.rb
+++ psd-web/spec/services/send_sms_spec.rb
@@ -8,12 +8,6 @@ RSpec.describe SendSMS do
       { phone_number: phone_number, template_id: described_class::TEMPLATES[:otp_code], personalisation: { code: code } }
     end
 
-    before do
-      allow(Rails.application.config).to receive(:notify_api_key).and_return(
-        "eef30646-d6cc-4708-bb7a-46d63caf752a.653d8bdf-03d1-4734-81a9-9cc687a5e1b3"
-      )
-    end
-
     it "sends the otp code" do
       notification = stub_request(:post, "https://api.notifications.service.gov.uk/v2/notifications/sms")
                       .with(body: expected_payload.to_json).and_return(body: {}.to_json)
