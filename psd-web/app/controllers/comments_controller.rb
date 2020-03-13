f643e7c45fd12937f46f0921056857dc4a002825
Frankie Roberto <frankie@frankieroberto.com>
2020-03-13 15:49:31 +0000

    Re-render the page if there was a validation error
    This enables us to display detailed error messages.
    
    Requires setting `@activity` as an instance variable so that it is available to the view.

--- psd-web/app/controllers/comments_controller.rb
+++ psd-web/app/controllers/comments_controller.rb
@@ -2,22 +2,26 @@ class CommentsController < ApplicationController
   before_action :set_investigation
 
   def create
-    activity = @investigation.activities.new(comment_activity_params.merge(investigation: @investigation))
-    activity.source = UserSource.new(user: current_user)
+    @activity = @investigation.activities.new(comment_activity_params.merge(investigation: @investigation))
+    @activity.source = UserSource.new(user: current_user)
 
     respond_to do |format|
-      if activity.save
+      if @activity.save
         format.html do
           redirect_to investigation_url(@investigation), flash: { success: "Comment was successfully added." }
         end
         format.json { render :show, status: :created, location: @activity }
       else
         format.html do
-          redirect_to new_investigation_activity_comment_url(@investigation), flash: { warning: "Comment was not successfully added." }
+          render :new
         end
-        format.json { render json: activity.errors, status: :unprocessable_entity }
+        format.json { render json: @activity.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def new
+    @activity = @investigation.activities.new
   end
 
 private
