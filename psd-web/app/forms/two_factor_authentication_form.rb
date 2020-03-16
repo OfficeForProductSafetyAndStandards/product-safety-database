3783475a82f1d0a2a5d070e8bf260b4fda32a3c3
Marc Sardon <pringaohowto@gmail.com>
2020-03-16 11:52:18 +0000

    Only accept digits in 2FA validation
    Numericality default regexp accepts "+ -"  symbols.
    We only want to accept digits.

--- psd-web/app/forms/two_factor_authentication_form.rb
+++ psd-web/app/forms/two_factor_authentication_form.rb
@@ -2,22 +2,21 @@ class TwoFactorAuthenticationForm
   include ActiveModel::Model
   include ActiveModel::Attributes
 
-  # From rails validations guide: https://guides.rubyonrails.org/active_record_validations.html#numericality
-  NUMERICALITY_REGEX = /\A[+-]?\d+\z/.freeze
+  INTEGER_REGEX = /\A\d+\z/.freeze
 
   attribute :otp_code
 
   validates_presence_of :otp_code, message: I18n.t(".otp_code.blank")
   validates :otp_code,
-            numericality: { only_integer: true, message: I18n.t(".otp_code.numericality") },
+            format: { with: INTEGER_REGEX, message: I18n.t(".otp_code.numericality") },
             allow_blank: true
   validates :otp_code,
-            length: { 
-              maximum: Devise.direct_otp_length, 
+            length: {
+              maximum: Devise.direct_otp_length,
               too_long: I18n.t(".otp_code.too_long"),
               minimum: Devise.direct_otp_length,
               too_short: I18n.t(".otp_code.too_short")
             },
             allow_blank: true,
-            if: -> { NUMERICALITY_REGEX === otp_code }
+            if: -> { INTEGER_REGEX === otp_code }
 end
