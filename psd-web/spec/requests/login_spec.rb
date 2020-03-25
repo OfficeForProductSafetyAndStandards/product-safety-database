10882b3d96ffccefcdc6a2ecf66cc649bbcefaf6
yann marquet <ymarquet@gmail.com>
2020-03-25 11:47:58 +0000

    add sutb notify helper

--- psd-web/spec/requests/login_spec.rb
+++ psd-web/spec/requests/login_spec.rb
@@ -12,13 +12,10 @@ RSpec.describe "Login", type: :request do
     it { expect(get("/sessions/signin")).to redirect_to(root_path) }
   end
 
-  describe "a user without a mobile number", :with_stubbed_mailer do
+  describe "a user without a mobile number", :with_stubbed_mailer, :with_stubbed_notify do
     let(:user) { create(:user, :activated, mobile_number: nil) }
 
     it "displays an error message" do
-      stub_request(:post, "https://api.notifications.service.gov.uk/v2/notifications/sms")
-        .and_return(body: {}.to_json, status: 200)
-
       expect(post(new_user_session_path, params: { user: { email: user.email, password: user.password } }))
         .to redirect_to(missing_mobile_number_path)
     end
