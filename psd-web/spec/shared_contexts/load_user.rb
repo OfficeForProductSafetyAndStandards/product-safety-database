0ebd9d626f1b98ae1240acd300a5e152a81ec83e
Frankie Roberto <frankie@frankieroberto.com>
2020-03-26 14:47:07 +0000

    Remove omniauth config (#430)
    This is no longer needed.

--- psd-web/spec/shared_contexts/load_user.rb
+++
@@ -1,5 +0,0 @@
-RSpec.shared_context "with mock user" do
-  let(:user)              { instance_double(User) }
-  let(:user_service)      { instance_double(CreateUserFromAuth, user: user) }
-  let(:omniauth_response) { double }
-end
--- psd-web/spec/shared_examples/create_user.rb
+++
@@ -1,6 +0,0 @@
-RSpec.shared_examples "creates a user" do
-  it "creates the user" do
-    conditions = { id: omniauth_response["uid"], email: omniauth_response["info"]["email"], name: omniauth_response["info"]["name"] }
-    expect { subject.user }.to change { User.where(conditions).count } .from(0).to(1)
-  end
-end
