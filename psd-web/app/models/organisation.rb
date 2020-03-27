c094bdbd7ed2c53db069ce19d4dc423619fef8db
Sławosz Sławiński <slawosz@gmail.com>
2020-03-27 11:55:22 +0000

    Remove keycloak syncing (#308)
    Removes creating User, Team and Organisation from keycloak.
    Only import code from Keycloak left is in KeycloakConnector. This code have to be run once, when service is stopped for maintenance to update passwords and other data from keycloak.

--- psd-web/app/models/organisation.rb
+++ psd-web/app/models/organisation.rb
@@ -3,22 +3,4 @@ class Organisation < ApplicationRecord
   has_many :teams, dependent: :nullify
 
   validates :name, presence: true
-
-  def self.load_from_keycloak(orgs = KeycloakClient.instance.all_organisations)
-    orgs.each do |org|
-      begin
-        record = find_or_create_by!(id: org[:id]) do |new_record|
-          new_record.name = org[:name]
-        end
-
-        record.update!(org.slice(:name))
-      rescue ActiveRecord::ActiveRecordError => e
-        if Rails.env.production?
-          Raven.capture_exception(e)
-        else
-          raise(e)
-        end
-      end
-    end
-  end
 end
