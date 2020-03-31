a148ee7d981a6d2015ac934b6b75877b7a24c0c0
yann marquet <yannmarquet@stupidcodefactory.home>
2020-03-31 14:55:21 +0100

    fix password field being also displayed as error

--- psd-web/app/controllers/users/sessions_controller.rb
+++ psd-web/app/controllers/users/sessions_controller.rb
@@ -25,7 +25,7 @@ module Users
         # warden.authenticate(auth_options) above.
         sign_out
         User.current = nil
-        resource.errors.add(:email, I18n.t(:wrong_email_or_password, scope: "sign_user_in.email"))
+        add_wrong_credentials_errors
         return render :new
       end
 
@@ -37,8 +37,7 @@ module Users
       end
 
       self.resource = resource_class.new(sign_in_params).decorate
-      resource.errors.add(:email, I18n.t(:wrong_email_or_password, scope: "sign_user_in.email"))
-      resource.errors.add(:password, nil)
+      add_wrong_credentials_errors
       render :new
     end
 
@@ -46,6 +45,11 @@ module Users
 
     def sign_in_form
       @sign_in_form ||= SignInForm.new(sign_in_params)
+    end
+
+    def add_wrong_credentials_errors
+      resource.errors.add(:email, I18n.t(:wrong_email_or_password, scope: "sign_user_in.email"))
+      resource.errors.add(:password, nil)
     end
   end
 end
