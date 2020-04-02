d18bc1d400c760d12dff0b0bfb70ca364b1017a9
Frankie Roberto <frankie@frankieroberto.com>
2020-04-02 17:04:38 +0100

    Add spec for the banner helper

---
+++ psd-web/spec/helpers/banner_helper_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe BannerHelper do
+  describe "#banner" do
+    context "with a text param" do
+      subject(:banner) { helper.banner(text: "There is a problem") }
+
+      it "outputs the text within a container" do
+        expect(banner).to eql("<div class=\"app-banner\"><div class=\"app-banner__message\">There is a problem</div></div>")
+      end
+    end
+
+
+    context "with an html param" do
+      subject(:banner) { helper.banner(html: tag.p("There is a problem")) }
+
+      it "outputs the text within a container" do
+        expect(banner).to eql("<div class=\"app-banner\"><div class=\"app-banner__message\"><p>There is a problem</p></div></div>")
+      end
+    end
+
+    context "with a block" do
+      subject(:banner) {
+        helper.banner do
+          tag.p("There is a problem")
+        end
+      }
+
+      it "outputs the text within a container" do
+        expect(banner).to eql("<div class=\"app-banner\"><div class=\"app-banner__message\"><p>There is a problem</p></div></div>")
+      end
+    end
+  end
+end
