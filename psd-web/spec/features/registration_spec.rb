1d71fe11df609689b8302a8a873536a687f03f52
yann marquet <yannmarquet@stupidcodefactory.home>
2020-04-02 18:05:12 +0100

    Add test that bailing registration works

---
+++ psd-web/spec/features/registration_spec.rb
@@ -0,0 +1,59 @@
+require "rails_helper"
+
+RSpec.describe "Registration process", :with_stubbed_mailer do
+  let(:team) { create(:team) }
+  let(:admin) { create(:user, :team_admin, has_accepted_declaration: true, has_viewed_introduction: true, teams: [team]) }
+  let(:invitee_email) { Faker::Internet.safe_email }
+
+  it "sending an invitation and recieving it" do
+    sign_in(admin)
+
+    visit "/teams/#{team.id}/invite"
+    invite_user_to_team
+
+    expect_user_invited_successfully
+
+    sign_out
+
+    invitee = User.find_by!(email: invitee_email)
+
+    visit "/users/#{invitee.id}/complete-registration?invitation=#{invitee.invitation_token}"
+    fill_in_registration_form
+
+    expect_to_be_on_two_factor_authentication_page
+
+    enter_two_factor_authentication_code(invitee.reload.direct_otp)
+
+    expect_to_be_on_declaration_page
+  end
+
+  def invite_user_to_team
+    fill_in "Email address", with: invitee_email
+    click_on "Send invitation email"
+  end
+
+  def expect_user_invited_successfully
+    expect(page).to have_title(team.display_name)
+    expect(page).to have_css(".teams--user .teams--user-email:contains(\"#{invitee_email}\")")
+  end
+
+  def fill_in_registration_form
+    fill_in "Full name",     with: Faker::Movies::Lebowski.character
+    fill_in "Mobile number", with: "71234567890"
+    fill_in "Password",      with: "testpassword"
+    click_on "Continue"
+  end
+
+  def expect_to_be_on_two_factor_authentication_page
+    expect(page).to have_title("Check your phone")
+  end
+
+  def enter_two_factor_authentication_code(otp_code)
+    fill_in "Enter security code", with: otp_code
+    click_on "Continue"
+  end
+
+  def expect_to_be_on_declaration_page
+    expect(page).to have_title("Declaration - Product safety database - GOV.UK")
+  end
+end
