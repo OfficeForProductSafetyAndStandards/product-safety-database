f9b415f5b774cd29909c8efc0aa1b23769e63b1d
Marc Sardon <pringaohowto@gmail.com>
2020-04-03 15:15:12 +0100

    Rename and re-shape Sign in specs
    To follow a scenario style approach to feature testing.

--- psd-web/spec/features/sign_in_spec.rb
+++ psd-web/spec/features/sign_in_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "Signing in", :with_elasticsearch, :with_stubbed_mailer, :with_stubbed_notify, type: :feature do
+RSpec.feature "Signing in", :with_elasticsearch, :with_stubbed_mailer, :with_stubbed_notify, :with_2fa, type: :feature do
   include ActiveSupport::Testing::TimeHelpers
 
   let(:investigation) { create(:project) }
@@ -25,56 +25,52 @@ RSpec.feature "Signing in", :with_elasticsearch, :with_stubbed_mailer, :with_stu
     expect(page).not_to have_link("Cases")
   end
 
-  context "when succeeding signin in", :with_2fa do
-    context "when in two factor authentication page" do
-      scenario "allows user to sign in with correct two factor authentication code" do
-        visit "/sign-in"
-        fill_in_credentials
+  scenario "user signs in with correct two factor authentication code" do
+    visit "/sign-in"
+    fill_in_credentials
 
-        expect(page).to have_css("h1", text: "Check your phone")
+    expect(page).to have_css("h1", text: "Check your phone")
 
-        fill_in "Enter security code", with: user.reload.direct_otp
-        click_on "Continue"
+    fill_in "Enter security code", with: user.reload.direct_otp
+    click_on "Continue"
 
-        expect(page).to have_css("h2", text: "Your cases")
-        expect(page).to have_link("Sign out", href: destroy_user_session_path)
-      end
-
-      scenario "allows user to sign out and be sent to the homepage" do
-        visit "/sign-in"
-        fill_in_credentials
-
-        expect(page).to have_css("h1", text: "Check your phone")
-
-        within(".psd-header__secondary-navigation") do
-          click_link("Sign out")
-        end
-
-        expect(page).to have_css("h1", text: "Product safety database")
-        expect(page).to have_link("Sign in to your account")
-      end
-
-      scenario "don't allow the user to sign in with a wrong two factor authentication code" do
-        visit "/sign-in"
-        fill_in_credentials
-
-        expect(page).to have_css("h1", text: "Check your phone")
-
-        fill_in "Enter security code", with: user.reload.direct_otp.reverse
-        click_on "Continue"
-
-        expect(page).to have_css("h1", text: "Check your phone")
-        expect(page).to have_css("h2#error-summary-title", text: "There is a problem")
-        expect(page).to have_css("#otp_code-error", text: "Error: Incorrect security code")
-      end
-    end
+    expect(page).to have_css("h2", text: "Your cases")
+    expect(page).to have_link("Sign out", href: destroy_user_session_path)
   end
 
-  context "when using wrong credentials over and over again", :with_2fa do
+  scenario "user signs out when required to fill two factor authentication code" do
+    visit "/sign-in"
+    fill_in_credentials
+
+    expect(page).to have_css("h1", text: "Check your phone")
+
+    within(".psd-header__secondary-navigation") do
+      click_link("Sign out")
+    end
+
+    expect(page).to have_css("h1", text: "Product safety database")
+    expect(page).to have_link("Sign in to your account")
+  end
+
+  scenario "user attempts to sign in with wrong two factor authentication code" do
+    visit "/sign-in"
+    fill_in_credentials
+
+    expect(page).to have_css("h1", text: "Check your phone")
+
+    fill_in "Enter security code", with: user.reload.direct_otp.reverse
+    click_on "Continue"
+
+    expect(page).to have_css("h1", text: "Check your phone")
+    expect(page).to have_css("h2#error-summary-title", text: "There is a problem")
+    expect(page).to have_css("#otp_code-error", text: "Error: Incorrect security code")
+  end
+
+  context "when using wrong credentials over and over again" do
     let(:unlock_email) { delivered_emails.last }
     let(:unlock_path) { unlock_email.personalization_path(:unlock_user_url_token) }
 
-    scenario "locks and sends email with unlock link" do
+    scenario "user gets locked and uses the unlock link received by email" do
       visit "/sign-in"
       fill_in_credentials
       fill_in "Enter security code", with: user.reload.direct_otp
@@ -104,34 +100,28 @@ RSpec.feature "Signing in", :with_elasticsearch, :with_stubbed_mailer, :with_stu
       expect(page).to have_link("Sign out")
     end
 
-    context "when logged in as different user" do
-      let(:user2) { create(:user, :activated, has_viewed_introduction: true) }
+    scenario "user tries to use unlock link when logged in as different user" do
+      user2 = create(:user, :activated, has_viewed_introduction: true)
+      user2.lock_access!
 
-      before do
-        user2.lock_access!
-      end
+      visit "/sign-in"
+      fill_in_credentials
+      fill_in "Enter security code", with: user.reload.direct_otp
+      click_on "Continue"
 
-      scenario "logouts currently logged in user" do
-        visit "/sign-in"
-        fill_in_credentials
-        fill_in "Enter security code", with: user.reload.direct_otp
-        click_on "Continue"
+      expect(page).to have_css("h2", text: "Your cases")
 
-        expect(page).to have_css("h2", text: "Your cases")
-
-        visit unlock_path
-        expect(page).to have_css("h1", text: "Check your phone")
-      end
+      visit unlock_path
+      expect(page).to have_css("h1", text: "Check your phone")
     end
 
-    scenario "shows invalid link page for invalid link" do
+    scenario "user follows an invalid unlock link" do
       visit "/unlock?unlock_token=wrong-token"
       expect(page).to have_css("h1", text: "Invalid link")
       expect(page.status_code).to eq(404)
     end
 
-
-    scenario "sends email with reset password link", :with_2fa do
+    scenario "locked user receives email with reset password link" do
       Devise.maximum_attempts.times do
         visit "/sign-in"
         fill_in_credentials(password_override: "XXX")
@@ -151,103 +141,77 @@ RSpec.feature "Signing in", :with_elasticsearch, :with_stubbed_mailer, :with_stu
     end
   end
 
-  context "when signed in" do
-    scenario "times you out in due time" do
+  scenario "user session expires" do
+    visit investigation_path(investigation)
+    expect(page).not_to have_css("h2#error-summary-title", text: "You need to sign in or sign up before continuing.")
+
+    travel_to 24.hours.from_now do
       visit investigation_path(investigation)
-      expect(page).not_to have_css("h2#error-summary-title", text: "You need to sign in or sign up before continuing.")
-
-      travel_to 24.hours.from_now do
-        visit investigation_path(investigation)
-        expect(page).not_to have_css("h2#error-summary-title", text: "Your session expired. Please sign in again to continue.")
-      end
+      expect(page).not_to have_css("h2#error-summary-title", text: "Your session expired. Please sign in again to continue.")
     end
   end
 
-  context "when the user hasn’t verified their mobile number", :with_2fa do
-    let(:user) { create(:user, mobile_number_verified: false) }
+  scenario "user tries to sign in without having verified its mobile number on registration" do
+    user.update_column(:mobile_number_verified, false)
 
-    scenario "doesn’t let them sign in and does not send them a 2FA code by sms" do
-      visit "/sign-in"
+    visit "/sign-in"
 
-      fill_in_credentials
-      expect_incorrect_email_or_password
-      expect(notify_stub).not_to have_received(:send_sms)
-    end
+    fill_in_credentials
+    expect_incorrect_email_or_password
+    expect(notify_stub).not_to have_received(:send_sms)
   end
 
-  context "when email address does not belong to any user", :with_2fa do
-    scenario "shows a generic error message" do
-      visit "/sign-in"
+  scenario "user tries to sign in with email address that does not belong to any user" do
+    visit "/sign-in"
 
-      fill_in "Email address", with: "notarealuser@example.com"
-      fill_in "Password", with: "notarealpassword"
-      click_on "Continue"
+    fill_in "Email address", with: "notarealuser@example.com"
+    fill_in "Password", with: "notarealpassword"
+    click_on "Continue"
 
-      expect_incorrect_email_or_password
-    end
+    expect_incorrect_email_or_password
   end
 
-  context "with credentials entered incorrectly" do
-    scenario "highlights email field" do
-      visit "/sign-in"
+  scenario "user introduces wrong password" do
+    visit "/sign-in"
 
-      fill_in "Email address", with: user.email
-      fill_in "Password", with: "passworD"
-      click_on "Continue"
+    fill_in "Email address", with: user.email
+    fill_in "Password", with: "passworD"
+    click_on "Continue"
 
-      expect_incorrect_email_or_password
-    end
+    expect_incorrect_email_or_password
+  end
 
-    scenario "does not work with email no in database" do
-      visit "/sign-in"
+  scenario "user introduces email address with incorrect format" do
+    visit "/sign-in"
 
-      fill_in "Email address", with: "user.email@foo.bar"
-      fill_in "Password", with: "passworD"
-      click_on "Continue"
-
-      expect_incorrect_email_or_password
-    end
-
-    context "when email address is not in correct format" do
-      scenario "shows an error message" do
-        visit "/sign-in"
-
-        fill_in "Email address", with: "test.email"
-        fill_in "Password", with: "password "
-        click_on "Continue"
+    fill_in "Email address", with: "test.email"
+    fill_in "Password", with: "password "
+    click_on "Continue"
 
 
-        expect(page).to have_css(".govuk-error-summary__list", text: "Enter your email address in the correct format, like name@example.com")
-        expect(page).to have_css(".govuk-error-message", text: "Enter your email address in the correct format, like name@example.com")
-      end
-    end
+    expect(page).to have_css(".govuk-error-summary__list", text: "Enter your email address in the correct format, like name@example.com")
+    expect(page).to have_css(".govuk-error-message", text: "Enter your email address in the correct format, like name@example.com")
+  end
 
-    context "when email and password fields left empty" do
-      scenario "shows error messages" do
-        visit "/sign-in"
+  scenario "user leaves email and password fields empty" do
+    visit "/sign-in"
 
-        fill_in "Email address", with: " "
-        fill_in "Password", with: " "
-        click_on "Continue"
+    fill_in "Email address", with: " "
+    fill_in "Password", with: " "
+    click_on "Continue"
 
-        expect(page).to have_css(".govuk-error-message", text: "Enter your email address")
-        expect(page).to have_css(".govuk-error-message", text: "Enter your password")
-      end
-    end
+    expect(page).to have_css(".govuk-error-message", text: "Enter your email address")
+    expect(page).to have_css(".govuk-error-message", text: "Enter your password")
+  end
 
+  scenario "user leaves password field empty" do
+    visit "/sign-in"
 
-    context "when password field is left empty" do
-      scenario "shows an error messages" do
-        visit "/sign-in"
+    fill_in "Email address", with: user.email
+    fill_in "Password", with: " "
+    click_on "Continue"
 
-
-        fill_in "Email address", with: user.email
-        fill_in "Password", with: " "
-        click_on "Continue"
-
-        expect(page).to have_css(".govuk-error-message", text: "Enter your password")
-        expect(page).to have_css(".govuk-error-summary__list", text: "Enter your password")
-      end
-    end
+    expect(page).to have_css(".govuk-error-message", text: "Enter your password")
+    expect(page).to have_css(".govuk-error-summary__list", text: "Enter your password")
   end
 end
