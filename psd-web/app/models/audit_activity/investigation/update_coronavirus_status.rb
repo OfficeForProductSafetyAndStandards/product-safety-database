b219d42bae1c9e9b2a81738539caf5e5b908c1c5
yann marquet <yannmarquet@stupidcodefactory.local>
2020-04-09 15:21:40 +0100

    localise and change coronavirus update email template

--- psd-web/app/models/audit_activity/investigation/update_coronavirus_status.rb
+++ psd-web/app/models/audit_activity/investigation/update_coronavirus_status.rb
@@ -1,13 +1,23 @@
 class AuditActivity::Investigation::UpdateCoronavirusStatus < AuditActivity::Investigation::Base
-  private_class_method def self.i18n_scope
+  def self.i18n_scope
     model_name.i18n_key.to_s.split("/")
   end
 
   def self.from(investigation)
-    super(investigation, I18n.t(".title.#{investigation.coronavirus_related}", scope: i18n_scope), I18n.t(".body.#{investigation.coronavirus_related}", scope: i18n_scope))
+    super(investigation, I18n.t(".title.#{investigation.coronavirus_related?}", scope: i18n_scope), I18n.t(".body.#{investigation.coronavirus_related?}", scope: i18n_scope))
   end
 
   def email_update_text
-    I18n.t(".email_update_text", scope: i18n_scope, case_type: investigation.case_type.titleize, source: source&.show&.titleize)
+    I18n.t(
+      ".email_update_text.#{investigation.coronavirus_related?}",
+      scope: self.class.i18n_scope,
+      case_type: investigation.case_type.titleize,
+      name: source&.show&.titleize,
+      pretty_id: investigation.pretty_id
+    )
+  end
+
+  def email_subject_text
+    I18n.t(".email_subject_text", scope: self.class.i18n_scope, case_type: investigation.case_type.downcase)
   end
 end
