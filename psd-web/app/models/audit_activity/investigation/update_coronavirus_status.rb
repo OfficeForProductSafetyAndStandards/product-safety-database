ade46f86162a03e2cfdb5197bace6a103012b252
yann marquet <yannmarquet@stupidcodefactory.local>
2020-04-09 09:54:58 +0100

    Fix cancel button and re-add cancel button

--- psd-web/app/models/audit_activity/investigation/update_coronavirus_status.rb
+++ psd-web/app/models/audit_activity/investigation/update_coronavirus_status.rb
@@ -1,10 +1,13 @@
 class AuditActivity::Investigation::UpdateCoronavirusStatus < AuditActivity::Investigation::Base
+  private_class_method def self.i18n_scope
+    model_name.i18n_key.to_s.split("/")
+  end
+
   def self.from(investigation)
-    i18n_scope = model_name.i18n_key.to_s.split("/")
     super(investigation, I18n.t(".title.#{investigation.coronavirus_related}", scope: i18n_scope), I18n.t(".body.#{investigation.coronavirus_related}", scope: i18n_scope))
   end
 
   def email_update_text
-    "#{investigation.case_type.titleize} coronavirus relared status was updated by #{source&.show&.titleize}."
+    I18n.t(".email_update_text", scope: i18n_scope, case_type: investigation.case_type.titleize, source: source&.show&.titleize)
   end
 end
