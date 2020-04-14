0c30834463bd1cb17d075449e52f9897b31fb2c3
yann marquet <yannmarquet@stupidcodefactory.local>
2020-04-14 15:18:44 +0100

    validates only one checkboxe is selected

--- psd-web/app/forms/why_reporting_form.rb
+++ psd-web/app/forms/why_reporting_form.rb
@@ -9,13 +9,31 @@ class WhyReportingForm
   attribute :reported_reason_non_compliant, :boolean, default: false
   attribute :reported_reason_safe_and_compliant, :boolean, default: false
 
-  validates :reported_reason, presence: true
+  validate :single_option_selected
 
   def reported_reason
-    @reported_reason ||= case [reported_reason_unsafe, reported_reason_non_compliant, reported_reason_safe_and_compliant]
+    @reported_reason ||= case boolean_checkboxes
                          when [true, false, false] then :unsafe
                          when [false, true, false] then :non_compliant
                          when [false, false, true] then :safe_and_compliant
                          end
+  end
+
+private
+
+  def single_option_selected
+    return unless multiple_reported_reasons_selected?
+
+    errors.add(:reported_reason_unsafe, I18n.t(:multiple_reported_reasons_selected))             if reported_reason_unsafe
+    errors.add(:reported_reason_non_compliant, I18n.t(:multiple_reported_reasons_selected))      if reported_reason_non_compliant
+    errors.add(:reported_reason_safe_and_compliant, I18n.t(:multiple_reported_reasons_selected)) if reported_reason_safe_and_compliant
+  end
+
+  def multiple_reported_reasons_selected?
+    boolean_checkboxes.many? { |reason| reason == true }
+  end
+
+  def boolean_checkboxes
+    [reported_reason_unsafe, reported_reason_non_compliant, reported_reason_safe_and_compliant]
   end
 end
