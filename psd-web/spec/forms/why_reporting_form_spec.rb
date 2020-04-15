44ad8cfa9cc1173e1353f4d7da81f000943e9305
yann marquet <yannmarquet@stupidcodefactory.local>
2020-04-15 12:47:41 +0100

    exclude mututally exclusive options

--- psd-web/spec/forms/why_reporting_form_spec.rb
+++ psd-web/spec/forms/why_reporting_form_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe WhyReportingForm do
-  subject(:form) { described_class.new(why_reporting_form) }
+  subject(:form) { described_class.new(why_reporting_params) }
 
   let(:reported_reason_unsafe) { nil }
   let(:reported_reason_non_compliant) { nil }
@@ -9,42 +9,46 @@ RSpec.describe WhyReportingForm do
 
   describe "validates the options are mututally exclusive" do
     context "when only option is set" do
-      let(:why_reporting_form) { { reported_reason_unsafe: true } }
+      let(:why_reporting_params) { { reported_reason_unsafe: true } }
 
       it { is_expected.to be_valid }
     end
 
-    context "when two options are set" do
-      let(:why_reporting_form) { { reported_reason_unsafe: true, reported_reason_non_compliant: true } }
+    context "when two mutually exclusive options are set" do
+      let(:why_reporting_params) do
+        { reported_reason_safe_and_compliant: true, reported_reason_non_compliant: true }
+      end
 
       it "is invalid and sets and error", :aggregate_failures do
         expect(form).to be_invalid
-        expect(form.errors[:reported_reason_unsafe]).to eq("Select only one answer")
-        expect(form.errors[:reported_reason_non_compliant]).to eq("Select only one answer")
+        expect(form.errors[:reported_reason_safe_and_compliant]).to eq(["Select only one answer"])
+        expect(form.errors[:reported_reason_non_compliant]).to eq(["Select only one answer"])
       end
     end
   end
 
   describe "#reported_reason" do
     context "when setting only unsafe to true" do
-      let(:why_reporting_form) { { reported_reason_unsafe: true } }
+      let(:why_reporting_params) { { reported_reason_unsafe: true } }
 
       it { expect(form.reported_reason).to eq(:unsafe) }
     end
 
     context "when setting only non_compliant to true" do
-      let(:why_reporting_form) { { reported_reason_non_compliant: true } }
+      let(:why_reporting_params) { { reported_reason_non_compliant: true } }
 
       it { expect(form.reported_reason).to eq(:non_compliant) }
     end
 
     context "when setting only safe_and_compliant to true" do
-      let(:why_reporting_form) { { reported_reason_safe_and_compliant: true } }
+      let(:why_reporting_params) { { reported_reason_safe_and_compliant: true } }
 
       it { expect(form.reported_reason).to eq(:safe_and_compliant) }
     end
 
     context "when not setting any reported_reason properties to true" do
+      let(:form) { described_class.new }
+
       it { expect(form.reported_reason).to be nil }
     end
   end
