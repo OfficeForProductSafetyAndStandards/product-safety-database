33fc02dbd0ab576dca7a79e61810177f12084721
Frankie Roberto <frankie@frankieroberto.com>
2020-04-16 15:34:20 +0100

    Add account view and allow users to change their name (#487)

--- psd-web/app/controllers/application_controller.rb
+++ psd-web/app/controllers/application_controller.rb
@@ -89,8 +89,7 @@ class ApplicationController < ActionController::Base
   def secondary_nav_items
     items = []
     if user_signed_in?
-      # TODO: Remove comment when account page is re-implemented in app
-      # items.push text: "Your account", href: TODO
+      items.push text: "Your account", href: account_path
       items.push text: "Sign out", href: destroy_user_session_path
     else
       items.push text: "Sign in", href: new_user_session_path
