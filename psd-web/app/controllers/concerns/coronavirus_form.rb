fe256210e48c991375e25a65b63c8b3ebdcaef95
Marc Sardon <pringaohowto@gmail.com>
2020-04-16 10:15:14 +0100

    Fix typo in variable names

--- psd-web/app/controllers/concerns/coronavirus_form.rb
+++ psd-web/app/controllers/concerns/coronavirus_form.rb
@@ -9,8 +9,8 @@ module CoronavirusForm
     @coronavirus_related_form = CoronavirusRelatedForm.new
   end
 
-  def assigns_coronavirus_related_from_form(investigation, coronvirus_form_params)
-    form = coronavirus_related_form(coronvirus_form_params)
+  def assigns_coronavirus_related_from_form(investigation, coronavirus_form_params)
+    form = coronavirus_related_form(coronavirus_form_params)
     if (form_valid = form.valid?)
       investigation.coronavirus_related = form.coronavirus_related
     end
@@ -18,7 +18,7 @@ module CoronavirusForm
     form_valid
   end
 
-  def coronavirus_related_form(coronvirus_form_params)
-    @coronavirus_related_form ||= CoronavirusRelatedForm.new(coronvirus_form_params)
+  def coronavirus_related_form(coronavirus_form_params)
+    @coronavirus_related_form ||= CoronavirusRelatedForm.new(coronavirus_form_params)
   end
 end
