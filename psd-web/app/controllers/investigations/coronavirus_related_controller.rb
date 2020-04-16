b63b00c5456d0ecb7f5f2d00df1b297846bd51c9
Marc Sardon <pringaohowto@gmail.com>
2020-04-16 12:08:46 +0100

    Use Coronavirus form obj in Coronavirus controller
    As we're now validating with the form object instead of with
    the investigation model value.

--- psd-web/app/controllers/investigations/coronavirus_related_controller.rb
+++ psd-web/app/controllers/investigations/coronavirus_related_controller.rb
@@ -2,13 +2,15 @@ module Investigations
   class CoronavirusRelatedController < ApplicationController
     def show
       @investigation = Investigation.find_by!(pretty_id: params.require(:investigation_pretty_id)).decorate
+      @coronavirus_related_form = CoronavirusRelatedForm.new(coronavirus_related: @investigation.coronavirus_related)
     end
 
     def update
       @investigation = Investigation.find_by!(pretty_id: params.require(:investigation_pretty_id)).decorate
-      @investigation.assign_attributes(params.require(:investigation).permit(:coronavirus_related))
+      @coronavirus_related_form = CoronavirusRelatedForm.new(params.require(:investigation).permit(:coronavirus_related))
+      return render :show unless @coronavirus_related_form.valid?
 
-      return render :show unless @investigation.valid?
+      @investigation.coronavirus_related = @coronavirus_related_form.coronavirus_related
 
       if @investigation.coronavirus_related_changed?
         @investigation.save
