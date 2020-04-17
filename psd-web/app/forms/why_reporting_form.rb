b8684a3cea2e6cbe1f9547222a509f6d65b62a87
yann marquet <yannmarquet@stupidcodefactory.local>
2020-04-17 12:47:28 +0100

    validayte at least one checkbox selected

--- psd-web/app/forms/why_reporting_form.rb
+++ psd-web/app/forms/why_reporting_form.rb
@@ -9,6 +9,7 @@ class WhyReportingForm
   attribute :reported_reason_non_compliant,      :boolean, default: false
   attribute :reported_reason_safe_and_compliant, :boolean, default: false
 
+  validate :selected_at_least_one_checkbox
   validate :mutually_exclusive_checkboxes
 
   def assign_to(investigation)
@@ -22,7 +23,7 @@ class WhyReportingForm
 private
 
   def reported_reason
-    @reported_reason ||= case [reported_reason_unsafe, reported_reason_non_compliant, reported_reason_safe_and_compliant]
+    @reported_reason ||= case checkboxes
                          when [true,  false, false] then Investigation.reported_reasons[:unsafe]
                          when [false, true,  false] then Investigation.reported_reasons[:non_compliant]
                          when [true,  true,  false] then Investigation.reported_reasons[:unsafe_and_non_compliant]
@@ -31,23 +32,43 @@ private
   end
 
   def reason_created
-    case reported_reason
-    when Investigation.reported_reasons[:unsafe]                   then "Product reported because it is unsafe."
-    when Investigation.reported_reasons[:non_compliant]            then "Product reported because it is non-compliant."
-    when Investigation.reported_reasons[:unsafe_and_non_compliant] then "Product reported because it is unsafe and non-compliant."
-    when Investigation.reported_reasons[:safe_and_compliant]       then "Product reported because it is safe and compliant."
-    end
+    I18n.t(reported_reason, scope: :why_reporting_form)
   end
 
   def mutually_exclusive_checkboxes
     return unless mutually_exclusive_checkboxes_selected?
 
-    errors.add(:reported_reason_unsafe, I18n.t(:multiple_reported_reasons_selected))
-    errors.add(:reported_reason_non_compliant, I18n.t(:multiple_reported_reasons_selected))      if reported_reason_non_compliant
-    errors.add(:reported_reason_safe_and_compliant, I18n.t(:multiple_reported_reasons_selected)) if reported_reason_safe_and_compliant
+    errors.add(:reported_reason_unsafe,
+               I18n.t(:mutually_exclusive_checkboxes_selected, scope: :why_reporting_form))
+    if reported_reason_non_compliant
+      errors.add(:reported_reason_non_compliant,
+                 I18n.t(:mutually_exclusive_checkboxes_selected, scope: :why_reporting_form))
+    end
+    if reported_reason_safe_and_compliant
+      errors.add(:reported_reason_safe_and_compliant,
+                 I18n.t(:mutually_exclusive_checkboxes_selected, scope: :why_reporting_form))
+    end
   end
 
   def mutually_exclusive_checkboxes_selected?
+    return false unless reported_reason_safe_and_compliant
+
     reported_reason_safe_and_compliant && [reported_reason_unsafe, reported_reason_non_compliant].any? { |reason| reason == true }
+  end
+
+  def selected_at_least_one_checkbox
+    return if at_least_one_checkbox_checked?
+
+    errors.add(:reported_reason_unsafe, I18n.t(:no_checkboxes_selected))
+    errors.add(:reported_reason_non_compliant, I18n.t(:no_checkboxes_selected))
+    errors.add(:reported_reason_safe_and_compliant, I18n.t(:mutually_exclusive_checkboxes_selected))
+  end
+
+  def at_least_one_checkbox_checked?
+    checkboxes.any? { |checkbox| checkbox == true }
+  end
+
+  def checkboxes
+    @checkboxes ||= [reported_reason_unsafe, reported_reason_non_compliant, reported_reason_safe_and_compliant]
   end
 end
