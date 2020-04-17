f457f532d750a6f58809024774cd514c8c7766e0
Frankie Roberto <frankie@frankieroberto.com>
2020-04-17 22:00:59 +0100

    make the factory valid

--- psd-web/spec/factories/collaborators.rb
+++ psd-web/spec/factories/collaborators.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :collaborator do
+    include_message { "false" }
     association :investigation, factory: :investigation
     association :team, factory: :team
     association :added_by_user, factory: :user
