51b63f0c393718397a0dcf7b9d24a8cc1dbc9498
Slawosz Slawinski <slawosz@gmail.com>
2020-04-21 14:04:55 +0100

    Fix migration

--- psd-web/db/migrate/20200309104244_two_factor_authentication_add_to_users.rb
+++ psd-web/db/migrate/20200309104244_two_factor_authentication_add_to_users.rb
@@ -1,4 +1,4 @@
-class SecondaryAuthenticationAddToUsers < ActiveRecord::Migration[5.2]
+class TwoFactorAuthenticationAddToUsers < ActiveRecord::Migration[5.2]
   def change
     safety_assured do
       change_table :users, bulk: true do
