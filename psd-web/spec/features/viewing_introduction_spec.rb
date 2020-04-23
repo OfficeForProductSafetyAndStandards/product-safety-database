88141173ada294a4d356482f38e94fba2c8f8fb1
Frankie Roberto <frankie@frankieroberto.com>
2020-04-23 10:13:28 +0100

    Update psd-web/spec/features/viewing_introduction_spec.rb
    Fix typo
    
    Co-Authored-By: Marc Sardón <pringaohowto@gmail.com>

--- psd-web/spec/features/viewing_introduction_spec.rb
+++ psd-web/spec/features/viewing_introduction_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Viewing the introduction", :with_stubbed_elasticsearch, :with_stu
 
   let(:user) { create(:user, :activated, has_viewed_introduction: false) }
 
-  scenario "Vieiwng the steps in order" do
+  scenario "Viewing the steps in order" do
     sign_in user
 
     visit "/introduction/overview"
