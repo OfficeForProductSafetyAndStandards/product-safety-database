2b812a87104e602e36156dacd35fb7ad352e5506
StupidCodeFactory <ymarquet@gmail.com>
2020-04-24 13:10:07 +0100

    split why reported checkbox method

--- psd-web/app/helpers/investigations/ts_instigations_helper.rb
+++ psd-web/app/helpers/investigations/ts_instigations_helper.rb
@@ -7,34 +7,14 @@ module Investigations::TsInstigationsHelper
            hint: { text: "Select all that apply" },
            errorMessage: errors.any? ? { text: errors.full_messages_for(:base).to_sentence } : nil,
            items: [
-             {
-               key: "reported_reason_unsafe",
-               text: "It’s unsafe (or suspected to be)",
-               id: "base",
-               value: true,
-               conditional: { html: unsafe_details_html(form, hazard_types) },
-               attributes: { class: "js-mutually-exclusive__item", data: { "mutually-exclusive-set-id": "reported-set" } }
-             },
-             {
-               key: "reported_reason_non_compliant",
-               text: "It’s non-compliant (or suspected to be)",
-               value: true,
-               conditional: { html: non_compliant_details_html(form) },
-               attributes: { class: "js-mutually-exclusive__item", data: { "mutually-exclusive-set-id": "reported-set" } }
-             },
-             {
-               key: "or",
-               divider: "or"
-             },
-             {
-               key: "reported_reason_safe_and_compliant",
-               text: "It’s safe and compliant",
-               hint: { text: "Help other market surveillance authorities avoid testing the same product again" },
-               value: true,
-               attributes: { class: "js-mutually-exclusive__item", data: { "mutually-exclusive-set-id": "safe-set" } }
-             }
+             reported_unsafe_checkbox(form, hazard_types),
+             reported_non_compliant_checkbox(form),
+             { key: "or", divider: "or" },
+             reported_safe_and_compliant_checkbox
            ]
   end
+
+private
 
   def page_heading_html(page_heading)
     render "investigations/ts_investigations/why_reporting_form_heading", page_heading: page_heading
@@ -52,5 +32,36 @@ module Investigations::TsInstigationsHelper
            attributes: { maxlength: 10000 },
            label: { text: "Why is the product non-compliant?" },
            classes: "govuk-!-width-three-quarters"
+  end
+
+  def reported_unsafe_checkbox(form, hazard_types)
+    {
+      key: "reported_reason_unsafe",
+      text: "It’s unsafe (or suspected to be)",
+      id: "base",
+      value: true,
+      conditional: { html: unsafe_details_html(form, hazard_types) },
+      attributes: { class: "js-mutually-exclusive__item", data: { "mutually-exclusive-set-id": "reported-set" } }
+    }
+  end
+
+  def reported_non_compliant_checkbox(form)
+    {
+      key: "reported_reason_non_compliant",
+      text: "It’s non-compliant (or suspected to be)",
+      value: true,
+      conditional: { html: non_compliant_details_html(form) },
+      attributes: { class: "js-mutually-exclusive__item", data: { "mutually-exclusive-set-id": "reported-set" } }
+    }
+  end
+
+  def reported_safe_and_compliant_checkbox
+    {
+      key: "reported_reason_safe_and_compliant",
+      text: "It’s safe and compliant",
+      hint: { text: "Help other market surveillance authorities avoid testing the same product again" },
+      value: true,
+      attributes: { class: "js-mutually-exclusive__item", data: { "mutually-exclusive-set-id": "safe-set" } }
+    }
   end
 end
