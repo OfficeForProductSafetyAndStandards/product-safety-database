10775455218efcf0dfc20a7410d76c687618828a
Steve Lorek <steve@stevelorek.com>
2020-04-27 10:50:07 +0100

    Remove old database migration files. These are causing a maintenence headache

--- psd-web/db/migrate/20200225103936_generate_uuid_for_organizations_teams_and_users.rb
+++
@@ -1,9 +0,0 @@
-class GenerateUuidForOrganizationsTeamsAndUsers < ActiveRecord::Migration[5.2]
-  def change
-    safety_assured do
-      change_column_default(:organisations, :id, from: nil, to: "gen_random_uuid()")
-      change_column_default(:users, :id, from: nil, to: "gen_random_uuid()")
-      change_column_default(:teams, :id, from: nil, to: "gen_random_uuid()")
-    end
-  end
-end
--- psd-web/db/migrate/20200402142659_add_coronavirus_related_to_investigations.rb
+++
@@ -1,7 +0,0 @@
-class AddCoronavirusRelatedToInvestigations < ActiveRecord::Migration[5.2]
-  def change
-    safety_assured do
-      add_column :investigations, :coronavirus_related, :boolean, default: false
-    end
-  end
-end
