d405b7bb387715de958704055768e0e7a89b5153
Steve Lorek <steve@stevelorek.com>
2020-04-28 11:29:10 +0100

    Ensure resend invite resets the user's invited_at time

--- psd-web/spec/features/team_spec.rb
+++ psd-web/spec/features/team_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Your team page", :with_stubbed_mailer, :with_stubbed_elasticsearc
   let(:user) { create(:user, :activated, :psd_user, teams: [team], has_viewed_introduction: true) }
 
   let!(:another_active_user) { create(:user, :activated, email: "active.sameteam@example.com", organisation: user.organisation, teams: [team], has_viewed_introduction: true) }
-  let!(:another_inactive_user) { create(:user, email: "inactive.sameteam@example.com", organisation: user.organisation, teams: [team]) }
+  let!(:another_inactive_user) { create(:user, email: "inactive.sameteam@example.com", invited_at: 1.year.ago, organisation: user.organisation, teams: [team]) }
   let!(:another_user_another_team) { create(:user, :activated, email: "active.otherteam@example.com", organisation: user.organisation, teams: [create(:team)]) }
 
   before do
@@ -28,11 +28,22 @@ RSpec.feature "Your team page", :with_stubbed_mailer, :with_stubbed_elasticsearc
 
   context "when the user is a team admin" do
     let(:user) { create(:user, :activated, :team_admin, teams: [team], has_viewed_introduction: true) }
+    let(:message_delivery_instance) { instance_double(ActionMailer::MessageDelivery, deliver_now: true) }
+
+    before do
+      allow(NotifyMailer).to receive(:invitation_email).with(another_inactive_user, user).and_return(message_delivery_instance)
+    end
 
     scenario "displays the invite a team member link and only displays the resend invite link for inactive users" do
       expect(page).to have_link("Invite a team member")
       expect(page).to have_resend_invite_link_for(another_inactive_user)
       expect(page).not_to have_resend_invite_link_for(another_active_user)
+    end
+
+    scenario "resending an invitation sends an email to the user and shows a confirmation message" do
+      click_link "Resend invitation to #{another_inactive_user.email}"
+      expect(message_delivery_instance).to have_received(:deliver_now)
+      expect_confirmation_banner "Invite sent to #{another_inactive_user.email}"
     end
   end
 
