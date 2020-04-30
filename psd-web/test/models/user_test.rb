5d9f2c4b0b20d5f12bc9c37d1df4b385d8c9d978
Marc Sardon <pringaohowto@gmail.com>
2020-04-30 14:57:43 +0100

    Remove redundant Minitest spec
    This is now tested in rspec decorator test.

--- psd-web/test/models/user_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class UserTest < ActiveSupport::TestCase
-  test "display name includes user's organisation for non-org-member viewers" do
-    assert_equal "Yann (Southampton Council)", users(:southampton).decorate.display_name(other_user: users(:opss))
-
-    assert_equal "Slawosz (Office for Product Safety and Standards)", users(:opss).decorate.display_name(other_user: users(:southampton))
-  end
-end
