70935942bed8b48e17dbbe53f6e8b5f526d1c549
Frankie Roberto <frankie@frankieroberto.com>
2020-04-30 11:25:59 +0100

    Revert change to migration

--- psd-web/db/migrate/20200429110552_rename_assignable_to_owner.rb
+++ psd-web/db/migrate/20200429110552_rename_assignable_to_owner.rb
@@ -1,8 +1,8 @@
 class RenameAssignableToOwner < ActiveRecord::Migration[5.2]
   def change
     safety_assured do
-      rename_column :investigations, :owner_type, :owner_type
-      rename_column :investigations, :owner_id, :owner_id
+      rename_column :investigations, :assignable_type, :owner_type
+      rename_column :investigations, :assignable_id, :owner_id
     end
   end
 end
