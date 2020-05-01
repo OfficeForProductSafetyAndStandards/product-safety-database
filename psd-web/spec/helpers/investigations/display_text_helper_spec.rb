ff1cedcc1e88a8b1c9c2ff9364009030934b1a9a
Frankie Roberto <frankie@frankieroberto.com>
2020-05-01 09:51:37 +0100

    Update spec

--- psd-web/spec/helpers/investigations/display_text_helper_spec.rb
+++ psd-web/spec/helpers/investigations/display_text_helper_spec.rb
@@ -2,10 +2,10 @@ require "rails_helper"
 
 RSpec.describe Investigations::DisplayTextHelper, type: :helper do
   describe "#investigation_owner", :with_stubbed_mailer, :with_stubbed_notify, :with_stubbed_elasticsearch do
-    context "when assignee has a team name that matches the organisation name" do
+    context "when owner has a team name that matches the organisation name" do
       let(:organisation) { create(:organisation, name: "Southampton Council") }
       let(:team) { create(:team, name: "Southampton Council", organisation: organisation) }
-      let(:investigation) { create(:investigation, assignable: team) }
+      let(:investigation) { create(:investigation, owner: team) }
 
       it "displays just the team name" do
         result = helper.investigation_owner(investigation)
@@ -13,10 +13,10 @@ RSpec.describe Investigations::DisplayTextHelper, type: :helper do
       end
     end
 
-    context "when assignee has a team name differs from the organisation name" do
+    context "when owner has a team name differs from the organisation name" do
       let(:organisation) { create(:organisation, name: "Office for Product Safety and Standards") }
       let(:team) { create(:team, name: "OPSS Processing", organisation: organisation) }
-      let(:investigation) { create(:investigation, assignable: team) }
+      let(:investigation) { create(:investigation, owner: team) }
 
       it "displays the team name and the organisation name" do
         result = helper.investigation_owner(investigation)
