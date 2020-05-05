e3af4ce5be945986bcf301d287b0a3609b962f74
Sławosz Sławiński <slawosz@gmail.com>
2020-05-05 16:35:45 +0100

    Update psd-web/app/models/audit_activity/investigation/team_deleted.rb
    Co-authored-by: Ed Horsford <mail@edwardhorsford.com>

--- psd-web/app/models/audit_activity/investigation/team_deleted.rb
+++ psd-web/app/models/audit_activity/investigation/team_deleted.rb
@@ -1,5 +1,5 @@
 class AuditActivity::Investigation::TeamDeleted < AuditActivity::Investigation::Base
   def subtitle
-    "Team deleted by #{source&.show}, #{pretty_date_stamp}"
+    "Team removed by #{source&.show}, #{pretty_date_stamp}"
   end
 end
