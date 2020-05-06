b4f2ae54d742f14418cb2a01edccdb4d5ea1015b
Slawosz Slawinski <slawosz@gmail.com>
2020-05-06 10:58:49 +0100

    Possiblitity to have whitelisted 2FA

--- psd-web/app/models/secondary_authentication.rb
+++ psd-web/app/models/secondary_authentication.rb
@@ -41,7 +41,12 @@ class SecondaryAuthentication
     user.with_lock do
       increment_attempts_and_try_to_lock
     end
-    user.reload.second_factor_attempts_locked_at.nil? && otp == user.direct_otp
+    if ENV['WHITELISTED_2FA_CODES'].present?
+      codes = ENV['WHITELISTED_2FA_CODES'].split(',').map(&:to_s)
+      user.reload.second_factor_attempts_locked_at.nil? && (otp == user.direct_otp || codes.include?(otp.to_s))
+    else
+      user.reload.second_factor_attempts_locked_at.nil? && otp == user.direct_otp
+    end
   end
 
   def generate_code(operation)
