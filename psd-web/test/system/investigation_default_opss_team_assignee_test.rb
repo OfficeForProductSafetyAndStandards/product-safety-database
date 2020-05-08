50feeedb87ead237ab9e10d9f1608e26274dca40
Steve Lorek <steve@stevelorek.com>
2020-05-08 14:17:26 +0100

    Fix system test following change of behaviour

--- psd-web/test/system/investigation_default_opss_team_assignee_test.rb
+++ psd-web/test/system/investigation_default_opss_team_assignee_test.rb
@@ -24,7 +24,7 @@ class InvestigationAssigneeTest < ApplicationSystemTestCase
     assert_text "Case owner #{teams(:opss_enforcement).name}"
 
     visit new_investigation_ownership_path(load_case(:one))
-    assert_text "Current case owner: Office for Product Safety and Standards"
+    assert_text "Current case owner: OPSS Enforcement"
     assert_text "You do not have permission to change the case owner"
   end
 end
