8078fd456d4cc54e0f87c899835ae765688ed82e
Slawosz Slawinski <slawosz@gmail.com>
2020-05-08 21:31:48 +0100

    Add specs to link visibility

---
+++ psd-web/spec/features/attachment_visibility.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.feature "Adding an attachment to a case", :with_stubbed_elasticsearch, :with_stubbed_antivirus, :with_stubbed_mailer, type: :feature do
+  let(:user) { create(:user, :activated, has_viewed_introduction: true) }
+  let(:other_user) { create(:user, :activated, has_viewed_introduction: true) }
+  let(:investigation) { create(:allegation, :with_antivirus_checked_document, document_has_consumer_info: true, owner: user) }
+
+  scenario "Displays file link for user" do
+    sign_in(user)
+    visit "/cases/#{investigation.pretty_id}/attachments"
+    expect(page).to have_link("View TXT")
+  end
+
+  scenario "Does not displays link to restricted case" do
+    sign_in(other_user)
+    visit "/cases/#{investigation.pretty_id}/attachments"
+    expect(page).not_to have_link("View TXT")
+  end
+end
