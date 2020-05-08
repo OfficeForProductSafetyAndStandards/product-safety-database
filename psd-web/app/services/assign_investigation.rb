02188d598c9a418847da3e4f06e4e9760d3edf9e
StupidCodeFactory <ymarquet@gmail.com>
2020-05-08 15:30:41 +0100

    bit off refactoring

--- psd-web/app/services/assign_investigation.rb
+++ psd-web/app/services/assign_investigation.rb
@@ -4,10 +4,22 @@ class AssignInvestigation
   delegate :investigation, :new_collaborating_case_owner, :current_user, to: :context
 
   def call
-    swap_with_co_collaborator! || create_new_case_owner!
+    swap_case_owner_to_co_collaborator!
+    create_new_case_owner!
   end
 
   def create_new_case_owner!
+    return if investigation.owners.exists?(team: new_collaborating_case_owner)
+
+    investigation.owners.create!(
+      type: "CaseOwner",
+      added_by_user: current_user,
+      team: new_collaborating_case_owner,
+      include_message: "Maybe this message should not be required when creating a case creator"
+    )
+  end
+
+  def swap_case_owner_to_co_collaborator!
     collaborator = investigation.collaborators.find_by(team: new_collaborating_case_owner)
 
     if collaborator
@@ -19,26 +31,13 @@ class AssignInvestigation
       # so the case_owner is either fetched from the database or is nil
       investigation.reload
     end
-
-    return if investigation.owners.exists?(team: new_collaborating_case_owner)
-
-    investigation.owners.create!(
-      type: "CaseOwner",
-      added_by_user: current_user,
-      team: new_collaborating_case_owner,
-      include_message: "Maybe this message should not be required when creating a case creator"
-    )
-  rescue ActiveRecord::RecordNotUnique
-    raise
-    investigation
   end
+  # def swap_with_co_collaborator!
+  #   co_collaborator = investigation.co_collaborators.find_by(team: new_collaborating_case_owner)
 
-  def swap_with_co_collaborator!
-    co_collaborator = investigation.co_collaborators.find_by(team: new_collaborating_case_owner)
+  #   return false if co_collaborator.nil?
 
-    return false if co_collaborator.nil?
-
-    co_collaborator.update!(type: CaseOwner, include_message: false)
-    investigation.case_owner.update!(type: CoCollaborator, include_message: false)
-  end
+  #   co_collaborator.update!(type: CaseOwner, include_message: false)
+  #   investigation.case_owner.update!(type: CoCollaborator, include_message: false)
+  # end
 end
