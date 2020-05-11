d6d5f56a895a057c10d99bcdfc9438e2fcb1ca9b
Sławosz Sławiński <slawosz@gmail.com>
2020-05-11 11:33:18 +0100

    Update psd-web/spec/features/attachment_visibility.rb
    Co-authored-by: Ed Horsford <mail@edwardhorsford.com>

--- psd-web/spec/features/attachment_visibility.rb
+++ psd-web/spec/features/attachment_visibility.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "Adding an attachment to a case", :with_stubbed_elasticsearch, :with_stubbed_antivirus, :with_stubbed_mailer, type: :feature do
+RSpec.feature "Viewing an attachment that contains consumer info", :with_stubbed_elasticsearch, :with_stubbed_antivirus, :with_stubbed_mailer, type: :feature do
   let(:user) { create(:user, :activated, has_viewed_introduction: true) }
   let(:other_user) { create(:user, :activated, has_viewed_introduction: true) }
   let(:investigation) { create(:allegation, :with_antivirus_checked_document, document_has_consumer_info: true, owner: user) }
