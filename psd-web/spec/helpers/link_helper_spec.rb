b43a02f7a467c5784c09fd288d1916913628c1d4
Ed Horsford <mail@edwardhorsford.com>
2020-05-14 17:39:45 +0100

    Remove brackets from link helper (#602)

--- psd-web/spec/helpers/link_helper_spec.rb
+++ psd-web/spec/helpers/link_helper_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe LinkHelper do
 
     let(:url) { root_path }
     let(:title) { "Home page" }
-    let(:hidden_text) { "hidden text" }
+    let(:hidden_text) { "(hidden text)" }
     let(:class_name) { "test" }
 
     it "outputs hidden text" do
-      expect(link).to eq("<a class=\"#{class_name}\" href=\"#{url}\">#{title}<span class=\"govuk-visually-hidden\"> (#{hidden_text})</span>\n</a>")
+      expect(link).to eq("<a class=\"#{class_name}\" href=\"#{url}\">#{title}<span class=\"govuk-visually-hidden\"> #{hidden_text}</span>\n</a>")
     end
   end
 end
