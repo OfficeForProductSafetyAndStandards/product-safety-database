e4d701c6addf5ef3bb3eaa6547b4db5747f4f8ff
Ed Horsford <mail@edwardhorsford.com>
2020-05-20 14:17:44 +0100

    Fix linting errors

--- psd-web/app/controllers/investigations/ownership_controller.rb
+++ psd-web/app/controllers/investigations/ownership_controller.rb
@@ -28,7 +28,7 @@ class Investigations::OwnershipController < ApplicationController
     @investigation.owner = potential_owner
     @investigation.owner_rationale = params[:investigation][:owner_rationale]
     @investigation.save
-    redirect_to investigation_path(@investigation), flash: {success: "#{@investigation.case_type.upcase_first} owner changed to #{@investigation.owner.display_name}"}
+    redirect_to investigation_path(@investigation), flash: { success: "#{@investigation.case_type.upcase_first} owner changed to #{@investigation.owner.display_name}" }
   end
 
 private
