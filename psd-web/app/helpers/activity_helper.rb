bd3fa8d97045a663b7e5734f173ff520eb39d6fb
Ed Horsford <mail@edwardhorsford.com>
2020-05-22 11:40:03 +0100

    Add restriction to case overview (#641)

--- psd-web/app/helpers/activity_helper.rb
+++ psd-web/app/helpers/activity_helper.rb
@@ -11,8 +11,6 @@ module ActivityHelper
       "product": "Add a product to the case",
       "business": "Add a business to the case"
     }
-    visibility_text = @investigation.is_private ? "Unrestrict this case" : "Restrict this case for legal privilege"
-    base_types["visibility"] = visibility_text if policy(@investigation).change_owner_or_status?
     base_types["alert"] = "Send email alert about this case" if policy(@investigation).send_email_alert?
 
     base_types
---
+++ psd-web/spec/features/change_case_visibility_restriction_spec.rb
@@ -0,0 +1,61 @@
+require "rails_helper"
+
+RSpec.feature "Change case restriction status", :with_stubbed_elasticsearch, :with_stubbed_antivirus, :with_stubbed_mailer, :with_stubbed_notify do
+  let(:user) do
+    create(
+      :user,
+      :activated,
+      team: create(:team, name: "Portsmouth Trading Standards"),
+      name: "Bob Jones"
+    )
+  end
+
+  let(:case_id) { investigation.pretty_id }
+
+  before do
+    sign_in(user)
+  end
+
+  context "when the user is the owner of the case" do
+    let(:investigation) do
+      create(
+        :allegation,
+        owner: user
+      )
+    end
+
+    scenario "user can change the restriction status of the case" do
+      visit "/cases/#{case_id}"
+
+      expect_to_be_on_case_page(case_id: case_id)
+      expect(page).to have_summary_item(key: "Case restriction", value: "Unrestricted")
+      expect(page).to have_link "Change case restriction"
+      click_link "Change case restriction"
+
+      expect(page).to have_h1("Legal privilege")
+      choose "Restricted for legal privilege"
+      fill_in "Comment / rationale", with: "Restriction reason"
+      click_button "Save"
+
+      expect_to_be_on_case_page(case_id: case_id)
+      expect_confirmation_banner("Allegation was successfully updated")
+      expect(page).to have_summary_item(key: "Case restriction", value: "Restricted")
+    end
+  end
+
+  context "when the user is not the case owner" do
+    let(:investigation) do
+      create(
+        :allegation,
+        owner: create(:user)
+      )
+    end
+
+    scenario "user can’t change the restriction status of the case" do
+      visit "/cases/#{case_id}"
+      expect_to_be_on_case_page(case_id: case_id)
+      expect(page).to have_summary_item(key: "Case restriction", value: "Unrestricted")
+      expect(page).not_to have_link "Change case restriction"
+    end
+  end
+end
