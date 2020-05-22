c5c683bd186ffcdf21778af477240d462d6980eb
Sławosz Sławiński <slawosz@gmail.com>
2020-05-22 14:27:05 +0100

    Prevent accidential otp whitelisting on production (#607)
    * Prevent accidential otp whitelisting on production
    
    * Extract domains whitelisting OTPs to a yaml file (#629)
    
    Extract the regular expressions defining the domains where secondary
    authentication One Time Password codes can be whitelisted to a constants
    YAML file.
    
    We do not want to set this in ENV VARS per environment due to strict
    security requirements. To avoid an easy (and hard to trace) mess up
    because an env var was incorrectly set.
    We neither want urls hardcoded in our business logic.
    Extracting these regular expressions defining our urls into a YAML
    file of constants is a solution that sits in the middle of the
    mentioned two approaches.
    
    * Change config
    
    Co-authored-by: Marc Sardón <pringaohowto@gmail.com>

--- psd-web/spec/models/secondary_authentication_spec.rb
+++ psd-web/spec/models/secondary_authentication_spec.rb
@@ -2,9 +2,10 @@ require "rails_helper"
 
 RSpec.describe SecondaryAuthentication do
   let(:attempts) { 0 }
+  let(:direct_otp) { "11111" }
   let(:direct_otp_sent_at) { Time.new.utc }
   let(:second_factor_attempts_locked_at) { nil }
-  let(:user) { create(:user, second_factor_attempts_count: attempts, direct_otp_sent_at: direct_otp_sent_at, second_factor_attempts_locked_at: second_factor_attempts_locked_at) }
+  let(:user) { create(:user, second_factor_attempts_count: attempts, direct_otp_sent_at: direct_otp_sent_at, second_factor_attempts_locked_at: second_factor_attempts_locked_at, direct_otp: direct_otp) }
   let(:secondary_authentication) { described_class.new(user) }
 
   describe "#valid_otp?" do
@@ -57,6 +58,101 @@ RSpec.describe SecondaryAuthentication do
 
           expect(user.reload.second_factor_attempts_locked_at).to eq(nil)
         end
+      end
+    end
+
+    context "when using WHITELISTED_2FA_CODE env" do
+      shared_examples_for "successful auth" do
+        specify do
+          expect(secondary_authentication).to be_valid_otp(whitelisted_otp)
+        end
+      end
+
+      shared_examples_for "failed auth" do
+        specify do
+          expect(secondary_authentication).not_to be_valid_otp(whitelisted_otp)
+        end
+      end
+
+      let(:whitelisted_otp) { "12345" }
+
+      let(:application_uris) { %w[foo] }
+
+      let(:vcap_application) do
+        { "application_uris" => application_uris }.to_json
+      end
+
+      before do
+        allow(Rails.configuration).to receive(:whitelisted_2fa_code).and_return(whitelisted_otp)
+        allow(Rails.configuration).to receive(:vcap_application).and_return(vcap_application)
+      end
+
+      context "when ENV['VCAP_APPLICATION'] doesn't exist" do
+        it_behaves_like "failed auth"
+      end
+
+      context "when ENV['VCAP_APPLICATION'] is string" do
+        let(:vcap_application) { "foo" }
+
+        it_behaves_like "failed auth"
+      end
+
+      context "when ENV['VCAP_APPLICATION'] is empty hash" do
+        let(:vcap_application) do
+          {}.to_json
+        end
+
+        it_behaves_like "failed auth"
+      end
+
+      context "when ENV['VCAP_APPLICATION'] does not have application_uris key" do
+        let(:vcap_application) do
+          { "foo" => "bar" }.to_json
+        end
+
+        it_behaves_like "failed auth"
+      end
+
+      context "when ENV['VCAP_APPLICATION'] application_uris key is string" do
+        let(:application_uris) { "foo" }
+
+        it_behaves_like "failed auth"
+      end
+
+      context "when ENV['VCAP_APPLICATION'] application_uris key is empty array" do
+        let(:application_uris) { [] }
+
+        it_behaves_like "failed auth"
+      end
+
+      context "when ENV['VCAP_APPLICATION'] application_uris doesn't fit allowed url" do
+        let(:application_uris) { %w[foo] }
+
+        it_behaves_like "failed auth"
+      end
+
+      context "when ENV['VCAP_APPLICATION'] application_uris has more than 1 value" do
+        let(:application_uris) { ["staging.product-safety-database.service.gov.uk", "psd-pr-594.london.cloudapps.digital"] }
+
+        it_behaves_like "failed auth"
+      end
+
+      context "when ENV['VCAP_APPLICATION'] application_uris is production url" do
+        let(:application_uris) { ["www.product-safety-database.service.gov.uk"] }
+
+        it_behaves_like "failed auth"
+      end
+
+      context "when ENV['VCAP_APPLICATION'] application_uris is staging url" do
+        let(:application_uris) { ["staging.product-safety-database.service.gov.uk"] }
+
+        it_behaves_like "successful auth"
+      end
+
+      context "when ENV['VCAP_APPLICATION'] application_uris is review app url" do
+        let(:application_uris) { ["psd-pr-594.london.cloudapps.digital"] }
+
+        it_behaves_like "successful auth"
       end
     end
   end
