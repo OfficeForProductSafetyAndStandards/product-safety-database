25dd97cc6edb902d6a54edc6c32084d8ef82d240
Slawosz Slawinski <slawosz@gmail.com>
2020-05-27 12:33:32 +0100

    Fix migration

--- psd-web/db/migrate/20200514123702_add_collaborators_references_to_collaborations.rb
+++ psd-web/db/migrate/20200514123702_add_collaborators_references_to_collaborations.rb
@@ -2,12 +2,14 @@ class AddCollaboratorsReferencesToCollaborations < ActiveRecord::Migration[5.2]
   def change
     safety_assured do
       change_table :collaborations, bulk: true do |t|
-        t.column :type, :string, null: false
-        t.column :collaborator_type, :string, null: false
+        t.column :type, :string
+        t.column :collaborator_type, :string
       end
       rename_column :collaborations, :team_id, :collaborator_id
       Collaboration.reset_column_information
       Collaboration.update_all(collaborator_type: "Team", type: "Collaborator::EditAccess")
+      change_column_null :collaborations, :collaborator_type, false
+      change_column_null :collaborations, :type, false
       add_index :collaborations, %i[investigation_id type collaborator_type collaborator_id], name: "investigation_collaborator_index", unique: true
     end
   end
