10f414df59fbb67180e8519fab4dfd2a4eee6ee7
StupidCodeFactory <ymarquet@gmail.com>
2020-05-28 15:55:42 +0100

    [WIP] - test for each type of supporting information

---
+++ psd-web/app/controllers/investigations/supporting_informations_controller.rb
@@ -0,0 +1,20 @@
+module Investigations
+  class SupportingInformationsController < ApplicationController
+    def index
+      investigation = Investigation
+                        .find_by!(pretty_id: params[:investigation_pretty_id])
+
+      authorize investigation, :view_non_protected_details?
+
+      @attachements = ActiveStorage::Attachment.where(record: [investigation.corrective_actions, investigation.correspondences])
+      @investigation = investigation.decorate
+
+      @breadcrumbs = {
+        items: [
+          { text: "Cases", href: investigations_path(previous_search_params) },
+          { text: @investigation.pretty_description }
+        ]
+      }
+    end
+  end
+end
