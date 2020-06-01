530de169ab075a3cc22d0542c00d9ed76ea6153e
Frankie Roberto <frankie@frankieroberto.com>
2020-06-01 15:12:33 +0100

    Remove unused helper methods
    These aren't called from anywhere, so deleting.

--- psd-web/app/helpers/documents_helper.rb
+++ psd-web/app/helpers/documents_helper.rb
@@ -14,48 +14,6 @@ module DocumentsHelper
     @parent.documents
   end
 
-  def document_type_label(document_type)
-    case document_type
-    when :correspondence_originator
-      "Correspondence from originator"
-    when :correspondence_business
-      "Correspondence from business"
-    when :correspondence_other
-      "Correspondence from other"
-    when :tech_specs
-      "Technical specifications"
-    when :test_results
-      "Test results"
-    when :risk_assessment
-      "Risk assessment"
-    else
-      "Other"
-    end
-  end
-
-  def document_filetype_label(document)
-    return "Audio" if document.audio?
-    return "Image" if document.image?
-    return "Video" if document.video?
-    return "Text" if document.text?
-
-    case document.content_type
-    when "application/pdf"
-      "PDF"
-    when "application/msword",
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-      "Word Document"
-    when "application/vnd.ms-excel",
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-      "Excel Document"
-    when "application/vnd.ms-powerpoint",
-      "application/vnd.openxmlformats-officedocument.presentationml.presentation"
-      "PowerPoint Document"
-    else
-      document_file_extension(document).upcase
-    end
-  end
-
   def document_file_extension(document)
     File.extname(document.filename.to_s)&.remove(".")&.upcase
   end
