10d277976e08b4c314c6316c4c834e9bb8cb9311
StupidCodeFactory <ymarquet@gmail.com>
2020-06-01 16:50:48 +0100

    tidy up

--- psd-web/spec/features/case_documents_tab_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-RSpec.feature "Manage a case's documents" do
-
-  let(:investiation) { creat(:investigation) }
-
-  before { sign_in create(:user, :activated, :has_viewed_introduction) }
-end
