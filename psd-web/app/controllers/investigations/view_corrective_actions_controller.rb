339050f5ec9a775a562e5cc3ac2f908fad606805
Frankie Roberto <frankie@frankieroberto.com>
2020-06-04 09:34:12 +0100

    Use bang method to trigger a 404 if not found.

--- psd-web/app/controllers/investigations/view_corrective_actions_controller.rb
+++ psd-web/app/controllers/investigations/view_corrective_actions_controller.rb
@@ -1,6 +1,6 @@
 class Investigations::ViewCorrectiveActionsController < ApplicationController
   def show
-    @investigation = Investigation.find_by(pretty_id: params[:investigation_pretty_id])
+    @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
     authorize @investigation, :view_non_protected_details?
     @corrective_action = @investigation.corrective_actions.find(params[:id])
 
