8c201fb95930b36f0839801c8a99b50d990b6519
Slawosz Slawinski <slawosz@gmail.com>
2020-06-08 15:20:20 +0100

    Change migration

--- psd-web/db/migrate/20200604192242_create_creator_users_from_user_source.rb
+++ psd-web/db/migrate/20200604192242_create_creator_users_from_user_source.rb
@@ -4,6 +4,8 @@ class CreateCreatorUsersFromUserSource < ActiveRecord::Migration[5.2]
       UserSource.transaction do
         UserSource.where(sourceable_type: "Investigation").each do |us|
           Collaboration::CreatorUser.create!(investigation_id: us.sourceable_id, collaborator_id: us.user_id, collaborator_type: User)
+          team = us.user.team
+          Collaboration::CreatorTeam.create!(investigation_id: us.sourceable_id, collaborator_id: team.id, collaborator_type: Team)
         end
       end
     end
