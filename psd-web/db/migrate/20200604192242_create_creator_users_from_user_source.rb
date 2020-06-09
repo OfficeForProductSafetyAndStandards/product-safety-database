1a864fd629d73ebbb25f047958940fee793ad164
Slawosz Slawinski <slawosz@gmail.com>
2020-06-09 09:58:15 +0100

    Make migration fail proof

--- psd-web/db/migrate/20200604192242_create_creator_users_from_user_source.rb
+++ psd-web/db/migrate/20200604192242_create_creator_users_from_user_source.rb
@@ -3,9 +3,13 @@ class CreateCreatorUsersFromUserSource < ActiveRecord::Migration[5.2]
     safety_assured do
       UserSource.transaction do
         UserSource.where(sourceable_type: "Investigation").each do |us|
+          user = us.user
+          next unless user
           Collaboration::CreatorUser.create!(investigation_id: us.sourceable_id, collaborator_id: us.user_id, collaborator_type: User)
           team = us.user.team
-          Collaboration::CreatorTeam.create!(investigation_id: us.sourceable_id, collaborator_id: team.id, collaborator_type: Team)
+          if team
+            Collaboration::CreatorTeam.create!(investigation_id: us.sourceable_id, collaborator_id: team.id, collaborator_type: Team)
+          end
         end
       end
     end
