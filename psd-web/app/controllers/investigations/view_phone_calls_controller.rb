17df5ec2c3d2cf179f6aef6f7451c76b899591c7
Frankie Roberto <frankie@frankieroberto.com>
2020-06-09 16:31:56 +0100

    Rename controllers (#699)
    This renames some controllers, to better handle the conflict between the controllers that handle creating things (eg corrective actions) and those that handle viewing the things. Conventionally these would be the same controller, but because the creation controllers use the `show` action as part of the creation flow (via WickedWizard), these conflict.
    
    This PR follows [a suggestion](https://github.com/UKGovernmentBEIS/beis-opss-psd/pull/687#pullrequestreview-426273229) from @slorek to rename the creation controllers as `RecordThingController` so that the view controllers can be `ThingController`, instead of `ViewThingController`. This will allow us to more easily merge the creation controllers back into the regular controllers, when we get around to refactoring away from using WickedWizard.
    
    ### Changes
    
    #### Corrective actions
    
    `ViewCorrectiveActionsController` ➡️  `CorrectiveActionsController`
    `CorrectiveActionsController` ➡️  `RecordCorrectiveActionsController`
    
    #### Correspondence
    
    `ViewEmailsController` ➡️  `EmailsController`
    `EmailsController` ➡️  `RecordEmailsController`
    `ViewPhoneCallsController` ➡️  `PhoneCallsController`
    `PhoneCallsController` ➡️  `RecordPhoneCallsController`
    `ViewMeetingsController` ➡️  `MeetingsController`
    `MeetingsController` ➡️  `RecordMeetingsController`

--- psd-web/app/controllers/investigations/view_phone_calls_controller.rb
+++
@@ -1,9 +0,0 @@
-class Investigations::ViewPhoneCallsController < ApplicationController
-  def show
-    @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
-    authorize @investigation, :view_protected_details?
-    @phone_call = @investigation.phone_calls.find(params[:id]).decorate
-
-    render "investigations/phone_calls/show"
-  end
-end
