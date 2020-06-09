8f6b65f43f45d6ddd7187babb4c2ef91f5dbb5cb
Slawosz Slawinski <slawosz@gmail.com>
2020-06-09 10:05:18 +0100

    Rubocop

--- psd-web/db/migrate/20200604192242_create_creator_users_from_user_source.rb
+++ psd-web/db/migrate/20200604192242_create_creator_users_from_user_source.rb
@@ -5,6 +5,7 @@ class CreateCreatorUsersFromUserSource < ActiveRecord::Migration[5.2]
         UserSource.where(sourceable_type: "Investigation").each do |us|
           user = us.user
           next unless user
+
           Collaboration::CreatorUser.create!(investigation_id: us.sourceable_id, collaborator_id: us.user_id, collaborator_type: User)
           team = us.user.team
           if team
