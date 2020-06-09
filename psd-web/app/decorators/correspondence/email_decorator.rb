7e072c4ec740859510d9b9328f30a568e8d6f00e
Frankie Roberto <frankie@frankieroberto.com>
2020-06-09 11:46:25 +0100

    Minor refactor
    the "govuk" date format shouldn’t be space-padded.

--- psd-web/app/decorators/correspondence/email_decorator.rb
+++ psd-web/app/decorators/correspondence/email_decorator.rb
@@ -2,7 +2,7 @@ class Correspondence < ApplicationRecord
   require_dependency "correspondence"
   class EmailDecorator < InvestigationDecorator
     def title
-      overview.presence || "Email on #{correspondence_date.to_s(:govuk).lstrip}"
+      overview.presence || "Email on #{correspondence_date.to_s(:govuk)}"
     end
   end
 end
--- psd-web/app/decorators/correspondence/meeting_decorator.rb
+++ psd-web/app/decorators/correspondence/meeting_decorator.rb
@@ -2,7 +2,7 @@ class Correspondence < ApplicationRecord
   require_dependency "correspondence"
   class MeetingDecorator < InvestigationDecorator
     def title
-      overview.presence || "Meeting on #{correspondence_date.to_s(:govuk).lstrip}"
+      overview.presence || "Meeting on #{correspondence_date.to_s(:govuk)}"
     end
   end
 end
