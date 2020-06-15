61e45f6caf379d24ddd51e11c89d5d03d56bb876
Slawosz Slawinski <slawosz@gmail.com>
2020-06-15 14:04:20 +0100

    Add migration

---
+++ psd-web/db/migrate/20200615121613_move_owners_to_collaboration.rb
@@ -0,0 +1,13 @@
+class MoveOwnersToCollaboration < ActiveRecord::Migration[5.2]
+  def change
+    safety_assured do
+      Investigation.transaction do
+        Investigation.all.each do |i|
+          owner = i.owner_type.constantize.find(i.owner_id)
+          i.owner = owner
+          i.save
+        end
+      end
+    end
+  end
+end
