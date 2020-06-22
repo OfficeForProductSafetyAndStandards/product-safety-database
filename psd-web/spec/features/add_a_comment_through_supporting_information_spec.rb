2e8792078942894d43ac8231591f8ef012286ec6
Marc Sardon <pringaohowto@gmail.com>
2020-06-22 09:38:20 +0100

    Tests fixes/renaming

--- psd-web/spec/features/add_a_comment_through_supporting_information_spec.rb
+++ psd-web/spec/features/add_a_comment_through_supporting_information_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "Add supporting information", :with_stubbed_elasticsearch, :with_stubbed_antivirus, :with_stubbed_mailer, type: :feature do
+RSpec.feature "Adding a comment through supporting information tab", :with_stubbed_elasticsearch, :with_stubbed_antivirus, :with_stubbed_mailer, type: :feature do
   let(:user)          { create(:user, :activated, has_viewed_introduction: true) }
   let(:investigation) { create(:allegation, owner: user.team) }
 
