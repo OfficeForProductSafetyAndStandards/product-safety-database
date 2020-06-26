204dd840443b10cefe5968ebbfa32f9ab3903b0f
Steve Lorek <steve@stevelorek.com>
2020-06-26 12:24:51 +0100

    Show images on activity timeline

--- psd-web/spec/features/case_images_tab_spec.rb
+++ psd-web/spec/features/case_images_tab_spec.rb
@@ -2,6 +2,8 @@ require "rails_helper"
 
 RSpec.feature "Manage Images", :with_stubbed_elasticsearch, :with_stubbed_antivirus, :with_stubbed_mailer do
   let(:user)          { create(:user, :activated, has_viewed_introduction: true) }
+  let(:other_user_different_org) { create(:user, :activated) }
+
   let(:investigation) { create(:allegation, owner: user.team) }
   let(:file)          { Rails.root + "test/fixtures/files/testImage.png" }
   let(:title)         { Faker::Lorem.sentence }
@@ -26,7 +28,21 @@ RSpec.feature "Manage Images", :with_stubbed_elasticsearch, :with_stubbed_antivi
 
     expect_to_be_on_images_page
 
-    expect_case_attachments_page_to_show_entered_information
+    expect_case_images_page_to_show_entered_information
+
+    click_link "Activity"
+
+    expect_case_activity_page_to_show_entered_information
+
+    # Test that another user in a different organisation can see case images
+    sign_out
+
+    sign_in(other_user_different_org)
+
+    visit "/cases/#{investigation.pretty_id}/images"
+    expect_to_be_on_images_page
+
+    expect_case_images_page_to_show_entered_information
 
     click_link "Activity"
 
@@ -76,7 +92,7 @@ RSpec.feature "Manage Images", :with_stubbed_elasticsearch, :with_stubbed_antivi
     expect(item).to have_selector("p", text: description)
   end
 
-  def expect_case_attachments_page_to_show_entered_information
+  def expect_case_images_page_to_show_entered_information
     expect(page).to have_selector("h2", text: title)
     expect(page).to have_selector("p", text: description)
   end
