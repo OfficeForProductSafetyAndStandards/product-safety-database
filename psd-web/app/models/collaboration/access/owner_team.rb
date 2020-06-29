72165e2fef7c7988b852fa305b6ced3ccc39af10
StupidCodeFactory <ymarquet@gmail.com>
2020-06-29 15:26:48 +0100

    swap recreates a new collaboration record

--- psd-web/app/models/collaboration/access/owner_team.rb
+++ psd-web/app/models/collaboration/access/owner_team.rb
@@ -2,7 +2,8 @@ class Collaboration < ApplicationRecord
   class Access < Collaboration
     class OwnerTeam < Owner
       def swap_to_edit_access!
-        update!(type: "Collaboration::Access::Edit")
+        investigation.edit_access_collaborations.create!(collaborator: collaborator)
+        destroy!
         investigation.owner_user_collaboration&.destroy!
       end
     end
