3c5eb2041ac1034d1181e6f873be5ff7f533f01c
StupidCodeFactory <ymarquet@gmail.com>
2020-06-29 14:02:05 +0100

    remove buggy optional: true

--- psd-web/app/models/collaboration.rb
+++ psd-web/app/models/collaboration.rb
@@ -1,5 +1,5 @@
 class Collaboration < ApplicationRecord
-  belongs_to :investigation, optional: true
+  belongs_to :investigation
   belongs_to :collaborator, polymorphic: true
 
   def creator_team?
