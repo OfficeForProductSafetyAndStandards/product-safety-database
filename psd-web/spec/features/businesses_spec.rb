1f3651285cc885927a1ca307a28085798cd98ffd
Sławosz Sławiński <slawosz@gmail.com>
2020-07-02 09:30:13 +0100

    Fix restricted cards (#760)
    * Make sure investigation is restricted everywhere
    
    * Fix rubocop

--- psd-web/spec/features/businesses_spec.rb
+++ psd-web/spec/features/businesses_spec.rb
@@ -6,11 +6,13 @@ RSpec.feature "Business listing", :with_elasticsearch, :with_stubbed_mailer, typ
   let!(:business_two)   { create(:business, trading_name: "mediocre stuff", created_at: 2.days.ago) }
   let!(:business_three) { create(:business, trading_name: "pretty bad",     created_at: 3.days.ago) }
 
-  before { create_list :business, 18, created_at: 4.days.ago }
+  before do
+    create_list :business, 18, created_at: 4.days.ago
+    sign_in(user)
+  end
 
   scenario "lists products according to search relevance" do
     Business.import refresh: :wait_for
-    sign_in(user)
     visit businesses_path
 
     within ".govuk-grid-row.psd-case-card:nth-child(1) > .govuk-grid-column-one-half:nth-child(1) span:nth-child(2)" do
@@ -34,6 +36,20 @@ RSpec.feature "Business listing", :with_elasticsearch, :with_stubbed_mailer, typ
 
     within ".govuk-grid-row.psd-case-card:nth-child(1) > .govuk-grid-column-one-half:nth-child(1) span:nth-child(2)" do
       expect(page).to have_link(business_three.trading_name, href: business_path(business_three))
+    end
+  end
+
+  scenario "displays cases for product" do
+    investigation = create(:allegation, :with_business, business_to_add: business_one)
+    visit "/businesses/#{business_one.id}"
+    save_page
+    within ".psd-case-card" do
+      expect(page).to have_link(investigation.title, href: "/cases/#{investigation.pretty_id}")
+    end
+    investigation.update(is_private: true)
+    visit "/businesses/#{business_one.id}"
+    within ".psd-case-card" do
+      expect(page).to have_css("span", text: "Allegation restricted")
     end
   end
 end
