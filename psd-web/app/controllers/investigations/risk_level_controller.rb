7fc46c9cd692a43ae3385a334399aa7ba8bd8e02
Marc Sardon <pringaohowto@gmail.com>
2020-07-04 00:15:16 +0100

    Remove unreachable branch in the controller logic
    Result will always be successful or we will get an exception instead.

--- psd-web/app/controllers/investigations/risk_level_controller.rb
+++ psd-web/app/controllers/investigations/risk_level_controller.rb
@@ -9,17 +9,16 @@ module Investigations
     def update
       @investigation = Investigation.find_by!(pretty_id: params.require(:investigation_pretty_id)).decorate
       authorize @investigation, :update?
+
       @risk_level_form = RiskLevelForm.new(params.require(:investigation).permit(:risk_level, :risk_level_other))
       return render :show unless @risk_level_form.valid?
 
-      result = ChangeCaseRiskLevel.call(investigation: @investigation, user: current_user, risk_level: @risk_level_form.risk_level.presence)
-      if result.success?
-        flash[:success] = I18n.t(".success", scope: "investigations.risk_level", action: result.change_action, case_type: @investigation.case_type) if result.change_action.present?
-        redirect_to investigation_path(@investigation)
-      else
-        flash[:error] = I18n.t(".error", scope: "investigations.risk_level", action: result.change_action)
-        render :show
+      result = ChangeCaseRiskLevel.call!(investigation: @investigation, user: current_user, risk_level: @risk_level_form.risk_level.presence)
+      if result.change_action.present?
+        flash[:success] = I18n.t(".success", scope: "investigations.risk_level", action: result.change_action, case_type: @investigation.case_type)
       end
+
+      redirect_to investigation_path(@investigation)
     end
   end
 end
