81a5bb469f01042f013e57fa7b29365cbadbbcdd
StupidCodeFactory <ymarquet@gmail.com>
2020-07-06 17:52:02 +0200

    tidy up

--- psd-web/app/models/collaboration/access.rb
+++ psd-web/app/models/collaboration/access.rb
@@ -5,7 +5,6 @@ class Collaboration < ApplicationRecord
 
     def self.sorted_by_team_name
       joins("INNER JOIN teams ON teams.id = collaborations.collaborator_id AND collaborations.collaborator_type = 'Team'").includes(investigation: :creator_team)
-        .where(collaborator_type: "Team")
         .order(Arel.sql("CASE collaborations.type WHEN 'Collaboration::Access::OwnerTeam' THEN 1 ELSE 2 END, teams.name"))
     end
   end
