2c555b9454ed9141bb5b5df55c2176b884ab494e
Yann Marquet <100796+StupidCodeFactory@users.noreply.github.com>
2020-07-07 13:28:06 +0100

    add view to team listing for read only teams (#753)
    * add view to team listing for read only teams
    
    * add test for team listing page
    
    * ensure read only viewers do not have acess to edit collaborators
    
    * ensure read only can view page teams added to the case
    
    * fix collaboration sorted team name order
    
    * test readonly user can view all the supporting information
    
    * use a shared context for read only team and user setup
    
    * ensure read only team member can not add supporting informations
    
    * user with read only access can view restricted details
    
    * Tidy up
    
    * rubocop offense
    
    * tidy up
    
    Co-authored-by: Sławosz Sławiński <slawosz@gmail.com>

--- psd-web/app/models/collaboration/access.rb
+++ psd-web/app/models/collaboration/access.rb
@@ -4,9 +4,8 @@ class Collaboration < ApplicationRecord
     require_dependency "collaboration/access/edit"
 
     def self.sorted_by_team_name
-      includes(:collaborator, investigation: :creator_team)
-        .where(collaborator_type: "Team")
-        .order(Arel.sql("CASE collaborations.type WHEN 'Collaboration::Access::OwnerTeam' THEN 1 ELSE 2 END"))
+      joins("INNER JOIN teams ON teams.id = collaborations.collaborator_id AND collaborations.collaborator_type = 'Team'").includes(investigation: :creator_team)
+        .order(Arel.sql("CASE collaborations.type WHEN 'Collaboration::Access::OwnerTeam' THEN 1 ELSE 2 END, teams.name"))
     end
   end
 end
