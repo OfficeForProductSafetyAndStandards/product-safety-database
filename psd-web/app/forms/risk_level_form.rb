8f8b122339b76380e4af77cc450d1bc5e44c9974
Marc Sardon <pringaohowto@gmail.com>
2020-07-07 12:10:27 +0100

    Make risk level form easier to understand
    Or at least is what I pretend to.
    The idea is that the form level contains the business logic that
    transforms the risk level selection or custom level into a single
    field in DB and also reads the value from DB and produces the
    attributes that will be displayed in the form.

--- psd-web/app/forms/risk_level_form.rb
+++ psd-web/app/forms/risk_level_form.rb
@@ -5,57 +5,63 @@ class RiskLevelForm
   OTHER = "other".freeze
   ALLOWED_LEVELS = Investigation::STANDARD_RISK_LEVELS + [OTHER]
 
-  attr_reader :original_level, :original_level_other
+  attr_reader :input_level, :input_other
 
   attribute :risk_level, :string, default: nil
   attribute :risk_level_other, :string, default: nil
 
-  validate :risk_level_allowed_validation
-  validate :risk_level_other_present_validation
+  validate :input_level_allowed_validation
+  validate :input_other_present_validation
 
   def initialize(attributes)
-    @original_level = normalise(attributes[:risk_level])
-    @original_level_other = normalise(attributes[:risk_level_other])
+    @input_level = normalise(attributes[:risk_level])
+    @input_other = normalise(attributes[:risk_level_other])
     attrs = set_attributes
     super(attrs)
   end
 
 private
 
+  # Transforms in 2 directions:
+  # 1. User selection/input (risk_level and risk_level_other) into risk_level stored in DB.
+  # 2. DB risk_level value into risk_level and risk_level other to be displayed in the form.
   def set_attributes
     attrs = {}
-    if original_level == OTHER
-      attrs[:risk_level] = original_level_other
-    elsif set_other_from_risk_level?
-      attrs[:risk_level_other] = original_level
+    # User selected "other" in the risk level selection and filled the custom input field
+    if input_level == OTHER
+      attrs[:risk_level] = input_other
+    # Building the form from DB value coming from 'risk_level' field
+    elsif other_set_as_risk_level?
+      attrs[:risk_level_other] = input_level
       attrs[:risk_level] = OTHER
+    # Just sets the risk level from the selection or from DB value
     else
-      attrs[:risk_level] = original_level
+      attrs[:risk_level] = input_level
     end
     attrs
   end
 
-  def risk_level_allowed_validation
-    if original_level.present? && !matching_level_in_list(original_level, ALLOWED_LEVELS)
+  def input_level_allowed_validation
+    if input_level.present? && !matching_level_in_list(input_level, ALLOWED_LEVELS)
       errors.add(:risk_level, :not_allowed)
-      restore_originals
+      restore_inputs # To display the invalid inputs with the errors.
     end
   end
 
-  def risk_level_other_present_validation
-    if original_level == OTHER && original_level_other.blank?
+  def input_other_present_validation
+    if input_level == OTHER && input_other.blank?
       errors.add(:risk_level_other, :blank)
-      restore_originals
+      restore_inputs
     end
   end
 
-  def set_other_from_risk_level?
-    original_level.present? && !matching_level_in_list(original_level, Investigation::STANDARD_RISK_LEVELS)
+  def other_set_as_risk_level?
+    input_level.present? && !matching_level_in_list(input_level, Investigation::STANDARD_RISK_LEVELS)
   end
 
-  def restore_originals
-    self.risk_level = original_level
-    self.risk_level_other = original_level_other
+  def restore_inputs
+    self.risk_level = input_level
+    self.risk_level_other = input_other
   end
 
   def equal_levels?(first_level, second_level)
