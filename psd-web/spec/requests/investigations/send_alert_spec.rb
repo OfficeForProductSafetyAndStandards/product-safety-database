9360ef0aa6fed0cdb36c0998cfec90fd7a9dae6f
Steven Lorek <steve@stevelorek.com>
2020-07-07 14:25:52 +0100

    Replace deprecated Minitest controller test with Rspec spec (#774)
    Co-authored-by: Sławosz Sławiński <slawosz@gmail.com>

---
+++ psd-web/spec/requests/investigations/send_alert_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe "Sending a product safety alert", :with_stubbed_elasticsearch, :with_stubbed_mailer, type: :request do
+  let(:user) { create(:user, :activated, :opss_user) }
+
+  before { sign_in user }
+
+  context "when the case is not restricted" do
+    let(:investigation) { create(:allegation, creator: user) }
+
+    it "responds with a 200 status code" do
+      get investigation_alert_url(investigation, id: "compose")
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  context "when the case is restricted" do
+    let(:investigation) { create(:allegation, :restricted, creator: user) }
+
+    it "raises a Pundit::NotAuthorizedError exception" do
+      expect { get investigation_alert_url(investigation, id: "compose") }.to raise_error(Pundit::NotAuthorizedError)
+    end
+  end
+end
