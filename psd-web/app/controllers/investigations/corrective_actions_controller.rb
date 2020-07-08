4804a9e9c0ff8037167750420d31a52f0252c7e1
StupidCodeFactory <ymarquet@gmail.com>
2020-07-08 14:00:57 +0200

    add controller action

--- psd-web/app/controllers/investigations/corrective_actions_controller.rb
+++ psd-web/app/controllers/investigations/corrective_actions_controller.rb
@@ -10,4 +10,35 @@ class Investigations::CorrectiveActionsController < ApplicationController
     authorize @investigation, :view_non_protected_details?
     @corrective_action = @investigation.corrective_actions.find(params[:id]).decorate
   end
+
+  def update
+    @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
+    authorize @investigation, :view_non_protected_details?
+    @corrective_action = @investigation.corrective_actions.find(params[:id])
+
+    result = UpdateCorrectiveAction.call!(
+      corrective_action: @corrective_action,
+      corrective_actions_params: corrective_actions_params,
+      user: current_user
+    )
+    return redirect_to [@investigation, result.corrective_action] if result.success?
+
+    render [@investigation, @corrective_action]
+  end
+
+private
+
+  def corrective_actions_params
+    params.require(:corrective_action).permit(
+      :product_id,
+      :business_id,
+      :legislation,
+      :summary,
+      :details,
+      :related_file,
+      :measure_type,
+      :duration,
+      :geographic_scope
+    )
+  end
 end
--- psd-web/spec/features/edit_corrective_action_spec.rb
+++ psd-web/spec/features/edit_corrective_action_spec.rb
@@ -1,15 +1,66 @@
 require "rails_helper"
 
 RSpec.feature "Edit corrective action", :with_stubbed_elasticsearch, :with_stubbed_mailer do
-  let(:user)              { create(:user, :activated, has_viewed_introduction: true) }
-  let(:investigation)     { create(:allegation, creator: user) }
-  let(:corrective_action) { create(:corrective_action, :with_file, investigation: investigation) }
+  let(:user)          { create(:user, :activated, has_viewed_introduction: true) }
+  let(:product_one)   { create(:product) }
+  let(:product_two)   { create(:product) }
+  let(:business_one)  { create(:business) }
+  let(:business_two)  { create(:business) }
+  let(:investigation) do
+    create(:allegation,
+           creator: user,
+           products: [product_one, product_two])
+  end
+  let(:corrective_action) do
+    create(:corrective_action,
+           :with_file,
+           investigation: investigation,
+           product: product_one,
+           business: business_one)
+  end
+  let(:new_summary)          { Faker::Lorem.sentence }
+  let(:new_date_received)    { corrective_action.date_decided - 1.day }
+  let(:new_legislation)      { (Rails.application.config.legislation_constants["legislation"] - [corrective_action.legislation]).sample }
+  let(:new_duration)         { (CorrectiveAction::DURATION_TYPES - [corrective_action.duration]).sample }
+  let(:new_details)          { Faker::Lorem.sentence }
+  let(:new_measure_type)      do
+    (CorrectiveAction::MEASURE_TYPES - [corrective_action.measure_type]).sample == CorrectiveAction::MEASURE_TYPES[0] ? "Yes" : "No, it’s voluntary"
+  end
+  let(:new_geographic_scope) do
+    (Rails.application.config.corrective_action_constants["geographic_scope"] - [corrective_action.geographic_scope]).sample
+  end
 
-  before { sign_in user }
+  before do
+    investigation.businesses << business_one << business_two
+    sign_in user
+  end
 
-  it "provides a mean to edit a corrective action" do
-    visit "/cases/#{investigation.pretty_id}/corrective-actions/#{corrective_action.id}"
+  context "when a single product is chosen" do
+    it "provides a mean to edit a corrective action" do
+      visit "/cases/#{investigation.pretty_id}/corrective-actions/#{corrective_action.id}"
 
-    click_link "Edit corrective action"
+      click_link "Edit corrective action"
+
+      fill_in "Summary",        with: new_summary
+      fill_in "Day",            with: new_date_received.day
+      fill_in "Month",          with: new_date_received.month
+      fill_in "Year",           with: new_date_received.year
+      select new_legislation,   from: "Under which legislation?"
+      select business_two.trading_name, from: "Business"
+      choose new_measure_type
+      choose CorrectiveAction.human_attribute_name("duration.#{new_duration}")
+      select new_geographic_scope, from: "What is the geographic scope of the action?"
+      fill_in "Further details (optional)", with: new_details
+
+      click_on "Update corrective action"
+
+      expect(page).to have_summary_item(key: "Date of action",      value: new_date_received.to_s(:govuk))
+      expect(page).to have_summary_item(key: "Product",             value: new_date_received.to_s(:govuk))
+      expect(page).to have_summary_item(key: "Legislation",         value: new_legislation)
+      expect(page).to have_summary_item(key: "Type of action",      value: new_measure_type)
+      expect(page).to have_summary_item(key: "Duration of measure", value: CorrectiveAction.human_attribute_name("duration.#{new_duration}"))
+      expect(page).to have_summary_item(key: "Scope",               value: new_geographic_scope)
+      expect(page).to have_summary_item(key: "Other details",       value: new_details)
+    end
   end
 end
