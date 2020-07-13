db439626515a28e331e08a74ad25e70cea4e55d4
Slawosz Slawinski <slawosz@gmail.com>
2020-07-13 10:05:23 +0100

    Use owner id from method

--- psd-web/app/models/concerns/investigation_elasticsearch.rb
+++ psd-web/app/models/concerns/investigation_elasticsearch.rb
@@ -21,7 +21,6 @@ module InvestigationElasticsearch
                  hazard_type
                  product_category
                  is_closed
-                 owner_id
                  type
                  updated_at
                  created_at
@@ -29,7 +28,7 @@ module InvestigationElasticsearch
                  hazard_description
                  non_compliant_reason
                  coronavirus_related],
-        methods: %i[title creator_id],
+        methods: %i[title creator_id owner_id],
         include: {
           documents: {
             only: [],
