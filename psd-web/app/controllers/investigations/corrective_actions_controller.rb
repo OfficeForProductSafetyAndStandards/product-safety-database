42622c509ac553e8c46a3f898dd53c36d2eaed7c
StupidCodeFactory <ymarquet@gmail.com>
2020-07-15 14:52:51 +0200

    Fix some of te bugs

--- psd-web/app/controllers/investigations/corrective_actions_controller.rb
+++ psd-web/app/controllers/investigations/corrective_actions_controller.rb
@@ -47,7 +47,7 @@ private
   end
 
   def edit
-    @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
+    @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id]).decorate
     authorize @investigation, :view_non_protected_details?
     @corrective_action = @investigation.corrective_actions.find(params[:id]).decorate
     @file_blob = @corrective_action.documents_blobs.first || @corrective_action.documents_blobs.new
