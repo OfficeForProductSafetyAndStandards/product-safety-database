cac2d2b38d076c9dfc65b06e92f498233a85197c
StupidCodeFactory <ymarquet@gmail.com>
2020-07-29 14:12:34 +0100

    tidy up

--- psd-web/app/controllers/investigations/corrective_actions_controller.rb
+++ psd-web/app/controllers/investigations/corrective_actions_controller.rb
@@ -1,98 +1,59 @@
-class Investigations::CorrectiveActionsController < ApplicationController
-  def show
-    @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id]).decorate
-    authorize @investigation, :view_non_protected_details?
-    @corrective_action = @investigation.corrective_actions.find(params[:id]).decorate
-  end
-
-  def edit
-    @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id]).decorate
-    authorize @investigation, :update?
-    @corrective_action = @investigation.corrective_actions.find!(params[:id]).decorate
-    @file_blob = @corrective_action.documents_blobs.first || @corrective_action.documents_blobs.new
-  end
-
-  def update
-    @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
-    authorize @investigation, :update?
-    corrective_action = @investigation.corrective_actions.find(params[:id])
-
-    service_form = UpdateCorrectiveActionForm.new(corrective_action_params)
-
-    if service_form.invalid?
-      @corrective_action = corrective_action.decorate
-      return render :edit
+module Investigations
+  class CorrectiveActionsController < ApplicationController
+    def show
+      @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id]).decorate
+      authorize @investigation, :view_non_protected_details?
+      @corrective_action = @investigation.corrective_actions.find(params[:id]).decorate
     end
 
-    corrective_action.assign_attributes(service_form.attributes)
+    def edit
+      @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id]).decorate
+      authorize @investigation, :update?
+      @corrective_action = @investigation.corrective_actions.find!(params[:id]).decorate
+      @file_blob = @corrective_action.documents_blobs.first || @corrective_action.documents_blobs.new
+    end
 
-    result = UpdateCorrectiveAction.call(
-      corrective_action: corrective_action,
-      file_description: service_form,
-      user: current_user
-    )
-    return redirect_to investigation_action_path(@investigation, result.corrective_action) if result.success?
+    def update
+      @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
+      authorize @investigation, :update?
+      corrective_action = @investigation.corrective_actions.find(params[:id])
 
-    @corrective_action = corrective_action.decorate
-    render :edit
-  end
+      service_form = UpdateCorrectiveActionForm.new(corrective_action_params)
 
-private
+      if service_form.invalid?
+        @corrective_action = corrective_action.decorate
+        return render :edit
+      end
 
-  def corrective_action_params
-    params.require(:corrective_action).permit(
-      :product_id,
-      :business_id,
-      :legislation,
-      :summary,
-      :details,
-      :related_file,
-      :measure_type,
-      :duration,
-      :geographic_scope,
-      file: %i[file description],
-      date_decided: %i[day month year]
-    )
-  end
+      corrective_action.assign_attributes(service_form.attributes)
 
-  def edit
-    @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id]).decorate
-    authorize @investigation, :view_non_protected_details?
-    @corrective_action = @investigation.corrective_actions.find(params[:id]).decorate
-    @file_blob = @corrective_action.documents_blobs.first || @corrective_action.documents_blobs.new
-  end
+      result = UpdateCorrectiveAction.call(
+        corrective_action: corrective_action,
+        file_description: service_form,
+        user: current_user
+      )
+      return redirect_to investigation_action_path(@investigation, result.corrective_action) if result.success?
 
-  def update
-    @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
-    authorize @investigation, :view_non_protected_details?
-    corrective_action = @investigation.corrective_actions.find(params[:id])
+      @corrective_action = corrective_action.decorate
+      render :edit
+    end
 
-    result = UpdateCorrectiveAction.call(
-      corrective_action: corrective_action,
-      corrective_action_params: corrective_action_params,
-      user: current_user
-    )
-    return redirect_to investigation_action_path(@investigation, result.corrective_action) if result.success?
+  private
 
-    @corrective_action = corrective_action.decorate
-    render :edit
-  end
-
-private
-
-  def corrective_action_params
-    params.require(:corrective_action).permit(
-      :product_id,
-      :business_id,
-      :legislation,
-      :summary,
-      :details,
-      :related_file,
-      :measure_type,
-      :duration,
-      :geographic_scope,
-      file: %i[file description],
-      date_decided: %i[day month year]
-    )
+    def corrective_action_params
+      params.require(:corrective_action).permit(
+        :product_id,
+        :business_id,
+        :legislation,
+        :summary,
+        :details,
+        :related_file,
+        :measure_type,
+        :duration,
+        :geographic_scope,
+        file: %i[file description],
+        date_decided: %i[day month year]
+      )
+    end
   end
 end
