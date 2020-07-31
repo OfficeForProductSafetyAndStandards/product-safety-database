413833b16d19303430a5f6399e07d6b4e8a54352
Frankie Roberto <frankie@frankieroberto.com>
2020-07-31 10:33:42 +0100

    Add model validations and model spec

--- psd-web/app/models/risk_assessment.rb
+++ psd-web/app/models/risk_assessment.rb
@@ -12,6 +12,26 @@ class RiskAssessment < ApplicationRecord
 
   has_one_attached :risk_assessment_file
 
+
+  validates :assessed_on, presence: true
+
+  validate :assessed_on_cannot_be_in_future
+  validate :assessed_on_cannot_be_older_than_1970
+
+  validate :at_least_one_product_associated
+
+  validates :risk_level, presence: true
+
+  validates :custom_risk_level, presence: true, if: -> { other? }
+  validates :custom_risk_level, absence: true, unless: -> { other? }
+
+
+  # Exactly 1 of team, business or "other" required
+  validates :assessed_by_business, absence: true, if: -> { assessed_by_team }
+  validates :assessed_by_other, presence: true, if: -> { assessed_by_team.nil? && assessed_by_business.nil? }
+  validates :assessed_by_other, absence: true, if: -> { assessed_by_team || assessed_by_business }
+
+
   enum risk_level: {
     serious: "serious",
     high: "high",
@@ -19,4 +39,26 @@ class RiskAssessment < ApplicationRecord
     low: "low",
     other: "other"
   }
+
+private
+
+  def assessed_on_cannot_be_in_future
+    if assessed_on.is_a?(Date) && assessed_on > Time.zone.today
+
+      errors.add(:assessed_on, :in_future)
+    end
+  end
+
+  def assessed_on_cannot_be_older_than_1970
+    if assessed_on.is_a?(Date) && assessed_on < Date.parse("1970-01-01")
+      errors.add(:assessed_on, :too_old)
+    end
+  end
+
+  def at_least_one_product_associated
+    return unless product_ids.to_a.empty?
+
+    errors.add(:products, :blank)
+  end
+
 end
