50c0d911b1fc3686c2619aff024714bf51568e0d
Frankie Roberto <frankie@frankieroberto.com>
2020-08-10 15:16:44 +0100

    Update psd-web references

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 set -ex
 
 # Set the manifest file
-MANIFEST_FILE=./psd-web/manifest.yml
+MANIFEST_FILE=./manifest.yml
 
 # Copy the environment helper script
-cp -a ./infrastructure/env/. ./psd-web/env/
+cp -a ./infrastructure/env/. ./env/
 
 # Set the amount of time in minutes that the CLI will wait for all instances to start.
 # Because of the rolling deployment strategy, this should be set to at least the amount of
@@ -18,4 +18,4 @@ export CF_STARTUP_TIMEOUT=15
 cf7 push $APP_NAME -f $MANIFEST_FILE --app-start-timeout 180 --var app-name=$APP_NAME --var psd-host=$DOMAIN --var web-memory=$WEB_MEMORY --var worker-memory=$WORKER_MEMORY --var web-instances=$WEB_INSTANCES --var worker-instances=$WORKER_INSTANCES --var web-max-threads=$WEB_MAX_THREADS --var worker-max-threads=$WORKER_MAX_THREADS --strategy rolling
 
 # Remove the copied infrastructure env files to clean up
-rm -R psd-web/env/
+rm -R env/
