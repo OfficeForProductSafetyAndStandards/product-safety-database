d7ddb918a7781aa12b18ae2fe3e3a825f558fb70
Yann Marquet <100796+StupidCodeFactory@users.noreply.github.com>
2020-09-23 18:49:03 +0200

    Add case risk level to xls export (#902)
    * Add case risk level to xls export
    
    * fix specs

--- spec/requests/export_investigations_spec.rb
+++ spec/requests/export_investigations_spec.rb
@@ -88,6 +88,28 @@ RSpec.describe "Export investigations as XLSX file", :with_elasticsearch, :with_
         end
       end
 
+      it "exports the case risk level" do
+        investigation = create(:allegation)
+        ChangeCaseRiskLevel.call!(
+          investigation: investigation,
+          user:
+            user,
+          risk_level: (Investigation.risk_levels.values - %w[other]).sample
+        )
+
+        Investigation.import refresh: true, force: true
+
+        get investigations_path format: :xlsx
+
+        categories_cell_title = exported_data.cell(1, 9)
+        categories_cell_content = exported_data.cell(2, 9)
+
+        aggregate_failures "risk level cells values" do
+          expect(categories_cell_title).to eq "Risk_Level"
+          expect(categories_cell_content).to eq investigation.decorate.risk_level_description
+        end
+      end
+
       it "exports owner team and user" do
         user = create(:user)
         team = create(:team)
@@ -101,16 +123,16 @@ RSpec.describe "Export investigations as XLSX file", :with_elasticsearch, :with_
         get investigations_path format: :xlsx
 
         aggregate_failures do
-          expect(exported_data.cell(1, 9)).to eq "Case_Owner_Team"
-          expect(exported_data.cell(1, 10)).to eq "Case_Owner_User"
+          expect(exported_data.cell(1, 10)).to eq "Case_Owner_Team"
+          expect(exported_data.cell(1, 11)).to eq "Case_Owner_User"
 
           expect(exported_data.cell(2, 1)).to eq case_with_team_owner.pretty_id
-          expect(exported_data.cell(2, 9)).to eq team.name
-          expect(exported_data.cell(2, 10)).to be_nil
+          expect(exported_data.cell(2, 10)).to eq team.name
+          expect(exported_data.cell(2, 11)).to be_nil
 
           expect(exported_data.cell(3, 1)).to eq case_with_user_owner.pretty_id
-          expect(exported_data.cell(3, 9)).to eq user.team.name
-          expect(exported_data.cell(3, 10)).to eq user.name
+          expect(exported_data.cell(3, 10)).to eq user.team.name
+          expect(exported_data.cell(3, 11)).to eq user.name
         end
       end
     end
