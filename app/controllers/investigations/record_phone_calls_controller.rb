97cd5b8d7cf42e3db6bda1274e787fe124b8f848
StupidCodeFactory <ymarquet@gmail.com>
2020-10-05 20:10:09 +0200

    change for to use ApplicationFormBuilder

--- app/controllers/investigations/record_phone_calls_controller.rb
+++ app/controllers/investigations/record_phone_calls_controller.rb
@@ -1,21 +1,26 @@
 class Investigations::RecordPhoneCallsController < ApplicationController
-
   def new
     investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
     authorize investigation, :update?
 
-    @correspondence = Correspondence::PhoneCall.new
+    @correspondence_form = PhoneCallCorrespondenceForm.new
     @investigation = investigation.decorate
   end
 
   def create
+    @correspondence_form = PhoneCallCorrespondenceForm.new(phone_call_params)
+    @correspondence = Correspondence::PhoneCall.new(@correspondence_form.attributes)
+    @correspondence.save!
 
+    return render :new unless @correspondence_form.valid?
+
+    redirect_to @correspondence
   end
 
 private
 
   def phone_call_params
-    params.require(:correspondence_phone_call).permit(
+    params.require(:phone_call_correspondence_form).permit(
       :correspondent_name,
       :phone_number,
       :overview,
