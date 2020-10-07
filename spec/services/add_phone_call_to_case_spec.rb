aeefe1466b189b384d1d1600f2e6ddc7338b65e2
StupidCodeFactory <ymarquet@gmail.com>
2020-10-07 18:27:43 +0200

    test rails saves record

--- spec/services/add_phone_call_to_case_spec.rb
+++ spec/services/add_phone_call_to_case_spec.rb
@@ -1,10 +1,29 @@
 require "rails_helper"
 
-RSpec.describe AddPhoneCallToCase, :with_stubbed_elasticsearch, :with_stubbed_mailer do
-  subject(:result) { described_class.call(investigation: investigation, user: user) }
+RSpec.describe AddPhoneCallToCase, :with_stubbed_elasticsearch, :with_stubbed_mailer, :with_stubbed_antivirus do
+  include ActionDispatch::TestProcess::FixtureFile
 
-  let(:user) { create :user }
-  let(:investigation) { create :allegation }
+  subject(:result) { described_class.call(params) }
+
+  let(:user)                { create :user }
+  let(:investigation)       { create :allegation }
+  let(:phone_number)        { Faker::PhoneNumber.phone_number }
+  let(:correspondence_date) { Date.parse("1-1-2020") }
+  let(:correspondent_name)  { Faker::Name.name  }
+  let(:overview)            { Faker::Hipster.paragraph }
+  let(:details)             { Faker::Hipster.paragraph }
+  let(:params) do
+    {
+      investigation: investigation,
+      user: user,
+      transcript: Rack::Test::UploadedFile.new(file_fixture("files/phone_call_transcript.txt")),
+      correspondence_date: correspondence_date,
+      phone_number: phone_number,
+      correspondent_name: correspondent_name,
+      overview: overview,
+      details: details
+    }
+  end
 
   describe "#call" do
     context "when no investigation is provided" do
@@ -19,6 +38,18 @@ RSpec.describe AddPhoneCallToCase, :with_stubbed_elasticsearch, :with_stubbed_ma
 
       it { expect(result).to be_a_failure }
       it { expect(result.error).to eq("No user supplied") }
+    end
+  end
+
+  describe "when providing all necessary arguments" do
+    it "creates a correspondence" do
+      expect(result.correspondence).to have_attributes(
+                                         transcript: instance_of(ActiveStorage::Attached::One),
+                                         correspondence_date: correspondence_date,
+                                         correspondent_name: correspondent_name,
+                                         overview: overview,
+                                         details: details
+                                       )
     end
   end
 end
