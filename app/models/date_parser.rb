538f19ed80dc68b7a36b0c94b4b6dc097a26e1a2
StupidCodeFactory <ymarquet@gmail.com>
2020-10-28 16:10:13 +0100

    refactor date parser

--- app/models/date_parser.rb
+++ app/models/date_parser.rb
@@ -10,21 +10,11 @@ class DateParser
 
   def date
     return nil if @date.nil?
-    return @date if @date.is_a?(Date)
 
-    if @date.is_a?(String)
-      return Date.parse(@date) rescue Date::Error nil # rubocop:disable Style/RescueModifier
-    end
+    @date = date_from_string(@date) if @date.is_a?(String)
 
-    @date.symbolize_keys! if @date.respond_to?(:symbolize_keys!)
-
-    date_values = @date.values_at(:year, :month, :day).map do |date_part|
-      date_part.is_a?(Integer) ? date_part : Integer(date_part.delete_prefix("0"))
-    rescue StandardError
-      nil
-    end
-
-    return nil if date_values.all?(&:blank?)
+    return @date            if @date.is_a?(Date) || @date.is_a?(Time)
+    return nil              if date_values.all?(&:blank?)
     return struct_from_hash if date_values.any?(&:blank?)
     return struct_from_hash if date_values[1].negative? || date_values[2].negative?
 
@@ -39,5 +29,20 @@ private
 
   def struct_from_hash
     OpenStruct.new(year: @date[:year], month: @date[:month], day: @date[:day])
+  end
+
+  def date_from_string(date_as_string)
+    Date.parse(date_as_string) rescue Date::Error # rubocop:disable Style/RescueModifier
+  end
+
+  def date_values
+    @date_values ||= begin
+      @date.symbolize_keys! if @date.respond_to?(:symbolize_keys!)
+      @date.values_at(:year, :month, :day).map do |date_part|
+        date_part.is_a?(Integer) ? date_part : Integer(date_part.delete_prefix("0"))
+      rescue StandardError
+        nil
+      end
+    end
   end
 end
