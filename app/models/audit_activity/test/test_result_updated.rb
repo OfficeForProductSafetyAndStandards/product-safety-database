560da5206bc874abe20bccdf4e59a123aa7f325a
Steve Lorek <steve@stevelorek.com>
2020-10-29 18:12:38 +0000

    Make overridden private method private

--- app/models/audit_activity/test/test_result_updated.rb
+++ app/models/audit_activity/test/test_result_updated.rb
@@ -68,13 +68,13 @@ class AuditActivity::Test::TestResultUpdated < AuditActivity::Test::Base
       end
   end
 
-  # Do not send investigation_updated mail when test result updated. This
-  # overrides inherited functionality in the Activity model :(
-  def notify_relevant_users; end
-
 private
 
   def updates
     metadata["updates"]
   end
+
+  # Do not send investigation_updated mail when test result updated. This
+  # overrides inherited functionality in the Activity model :(
+  def notify_relevant_users; end
 end
