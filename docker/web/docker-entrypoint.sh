3000f3ea9ed897f872fc9e9504aa8565ec346b4a
Steve Lorek <steve@stevelorek.com>
2020-11-10 14:58:30 +0000

    Remove redundant X frame buffer system test process

--- docker/web/docker-entrypoint.sh
+++ docker/web/docker-entrypoint.sh
@@ -9,11 +9,6 @@ bundle check || bundle install
 # Ensure all node packages are installed.
 yarn install
 
-# Setup display for system tests
-SCREEN="${SCREEN:-1280x1024x16}"
-echo "Starting X virtual framebuffer (Xvfb) for $SCREEN screen in background..."
-Xvfb -ac :99 -screen 0 $SCREEN > /dev/null 2>&1 &
-export DISPLAY=:99
 
 bin/webpack-dev-server --progress &
 
