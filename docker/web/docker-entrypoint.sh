d3a453dcd99ff5abb502cd22c3be579f7cdbb65a
Steve Lorek <steve@stevelorek.com>
2020-11-10 14:59:04 +0000

    Remove redundant processes from Docker config

--- docker/web/docker-entrypoint.sh
+++ docker/web/docker-entrypoint.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-gem install bundler:2.0.2
-
 # Ensure all gems are installed.
-bundle check || bundle install
+bin/bundle check || bin/bundle install
 
 # Ensure all node packages are installed.
 yarn install
-
-
-bin/webpack-dev-server --progress &
 
 # Run the passed in command
 exec "$@"
