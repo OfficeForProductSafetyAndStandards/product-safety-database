4b7086bd032abf7d637c4935fd2ab5397e2f9b57
StupidCodeFactory <ymarquet@gmail.com>
2020-12-01 17:44:15 +0100

    fix standards_product_was_tested_against field

--- lib/active_model/types/comma_separated_list.rb
+++ lib/active_model/types/comma_separated_list.rb
@@ -3,7 +3,7 @@ module ActiveModel
     class CommaSeparatedList < ActiveRecord::Type::Value
       def cast(value)
         return value if value.is_a?(Array)
-        return [] if value.nil?
+        return nil if value.nil? || value.blank?
 
         value.split(",").map(&:squish)
       end
