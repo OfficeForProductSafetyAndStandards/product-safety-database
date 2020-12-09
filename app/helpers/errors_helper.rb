dde56a6ce60f428bfa7d522c4bd303686a04ac37
StupidCodeFactory <ymarquet@gmail.com>
2020-12-09 18:30:32 +0100

    fix error anchor

--- app/helpers/errors_helper.rb
+++ app/helpers/errors_helper.rb
@@ -1,5 +1,5 @@
 module ErrorsHelper
-  def file_validation_errors?(errors)
-    errors.details[:base].any? { |error| (error.value? :file_too_large) || (error.value? :file_missing) }
+  def file_validation_errors?(errors, attribute: )
+    errors.details[attribute].any? { |error| (error.value? :file_too_large) || (error.value? :file_missing) || (error.value? :blank) }
   end
 end
