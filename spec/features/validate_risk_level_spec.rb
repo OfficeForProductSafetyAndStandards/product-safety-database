ddcfbc2a2073c3051ed687a64647008e66193a35
kyle macpherson <kaimac2010@gmail.com>
2020-12-14 17:50:09 +0000

    Add tests

--- spec/features/validate_risk_level_spec.rb
+++ spec/features/validate_risk_level_spec.rb
@@ -9,13 +9,13 @@ RSpec.feature "Validate risk level", :with_elasticsearch, :with_stubbed_mailer, 
     sign_in user
     visit investigation_path(investigation)
 
+    validation_link = page.find(:css, "a[href='/cases/#{investigation.pretty_id}/validate-risk-level/edit']")
+
+    expect(validation_link.text).to eq "Validate"
+
     click_link "Validate"
 
     expect(page).to have_current_path("/cases/#{investigation.pretty_id}/validate-risk-level/edit")
-
-    click_on "Continue"
-
-    expect(page).to have_content("Select yes if you have validated the case risk level")
 
     within_fieldset("Have you validated the case risk level?") do
       choose "Yes"
@@ -27,11 +27,18 @@ RSpec.feature "Validate risk level", :with_elasticsearch, :with_stubbed_mailer, 
     expect(page).to have_content("Case risk level validated")
     expect(page).to have_content("Validated by #{user.team.name} on #{investigation.risk_validated_at}")
     expect(page).not_to have_link("Validate")
+
+    click_on "Activity"
+    expect(page).to have_content "Case risk level validation added"
   end
 
   scenario "do not validate the level" do
     sign_in user
     visit investigation_path(investigation)
+
+    validation_link = page.find(:css, "a[href='/cases/#{investigation.pretty_id}/validate-risk-level/edit']")
+
+    expect(validation_link.text).to eq "Validate"
 
     click_link "Validate"
 
@@ -47,5 +54,42 @@ RSpec.feature "Validate risk level", :with_elasticsearch, :with_stubbed_mailer, 
     expect(page).not_to have_content("Case risk level validated")
     expect(page).not_to have_content("Validated by #{user.team.name} on #{investigation.risk_validated_at}")
     expect(page).to have_link("Validate")
+
+    click_on "Activity"
+    expect(page).not_to have_content "Case risk level validation added"
+  end
+
+  scenario "remove validation" do
+    sign_in user
+    visit("/cases/#{investigation.pretty_id}/validate-risk-level/edit")
+
+    within_fieldset("Have you validated the case risk level?") do
+      choose "Yes"
+    end
+
+    click_on "Continue"
+
+    expect(page).to have_current_path("/cases/#{investigation.pretty_id}")
+
+    validation_link = page.find(:css, "a[href='/cases/#{investigation.pretty_id}/validate-risk-level/edit']")
+    expect(validation_link.text).to eq 'Change'
+
+    validation_link.click
+
+    within_fieldset("Have you validated the case risk level?") do
+      choose "No"
+      fill_in "Why?", with: "Mistake made by team member"
+    end
+
+    click_on "Continue"
+
+    expect(page).to have_current_path("/cases/#{investigation.pretty_id}")
+    expect(page).not_to have_content("Case risk level validated")
+    expect(page).not_to have_content("Validated by #{user.team.name} on #{investigation.risk_validated_at}")
+    expect(page).to have_link("Validate")
+
+    click_on "Activity"
+    expect(page).to have_content "Case risk level validation removed"
+    expect(page).to have_content "Mistake made by team member"
   end
 end
