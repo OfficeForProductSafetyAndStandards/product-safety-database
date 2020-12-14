3adc869a6aadd07d96929ef81e18b1d3d56cd497
StupidCodeFactory <ymarquet@gmail.com>
2020-12-14 15:42:34 +0100

    tidy up

--- db/migrate/20201125105949_add_standars_product_was_testing_against_to_tests.rb
+++ db/migrate/20201125105949_add_standars_product_was_testing_against_to_tests.rb
@@ -1,7 +1,6 @@
 class AddStandardsProductWasTestedAgainstToTests < ActiveRecord::Migration[6.0]
   def change
     safety_assured do
-      # TODO: add default to empty array in later migration
       add_column :tests, :standards_product_was_tested_against, :string, array: true
     end
   end
