565db54399b15c1da1e9003cc39c217f2c8ead59
StupidCodeFactory <ymarquet@gmail.com>
2020-12-16 19:19:17 +0100

    refactor code

--- app/validators/gtin_validator.rb
+++ app/validators/gtin_validator.rb
@@ -5,16 +5,22 @@ class GtinValidator < ActiveModel::EachValidator
     return if value.nil?
 
     begin
-      gtin = Barkick::GTIN.new(value)
+      gtin = gtin_for(value)
       # 8 digit codes raise an ArgumentError, as a type (UPC-E or EAN-8) needs
       # to be specified in order to convert the code to a valid GTIN
     rescue ArgumentError
-      gtin = Barkick::GTIN.new(record.public_send(attribute), type: :upc_e)
-      gtin = Barkick::GTIN.new(record.public_send(attribute), type: :ean8) unless gtin.valid?
+      gtin = gtin_for(record.public_send(attribute), type: :upc_e)
+      gtin = gtin_for(record.public_send(attribute), type: :ean8) unless gtin.valid?
     end
 
     unless gtin.valid?
       record.errors.add(attribute, :invalid) unless record.errors.of_kind?(attribute, :wrong_length)
     end
+  end
+
+private
+
+  def gtin_for(code, type: nil)
+    Barkick::GTIN.new(code, type: type)
   end
 end
