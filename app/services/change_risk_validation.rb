0295eb366fc18e1e601669e620aeb66176413ed3
kyle macpherson <kaimac2010@gmail.com>
2020-12-16 18:58:42 +0000

    Add notifications

--- app/services/change_risk_validation.rb
+++ app/services/change_risk_validation.rb
@@ -2,10 +2,9 @@ class ChangeRiskValidation
   include Interactor
   include EntitiesToNotify
 
-  delegate :investigation, :risk_validated_at, :risk_validated_by, :is_risk_validated, :risk_validation_change_rationale, :user, to: :context
+  delegate :investigation, :risk_validated_at, :risk_validated_by, :is_risk_validated, :risk_validation_change_rationale, :user, :change_action, to: :context
 
   def call
-    # we need risk_validation_change_rationale
     context.fail!(error: "No investigation supplied") unless investigation.is_a?(Investigation)
     context.fail!(error: "No user supplied") unless user.is_a?(User)
 
@@ -21,13 +20,14 @@ class ChangeRiskValidation
 
     context.changes_made = true
 
+    send_notification_email
+
     investigation
   end
 
 private
 
   def create_audit_activity_for_risk_validation_changed
-    # we need risk_validation_change_rationale
     metadata = activity_class.build_metadata(investigation, risk_validation_change_rationale)
 
     activity_class.create!(
@@ -46,8 +46,23 @@ private
   def assign_risk_validation_attributes
     if is_risk_validated
       investigation.assign_attributes(risk_validated_at: risk_validated_at, risk_validated_by: risk_validated_by)
+      context.change_action = "has been validated"
     else
       investigation.assign_attributes(risk_validated_at: nil, risk_validated_by: nil)
+      context.change_action = "has had validation removed"
+    end
+  end
+
+  def send_notification_email
+    email_recipients_for_team_with_access.each do |entity|
+      email = entity.is_a?(Team) ? entity.team_recipient_email : entity.email
+      NotifyMailer.risk_validation_updated(
+        email: email,
+        updater: user,
+        name: entity.name,
+        investigation: investigation,
+        action: change_action.to_s,
+      ).deliver_later
     end
   end
 end
