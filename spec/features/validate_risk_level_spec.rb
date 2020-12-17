86f8ea879055488e043b20f88c83595d44d4e6b8
kyle macpherson <kaimac2010@gmail.com>
2020-12-17 17:21:37 +0000

    Add tests for validation role

--- spec/features/validate_risk_level_spec.rb
+++ spec/features/validate_risk_level_spec.rb
@@ -5,125 +5,137 @@ RSpec.feature "Validate risk level", :with_stubbed_elasticsearch, :with_stubbed_
   let(:user) { create(:user, :activated) }
   let(:creator_user) { create(:user, :activated, team: user.team) }
 
-  before do
-    sign_in user
-    delivered_emails.clear
+  context "when user does not have `risk_level_validator` role" do
+    it "does not show the validate button" do
+      sign_in user
+      visit investigation_path(investigation)
+
+      expect(page).to_not have_link "Validate"
+    end
   end
 
-  scenario "validate the level" do
-    visit investigation_path(investigation)
-
-    validation_link = page.find(:css, "a[href='/cases/#{investigation.pretty_id}/validate-risk-level/edit']")
-
-    expect(validation_link.text).to eq "Validate"
-
-    click_link "Validate"
-
-    expect(page).to have_current_path("/cases/#{investigation.pretty_id}/validate-risk-level/edit")
-
-    within_fieldset("Have you validated the case risk level?") do
-      choose "Yes"
+  context "when user has `risk_level_validator` role" do
+    before do
+      user.user_roles.create!(name: "risk_level_validator")
+      sign_in user
+      delivered_emails.clear
     end
 
-    click_on "Continue"
+    scenario "validate the level" do
+      visit investigation_path(investigation)
 
-    expect(page).to have_current_path("/cases/#{investigation.pretty_id}")
-    expect(page).to have_content("Case risk level validated")
-    expect(page).to have_content("Validated by #{user.team.name} on #{investigation.risk_validated_at}")
-    expect(page).not_to have_link("Validate")
+      validation_link = page.find(:css, "a[href='/cases/#{investigation.pretty_id}/validate-risk-level/edit']")
 
-    click_on "Activity"
-    expect(page).to have_content "Case risk level validation added"
+      expect(validation_link.text).to eq "Validate"
 
-    expect_email_with_correct_details_to_be_set("has been validated")
-  end
+      click_link "Validate"
 
-  scenario "do not validate the level" do
-    visit investigation_path(investigation)
+      expect(page).to have_current_path("/cases/#{investigation.pretty_id}/validate-risk-level/edit")
 
-    validation_link = page.find(:css, "a[href='/cases/#{investigation.pretty_id}/validate-risk-level/edit']")
+      within_fieldset("Have you validated the case risk level?") do
+        choose "Yes"
+      end
 
-    expect(validation_link.text).to eq "Validate"
+      click_on "Continue"
 
-    click_link "Validate"
+      expect(page).to have_current_path("/cases/#{investigation.pretty_id}")
+      expect(page).to have_content("Case risk level validated")
+      expect(page).to have_content("Validated by #{user.team.name} on #{investigation.risk_validated_at}")
+      expect(page).not_to have_link("Validate")
 
-    expect(page).to have_current_path("/cases/#{investigation.pretty_id}/validate-risk-level/edit")
+      click_on "Activity"
+      expect(page).to have_content "Case risk level validation added"
 
-    within_fieldset("Have you validated the case risk level?") do
-      choose "No"
+      expect_email_with_correct_details_to_be_set("has been validated")
     end
 
-    click_on "Continue"
+    scenario "do not validate the level" do
+      visit investigation_path(investigation)
 
-    expect(page).to have_current_path("/cases/#{investigation.pretty_id}")
-    expect(page).not_to have_content("Case risk level validated")
-    expect(page).not_to have_content("Validated by #{user.team.name} on #{investigation.risk_validated_at}")
-    expect(page).to have_link("Validate")
+      validation_link = page.find(:css, "a[href='/cases/#{investigation.pretty_id}/validate-risk-level/edit']")
 
-    click_on "Activity"
-    expect(page).not_to have_content "Case risk level validation added"
+      expect(validation_link.text).to eq "Validate"
 
-    email = delivered_emails.last
+      click_link "Validate"
 
-    expect(email.action_name).not_to eq "risk_validation_updated"
-  end
+      expect(page).to have_current_path("/cases/#{investigation.pretty_id}/validate-risk-level/edit")
 
-  scenario "remove validation" do
-    visit("/cases/#{investigation.pretty_id}/validate-risk-level/edit")
+      within_fieldset("Have you validated the case risk level?") do
+        choose "No"
+      end
 
-    within_fieldset("Have you validated the case risk level?") do
-      choose "Yes"
+      click_on "Continue"
+
+      expect(page).to have_current_path("/cases/#{investigation.pretty_id}")
+      expect(page).not_to have_content("Case risk level validated")
+      expect(page).not_to have_content("Validated by #{user.team.name} on #{investigation.risk_validated_at}")
+      expect(page).to have_link("Validate")
+
+      click_on "Activity"
+      expect(page).not_to have_content "Case risk level validation added"
+
+      email = delivered_emails.last
+
+      expect(email.action_name).not_to eq "risk_validation_updated"
     end
 
-    click_on "Continue"
+    scenario "remove validation" do
+      visit("/cases/#{investigation.pretty_id}/validate-risk-level/edit")
 
-    expect(page).to have_current_path("/cases/#{investigation.pretty_id}")
+      within_fieldset("Have you validated the case risk level?") do
+        choose "Yes"
+      end
 
-    validation_link = page.find(:css, "a[href='/cases/#{investigation.pretty_id}/validate-risk-level/edit']")
-    expect(validation_link.text).to eq "Change"
+      click_on "Continue"
 
-    validation_link.click
+      expect(page).to have_current_path("/cases/#{investigation.pretty_id}")
 
-    within_fieldset("Have you validated the case risk level?") do
-      choose "No"
+      validation_link = page.find(:css, "a[href='/cases/#{investigation.pretty_id}/validate-risk-level/edit']")
+      expect(validation_link.text).to eq "Change"
+
+      validation_link.click
+
+      within_fieldset("Have you validated the case risk level?") do
+        choose "No"
+      end
+
+      click_on "Continue"
+
+      expect(page).to have_content("Enter details")
+
+      within_fieldset("Have you validated the case risk level?") do
+        choose "No"
+        fill_in "Why?", with: "Mistake made by team member"
+      end
+
+      click_on "Continue"
+
+      expect(page).to have_current_path("/cases/#{investigation.pretty_id}")
+      expect(page).not_to have_content("Case risk level validated")
+      expect(page).not_to have_content("Validated by #{user.team.name} on #{investigation.risk_validated_at}")
+      expect(page).to have_link("Validate")
+
+      click_on "Activity"
+      expect(page).to have_content "Case risk level validation removed"
+      expect(page).to have_content "Mistake made by team member"
+
+      expect_email_with_correct_details_to_be_set("has had validation removed")
     end
 
-    click_on "Continue"
+    def expect_email_with_correct_details_to_be_set(action)
+      email = delivered_emails.last
 
-    expect(page).to have_content("Enter details")
-
-    within_fieldset("Have you validated the case risk level?") do
-      choose "No"
-      fill_in "Why?", with: "Mistake made by team member"
+      expect(email.recipient).to eq creator_user.team.email
+      expect(email.action_name).to eq "risk_validation_updated"
+      expect(email.personalization).to include(
+        name: creator_user.team.name,
+        case_title: investigation.user_title,
+        case_type: "project",
+        case_id: investigation.pretty_id,
+        updater_name: user.name,
+        updater_team_name: user.team.name,
+        action: action
+      )
     end
-
-    click_on "Continue"
-
-    expect(page).to have_current_path("/cases/#{investigation.pretty_id}")
-    expect(page).not_to have_content("Case risk level validated")
-    expect(page).not_to have_content("Validated by #{user.team.name} on #{investigation.risk_validated_at}")
-    expect(page).to have_link("Validate")
-
-    click_on "Activity"
-    expect(page).to have_content "Case risk level validation removed"
-    expect(page).to have_content "Mistake made by team member"
-
-    expect_email_with_correct_details_to_be_set("has had validation removed")
-  end
-
-  def expect_email_with_correct_details_to_be_set(action)
-    email = delivered_emails.last
-
-    expect(email.recipient).to eq creator_user.team.email
-    expect(email.action_name).to eq "risk_validation_updated"
-    expect(email.personalization).to include(
-      name: creator_user.team.name,
-      case_title: investigation.user_title,
-      case_type: "project",
-      case_id: investigation.pretty_id,
-      updater_name: user.name,
-      updater_team_name: user.team.name,
-      action: action
-    )
   end
 end
