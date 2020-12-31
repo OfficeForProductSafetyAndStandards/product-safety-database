5a2a5d920bb030feb4c1031b8ce27d105ef495e1
Kyle MacPherson <kaimac2010@gmail.com>
2020-12-31 13:45:32 +0000

    Risk validation security fix (#1101)
    * Authorize users with risk_level_validation role for the update and error actions in risk_validations_controller
    
    * Use policy instead of user method
    
    Co-authored-by: Steven Lorek <steve@stevelorek.com>

---
+++ spec/requests/investigations/validate_risk_level_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.describe "Validate risk level", type: :request, with_stubbed_mailer: true, with_stubbed_elasticsearch: true do
+  let(:user) { create(:user, :activated) }
+  let(:other_user) { create(:user, :activated) }
+
+  let(:investigation) do
+    create(
+      :allegation,
+      is_closed: false,
+      creator: user
+    )
+  end
+
+  let(:params) do
+    {
+      investigation: { is_risk_validated: true }
+    }
+  end
+
+  context "when the user has the `risk_level_validator` role", :with_errors_rendered do
+    before do
+      other_user.user_roles.create!(name: "risk_level_validator")
+      sign_in other_user
+      put investigation_risk_validations_url(investigation.pretty_id), params: params
+    end
+
+    it "does not return a forbidden status code" do
+      expect(response).not_to have_http_status(:forbidden)
+    end
+  end
+
+  context "when the user does not have the `risk_level_validator` role", :with_errors_rendered do
+    before do
+      sign_in other_user
+      put investigation_risk_validations_url(investigation.pretty_id), params: params
+    end
+
+    it "returns a forbidden status code" do
+      expect(response).to have_http_status(:forbidden)
+    end
+  end
+end
