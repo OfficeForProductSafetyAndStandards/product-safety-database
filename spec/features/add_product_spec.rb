0eb2e4b779567fc324b5b2b93e0bbee7ca50af0e
Kyle MacPherson <kaimac2010@gmail.com>
2021-01-05 12:50:55 +0000

    Customs codes new (#1114)
    * Migration to add customs code field
    
    * Add customs code test
    
    * Allow adding of customs code to new product
    
    * Add test to ensure ability to edit product customs code
    
    * Linting
    
    * Add test for ts flow
    
    * Lint

--- spec/features/add_product_spec.rb
+++ spec/features/add_product_spec.rb
@@ -5,7 +5,8 @@ RSpec.feature "Adding a product", :with_stubbed_mailer, :with_stubbed_elasticsea
   let(:investigation) { create(:enquiry, creator: user) }
   let(:attributes)    do
     attributes_for(:product_iphone, authenticity: Product.authenticities.keys.without("missing").sample,
-                                    affected_units_status: Product.affected_units_statuses.keys.sample)
+                                    affected_units_status: Product.affected_units_statuses.keys.sample,
+                                    customs_code: "12345678, abc123, xyz987")
   end
   let(:other_user) { create(:user, :activated) }
 
@@ -42,6 +43,7 @@ RSpec.feature "Adding a product", :with_stubbed_mailer, :with_stubbed_elasticsea
     fill_in "Other product identifiers",         with: attributes[:product_code]
     fill_in "Batch number",                      with: attributes[:batch_number]
     fill_in "Webpage",                           with: attributes[:webpage]
+    fill_in "Customs code",                      with: attributes[:customs_code]
 
     within_fieldset("Was the product placed on the market before 1 January 2021?") do
       choose when_placed_on_market_answer(attributes[:when_placed_on_market])
@@ -93,6 +95,7 @@ RSpec.feature "Adding a product", :with_stubbed_mailer, :with_stubbed_elasticsea
     expect(page).to have_summary_item(key: "Country of origin",         value: attributes[:country])
     expect(page).to have_summary_item(key: "Description",               value: attributes[:description])
     expect(page).to have_summary_item(key: "When placed on market",     value: I18n.t(attributes[:when_placed_on_market], scope: Product.model_name.i18n_key))
+    expect(page).to have_summary_item(key: "Customs code",              value: attributes[:customs_code])
   end
 
   scenario "Not being able to add a product to another team’s case" do
