205bcb48edfe6b0317e1f3dabedce40b17757f46
StupidCodeFactory <ymarquet@gmail.com>
2021-01-11 13:34:52 +0100

    fix dodgy migration

--- db/migrate/20201222142417_add_has_online_recall_information_to_corrective_actions.rb
+++
@@ -1,7 +0,0 @@
-class AddHasOnlineRecallInformationToCorrectiveActions < ActiveRecord::Migration[6.0]
-  def change
-    safety_assured do
-      add_column :corrective_actions, :online_recall_information, :string
-    end
-  end
-end
