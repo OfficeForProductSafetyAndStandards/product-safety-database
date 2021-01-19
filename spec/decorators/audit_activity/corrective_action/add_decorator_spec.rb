07d397e1cd81be1e60e42b62dbfdde532d708b4b
StupidCodeFactory <ymarquet@gmail.com>
2021-01-19 19:11:32 +0100

    get image record from upates hash

--- spec/decorators/audit_activity/corrective_action/add_decorator_spec.rb
+++ spec/decorators/audit_activity/corrective_action/add_decorator_spec.rb
@@ -1,15 +1,18 @@
 require "rails_helper"
 
-RSpec.describe AuditActivity::CorrectiveAction::AddDecorator, :with_stubbed_elasticsearch, :with_stubbed_mailer do
+RSpec.describe AuditActivity::CorrectiveAction::AddDecorator, :with_stubbed_elasticsearch, :with_stubbed_mailer, :with_stubbed_antivirus do
+  include ActionDispatch::TestProcess::FixtureFile
+
   subject(:decorated_activity) do
     described_class.decorate(
       AuditActivity::CorrectiveAction::Base.new(
         business: business,
-        metadata: { corrective_action_id: corrective_action.id, updates: corrective_action.previous_changes }
+        metadata: { corrective_action_id: corrective_action.id, updates: changes }
       )
     )
   end
 
+  let(:changes)                       { corrective_action.previous_changes }
   let(:business)                      { create(:business) }
   let(:corrective_action)             { create(:corrective_action, business: business, has_online_recall_information: has_online_recall_information, online_recall_information: online_recall_information) }
   let(:online_recall_information)     { Faker::Internet.url(host: "example.com") }
@@ -37,6 +40,41 @@ RSpec.describe AuditActivity::CorrectiveAction::AddDecorator, :with_stubbed_elas
 
       specify do
         expect(decorated_activity.online_recall_information).to eq("Not relevant")
+      end
+    end
+  end
+
+  describe "#attachment and #attached_image" do
+    context "with an exisiting document id metadata" do
+      let(:blob) do
+        ActiveStorage::Blob.create_and_upload!(
+          io: document,
+          filename: Faker::Hipster.word,
+          content_type: "text/plain",
+          metadata: {}
+        )
+      end
+
+      let(:changes) do
+        corrective_action.previous_changes.merge("existing_document_id" => [nil, blob.signed_id])
+      end
+
+      context "when the document is not an image" do
+        let(:document) { fixture_file_upload("corrective_action.txt") }
+
+        it "returns the saved record in the update metadata", :aggregate_failures do
+          expect(decorated_activity.attachment).to eq(blob)
+          expect(decorated_activity).not_to be_attached_image
+        end
+      end
+
+      context "when the document is an image" do
+        let(:document) { fixture_file_upload("testImage.png") }
+
+        it "returns the saved record in the update metadata", :aggregate_failures do
+          expect(decorated_activity.attachment).to eq(blob)
+          expect(decorated_activity).to be_attached_image
+        end
       end
     end
   end
