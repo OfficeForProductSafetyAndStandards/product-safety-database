d16f25a438716307bb84e92d446cb26f357fca16
kyle macpherson <kaimac2010@gmail.com>
2021-02-08 17:05:18 +0000

    Add spec for accident incident type form

--- app/forms/accident_or_incident_type_form.rb
+++ app/forms/accident_or_incident_type_form.rb
@@ -5,5 +5,6 @@ class AccidentOrIncidentTypeForm
   attribute :event_type, :string, default: nil
 
   validates :event_type,
+            presence: true,
             inclusion: { in: AccidentOrIncident.event_types.values, message: "Select yes if you know when the accident or incident happened" }
 end
---
+++ spec/forms/accident_or_incident_type_form_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe AccidentOrIncidentTypeForm, :with_stubbed_elasticsearch, :with_test_queue_adapter do
+  # Default set of valid attributes
+  let(:event_type) { "accident" }
+  let(:params) do
+    {
+      event_type: event_type
+    }
+  end
+
+  let(:form) { described_class.new(params) }
+
+  describe "validations" do
+    context "with valid attributes" do
+      let(:event_type) { "accident" }
+      it "is valid" do
+        expect(form).to be_valid
+      end
+    end
+
+    context "if missing event_type" do
+      let(:event_type) { nil }
+      it "is not valid" do
+        expect(form).not_to be_valid
+      end
+    end
+
+    context "if event_type is not `accident` or `incident`" do
+      let(:event_type) { 'disaster' }
+      it "is not valid" do
+        expect(form).not_to be_valid
+      end
+    end
+  end
+end
