fe462689376562de01eb59cf677fc28edb4492f6
StupidCodeFactory <ymarquet@gmail.com>
2021-02-10 14:46:26 +0100

    reshuffle migration

--- db/migrate/20210202145809_migrate_audit_activities_corrective_action_add.rb
+++
@@ -1,8 +0,0 @@
-class MigrateAuditActivitiesCorrectiveActionAdd < ActiveRecord::Migration[6.1]
-  def change
-    AuditActivity::CorrectiveAction::Add.where(metadata: nil).find_each do |audit_activity|
-      audit_activity
-        .update!(metadata: AuditActivity::CorrectiveAction::Add.metadata_from_legacy_audit_activity(audit_activity))
-    end
-  end
-end
