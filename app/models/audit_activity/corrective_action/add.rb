d0cc237250e74b4aa00cb98d76a702847acc0119
Yann Marquet <100796+StupidCodeFactory@users.noreply.github.com>
2021-02-11 15:21:27 +0100

    migration to repopulate missing audit data (#1165)
    * migration to repopulate missing audit data
    
    * fix spec
    
    * fix specs
    
    * test migration path
    
    * reshuffle migration

--- app/models/audit_activity/corrective_action/add.rb
+++ app/models/audit_activity/corrective_action/add.rb
@@ -3,6 +3,43 @@ class AuditActivity::CorrectiveAction::Add < AuditActivity::CorrectiveAction::Ba
     { corrective_action: corrective_action.attributes, document: corrective_action.document_blob&.attributes }
   end
 
+  def self.possible_corrective_action_for(audit_activity)
+    if (corrective_action = audit_activity.corrective_action)
+      return corrective_action
+    end
+
+    corrective_actions =
+      CorrectiveAction
+        .where(investigation: audit_activity.investigation)
+        .joins(investigation: :activities)
+        .where.not(investigation: { activities: { type: AuditActivity::CorrectiveAction::Update.name } })
+        .where(investigation: { activities: { type: AuditActivity::CorrectiveAction::Add.name } })
+        .distinct
+
+    corrective_actions = corrective_actions.reject { |ca| ca.document.attached? }
+
+    if corrective_actions.one?
+      corrective_action = corrective_actions.first
+    else
+      raise AuditActivity::CorrectiveAction::CouldNotDeterminCorrectiveAction
+    end
+
+    corrective_action
+  end
+
+  def self.populate_missing_fields(metadata, audit_activity)
+    return unless (corrective_action = possible_corrective_action_for(audit_activity))
+
+    metadata[:corrective_action][:legislation]      ||= corrective_action.legislation
+    metadata[:corrective_action][:date_decided]     ||= corrective_action.date_decided
+    metadata[:corrective_action][:measure_type]     ||= corrective_action.measure_type
+    metadata[:corrective_action][:duration]         ||= corrective_action.duration
+    metadata[:corrective_action][:geographic_scope] ||= corrective_action.geographic_scope
+    metadata[:corrective_action][:id] = corrective_action.id
+
+    metadata
+  end
+
   def self.metadata_from_legacy_audit_activity(audit_activity)
     attributes = audit_activity.body.split("<br>").each_with_object(details: nil) do |fragment, corrective_action_attributes|
       next if fragment.empty?
@@ -13,11 +50,11 @@ class AuditActivity::CorrectiveAction::Add < AuditActivity::CorrectiveAction::Ba
       when /\ADate came into effect: \*\*(.*)\*\*\z/
         corrective_action_attributes[:date_decided] = Date.parse(Regexp.last_match(1))
       when /\AType of measure: \*\*(.*)\*\*\z/
-        corrective_action_attributes[:measure] = Regexp.last_match(1)
+        corrective_action_attributes[:measure_type] = Regexp.last_match(1)
       when /\ADuration of action: \*\*(.*)\*\*\z/
         corrective_action_attributes[:duration] = Regexp.last_match(1)
       when /\AGeographic scope: \*\*(.*)\*\*\z/
-        corrective_action_attributes[:geographic_scopes] = Regexp.last_match(1)
+        corrective_action_attributes[:geographic_scope] = Regexp.last_match(1)
       when /\AAttached|\AProduct|\ABusiness responsible:/
         next
       else
@@ -28,7 +65,7 @@ class AuditActivity::CorrectiveAction::Add < AuditActivity::CorrectiveAction::Ba
     metadata = { corrective_action: attributes }
     metadata[:document] = audit_activity.attachment.attributes if audit_activity.attachment.attached?
 
-    metadata
+    populate_missing_fields(metadata, audit_activity)
   end
 
   def email_update_text(viewer = nil)
