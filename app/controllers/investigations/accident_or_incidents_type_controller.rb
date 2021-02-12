e800fff5416ca06a88f3e0b9758f6ee6909e1b18
kyle macpherson <kaimac2010@gmail.com>
2021-02-12 14:47:50 +0000

    Prefill field on type page when user clicks back from main form

--- app/controllers/investigations/accident_or_incidents_type_controller.rb
+++ app/controllers/investigations/accident_or_incidents_type_controller.rb
@@ -2,21 +2,18 @@ module Investigations
   class AccidentOrIncidentsTypeController < ApplicationController
     def new
       authorize investigation, :update?
-      accident_or_incident_type_form
+      @accident_or_incident_type_form = AccidentOrIncidentTypeForm.new(type: params[:type])
     end
 
     def create
       authorize investigation, :update?
-      return render(:new) if accident_or_incident_type_form.invalid?
+      @accident_or_incident_type_form = AccidentOrIncidentTypeForm.new(type: type)
+      return render(:new) if @accident_or_incident_type_form.invalid?
 
       redirect_to new_investigation_accident_or_incident_path(@investigation, type: type)
     end
 
   private
-
-    def accident_or_incident_type_form
-      @accident_or_incident_type_form ||= AccidentOrIncidentTypeForm.new(type: type)
-    end
 
     def investigation
       @investigation ||= Investigation
