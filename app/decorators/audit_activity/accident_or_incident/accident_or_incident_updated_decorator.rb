327d23f45dc89388ea71524d67462b18725cf920
kyle macpherson <kaimac2010@gmail.com>
2021-02-15 19:39:42 +0000

    Linting fix

--- app/decorators/audit_activity/accident_or_incident/accident_or_incident_updated_decorator.rb
+++ app/decorators/audit_activity/accident_or_incident/accident_or_incident_updated_decorator.rb
@@ -53,7 +53,6 @@ class AuditActivity::AccidentOrIncident::AccidentOrIncidentUpdatedDecorator < Ac
   def updated_date_is_not_known?
     updated_date_is_not_known = metadata.dig("updates", "is_date_known", 1)
 
-
     ActiveModel::Type::Boolean.new.cast(updated_date_is_not_known) == false
   end
 end
