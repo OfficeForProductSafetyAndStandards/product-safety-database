da77ebda26e5d55ac5a7c86b56697d101a0fc033
StupidCodeFactory <ymarquet@gmail.com>
2021-02-16 15:58:56 +0100

    fix migration

--- db/migrate/20210216134638_migrate_corrective_action_update_audit_activity.rb
+++ db/migrate/20210216134638_migrate_corrective_action_update_audit_activity.rb
@@ -11,7 +11,7 @@ private
 
   def migrate_corrective_action_update_audit_activities
     CorrectiveAction.find_each do |corrective_action|
-      AuditActivity::CorrectiveAction::Update.where("metadata->>'corrective_action_id' = ?", corrective_action.id).order(created_at: :desc).find_each do |audit_activity|
+      AuditActivity::CorrectiveAction::Update.where("metadata->>'corrective_action_id' = ?", corrective_action.id.to_s).order(created_at: :desc).find_each do |audit_activity|
         AuditActivity::CorrectiveAction::Update.migrate_geographic_scope!(audit_activity)
       end
     end
