0351a278b653fa1e5ebabc8920b7bced923ee27b
kyle macpherson <kaimac2010@gmail.com>
2021-02-18 18:09:09 +0000

    Add tests

--- spec/features/add_test_results_spec.rb
+++ spec/features/add_test_results_spec.rb
@@ -8,8 +8,9 @@ RSpec.feature "Adding a test result", :with_stubbed_elasticsearch, :with_stubbed
   let(:file) { Rails.root + "test/fixtures/files/test_result.txt" }
   let(:other_user) { create(:user, :activated) }
   let(:legislation) { "General Product Safety Regulations 2005" }
+  let(:failure_details) { "Something went wrong" }
 
-  scenario "Adding a test result (with validation errors)" do
+  scenario "Adding a passing test result (with validation errors)" do
     travel_to Date.parse("2 April 2020") do
       sign_in(user)
       visit "/cases/#{investigation.pretty_id}/supporting-information"
@@ -71,6 +72,78 @@ RSpec.feature "Adding a test result", :with_stubbed_elasticsearch, :with_stubbed
     end
   end
 
+  scenario "Adding a failing test result (with validation errors)" do
+    travel_to Date.parse("2 April 2020") do
+      sign_in(user)
+      visit "/cases/#{investigation.pretty_id}/supporting-information"
+
+      click_link "Add supporting information"
+
+      expect_to_be_on_add_supporting_information_page
+
+      within_fieldset "What type of information are you adding?" do
+        page.choose "Test result"
+      end
+      click_button "Continue"
+
+      expect_to_be_on_record_test_result_page
+      expect_test_result_form_to_be_blank
+
+      click_button "Add test result"
+
+      errors_list = page.find(".govuk-error-summary__list").all("li")
+      expect(errors_list[0].text).to eq "Select the legislation that relates to this test"
+      expect(errors_list[1].text).to eq "Enter the standard the product was tested against"
+      expect(errors_list[2].text).to eq "Enter date of the test"
+      expect(errors_list[3].text).to eq "Select result of the test"
+      expect(errors_list[4].text).to eq "Provide the test results file"
+
+      within_fieldset "What was the result?" do
+        choose "Fail"
+      end
+
+      click_button "Add test result"
+
+      errors_list = page.find(".govuk-error-summary__list").all("li")
+      expect(errors_list[3].text).to eq "Enter details about how the product failed to meet the requirements"
+
+      fill_in "Further details", with: "Test result includes certificate of conformity"
+      fill_in_test_result_submit_form(legislation: "General Product Safety Regulations 2005", date: date, test_result: "Fail", failure_details: failure_details, file: file, standards: "EN71, EN73")
+
+      expect_confirmation_banner("Test result was successfully recorded.")
+      expect_page_to_have_h1("Supporting information")
+
+      click_link "Activity"
+
+      expect_to_be_on_case_activity_page(case_id: investigation.pretty_id)
+      expect(page).to have_text("Failed test: MyBrand washing machine")
+      expect(page).to have_link(product.name, href: product_path(product))
+      expect(page).to have_text("Legislation: General Product Safety Regulations 2005")
+      expect(page).to have_text("Standards: EN71, EN73")
+      expect(page).to have_text("Date of test: 1 January 2020")
+      expect(page).to have_text("Test result includes certificate of conformity")
+      expect(page).to have_link("test_result.txt")
+      expect(page).to have_link("View product details", href: product_path(product))
+
+      click_link "View test result"
+
+      expect_to_be_on_test_result_page(case_id: investigation.pretty_id)
+
+      expect(page).to have_summary_item(key: "Date of test", value: "1 January 2020")
+      expect(page).to have_summary_item(key: "Legislation", value: "General Product Safety Regulations 2005")
+      expect(page).to have_summary_item(key: "Standards", value: "EN71, EN73")
+      expect(page).to have_summary_item(key: "Result", value: "Failed")
+      expect(page).to have_summary_item(key: "Reason for failure", value: failure_details)
+      expect(page).to have_summary_item(key: "Further details", value: "Test result includes certificate of conformity")
+      expect(page).to have_summary_item(key: "Attachment description", value: "test result file")
+
+      expect(page).to have_text("test_result.txt")
+
+      visit "/cases/#{investigation.pretty_id}/test-results/new"
+      expect_test_result_form_to_be_blank
+    end
+  end
+
   scenario "Not being able to add test results to another team’s case" do
     sign_in(other_user)
     visit "/cases/#{investigation.pretty_id}/activity"
@@ -78,7 +151,7 @@ RSpec.feature "Adding a test result", :with_stubbed_elasticsearch, :with_stubbed
     expect(page).not_to have_link("Add supporting information")
   end
 
-  def fill_in_test_result_submit_form(legislation:, date:, test_result:, file:, standards:)
+  def fill_in_test_result_submit_form(legislation:, date:, test_result:, failure_details: nil, file:, standards:)
     select legislation, from: "Against which legislation?"
     fill_in "Day",   with: date.day if date
     fill_in "Month", with: date.month if date
@@ -86,6 +159,9 @@ RSpec.feature "Adding a test result", :with_stubbed_elasticsearch, :with_stubbed
     fill_in "Which standard was the product tested against?", with: standards
     within_fieldset "What was the result?" do
       choose test_result
+      if test_result == "Fail"
+        fill_in "How the product failed", with: failure_details
+      end
     end
     within_fieldset "Test report attachment" do
       attach_file file
--- spec/features/edit_test_result_spec.rb
+++ spec/features/edit_test_result_spec.rb
@@ -8,6 +8,8 @@ RSpec.feature "Editing a test result", :with_stubbed_elasticsearch, :with_stubbe
   let(:product2) { create(:product_washing_machine, name: "MyBrand blue washing machine") }
   let(:investigation) { create(:allegation, products: [product1, product2], creator: user) }
   let(:standards_product_was_tested_against) { %w[test] }
+  let(:failure_details) { "Something terrible happened" }
+  let(:result) { "passed" }
 
   let!(:test_result) do
     AddTestResultToInvestigation.call!(
@@ -16,7 +18,7 @@ RSpec.feature "Editing a test result", :with_stubbed_elasticsearch, :with_stubbe
       date: Date.parse("2019-05-01"),
       legislation: "General Product Safety Regulations 2005",
       details: "Provisional",
-      result: "passed",
+      result: result,
       product_id: product1.id,
       document: fixture_file_upload("test_result.txt"),
       standards_product_was_tested_against: standards_product_was_tested_against
@@ -38,7 +40,7 @@ RSpec.feature "Editing a test result", :with_stubbed_elasticsearch, :with_stubbe
     click_link "Edit test result"
   end
 
-  scenario "Editing a test result (with validation errors)" do
+  scenario "Editing a passed test result (with validation errors)" do
     go_edit_test_result
 
     # Check that form is pre-filled with existing values
@@ -108,6 +110,102 @@ RSpec.feature "Editing a test result", :with_stubbed_elasticsearch, :with_stubbe
     expect(activity_card_body).to have_text("Result: Passed")
     expect(activity_card_body).to have_text("Provisional")
     expect(activity_card_body).to have_text("Attached: test_result.txt")
+  end
+
+  context "with legacy test result" do
+    let(:standards_product_was_tested_against) { nil }
+
+    scenario "legacy test results with no standard tested against should enforce adding the standard" do
+      go_edit_test_result
+
+      click_button "Update test result"
+      expect(page).to have_error_summary "Enter the standard the product was tested against"
+    end
+  end
+
+  context "Editing a failed test result (with validation errors)" do
+    let(:result) { "failed" }
+    it 'handles old test results with no failure_details' do
+      sign_in(user)
+
+      visit "/cases/#{investigation.pretty_id}/test-results/#{test_result.id}"
+
+      expect(page).to have_summary_item(key: "Reason for failure", value: "Not provided")
+
+      sign_out
+
+      go_edit_test_result
+
+      # Check that form is pre-filled with existing values
+      expect(page).to have_field("Against which legislation?", with: "General Product Safety Regulations 2005")
+      within_fieldset "Date of test" do
+        expect(page).to have_field("Day", with: "1")
+        expect(page).to have_field("Month", with: "5")
+        expect(page).to have_field("Year", with: "2019")
+      end
+      within_fieldset "What was the result?" do
+        expect(page).to have_checked_field("Fail")
+      end
+      expect(page).to have_field("Further details", with: /\A\s*Provisional\z/)
+
+      expect(page).to have_text("Currently selected file: test_result.txt")
+
+      # Change some of the fields
+      select "Consumer Protection Act 1987", from: "Against which legislation?"
+      fill_in "Which standard was the product tested against?", with: "EN72,EN73"
+
+      within_fieldset "Date of test" do
+        fill_in "Day",   with: "2"
+        fill_in "Month", with: "6"
+      end
+
+      within_fieldset "What was the result?" do
+        choose "Fail"
+        fill_in "How the product failed", with: failure_details
+      end
+
+      fill_in "Further details", with: "Final result"
+
+      find("details > summary", text: "Replace this file").click
+
+      attach_file "Upload a file", Rails.root + "test/fixtures/files/test_result_2.txt"
+      fill_in "Attachment description", with: "Final test result certificate"
+
+      click_button "Update test result"
+
+      expect_to_be_on_test_result_page(case_id: investigation.pretty_id)
+
+      expect(page).to have_summary_item(key: "Date of test", value: "2 June 2019")
+      expect(page).to have_summary_item(key: "Legislation", value: "Consumer Protection Act 1987")
+      expect(page).to have_summary_item(key: "Standards", value: "EN72, EN73")
+      expect(page).to have_summary_item(key: "Result", value: "Failed")
+      expect(page).to have_summary_item(key: "Reason for failure", value: failure_details)
+      expect(page).to have_summary_item(key: "Further details", value: "Final result")
+      expect(page).to have_summary_item(key: "Attachment description", value: "Final test result certificate")
+
+      expect(page).to have_link("test_result_2.txt")
+
+      click_link "Back to #{investigation.decorate.pretty_description.downcase}"
+      click_link "Activity"
+
+      activity_card_body = page.find("p", text: "Edited by #{UserSource.new(user: investigation.creator_user).show(user)}").find(:xpath, "..")
+      expect(activity_card_body).to have_text("Date of test: 2 June 2019")
+      expect(activity_card_body).to have_text("Legislation: Consumer Protection Act 1987")
+      expect(activity_card_body).to have_text("Standards: EN72, EN73")
+      expect(activity_card_body).to have_text("Reason for failure: #{failure_details}")
+      expect(activity_card_body).to have_text("Further details: Final result")
+      expect(activity_card_body).to have_text("Attached: test_result_2.txt")
+      expect(activity_card_body).to have_text("Attachment description: Final test result certificate")
+
+      # The original result added activity should display the original values
+      activity_card_body = page.find("p", text: "Test result recorded by #{UserSource.new(user: investigation.creator_user).show(user)}").find(:xpath, "..")
+      expect(activity_card_body).to have_text("Date of test: 1 May 2019")
+      expect(activity_card_body).to have_text("Legislation: General Product Safety Regulations 2005")
+      expect(activity_card_body).to have_text("Standards: test")
+      expect(activity_card_body).to have_text("Result: Failed")
+      expect(activity_card_body).to have_text("Provisional")
+      expect(activity_card_body).to have_text("Attached: test_result.txt")
+    end
   end
 
   context "with legacy test result" do
