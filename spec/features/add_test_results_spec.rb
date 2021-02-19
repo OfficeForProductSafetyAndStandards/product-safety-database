028ce6924add8dc62ac4b00cb57db56ea56aca89
kyle macpherson <kaimac2010@gmail.com>
2021-02-19 15:37:48 +0000

    Refactor add spec

--- spec/features/add_test_results_spec.rb
+++ spec/features/add_test_results_spec.rb
@@ -29,12 +29,7 @@ RSpec.feature "Adding a test result", :with_stubbed_elasticsearch, :with_stubbed
 
       click_button "Add test result"
 
-      errors_list = page.find(".govuk-error-summary__list").all("li")
-      expect(errors_list[0].text).to eq "Select the legislation that relates to this test"
-      expect(errors_list[1].text).to eq "Enter the standard the product was tested against"
-      expect(errors_list[2].text).to eq "Enter date of the test"
-      expect(errors_list[3].text).to eq "Select result of the test"
-      expect(errors_list[4].text).to eq "Provide the test results file"
+      expect_full_error_list
 
       fill_in "Further details", with: "Test result includes certificate of conformity"
       fill_in_test_result_submit_form(legislation: "General Product Safety Regulations 2005", date: date, test_result: "Pass", file: file, standards: "EN71, EN73")
@@ -45,25 +40,13 @@ RSpec.feature "Adding a test result", :with_stubbed_elasticsearch, :with_stubbed
       click_link "Activity"
 
       expect_to_be_on_case_activity_page(case_id: investigation.pretty_id)
-      expect(page).to have_text("Passed test: MyBrand washing machine")
-      expect(page).to have_link(product.name, href: product_path(product))
-      expect(page).to have_text("Legislation: General Product Safety Regulations 2005")
-      expect(page).to have_text("Standards: EN71, EN73")
-      expect(page).to have_text("Date of test: 1 January 2020")
-      expect(page).to have_text("Test result includes certificate of conformity")
-      expect(page).to have_link("test_result.txt")
-      expect(page).to have_link("View product details", href: product_path(product))
+      expect_activity_page_to_show_created_test_result_values(result: "Passed")
 
       click_link "View test result"
 
       expect_to_be_on_test_result_page(case_id: investigation.pretty_id)
 
-      expect(page).to have_summary_item(key: "Date of test", value: "1 January 2020")
-      expect(page).to have_summary_item(key: "Legislation", value: "General Product Safety Regulations 2005")
-      expect(page).to have_summary_item(key: "Standards", value: "EN71, EN73")
-      expect(page).to have_summary_item(key: "Result", value: "Passed")
-      expect(page).to have_summary_item(key: "Further details", value: "Test result includes certificate of conformity")
-      expect(page).to have_summary_item(key: "Attachment description", value: "test result file")
+      expect_summary_to_reflect_values(result: "Passed")
 
       expect(page).to have_text("test_result.txt")
 
@@ -91,12 +74,7 @@ RSpec.feature "Adding a test result", :with_stubbed_elasticsearch, :with_stubbed
 
       click_button "Add test result"
 
-      errors_list = page.find(".govuk-error-summary__list").all("li")
-      expect(errors_list[0].text).to eq "Select the legislation that relates to this test"
-      expect(errors_list[1].text).to eq "Enter the standard the product was tested against"
-      expect(errors_list[2].text).to eq "Enter date of the test"
-      expect(errors_list[3].text).to eq "Select result of the test"
-      expect(errors_list[4].text).to eq "Provide the test results file"
+      expect_full_error_list
 
       within_fieldset "What was the result?" do
         choose "Fail"
@@ -116,26 +94,13 @@ RSpec.feature "Adding a test result", :with_stubbed_elasticsearch, :with_stubbed
       click_link "Activity"
 
       expect_to_be_on_case_activity_page(case_id: investigation.pretty_id)
-      expect(page).to have_text("Failed test: MyBrand washing machine")
-      expect(page).to have_link(product.name, href: product_path(product))
-      expect(page).to have_text("Legislation: General Product Safety Regulations 2005")
-      expect(page).to have_text("Standards: EN71, EN73")
-      expect(page).to have_text("Date of test: 1 January 2020")
-      expect(page).to have_text("Test result includes certificate of conformity")
-      expect(page).to have_link("test_result.txt")
-      expect(page).to have_link("View product details", href: product_path(product))
+      expect_activity_page_to_show_created_test_result_values(result: "Failed")
 
       click_link "View test result"
 
       expect_to_be_on_test_result_page(case_id: investigation.pretty_id)
 
-      expect(page).to have_summary_item(key: "Date of test", value: "1 January 2020")
-      expect(page).to have_summary_item(key: "Legislation", value: "General Product Safety Regulations 2005")
-      expect(page).to have_summary_item(key: "Standards", value: "EN71, EN73")
-      expect(page).to have_summary_item(key: "Result", value: "Failed")
-      expect(page).to have_summary_item(key: "Reason for failure", value: failure_details)
-      expect(page).to have_summary_item(key: "Further details", value: "Test result includes certificate of conformity")
-      expect(page).to have_summary_item(key: "Attachment description", value: "test result file")
+      expect_summary_to_reflect_values(result: "Failed")
 
       expect(page).to have_text("test_result.txt")
 
@@ -198,5 +163,34 @@ RSpec.feature "Adding a test result", :with_stubbed_elasticsearch, :with_stubbed
     within_fieldset "Test report attachment" do
       expect(page).to have_field("Attachment description", with: "\r\ntest result file")
     end
+  end
+
+  def expect_full_error_list
+    errors_list = page.find(".govuk-error-summary__list").all("li")
+    expect(errors_list[0].text).to eq "Select the legislation that relates to this test"
+    expect(errors_list[1].text).to eq "Enter the standard the product was tested against"
+    expect(errors_list[2].text).to eq "Enter date of the test"
+    expect(errors_list[3].text).to eq "Select result of the test"
+    expect(errors_list[4].text).to eq "Provide the test results file"
+  end
+
+  def expect_summary_to_reflect_values(result:)
+    expect(page).to have_summary_item(key: "Date of test", value: "1 January 2020")
+    expect(page).to have_summary_item(key: "Legislation", value: "General Product Safety Regulations 2005")
+    expect(page).to have_summary_item(key: "Standards", value: "EN71, EN73")
+    expect(page).to have_summary_item(key: "Result", value: result)
+    expect(page).to have_summary_item(key: "Further details", value: "Test result includes certificate of conformity")
+    expect(page).to have_summary_item(key: "Attachment description", value: "test result file")
+  end
+
+  def expect_activity_page_to_show_created_test_result_values(result:)
+    expect(page).to have_text("#{result} test: MyBrand washing machine")
+    expect(page).to have_link(product.name, href: product_path(product))
+    expect(page).to have_text("Legislation: General Product Safety Regulations 2005")
+    expect(page).to have_text("Standards: EN71, EN73")
+    expect(page).to have_text("Date of test: 1 January 2020")
+    expect(page).to have_text("Test result includes certificate of conformity")
+    expect(page).to have_link("test_result.txt")
+    expect(page).to have_link("View product details", href: product_path(product))
   end
 end
