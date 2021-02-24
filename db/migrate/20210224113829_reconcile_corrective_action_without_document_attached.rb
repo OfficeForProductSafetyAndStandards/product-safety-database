4ad101f444210be84aa01b686bd833cf377fd655
StupidCodeFactory <ymarquet@gmail.com>
2021-02-24 13:38:42 +0100

    reconcile audit an corrective action by created_at time

---
+++ db/migrate/20210224113829_reconcile_corrective_action_without_document_attached.rb
@@ -0,0 +1,26 @@
+class ReconcileCorrectiveActionWithoutDocumentAttached < ActiveRecord::Migration[6.1]
+  def change
+    safety_assured do
+      reversible do |dir|
+        dir.up do
+          wihtout_attachemnt = audit_activities.select { |a| a.investigation.corrective_actions.any? { |ca| !ca.document.attached? } }
+          wihtout_attachemnt.each do |audit_activity|
+            corrective_actions = audit_activity.investigation.corrective_actions.select { |ca| ca.document.attached? }.sort(&:created_at)
+            audits = audit_activity.investigation.where(type: AuditActivity::CorrectiveAction::Add.to_s).select { |a| a.corrective_action.nil? }.sort(&:created_at)
+            if corrective_actions.size != audits.size
+              Rails.logger.tagged(self.class.to_s) { "Eisenbug detected: for investigation: #{audit_activity.investigation_id}" }
+            else
+              audits.zip(corrective_actions).each { |corrective_action, audit| audit.metatada["corrective_action"]["id"] = corrective_action.id }
+            end
+          end
+        end
+      end
+    end
+  end
+
+private
+
+  def audit_activities
+    @audit_activities ||= AuditActivity::CorrectiveAction::Add.where("metadata->'corrective_action'->'id' IS NULL").load
+  end
+end
