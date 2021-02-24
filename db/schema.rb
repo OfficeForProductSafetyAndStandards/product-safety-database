9edf70f03f37bf0eb719a8472e4737c3700f35eb
Yann Marquet <100796+StupidCodeFactory@users.noreply.github.com>
2021-02-24 17:39:31 +0100

    reconcile audit an corrective action by created_at time (#1200)
    * reconcile audit an corrective action by created_at time
    
    * fix migration
    
    * fix rubocop offense
    
    * Update db/migrate/20210224113829_reconcile_corrective_action_without_document_attached.rb
    
    Co-authored-by: Steven Lorek <steve@stevelorek.com>
    
    Co-authored-by: Steven Lorek <steve@stevelorek.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_155926) do
+ActiveRecord::Schema.define(version: 2021_02_24_113829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
