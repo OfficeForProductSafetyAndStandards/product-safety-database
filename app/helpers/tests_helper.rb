890c615ab4df57ff4f9fa36966ef953986112dc7
kyle macpherson <kaimac2010@gmail.com>
2021-02-24 15:00:32 +0000

    Add value 'other' to result options

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -54,7 +54,8 @@ module TestsHelper
   def result_items(form)
     [
       { text: Test::Result.results["passed"], value: "passed" },
-      { text: Test::Result.results["failed"], value: "failed", conditional: { html: form.govuk_text_area(:failure_details, label: "How the product failed", hint: "Describe how the product was tested and how it failed to meet the requirements") } }
+      { text: Test::Result.results["failed"], value: "failed", conditional: { html: form.govuk_text_area(:failure_details, label: "How the product failed", hint: "Describe how the product was tested and how it failed to meet the requirements") } },
+      { text: Test::Result.results["other"], value: "other" }
     ]
   end
 end
