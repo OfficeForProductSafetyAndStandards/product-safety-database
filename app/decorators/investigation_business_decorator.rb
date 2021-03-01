eb4a64ecd491c63d6c0086a1a15b94573c591c6c
kyle macpherson <kaimac2010@gmail.com>
2021-03-01 16:41:47 +0000

    Use en.yml to store business types

---
+++ app/decorators/investigation_business_decorator.rb
@@ -0,0 +1,7 @@
+class InvestigationBusinessDecorator < ApplicationDecorator
+  delegate_all
+
+  def pretty_relationship
+    I18n.t(".business.type.#{relationship}", default: relationship.capitalize)
+  end
+end
