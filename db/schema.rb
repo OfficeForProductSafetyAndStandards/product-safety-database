b5fb5cb6bb629d1a08062496f58bad780c1e38e5
Yann Marquet <100796+StupidCodeFactory@users.noreply.github.com>
2021-03-10 13:27:02 +0100

    Populate audit decided date (#1232)
    * Fix corrective action added audit with empty decided date
    
    * Fix query for audit with empty decided_date
    
    * Update 20210310102912_fix_corrective_action_with_no_date_decided.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_114228) do
+ActiveRecord::Schema.define(version: 2021_03_10_102912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
