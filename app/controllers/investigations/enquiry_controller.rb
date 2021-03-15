1fc2f4670d3b4326c86dfda267fc861a3102601a
kyle macpherson <kaimac2010@gmail.com>
2021-03-15 19:36:30 +0000

    Remove set_notifying_country

--- app/controllers/investigations/enquiry_controller.rb
+++ app/controllers/investigations/enquiry_controller.rb
@@ -16,7 +16,6 @@ private
 
   def set_investigation
     @investigation = Investigation::Enquiry.new(investigation_params).build_owner_collaborations_from(current_user)
-    set_notifying_country
   end
 
   def assign_type
