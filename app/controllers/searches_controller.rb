cdedff86a7a71dc6ec2d5ffc89b3f29bc60452e7
StupidCodeFactory <ymarquet@gmail.com>
2021-03-15 17:01:38 +0100

    clean up session saving of search form

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,15 +2,15 @@ class SearchesController < ApplicationController
   include InvestigationsHelper
 
   def show
-    params_to_save = params.dup
-    params_to_save.delete(:sort_by) if params[:sort_by] == SearchParams::RELEVANT
-
     if params[:override_sort_by]
       params[:sort_by] = params[:override_sort_by]
     end
 
     @search = SearchParams.new(query_params)
-    session[:previous_search_params] = @search.serializable_hash
+
+    params_to_save = @search.serializable_hash
+    params_to_save.delete(:sort_by) if params[:sort_by] == SearchParams::RELEVANT
+    session[:previous_search_params] = params_to_save
 
     if @search.q.blank?
       redirect_to investigations_path(previous_search_params)
