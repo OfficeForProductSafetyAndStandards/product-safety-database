32d1661d434c332d9624efdbc64cf680438977be
kyle macpherson <kaimac2010@gmail.com>
2021-03-24 17:54:42 +0000

    Refactor feature specs

--- spec/features/change_safety_and_compliance_spec.rb
+++ spec/features/change_safety_and_compliance_spec.rb
@@ -5,25 +5,12 @@ RSpec.feature "Change safety and compliance details for a case", :with_stubbed_m
   let(:investigation)  { create(:allegation, creator: user, reported_reason: "unsafe_and_non_compliant", hazard_type: "Burns", hazard_description: "FIRE FIRE FIRE", non_compliant_reason: "Covered in petrol") }
 
   context "when user is allowed to edit the case" do
+    before do
+      sign_in_and_visit_investigation_page
+      visit_change_safety_and_compliance_page_and_expect_prefilled_values
+    end
     context "when the case is unsafe and non compliant" do
       it "allows user to change details and make the case safe but uncompliant" do
-        sign_in user
-        visit "/cases/#{investigation.pretty_id}"
-        expect(page.find("dt", text: "Reported as")).to have_sibling("dd", text: "Unsafe and non-compliant")
-        expect(page.find("dt", text: "Primary hazard")).to have_sibling("dd", text: investigation.hazard_type)
-        expect(page.find("dt", text: "Description of hazard")).to have_sibling("dd", text: investigation.hazard_description)
-        expect(page.find("dt", text: "Compliance")).to have_sibling("dd", text: investigation.non_compliant_reason)
-
-        click_link "Change hazard_type"
-
-        expect(page).to have_css("h1", text: "Why was the product reported?")
-        expect(page).to have_checked_field("It’s unsafe (or suspected to be)")
-        expect(page).to have_select("What is the primary hazard?", selected: investigation.hazard_type)
-        expect(page).to have_field("Why is the product unsafe?", with: "\r\nFIRE FIRE FIRE")
-        expect(page).to have_checked_field("It’s non-compliant (or suspected to be)")
-        expect(page).to have_field("Why is the product non-compliant?", with: "\r\nCovered in petrol")
-        expect(page).to have_unchecked_field("It’s safe and compliant")
-
         uncheck("It’s unsafe (or suspected to be)")
         fill_in("Why is the product non-compliant?", with: "No one really knows")
 
@@ -44,23 +31,6 @@ RSpec.feature "Change safety and compliance details for a case", :with_stubbed_m
       end
 
       it "allows user to change details and make the case unsafe but compliant" do
-        sign_in user
-        visit "/cases/#{investigation.pretty_id}"
-        expect(page.find("dt", text: "Reported as")).to have_sibling("dd", text: "Unsafe and non-compliant")
-        expect(page.find("dt", text: "Primary hazard")).to have_sibling("dd", text: investigation.hazard_type)
-        expect(page.find("dt", text: "Description of hazard")).to have_sibling("dd", text: investigation.hazard_description)
-        expect(page.find("dt", text: "Compliance")).to have_sibling("dd", text: investigation.non_compliant_reason)
-
-        click_link "Change hazard_type"
-
-        expect(page).to have_css("h1", text: "Why was the product reported?")
-        expect(page).to have_checked_field("It’s unsafe (or suspected to be)")
-        expect(page).to have_select("What is the primary hazard?", selected: investigation.hazard_type)
-        expect(page).to have_field("Why is the product unsafe?", with: "\r\nFIRE FIRE FIRE")
-        expect(page).to have_checked_field("It’s non-compliant (or suspected to be)")
-        expect(page).to have_field("Why is the product non-compliant?", with: "\r\nCovered in petrol")
-        expect(page).to have_unchecked_field("It’s safe and compliant")
-
         uncheck("It’s non-compliant (or suspected to be)")
         select "Cuts", from: "What is the primary hazard?"
         fill_in("Why is the product unsafe?", with: "Far too sharp")
@@ -78,28 +48,11 @@ RSpec.feature "Change safety and compliance details for a case", :with_stubbed_m
         expect(page).to have_css("h3", text: "Allegation changed")
         expect(page).to have_content("Changes:")
         expect(page).to have_content("Reported reason: Unsafe")
-        expect(page).to have_content("Primary hazard: Cuts")
+        expect(page).to have_content("Hazard type: Cuts")
         expect(page).to have_content("Hazard description: Far too sharp")
       end
 
       it "allows user to change details and make the case safe and compliant" do
-        sign_in user
-        visit "/cases/#{investigation.pretty_id}"
-        expect(page.find("dt", text: "Reported as")).to have_sibling("dd", text: "Unsafe and non-compliant")
-        expect(page.find("dt", text: "Primary hazard")).to have_sibling("dd", text: investigation.hazard_type)
-        expect(page.find("dt", text: "Description of hazard")).to have_sibling("dd", text: investigation.hazard_description)
-        expect(page.find("dt", text: "Compliance")).to have_sibling("dd", text: investigation.non_compliant_reason)
-
-        click_link "Change hazard_type"
-
-        expect(page).to have_css("h1", text: "Why was the product reported?")
-        expect(page).to have_checked_field("It’s unsafe (or suspected to be)")
-        expect(page).to have_select("What is the primary hazard?", selected: investigation.hazard_type)
-        expect(page).to have_field("Why is the product unsafe?", with: "\r\nFIRE FIRE FIRE")
-        expect(page).to have_checked_field("It’s non-compliant (or suspected to be)")
-        expect(page).to have_field("Why is the product non-compliant?", with: "\r\nCovered in petrol")
-        expect(page).to have_unchecked_field("It’s safe and compliant")
-
         uncheck("It’s non-compliant (or suspected to be)")
         uncheck("It’s unsafe (or suspected to be)")
         check("It’s safe and compliant")
@@ -117,6 +70,27 @@ RSpec.feature "Change safety and compliance details for a case", :with_stubbed_m
         expect(page).to have_css("h3", text: "Allegation changed")
         expect(page).to have_content("Changes:")
         expect(page).to have_content("Reported reason: Safe And Compliant")
+      end
+
+      def sign_in_and_visit_investigation_page
+        sign_in user
+        visit "/cases/#{investigation.pretty_id}"
+        expect(page.find("dt", text: "Reported as")).to have_sibling("dd", text: "Unsafe and non-compliant")
+        expect(page.find("dt", text: "Primary hazard")).to have_sibling("dd", text: investigation.hazard_type)
+        expect(page.find("dt", text: "Description of hazard")).to have_sibling("dd", text: investigation.hazard_description)
+        expect(page.find("dt", text: "Compliance")).to have_sibling("dd", text: investigation.non_compliant_reason)
+      end
+
+      def visit_change_safety_and_compliance_page_and_expect_prefilled_values
+        click_link "Change hazard_type"
+
+        expect(page).to have_css("h1", text: "Why was the product reported?")
+        expect(page).to have_checked_field("It’s unsafe (or suspected to be)")
+        expect(page).to have_select("What is the primary hazard?", selected: investigation.hazard_type)
+        expect(page).to have_field("Why is the product unsafe?", with: "\r\nFIRE FIRE FIRE")
+        expect(page).to have_checked_field("It’s non-compliant (or suspected to be)")
+        expect(page).to have_field("Why is the product non-compliant?", with: "\r\nCovered in petrol")
+        expect(page).to have_unchecked_field("It’s safe and compliant")
       end
     end
   end
