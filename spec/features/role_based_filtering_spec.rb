756525984b9b4ab1e56d7210e69c3edefe202e92
kyle macpherson <kaimac2010@gmail.com>
2021-03-31 17:06:06 +0100

    Add further test

--- spec/features/role_based_filtering_spec.rb
+++ spec/features/role_based_filtering_spec.rb
@@ -2,28 +2,49 @@ require "rails_helper"
 
 RSpec.feature "List cases based on role", :with_elasticsearch, :with_stubbed_mailer do
   let(:user) { create(:user, :activated, :crown_dependency, has_viewed_introduction: true, team: french_team) }
+  let(:opss_user) { create(:user, :activated, has_viewed_introduction: true, team: french_team) }
   let(:french_team) { create(:team, country: "country:FR") }
 
-  let!(:gb_case)               { create(:allegation, notifying_country: "country:GB") }
-  let!(:england_case)          { create(:allegation, notifying_country: "country:GB-ENG") }
-  let!(:scotland_case)         { create(:allegation, notifying_country: "country:GB-SCT") }
-  let!(:wales_case)            { create(:allegation, notifying_country: "country:GB-WLS") }
-  let!(:northern_ireland_case) { create(:allegation, notifying_country: "country:GB-NIR") }
+  let!(:gb_case)               { create(:allegation, notifying_country: "country:GB", creator: opss_user) }
+  let!(:england_case)          { create(:allegation, notifying_country: "country:GB-ENG", creator: opss_user) }
+  let!(:scotland_case)         { create(:allegation, notifying_country: "country:GB-SCT", creator: opss_user) }
+  let!(:wales_case)            { create(:allegation, notifying_country: "country:GB-WLS", creator: opss_user) }
+  let!(:northern_ireland_case) { create(:allegation, notifying_country: "country:GB-NIR", creator: opss_user) }
 
   let!(:french_case) { create(:allegation, notifying_country: "country:FR", creator: user) }
 
-  before { sign_in user }
 
-  scenario "should not see CROWN_DEPENDENCIES_HIDDEN_NOTIFYING_COUNTRY" do
-    click_link "All cases"
+  context "when user has crown_dependcy role" do
 
-    expect(page).to have_listed_case(french_case.pretty_id)
+    before { sign_in user }
 
-    save_and_open_page
-    expect(page).not_to have_listed_case(gb_case.pretty_id)
-    expect(page).not_to have_listed_case(england_case.pretty_id)
-    expect(page).not_to have_listed_case(scotland_case.pretty_id)
-    expect(page).not_to have_listed_case(wales_case.pretty_id)
-    expect(page).not_to have_listed_case(northern_ireland_case.pretty_id)
+    scenario "should not see CROWN_DEPENDENCIES_HIDDEN_NOTIFYING_COUNTRY" do
+      click_link "All cases"
+
+      expect(page).to have_listed_case(french_case.pretty_id)
+
+      expect(page).not_to have_listed_case(gb_case.pretty_id)
+      expect(page).not_to have_listed_case(england_case.pretty_id)
+      expect(page).not_to have_listed_case(scotland_case.pretty_id)
+      expect(page).not_to have_listed_case(wales_case.pretty_id)
+      expect(page).not_to have_listed_case(northern_ireland_case.pretty_id)
+    end
+  end
+
+  context "when user has opss role" do
+
+    before { sign_in opss_user }
+
+    scenario "should see CROWN_DEPENDENCIES_HIDDEN_NOTIFYING_COUNTRY" do
+      click_link "All cases"
+
+      expect(page).not_to have_listed_case(french_case.pretty_id)
+
+      expect(page).to have_listed_case(gb_case.pretty_id)
+      expect(page).to have_listed_case(england_case.pretty_id)
+      expect(page).to have_listed_case(scotland_case.pretty_id)
+      expect(page).to have_listed_case(wales_case.pretty_id)
+      expect(page).to have_listed_case(northern_ireland_case.pretty_id)
+    end
   end
 end
