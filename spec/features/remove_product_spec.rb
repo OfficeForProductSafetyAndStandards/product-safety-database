dc345f21b0f4fed8004019f119972fceb7e71ff6
kyle macpherson <kaimac2010@gmail.com>
2021-04-07 11:08:20 +0100

    Add tests

---
+++ spec/features/remove_product_spec.rb
@@ -0,0 +1,60 @@
+require "rails_helper"
+
+RSpec.feature "Remove product from investigation", :with_stubbed_elasticsearch, :with_stubbed_antivirus, :with_stubbed_mailer, type: :feature do
+  let(:user)           { create(:user, :activated) }
+  let(:investigation)  { create(:enquiry, :with_products, creator: user) }
+  let(:removal_reason) { "I made a mistake" }
+  let(:product)        { investigation.products.first }
+
+  context "Product does not have any linked supporting information" do
+    it "allows user to remove product from investigation" do
+      sign_in user
+      visit "/cases/#{investigation.pretty_id}/products"
+      click_link "Remove product"
+
+      expect(page).to have_content "Remove #{product.name}"
+
+      click_on "Submit"
+
+      expect(page).to have_error_messages
+
+      errors_list = page.find(".govuk-error-summary__list").all("li")
+      expect(errors_list[0].text).to eq "Select yes if you want to remove the product from the case"
+
+      choose("Yes")
+      click_on "Submit"
+
+      expect(page).to have_error_messages
+      errors_list = page.find(".govuk-error-summary__list").all("li")
+      expect(errors_list[0].text).to eq "Reason cannot be blank"
+
+      find("#reason").set(removal_reason)
+      click_on "Submit"
+
+      expect(page).to have_content "Product was successfully removed."
+      expect_to_be_on_investigation_products_page(case_id: investigation.pretty_id)
+
+      click_link "Activity"
+      expect_to_be_on_case_activity_page(case_id: investigation.pretty_id)
+      expect(page).to have_content("#{product.name} removed")
+      expect(page).to have_content(removal_reason)
+    end
+  end
+
+  context "Product has linked supporting information" do
+    before do
+      create(:accident, product: product)
+      create(:risk_assessment, products: [product])
+      create(:corrective_action, product: product)
+    end
+
+    it "does not allow user to remove product from investigation" do
+      sign_in user
+      visit "/cases/#{investigation.pretty_id}/products"
+      click_link "Remove product"
+
+      expect(page).not_to have_content "Remove #{product.name}"
+      expect(page).to have_content "Cannot remove product from the case because it's associated with following supporting information"
+    end
+  end
+end
