3fe1510c866462007f218df3d3861234f873830d
kyle macpherson <kaimac2010@gmail.com>
2021-05-13 17:38:28 +0100

    Fix display name bug

--- app/helpers/ownership_helper.rb
+++ app/helpers/ownership_helper.rb
@@ -5,12 +5,12 @@ module OwnershipHelper
     items << { text: investigation.owner.decorate.display_name(viewer: current_user), value: investigation.owner.id, checked: true }
 
     unless investigation.owner == current_user
-      items << { text: current_user.display_name(viewer: current_user), value: current_user.id, checked: false }
+      items << { text: current_user.decorate.display_name(viewer: current_user), value: current_user.id, checked: false }
     end
 
     items << { text: "Someone else in your team", value: "someone_else_in_your_team", conditional: { html: someone_else_in_your_team_dropdown } }
 
-    items << { text: current_user.team.display_name(viewer: current_user), value: current_user.team.id, checked: false }
+    items << { text: current_user.team.descorate.display_name(viewer: current_user), value: current_user.team.id, checked: false }
 
     items
   end
