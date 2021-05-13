cc1fe447a254f250e7f5e94241cdfc484ea99540
kyle macpherson <kaimac2010@gmail.com>
2021-05-13 17:20:34 +0100

    Lint

--- app/helpers/ownership_helper.rb
+++ app/helpers/ownership_helper.rb
@@ -1,6 +1,6 @@
 module OwnershipHelper
   def add_your_team_values(items, investigation, someone_else_in_your_team_dropdown)
-    items << { divider: "Your team"}
+    items << { divider: "Your team" }
 
     items << { text: investigation.owner.decorate.display_name(viewer: current_user), value: investigation.owner.id, checked: true }
 
@@ -8,7 +8,7 @@ module OwnershipHelper
       items << { text: current_user.display_name(viewer: current_user), value: current_user.id, checked: false }
     end
 
-    items << { text: "Someone else in your team", value: "someone_else_in_your_team", conditional: { html: someone_else_in_your_team_dropdown }}
+    items << { text: "Someone else in your team", value: "someone_else_in_your_team", conditional: { html: someone_else_in_your_team_dropdown } }
 
     items << { text: current_user.team.display_name(viewer: current_user), value: current_user.team.id, checked: false }
 
