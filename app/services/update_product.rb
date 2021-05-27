f58e61e9c956fb5f4e751b2f96898323ea060e7c
kyle macpherson <kaimac2010@gmail.com>
2021-05-27 15:25:22 +0100

    Add more clear error message

--- app/services/update_product.rb
+++ app/services/update_product.rb
@@ -5,7 +5,7 @@ class UpdateProduct
 
   def call
     context.fail!(error: "No product supplied") unless product.is_a?(Product)
-    context.fail!(error: "No product supplied") unless product_params.is_a?(Hash)
+    context.fail!(error: "No product params supplied") unless product_params.is_a?(Hash)
 
     Product.transaction do
       product.update!(product_params)
