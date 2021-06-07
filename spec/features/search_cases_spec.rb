936d24e8e7f825a465e9967b074bc45c5412fc9a
kyle macpherson <kaimac2010@gmail.com>
2021-06-08 00:48:50 +0100

    Add test coverage

--- spec/features/search_cases_spec.rb
+++ spec/features/search_cases_spec.rb
@@ -16,7 +16,29 @@ RSpec.feature "Searching cases", :with_elasticsearch, :with_stubbed_mailer, type
            name: "T12 mobile phone",
            category: "consumer electronics")
   end
+
+  let(:mobilz_phont) do
+    create(:product,
+           name: "T12 mobilz phont",
+           category: "consumer electronics")
+  end
+
+  let(:thirteenproduct) do
+    create(:product,
+           name: "thirteenproduct",
+           category: "consumer electronics")
+  end
+
+  let(:eeirteenproduct) do
+    create(:product,
+           name: "eeirteenproduct",
+           category: "consumer electronics")
+  end
+
   let!(:mobile_phone_investigation) { create(:allegation, products: [mobile_phone]) }
+  let!(:mobilz_phont_investigation) { create(:allegation, products: [mobilz_phont]) }
+  let!(:thirteenproduct_investigation) { create(:allegation, products: [thirteenproduct]) }
+  let!(:eeirteenproduct_investigation) { create(:allegation, products: [eeirteenproduct]) }
 
   before do
     # Import products syncronously into ElasticSearch
@@ -83,5 +105,87 @@ RSpec.feature "Searching cases", :with_elasticsearch, :with_stubbed_mailer, type
 
     expect(page).to have_text(mobile_phone_investigation.pretty_id)
     expect(page).to have_text("T12 mobile phone")
+  end
+
+  context "fuzzy matching" do
+    it "does not allow any edits for words less than 6 letters long" do
+      sign_in(user)
+      visit "/cases"
+
+      fill_in "Keywords", with: "phone"
+      click_button "Search"
+
+      expect_to_be_on_cases_search_results_page(search_term: "phone")
+
+      expect(page).to have_text(mobile_phone_investigation.pretty_id)
+      expect(page).to have_text("T12 mobile phone")
+
+      expect(page).not_to have_text(mobilz_phont_investigation.pretty_id)
+      expect(page).not_to have_text("T12 mobilz phont")
+    end
+
+    it "allows 1 edit for words more than 6 letters but less than 13 long" do
+      sign_in(user)
+      visit "/cases"
+
+      fill_in "Keywords", with: "mobile"
+      click_button "Search"
+
+      expect_to_be_on_cases_search_results_page(search_term: "mobile")
+
+      expect(page).to have_text(mobile_phone_investigation.pretty_id)
+      expect(page).to have_text("T12 mobile phone")
+
+      expect(page).to have_text(mobilz_phont_investigation.pretty_id)
+      expect(page).to have_text("T12 mobilz phont")
+    end
+
+    it "does not allow 2 edits for words more than 6 letters but less than 13 long" do
+      sign_in(user)
+      visit "/cases"
+
+      fill_in "Keywords", with: "mobiee"
+      click_button "Search"
+
+      expect_to_be_on_cases_search_results_page(search_term: "mobiee")
+
+      expect(page).to have_text(mobile_phone_investigation.pretty_id)
+      expect(page).to have_text("T12 mobile phone")
+
+      expect(page).not_to have_text(mobilz_phont_investigation.pretty_id)
+      expect(page).not_to have_text("T12 mobilz phont")
+    end
+
+    it "allows 2 edits for words more than 12 long" do
+      sign_in(user)
+      visit "/cases"
+
+      fill_in "Keywords", with: "thirteenproduct"
+      click_button "Search"
+
+      expect_to_be_on_cases_search_results_page(search_term: "thirteenproduct")
+
+      expect(page).to have_text(thirteenproduct_investigation.pretty_id)
+      expect(page).to have_text("thirteenproduct")
+
+      expect(page).to have_text(eeirteenproduct_investigation.pretty_id)
+      expect(page).to have_text("eeirteenproduct")
+    end
+
+    it "does not allow 3 edits for words more than 12 long" do
+      sign_in(user)
+      visit "/cases"
+
+      fill_in "Keywords", with: "thirteenproduce"
+      click_button "Search"
+
+      expect_to_be_on_cases_search_results_page(search_term: "thirteenproduce")
+
+      expect(page).to have_text(thirteenproduct_investigation.pretty_id)
+      expect(page).to have_text("thirteenproduct")
+
+      expect(page).not_to have_text(eeirteenproduct_investigation.pretty_id)
+      expect(page).not_to have_text("eeirteenproduct")
+    end
   end
 end
