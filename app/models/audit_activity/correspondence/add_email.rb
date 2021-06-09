00e54a1a44cb752c8fe9fe47556cc77d9921c13e
kyle macpherson <kaimac2010@gmail.com>
2021-06-09 13:14:54 +0100

    Refactor to remove activity notification

--- app/models/audit_activity/correspondence/add_email.rb
+++ app/models/audit_activity/correspondence/add_email.rb
@@ -1,74 +1,7 @@
 class AuditActivity::Correspondence::AddEmail < AuditActivity::Correspondence::Base
   belongs_to :correspondence, class_name: "Correspondence::Email"
   include ActivityAttachable
-  include ActivityNotification
   with_attachments email_file: "email file", email_attachment: "email attachment"
-
-  def self.from(correspondence, investigation)
-    activity = create!(
-      body: build_body(correspondence) || sanitize_text(correspondence.details),
-      source: UserSource.new(user: User.current),
-      investigation: investigation,
-      title: correspondence.overview,
-      correspondence: correspondence
-    )
-
-    activity.attach_blob(correspondence.email_file.blob, :email_file) if correspondence.email_file.attached?
-    activity.attach_blob(correspondence.email_attachment.blob, :email_attachment) if correspondence.email_attachment.attached?
-  end
-
-  def self.build_body(correspondence)
-    body = ""
-    body += build_correspondent_details correspondence
-    body += "Subject: **#{sanitize_text correspondence.email_subject}**<br>" if correspondence.email_subject.present?
-    body += "Date sent: **#{correspondence.correspondence_date.strftime('%d/%m/%Y')}**<br>" if correspondence.correspondence_date.present?
-    body += build_email_file_body correspondence
-    body += build_attachment_body correspondence
-    body += "<br>#{sanitize_text correspondence.details}" if correspondence.details.present?
-    body
-  end
-
-  def self.build_correspondent_details(correspondence)
-    return "" unless correspondence.correspondent_name || correspondence.email_address
-
-    output = ""
-    output += "#{Correspondence::Email.email_directions[correspondence.email_direction]}: " if correspondence.email_direction.present?
-    output += "**#{sanitize_text correspondence.correspondent_name}** " if correspondence.correspondent_name.present?
-    output += build_email_address correspondence if correspondence.email_address.present?
-    output
-  end
-
-  def self.build_email_file_body(correspondence)
-    file = correspondence.email_file
-    file.attached? ? "Email: #{sanitize_text file.filename}<br>" : ""
-  end
-
-  def self.build_attachment_body(correspondence)
-    file = correspondence.email_attachment
-    file.attached? ? "Attached: #{sanitize_text file.filename}<br>" : ""
-  end
-
-  def self.build_email_address(correspondence)
-    output = ""
-    output += "(" if correspondence.correspondent_name.present?
-    output += sanitize_text correspondence.email_address
-    output += ")" if correspondence.correspondent_name.present?
-    "#{output}<br>"
-  end
-
-  def restricted_title(_user)
-    "Email added"
-  end
-
-  def activity_type
-    "email"
-  end
-
-  def email_update_text(viewer = nil)
-    "Email details added to the #{investigation.case_type.upcase_first} by #{source&.show(viewer)}."
-  end
-
-private
 
   def subtitle_slug
     "Email recorded"
--- app/services/add_email_to_case.rb
+++ app/services/add_email_to_case.rb
@@ -1,5 +1,6 @@
 class AddEmailToCase
   include Interactor
+  include EntitiesToNotify
 
   delegate :investigation, :user, :email, :correspondence_date, :correspondent_name, :email_address, :email_direction, :overview, :details, :email_subject, :email_file, :email_attachment, :attachment_description, to: :context
 
@@ -23,14 +24,94 @@ class AddEmailToCase
       update_attachment_description!
     end
 
+    create_audit_activity(email, investigation)
+
+    send_notification_email(investigation, user)
+
     # TODO: refactor into this class
-    AuditActivity::Correspondence::AddEmail.from(email, investigation)
+    # AuditActivity::Correspondence::AddEmail.from(email, investigation)
   end
 
 private
 
+  def create_audit_activity(correspondence, investigation)
+    activity = AuditActivity::Correspondence::AddEmail.create!(
+      body: build_body(correspondence) || sanitize_text(correspondence.details),
+      source: UserSource.new(user: User.current),
+      investigation: investigation,
+      title: correspondence.overview,
+      correspondence: correspondence
+    )
+
+    activity.attach_blob(correspondence.email_file.blob, :email_file) if correspondence.email_file.attached?
+    activity.attach_blob(correspondence.email_attachment.blob, :email_attachment) if correspondence.email_attachment.attached?
+  end
+
+  def build_body(correspondence)
+    body = ""
+    body += build_correspondent_details correspondence
+    body += "Subject: **#{sanitize_text correspondence.email_subject}**<br>" if correspondence.email_subject.present?
+    body += "Date sent: **#{correspondence.correspondence_date.strftime('%d/%m/%Y')}**<br>" if correspondence.correspondence_date.present?
+    body += build_email_file_body correspondence
+    body += build_attachment_body correspondence
+    body += "<br>#{sanitize_text correspondence.details}" if correspondence.details.present?
+    body
+  end
+
+  def build_correspondent_details(correspondence)
+    return "" unless correspondence.correspondent_name || correspondence.email_address
+
+    output = ""
+    output += "#{Correspondence::Email.email_directions[correspondence.email_direction]}: " if correspondence.email_direction.present?
+    output += "**#{sanitize_text correspondence.correspondent_name}** " if correspondence.correspondent_name.present?
+    output += build_email_address correspondence if correspondence.email_address.present?
+    output
+  end
+
+  def build_email_file_body(correspondence)
+    file = correspondence.email_file
+    file.attached? ? "Email: #{sanitize_text file.filename}<br>" : ""
+  end
+
+  def build_attachment_body(correspondence)
+    file = correspondence.email_attachment
+    file.attached? ? "Attached: #{sanitize_text file.filename}<br>" : ""
+  end
+
+  def build_email_address(correspondence)
+    output = ""
+    output += "(" if correspondence.correspondent_name.present?
+    output += sanitize_text correspondence.email_address
+    output += ")" if correspondence.correspondent_name.present?
+    output + "<br>"
+  end
+
+  def sanitize_text(text)
+    return text.to_s.strip.gsub(/[*_~]/) { |match| "\\#{match}" } if text
+  end
+
   def update_attachment_description!
     context.email.email_attachment.blob.metadata[:description] = attachment_description
     context.email.email_attachment.blob.save!
+  end
+
+  def source
+    UserSource.new(user: user)
+  end
+
+  def send_notification_email(investigation, _user)
+    email_recipients_for_case_owner.each do |recipient|
+      NotifyMailer.investigation_updated(
+        investigation.pretty_id,
+        recipient.name,
+        recipient.email,
+        email_update_text(recipient),
+        email_subject
+      ).deliver_later
+    end
+  end
+
+  def email_update_text(viewer = nil)
+    "Email details added to the #{investigation.case_type.upcase_first} by #{source&.show(viewer)}."
   end
 end
