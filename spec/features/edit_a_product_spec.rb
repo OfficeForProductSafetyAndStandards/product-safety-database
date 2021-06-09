5578e234a05036dfe73ea77c3f05d7079c762238
kyle macpherson <kaimac2010@gmail.com>
2021-06-09 15:16:27 +0100

    Add sleep explanation

--- spec/features/edit_a_product_spec.rb
+++ spec/features/edit_a_product_spec.rb
@@ -154,6 +154,7 @@ RSpec.feature "Editing a product", :with_elasticsearch, :with_stubbed_mailer, :w
     within("header") { click_on "Cases" }
 
     fill_in "Keywords", with: new_name
+    # sleep in order to wait for the changes to be indexed
     sleep 1
     click_on "Apply filters"
 
