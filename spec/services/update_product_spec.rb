b2369c52107430dfdd1b431d2598fd30bca08e35
Yann Marquet <100796+StupidCodeFactory@users.noreply.github.com>
2021-06-10 10:56:00 +0100

    Improve searchability re index when product is updated (#1403)
    * feature spec to ensure updated product are searchable via the investigation
    
    * add service class to perform database update and ES index
    
    * Fix tests
    
    * Use rubocops preferred methods
    
    * Do not pass decorated product to updateproduct service
    
    * Add more clear error message
    
    * Fix test
    
    * Add sleep to allow time to index changes in test
    
    * Add sleep explanation
    
    Co-authored-by: kyle macpherson <kaimac2010@gmail.com>

---
+++ spec/services/update_product_spec.rb
@@ -0,0 +1,48 @@
+require "rails_helper"
+
+RSpec.describe UpdateProduct, :with_elasticsearch, :with_stubbed_mailer do
+  subject(:result) { described_class.call(product: product, product_params: product_params) }
+
+  let(:investigation)  { create(:allegation) }
+  let(:product)        { create(:product, investigations: [investigation]) }
+  let(:product_params) { attributes_for(:product) }
+
+  describe "#call" do
+    context "without a product" do
+      let(:product) { nil }
+
+      it { is_expected.to be_a_failure }
+    end
+
+    context "without product_params" do
+      let(:product_params) { nil }
+
+      it { is_expected.to be_a_failure }
+    end
+
+    context "with all the required parameters" do
+      let(:perform_product_search) { Product.full_search(ElasticsearchQuery.new(product_params[:name], {}, {})) }
+
+      before do
+        allow(product.__elasticsearch__).to receive(:update_document)
+        allow(product.investigations).to receive(:import)
+      end
+
+      it "updates the product", :aggregate_failures do
+        expect(result).to be_a_success
+
+        expect(product.reload).to have_attributes(product_params)
+      end
+
+      it "reindexes the product" do
+        result
+        expect(product.__elasticsearch__).to have_received(:update_document)
+      end
+
+      it "reindexes the product's investigations" do
+        result
+        expect(product.investigations).to have_received(:import)
+      end
+    end
+  end
+end
