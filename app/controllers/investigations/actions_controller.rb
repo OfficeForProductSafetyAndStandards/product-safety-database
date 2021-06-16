3972a082d006f4312b90b578211f8c97ad308284
kyle macpherson <kaimac2010@gmail.com>
2021-06-16 17:56:52 +0100

    Fixing tests

--- app/controllers/investigations/actions_controller.rb
+++ app/controllers/investigations/actions_controller.rb
@@ -36,7 +36,7 @@ module Investigations
       when "change_case_visibility"
         investigation_visibility_path(@investigation)
       when "send_email_alert"
-        about_investigation_alert_path(@investigation)
+        about_investigation_alerts_path(@investigation)
       when "change_case_risk_level"
         investigation_risk_level_path(@investigation)
       end
