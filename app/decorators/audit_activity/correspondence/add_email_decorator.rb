e2a90c981ef2638a4d6fea6d41092552322899dd
kyle macpherson <kaimac2010@gmail.com>
2021-06-23 20:08:07 +0100

    Make sure legacy activities don’t break without metadata

--- app/decorators/audit_activity/correspondence/add_email_decorator.rb
+++ app/decorators/audit_activity/correspondence/add_email_decorator.rb
@@ -2,11 +2,13 @@ module AuditActivity
   module Correspondence
     class AddEmailDecorator < AuditActivity::Correspondence::BaseDecorator
       def correspondence_date
-        Date.parse(metadata["correspondence_date"]).to_s(:govuk)
+        Date.parse(metadata["correspondence_date"]).to_s(:govuk) if object.metadata
+        object.correspondence_date
       end
 
       def title(_user)
-        object.metadata["overview"]
+        return object.metadata["overview"] if object.metadata
+        object.title
       end
     end
   end
