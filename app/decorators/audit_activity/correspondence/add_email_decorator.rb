899fc4b09eb72ee26f1b9d3bbd1abea712c4e524
kyle macpherson <kaimac2010@gmail.com>
2021-06-23 20:12:26 +0100

    Linting

--- app/decorators/audit_activity/correspondence/add_email_decorator.rb
+++ app/decorators/audit_activity/correspondence/add_email_decorator.rb
@@ -2,12 +2,14 @@ module AuditActivity
   module Correspondence
     class AddEmailDecorator < AuditActivity::Correspondence::BaseDecorator
       def correspondence_date
-        Date.parse(metadata["correspondence_date"]).to_s(:govuk) if object.metadata
+        return Date.parse(metadata["correspondence_date"]).to_s(:govuk) if object.metadata
+
         object.correspondence_date
       end
 
       def title(_user)
         return object.metadata["overview"] if object.metadata
+
         object.title
       end
     end
