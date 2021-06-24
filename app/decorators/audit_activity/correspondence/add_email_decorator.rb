40a25062ec5b9960b58187794305550d3685d2bc
kyle macpherson <kaimac2010@gmail.com>
2021-06-24 21:11:08 +0100

    No need to handle title and date for old activities

--- app/decorators/audit_activity/correspondence/add_email_decorator.rb
+++ app/decorators/audit_activity/correspondence/add_email_decorator.rb
@@ -2,15 +2,11 @@ module AuditActivity
   module Correspondence
     class AddEmailDecorator < AuditActivity::Correspondence::BaseDecorator
       def correspondence_date
-        return Date.parse(metadata["correspondence_date"]).to_s(:govuk) if object.metadata
-
-        Date.parse(::Correspondence::Email.find(object.correspondence_id).correspondence_date)
+        Date.parse(metadata["correspondence_date"]).to_s(:govuk) if object.metadata
       end
 
       def title(_user)
-        return object.metadata["overview"] if object.metadata
-
-        object.title
+        object.metadata["overview"] if object.metadata
       end
     end
   end
