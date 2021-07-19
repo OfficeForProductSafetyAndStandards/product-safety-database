da12c3e8c0ff6cbb87b1290c19434d8b530c49f5
kyle macpherson <kaimac2010@gmail.com>
2021-07-19 09:35:09 +0100

    Add feature test

--- app/controllers/product_exports_controller.rb
+++ app/controllers/product_exports_controller.rb
@@ -4,19 +4,18 @@ class ProductExportsController < ApplicationController
 
   before_action :set_search_params, only: %i[index]
 
-  # GET /products/1/edit
   def generate
+    authorize Product, :export?
+
     @products = search_for_products(Product.count, [:investigations, :test_results, corrective_actions: [:business], risk_assessments: [:assessed_by_business, :assessed_by_team]]).sort
     product_export = ProductExport.create
     ProductExportWorker.perform_later(@products, product_export.id, current_user)
     redirect_to products_path, flash: { success: "Your product export is being prepared. You will receive an email when your export is ready to download." }
   end
 
-  def index
-    @product_export = ProductExport.last
-  end
-
   def show
+    authorize Product, :export?
+
     @product_export = ProductExport.find(params[:id])
   end
 end
