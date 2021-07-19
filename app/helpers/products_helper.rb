7a582bbc8e13cc10ef2185d43f0c0c78874a4ca4
kyle macpherson <kaimac2010@gmail.com>
2021-07-19 16:24:09 +0100

    Remove unused method

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -19,10 +19,6 @@ module ProductsHelper
       .page(params[:page]).per_page(page_size).records
   end
 
-  def search_for_products_for_export(include_models = [])
-    Product.includes(include_models).full_search(search_query).records
-  end
-
   def sorting_params
     return {} if params[:sort] == SearchParams::RELEVANT
 
