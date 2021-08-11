5d01544fe1d21de5fdb38978a6a805efc8516802
Kyle MacPherson <kaimac2010@gmail.com>
2021-08-11 12:59:08 +0100

    Product edit bug (#1507)
    * Only allow users with permission to edit the products case to edit the product
    
    * Add feature tests to ensure users without edit perms cannot edit products
    
    * Authorise requests to products#update
    
    * Linting
    
    * Fix test
    
    * Fix request spec
    
    * Linting
    
    * Refactor product policy
    
    * Remove unused method
    
    * Use expect to receive instead of expect to have received as it seems to be flaky
    
    * Fix test by ensuring has_markings/markings_validity is always valid
    
    * Lint

--- app/policies/product_policy.rb
+++ app/policies/product_policy.rb
@@ -2,4 +2,8 @@ class ProductPolicy < ApplicationPolicy
   def export?
     user.is_psd_admin?
   end
+
+  def update?
+    record.investigations.none? || Pundit.policy(user, record.investigations.first).update?
+  end
 end
