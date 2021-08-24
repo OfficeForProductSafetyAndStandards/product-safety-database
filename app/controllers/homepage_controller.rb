e8815da914be5f773b10d0d32febe8674ea94765
kyle macpherson <kaimac2010@gmail.com>
2021-08-24 12:55:38 +0100

    Remove “beta” from headers

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,10 +3,6 @@ class HomepageController < ApplicationController
   skip_before_action :authorize_user
   skip_before_action :require_secondary_authentication, if: -> { current_user.nil? }
 
-  def show
-    @show_full_phase_banner = true
-  end
-
 private
 
   def secondary_nav_items
