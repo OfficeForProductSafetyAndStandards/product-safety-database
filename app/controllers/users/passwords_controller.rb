12bf9c72002df61ef8e3f6d8768328b03b6d0ec5
kyle macpherson <kaimac2010@gmail.com>
2021-08-25 14:44:12 +0100

    Change order of rendering to fix password reset bug

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -11,9 +11,9 @@ module Users
     before_action :require_secondary_authentication, only: :update
 
     def edit
-      return render :signed_in_as_another_user, locals: { reset_password_token: params[:reset_password_token] } if wrong_user?
       return render :invalid_link, status: :not_found if reset_token_invalid?
       return render :expired, status: :gone if reset_token_expired?
+      return render :signed_in_as_another_user, locals: { reset_password_token: params[:reset_password_token] } if wrong_user?
 
       require_secondary_authentication
 
