b6cfd8493258ed3051dbf7903b86976cd9bad93c
Kyle MacPherson <kaimac2010@gmail.com>
2021-09-06 15:26:00 +0100

    Don’t use HTML 5 validation for corrective action date (#1564)
    * Don’t use HTML 5 validation for corrective action date
    
    * Add feature tests
    
    * Fix test
    
    * Add novalidate to all forms that are missing it
    
    * Linting

--- spec/features/edit_corrective_action_spec.rb
+++ spec/features/edit_corrective_action_spec.rb
@@ -58,10 +58,10 @@ RSpec.feature "Edit corrective action", :with_stubbed_elasticsearch, :with_stubb
       click_link "Edit corrective action"
 
       # check error rendering and attachment details are retained
-      fill_in "Year", with: ""
+      fill_in "Year", with: "abcdefdef"
       click_on "Update corrective action"
 
-      expect(page).to have_error_summary("The date the corrective action came into effect must include a year")
+      expect(page).to have_error_summary("Enter a real date when the corrective action came into effect")
 
       within_fieldset("Are there any files related to the action?") do
         expect(page).to have_checked_field("Yes")
