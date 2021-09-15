ef17e89c303149ed06097c2f7c6088c95472ec2a
kyle macpherson <kaimac2010@gmail.com>
2021-09-15 16:36:08 +0100

    Change rubicon and capybara driver settings

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,7 +6,3 @@ require "capybara/mechanize"
 Capybara.register_driver :rack_test do |app|
   Capybara::RackTest::Driver.new(app, respect_data_method: true, redirect_limit: 6)
 end
-
-Capybara.register_driver :mechanize do |_app|
-  Capybara::Mechanize::Driver.new(proc {})
-end
