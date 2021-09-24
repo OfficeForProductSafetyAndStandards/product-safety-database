975838a3acde73c324a550a6dbacf454216f59d4
kyle macpherson <kaimac2010@gmail.com>
2021-09-24 16:37:20 +0100

    Revert to finding exact cases

--- smoke_test/cases_page_spec.rb
+++ smoke_test/cases_page_spec.rb
@@ -38,9 +38,15 @@ RSpec.feature "Search smoke test" do
 
     session.click_link "Cases"
 
-    number_of_cases = session.all(".govuk-grid-row.psd-case-card").count
-    session.save_page("tmp/capybara/screenshot.html") if number_of_cases < 10
-    expect(number_of_cases).to be > 9
+    begin
+      session.find(".govuk-grid-row.psd-case-card:nth-child(1)")
+      session.find(".govuk-grid-row.psd-case-card:nth-child(10)")
+    rescue
+      session.save_page("tmp/capybara/screenshot.html")
+    end
+
+    expect(session).to have_css(".govuk-grid-row.psd-case-card:nth-child(1)")
+    expect(session).to have_css(".govuk-grid-row.psd-case-card:nth-child(10)")
   end
 end
 
