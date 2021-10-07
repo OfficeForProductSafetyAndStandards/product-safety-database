e155c5931f6aed6976e5c0c68d17ef3d10fb028f
Kyle MacPherson <kaimac2010@gmail.com>
2021-10-07 11:57:47 +0100

    wip

---
+++ app/controllers/business_exports_controller.rb
@@ -0,0 +1,18 @@
+class BusinessExportsController < ApplicationController
+  include BusinessesHelper
+
+  before_action :set_search_params, only: %i[generate]
+
+  def generate
+    results = search_for_businesses.records.includes(:investigations, :locations, :contacts)
+
+    # TODO: move this query to the job once we are satisfied that this is working well.
+    business_export = BusinessExport.create!
+    BusinessExportJob.perform_later(results, business_export.id, current_user)
+    redirect_to busineses_path(q: params[:q]), flash: { success: "Your business export is being prepared. You will receive an email when your export is ready to download." }
+  end
+
+  def show
+    @case_export = BusinessExport.find(params[:id])
+  end
+end
