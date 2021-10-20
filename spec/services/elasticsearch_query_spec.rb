8f0e3c2996911920f787cce2d9dc5ffe3c80421f
kyle macpherson <kaimac2010@gmail.com>
2021-10-20 13:51:35 +0100

    Try sleep

--- spec/services/elasticsearch_query_spec.rb
+++ spec/services/elasticsearch_query_spec.rb
@@ -160,6 +160,7 @@ RSpec.describe ElasticsearchQuery, :with_elasticsearch, :with_stubbed_mailer do
       create(:business, trading_name: "Google")
       Business.__elasticsearch__.create_index! force: true
       Business.import
+      sleep 2
     end
 
     context "when searching in batches of 1" do
