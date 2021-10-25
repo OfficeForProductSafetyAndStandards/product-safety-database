1d5d3b8cde3b6b51d72de86e674778155a805e3d
stdstaff <85587097+stdstaff@users.noreply.github.com>
2021-10-25 15:53:53 +0100

    Pull AWS creds directly from VCAP_SERVICES

--- redex/task.sh
+++ redex/task.sh
@@ -5,24 +5,16 @@ if [[ ! $VCAP_SERVICES ]]; then
     exit 1
 fi
 
-echo $VCAP_SERVICES | ./jq -r "
-    .\"user-provided\"
-        | map(
-            select(.name[-4:] == \"$ENV_NAME\")
-                | .credentials
-                | to_entries[]
-                | \"\(.key)=\(.value)\"
-        )
-        | @tsv"
-
 export PGHOST=$(echo $VCAP_SERVICES | ./jq -r ".postgres[] | select(.name|test(\"$DB_NAME\")) .credentials .host")
 export PGPORT=$(echo $VCAP_SERVICES | ./jq -r ".postgres[] | select(.name|test(\"$DB_NAME\")) .credentials .port")
 export PGDATABASE=$(echo $VCAP_SERVICES | ./jq -r ".postgres[] | select(.name|test(\"$DB_NAME\")) .credentials .name")
 export PGUSER=$(echo $VCAP_SERVICES | ./jq -r ".postgres[] | select(.name|test(\"$DB_NAME\")) .credentials .username")
 export PGPASSWORD=$(echo $VCAP_SERVICES | ./jq -r ".postgres[] | select(.name|test(\"$DB_NAME\")) .credentials .password")
-export AWS_ACCESS_KEY_ID=$REDEX_AWS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$REDEX_AWS_SECRET_KEY
-export AWS_DEFAULT_REGION=$REDEX_AWS_REGION
+
+export AWS_ACCESS_KEY_ID=$(echo $VCAP_SERVICES | ./jq -r ".\"user-provided\"[] | select(.name==\"$ENV_NAME\") | .credentials .REDEX_AWS_KEY_ID")
+export AWS_SECRET_ACCESS_KEY=$(echo $VCAP_SERVICES | ./jq -r ".\"user-provided\"[] | select(.name==\"$ENV_NAME\") | .credentials .REDEX_AWS_SECRET_KEY")
+export AWS_DEFAULT_REGION=$(echo $VCAP_SERVICES | ./jq -r ".\"user-provided\"[] | select(.name==\"$ENV_NAME\") | .credentials .REDEX_AWS_REGION")
+export AWS_BUCKET=$(echo $VCAP_SERVICES | ./jq -r ".\"user-provided\"[] | select(.name==\"$ENV_NAME\") | .credentials .REDEX_AWS_S3_BUCKET")
 
 # NOTE: The apt buildpack installs into a non-globally-writable directory, so explicit paths
 #   must be used: https://stackoverflow.com/questions/68861126/getting-an-error-when-running-pg-dump-on-cloud-foundry#comment121702201_68861126
@@ -30,7 +22,7 @@ export PERL5LIB=/home/vcap/deps/0/apt/usr/lib/x86_64-linux-gnu:/home/vcap/deps/0
 export PYTHONPATH=/home/vcap/deps/0/apt/usr/lib/python3/dist-packages:$PYTHONPATH
 
 TIMESTAMP=$(date +'%F-%H-%M-%S')
-FINAL_S3_URL="s3://${REDEX_AWS_S3_BUCKET}/${TIMESTAMP}.sql.gz"
+FINAL_S3_URL="s3://${AWS_BUCKET}/${TIMESTAMP}.sql.gz"
 
 /home/vcap/deps/0/apt/usr/lib/postgresql/11/bin/psql < create_redacted_schema.sql
 /home/vcap/deps/0/apt/usr/lib/postgresql/11/bin/pg_dump --table='redacted.*' --file=psd_redacted_export.sql --no-acl --no-owner --quote-all-identifiers --format=p --inserts --encoding=UTF8
