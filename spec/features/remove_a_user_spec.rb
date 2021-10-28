ef465e835d3fc9c4953b367a9cf1f2a13f5f76b9
kyle macpherson <kaimac2010@gmail.com>
2021-10-28 13:29:12 +0100

    Add feature test for when user is not authorised to remove user

--- spec/features/remove_a_user_spec.rb
+++ spec/features/remove_a_user_spec.rb
@@ -3,10 +3,10 @@ require "rails_helper"
 RSpec.feature "Removing a user", :with_stubbed_mailer, :with_stubbed_elasticsearch, :with_errors_rendered, type: :feature do
   let(:team) { create(:team) }
   let(:email) { Faker::Internet.safe_email }
+  let!(:other_user) { create(:user, :activated, team: team, has_viewed_introduction: true, name: "BBBBB") }
 
   context "when the user is a team admin" do
     let(:user) { create(:user, :activated, :team_admin, team: team, has_viewed_introduction: true, name: "AAAAA") }
-    let!(:other_user) { create(:user, :activated, team: team, has_viewed_introduction: true, name: "BBBBB") }
 
     before do
       sign_in(user)
@@ -20,9 +20,6 @@ RSpec.feature "Removing a user", :with_stubbed_mailer, :with_stubbed_elasticsear
 
           expect(page.current_path).to eq "/remove_user"
 
-          p user.name
-          p other_user.name
-
           expect(page).to have_content "Do you want to remove #{other_user.name} from your team"
 
           choose("Yes")
@@ -33,27 +30,55 @@ RSpec.feature "Removing a user", :with_stubbed_mailer, :with_stubbed_elasticsear
           expect(page).to have_content "The team member was removed"
           expect(page).not_to have_content other_user.name
         end
+
+        context "when admin clicks no to remove user" do
+          scenario "user is not deleted" do
+            visit "/teams/#{team.id}"
+            click_link "Remove"
+
+            expect(page.current_path).to eq "/remove_user"
+
+            expect(page).to have_content "Do you want to remove #{other_user.name} from your team"
+
+            choose("No")
+
+            click_button("Save and continue")
+
+            expect(page.current_path).to eq "/teams/#{team.id}"
+            expect(page).to have_content other_user.name
+          end
+        end
       end
 
-      context "when admin clicks no to remove user" do
-        scenario "user is not deleted" do
-          visit "/teams/#{team.id}"
-          click_link "Remove"
+      context "when user to be removed is activated and not an admin" do
+        let!(:other_user) { create(:user, team: team, has_viewed_introduction: true, name: "BBBBB") }
 
-          expect(page.current_path).to eq "/remove_user"
+        context "when admin clicks yes to remove user" do
+          scenario "user is deleted" do
+            visit "/teams/#{team.id}"
+            click_link "Remove"
 
-          p user.name
-          p other_user.name
+            expect(page.current_path).to eq "/remove_user"
 
-          expect(page).to have_content "Do you want to remove #{other_user.name} from your team"
+            expect(page).to have_content "Do you want to remove #{other_user.name} from your team"
 
-          choose("No")
+            choose("Yes")
 
-          click_button("Save and continue")
+            click_button("Save and continue")
 
-          expect(page.current_path).to eq "/teams/#{team.id}"
-          expect(page).to have_content other_user.name
+            expect(page.current_path).to eq "/teams/#{team.id}"
+            expect(page).to have_content "The team member was removed"
+          end
         end
+      end
+    end
+
+    context "when the user is not a team admin" do
+      let(:user) { create(:user, :activated, team: team, has_viewed_introduction: true, name: "AAAAA") }
+
+      scenario "does not allow user to remove other users" do
+        visit "/teams/#{team.id}"
+        expect(page).not_to have_link "Remove"
       end
     end
   end
