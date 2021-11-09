3975b637a8939dad8f18a107ddd669dcd08511ce
kyle macpherson <kaimac2010@gmail.com>
2021-11-09 15:43:24 +0000

    Renders 401 if non-user tries to click link to attachment, however doesn’t handle it gracefully

--- app/controllers/secure_blobs_controller.rb
+++ app/controllers/secure_blobs_controller.rb
@@ -1,5 +1,6 @@
 class SecureBlobsController < ApplicationController
   include ActiveStorage::SetBlob
+  include ActiveStorage::SetCurrent
 
   def show
     redirect_to @blob.service_url(disposition: params[:disposition])
