c90bce56825b7630d642a7de2ef0cc09c268e46f
kyle macpherson <kaimac2010@gmail.com>
2021-11-10 14:32:47 +0000

    Use proxy to avoid exposing s3 link

---
+++ app/controllers/active_storage/blobs/proxy_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Proxy files through application. This avoids having a redirect and makes files easier to cache.
+# Overrides Rails Controller to enforce access protection beyond the security-through-obscurity
+# factor of the signed blob and variation reference.
+# Only owners and search users have access to files.
+
+class ActiveStorage::Blobs::ProxyController < ActiveStorage::BaseController
+  include ActiveStorage::SetBlob
+  include ActiveStorage::SetHeaders
+  include Pundit
+
+  before_action :authorize_blob
+
+  def show
+    http_cache_forever public: true do
+      set_content_headers_from @blob
+      stream @blob
+    end
+  end
+
+  def authorize_blob
+    raise Pundit::NotAuthorizedError unless user_signed_in?
+  end
+end
