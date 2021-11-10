cab310b3254b52d8114fa7145c8977e45ce2208a
kyle macpherson <kaimac2010@gmail.com>
2021-11-10 12:25:20 +0000

    Redirect after failed auth

--- app/controllers/secure_blobs_controller.rb
+++ app/controllers/secure_blobs_controller.rb
@@ -5,4 +5,14 @@ class SecureBlobsController < ApplicationController
   def show
     redirect_to @blob.service_url(disposition: params[:disposition])
   end
+
+  private
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to unauthenticated_root_path
+    end
+  end
 end
