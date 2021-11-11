3a6d204e82597f21808dfe48f5b6a9ecfdfdf840
kyle macpherson <kaimac2010@gmail.com>
2021-11-11 15:30:37 +0000

    Show previews of images

--- app/controllers/active_storage/representations/proxy_controller.rb
+++ app/controllers/active_storage/representations/proxy_controller.rb
@@ -4,7 +4,27 @@
 # /rails/active_storage/representations/proxy/:signed_blob_id/:variation_key/*filename(.:format)
 # We use "rails storage proxy" through ActiveStorage::Blobs::ProxyController
 class ActiveStorage::Representations::ProxyController < ActiveStorage::BaseController
+  include ActiveStorage::SetBlob
+  include ActiveStorage::SetHeaders
+
   def show
-    redirect_to "/"
+    http_cache_forever public: true do
+      set_content_headers_from representation.image
+      stream representation
+    end
+  end
+
+  private
+
+  def pundit_user
+    current_user
+  end
+
+  def authorize_blob
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def representation
+    @representation ||= @blob.representation(params[:variation_key]).processed
   end
 end
