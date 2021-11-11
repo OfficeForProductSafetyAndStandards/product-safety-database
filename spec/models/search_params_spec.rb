19e15a999792ea6bcba0450abd05c0b4f9044e6d
stdstaff <85587097+stdstaff@users.noreply.github.com>
2021-11-11 11:32:29 +0000

    Name subject (rubocop)

--- spec/models/search_params_spec.rb
+++ spec/models/search_params_spec.rb
@@ -1,25 +1,25 @@
 require "rails_helper"
 
 RSpec.describe SearchParams do
-  subject { described_class.new(search_params) }
+  subject(:model_instance) { described_class.new(search_params) }
 
   context "when sort_by is blank" do
     let(:search_params) { { sort_by: SearchParams::BLANK } }
 
-    describe 'sort_by_option' do
+    describe "sort_by_option" do
       it "returns recent by default" do
-        expect(subject.sort_by_option).to eq(SearchParams::RECENT)
+        expect(model_instance.sort_by_option).to eq(SearchParams::RECENT)
       end
 
       it "returns relevant if there is a query present" do
-        search_params.merge!(q: 'query')
-        expect(subject.sort_by_option).to eq(SearchParams::RELEVANT)
+        search_params[:q] = "query"
+        expect(model_instance.sort_by_option).to eq(SearchParams::RELEVANT)
       end
     end
 
-    describe 'sorting_params' do
+    describe "sorting_params" do
       it "returns updated_at descending by default" do
-        expect(subject.sorting_params).to eq({ updated_at: "desc" })
+        expect(model_instance.sorting_params).to eq({ updated_at: "desc" })
       end
     end
   end
