7944f37d9b6f79a9659261ef71ff849b3e3a8b6b
stdstaff <85587097+stdstaff@users.noreply.github.com>
2021-12-09 09:22:19 +0000

    Design revisions for the products page

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,12 @@ class Product < ApplicationRecord
   include Searchable
   include AttachmentConcern
 
+  SORT_BY_ITEMS = [
+    ["Relevance", SearchParams::RELEVANT],
+    ["Newest cases", SearchParams::NEWEST],
+    ["Name", SearchParams::NAME]
+  ].freeze
+
   enum authenticity: {
     "counterfeit" => "counterfeit",
     "genuine" => "genuine",
