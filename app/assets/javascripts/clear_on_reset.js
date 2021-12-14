e6132056948bb85438b3cf57b574cbb35b09e44e
Kyle MacPherson <kaimac2010@gmail.com>
2021-12-14 16:31:24 +0000

    Hide conditional on reset

--- app/assets/javascripts/clear_on_reset.js
+++ app/assets/javascripts/clear_on_reset.js
@@ -4,14 +4,10 @@ $(document).ready(() => {
   $('[data-opss-clear-on-reset="trigger"]').click((e) => {
     e.preventDefault()
     $('[data-opss-clear-on-reset="element"]').val(null)
-<<<<<<< HEAD
-    $('[data-opss-clear-on-reset="element"]').find(':selected').prop('selected', false)
-    $('[data-opss-clear-on-reset="element"]').find(':checked').prop('checked', false)
-=======
     $('[data-opss-clear-on-reset="element"]').find(':selected').prop('selected', "all")
     $('[data-opss-clear-on-reset="element"]').find(':checked').prop('checked', false)
     $('[data-opss-reset-to-default="element"]').find(':radio[checked]').prop('checked', true)
->>>>>>> 1b08f0b95 (Add reset button)
+    $('.govuk-radios__conditional').addClass("govuk-radios__conditional--hidden")
     $('[data-opss-clear-on-reset="autocomplete"]').find(':selected').prop('selected', false)
     $('[data-opss-clear-on-reset="autocomplete"]').each((idx, ele) => {
       $(ele).parent().find('input.autocomplete__input').val(null)
