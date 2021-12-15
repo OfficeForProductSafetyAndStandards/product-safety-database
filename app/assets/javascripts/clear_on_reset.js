51f69eabb3f1ac0a6439c8ad4ba096b4f18184f1
Kyle MacPherson <kaimac2010@gmail.com>
2021-12-15 14:19:16 +0000

    Correct rebase error

--- app/assets/javascripts/clear_on_reset.js
+++ app/assets/javascripts/clear_on_reset.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
   $('[data-opss-clear-on-reset="trigger"]').click((e) => {
     e.preventDefault()
     $('[data-opss-clear-on-reset="element"]').val(null)
-    $('[data-opss-clear-on-reset="element"]').find(':selected').prop('selected', 'all')
+    $('[data-opss-clear-on-reset="element"]').find(':selected').prop('selected', false)
     $('[data-opss-clear-on-reset="element"]').find(':checked').prop('checked', false)
     $('[data-opss-reset-to-default="element"]').find(':radio[checked]').prop('checked', true)
     $('.govuk-radios__conditional').addClass('govuk-radios__conditional--hidden')
