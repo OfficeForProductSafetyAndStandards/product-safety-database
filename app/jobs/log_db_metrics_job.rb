dda0f4faae680c8ac07202d5a0989db67477c3d3
Kyle MacPherson <kaimac2010@gmail.com>
2022-01-05 15:56:22 +0000

    Further logging metrics (#1716)
    * Add logging for forgotten password requests
    
    * Add logging for failed 2FA attempt
    
    * Add number of failed sign ins and number of locked user metrics

--- app/jobs/log_db_metrics_job.rb
+++ app/jobs/log_db_metrics_job.rb
@@ -4,7 +4,9 @@ class LogDbMetricsJob < ApplicationJob
       total_number_of_cases: Investigation.count,
       total_number_of_users: User.count,
       total_number_of_products: Product.count,
-      total_number_of_businesses: Business.count
+      total_number_of_businesses: Business.count,
+      total_number_of_locked_users: User.where.not(locked_at: nil).count,
+      total_number_of_failed_sign_ins: User.sum(:failed_attempts)
     }
 
     Sidekiq.logger.info "PsdStatistics #{stats.to_a.map { |x| x.join('=') }.join(' ')}"
