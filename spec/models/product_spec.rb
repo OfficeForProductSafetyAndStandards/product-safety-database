55cefd7bae2625067f80fccef94e6dcd0e3db15e
stdstaff <85587097+stdstaff@users.noreply.github.com>
2022-01-17 13:30:35 +0000

    Introduce a unique 'PSD ref' Product reference

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,15 @@
 require "rails_helper"
 
-RSpec.describe Product do
+RSpec.describe Product, :with_opensearch do
   it_behaves_like "a batched search model" do
     let(:factory_name) { :product }
+  end
+
+  describe "#psd_ref" do
+    let(:product) { create :product }
+
+    it "returns a reference formed with 'psd-' and the product's ID" do
+      expect(product.psd_ref).to eq("psd-#{product.id}")
+    end
   end
 end
