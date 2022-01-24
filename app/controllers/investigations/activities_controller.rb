7e746f6780dc537ca60819f0812d439c2a29dacf
stdstaff <85587097+stdstaff@users.noreply.github.com>
2022-01-24 14:39:42 +0000

    Fix the manual calls to the changed Preloader API

--- app/controllers/investigations/activities_controller.rb
+++ app/controllers/investigations/activities_controller.rb
@@ -52,7 +52,7 @@ module Investigations
     end
 
     def preload_manually(records, associations)
-      ActiveRecord::Associations::Preloader.new.preload(records, associations)
+      ActiveRecord::Associations::Preloader.new(records: records, associations: associations).call
     end
   end
 end
