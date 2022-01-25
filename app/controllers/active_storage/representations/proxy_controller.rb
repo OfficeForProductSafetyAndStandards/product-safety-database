a2ef2c79af6bb452bda37d4a0a0d13d2efcd8dcd
stdstaff <85587097+stdstaff@users.noreply.github.com>
2022-01-25 16:38:37 +0000

    Switch to vips and fix image thumbnailing

--- app/controllers/active_storage/representations/proxy_controller.rb
+++ app/controllers/active_storage/representations/proxy_controller.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
 
-#
 # Overrides original Rails implementation to disable route:
 # /rails/active_storage/representations/proxy/:signed_blob_id/:variation_key/*filename(.:format)
 # We use "rails storage proxy" through ActiveStorage::Blobs::ProxyController
 
-class ActiveStorage::Representations::ProxyController < ActiveStorage::BaseController
-  include ActiveStorage::SetBlob
+class ActiveStorage::Representations::ProxyController < ActiveStorage::Representations::BaseController
   include HttpAuthConcern
   include SentryConfigurationConcern
 
   before_action :authorize_blob
 
   def show
-    set_content_headers_from representation.image
-    stream representation
+    http_cache_forever public: true do
+      send_blob_stream @representation.image, disposition: params[:disposition]
+    end
   end
 
 private
