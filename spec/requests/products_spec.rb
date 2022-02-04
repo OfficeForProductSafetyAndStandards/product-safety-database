eef4af03cfa5bb334e85ff7db3f51171b5a91631
Kyle MacPherson <kaimac2010@gmail.com>
2022-02-04 12:08:52 +0000

    Add request specs

---
+++ spec/requests/products_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe "Product page", type: :request, with_stubbed_mailer: true do
+  let(:user) { create(:user, :activated) }
+
+  it "does not raise an error with a page value > 500" do
+    sign_in user
+    expect { get "/business?page=501" }.not_to raise_error
+  end
+end
