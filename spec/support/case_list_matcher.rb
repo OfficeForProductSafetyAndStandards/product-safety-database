aacf929bf80c0ea87d12894d77f0307c675d4284
Kyle MacPherson <kaimac2010@gmail.com>
2022-02-14 17:42:25 +0000

    Fix list_cases_in_order method

--- spec/support/case_list_matcher.rb
+++ spec/support/case_list_matcher.rb
@@ -7,7 +7,7 @@ end
 RSpec::Matchers.define :list_cases_in_order do |expected_cases_ids|
   match do |element|
     pattern = /\d{4}-\d{4}/
-    page_case_ids = element.all("table#results td.govuk-table__cell", text: pattern).map { |e| e.text.strip }
+    page_case_ids = element.all("td.govuk-table__cell", text: pattern).map { |e| e.text.strip }
     page_case_ids == expected_cases_ids
   end
 end
