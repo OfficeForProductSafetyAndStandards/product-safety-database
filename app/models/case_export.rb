74aee23e83e4a2248cd33e18ab121e0df810e771
Kyle MacPherson <kaimac2010@gmail.com>
2022-04-11 13:55:25 +0100

    Correctly use policy

--- app/models/case_export.rb
+++ app/models/case_export.rb
@@ -114,7 +114,7 @@ private
   end
 
   def serialize_case(investigation)
-    Pundit.policy!(user, investigation).view_protected_details?(user:) ? non_restricted_data(investigation) : restricted_data(investigation)
+    Pundit.policy!(user, investigation).view_protected_details?(user:) || !investigation.is_private? ? non_restricted_data(investigation) : restricted_data(investigation)
   end
 
   def non_restricted_data(investigation)
