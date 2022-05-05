94e7ba5c8a89e46578ca122d065917c0b6f2f291
Kyle MacPherson <kaimac2010@gmail.com>
2022-05-05 15:47:32 +0100

    Add artificial delay to documents controller and handle antivirus analyzer processing

--- app/forms/document_form.rb
+++ app/forms/document_form.rb
@@ -16,6 +16,7 @@ class DocumentForm
   validates :description, length: { maximum: 10_000 }
   validate :file_size_below_max, if: -> { document.present? && existing_document_file_id.present? }
   validate :file_size_above_min, if: -> { document.present? && existing_document_file_id.present? }
+  validate :file_is_free_of_viruses, if: -> { document.present? && existing_document_file_id.present? }
 
   before_validation do
     trim_line_endings(:description)
@@ -73,5 +74,14 @@ private
 
   def min_file_byte_size
     1.bytes
+  end
+
+  def file_is_free_of_viruses
+    # don't run this validation unless document has been analyzed by antivirus analyzer
+    return unless document.metadata.key?("safe")
+
+    return if document.metadata["safe"] == true
+
+    errors.add(:base, :virus, message: "Files must be virus free")
   end
 end
