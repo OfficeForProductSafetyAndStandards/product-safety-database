3b65474099c674892a10b9efa4fd65b93eb59bc0
Kyle MacPherson <kaimac2010@gmail.com>
2022-05-10 11:22:39 +0100

    Remove unnecessary decorator

--- app/decorators/active_storage/attachment_decorator.rb
+++ app/decorators/active_storage/attachment_decorator.rb
@@ -2,10 +2,6 @@ module ActiveStorage
   class AttachmentDecorator < ApplicationDecorator
     delegate_all
 
-    def description
-      h.simple_format(object.description)
-    end
-
     def title
       object.metadata[:title] || object.filename.to_s
     end
