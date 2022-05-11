94f819add20019f89db75660a93d511db2b4db09
Kyle MacPherson <kaimac2010@gmail.com>
2022-05-11 17:37:49 +0100

    Logging and test sending email from analyzer

--- app/analyzers/anti_virus_analyzer.rb
+++ app/analyzers/anti_virus_analyzer.rb
@@ -7,11 +7,12 @@ class AntiVirusAnalyzer < ActiveStorage::Analyzer
     download_blob_to_tempfile do |file|
       response = RestClient::Request.execute method: :post, url: Rails.application.config.antivirus_url, user: ENV["ANTIVIRUS_USERNAME"], password: ENV["ANTIVIRUS_PASSWORD"], payload: { file: }
       body = JSON.parse(response.body)
+      NotifyMailer.welcome("kyle", "macphersonkd@gmail.com").deliver_later
+      Rails.logger.info "££££££"
+      Rails.logger.info blob
+      Rails.logger.info "%%%%%%"
+      Rails.logger.info ActiveStorage::Attachment.find_by(blob_id: blob.id)
       { safe: body["safe"] }
     end
-    Rails.logger.info "££££££"
-    Rails.logger.info blob
-    Rails.logger.info "%%%%%%"
-    Rails.logger.info ActiveStorage::Attachment.find_by(blob_id: blob.id)
   end
 end
