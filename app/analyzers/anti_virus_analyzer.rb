51976e583fc7f3b33de2ab74223be9bcca5ab9e6
Kyle MacPherson <kaimac2010@gmail.com>
2022-05-12 17:39:17 +0100

    Add logging

--- app/analyzers/anti_virus_analyzer.rb
+++ app/analyzers/anti_virus_analyzer.rb
@@ -11,19 +11,22 @@ class AntiVirusAnalyzer < ActiveStorage::Analyzer
       attachment = ActiveStorage::Attachment.find_by(blob_id: blob.id)
       user = User.find(blob.metadata["created_by"])
 
-      if body["safe"] == false
+      # if body["safe"] == false - allow this to run all the time for logging purposes etc.
         # if blob is currently attached we need to purge the attachment which in turn will purge the blob
         if attachment
+          Rails.logger.info "Attachment!!!!!"
+          Rails.logger.info attachment.record_type
           NotifyMailer.unsafe_attachment(user: user, record_type: attachment.record_type, id: attachment.record_id).deliver_later
           attachment.purge_later
           # delete file attached activity as it is no longer needed
           # Activity.find(attachment.record_id).destroy if attachment.record_type == "Activity"
         else
+          Rails.logger.info "Blob!!!!!"
           blob.purge_later
         end
-      else
-        { safe: body["safe"] }
-      end
+      # else
+      #   { safe: body["safe"] }
+      # end
     end
   end
 end
