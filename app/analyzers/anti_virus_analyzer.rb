ea95a5f97aa61d8bcca895056d53bdaf3c996a56
Kyle MacPherson <kaimac2010@gmail.com>
2022-05-12 15:28:19 +0100

    Add extra logging

--- app/analyzers/anti_virus_analyzer.rb
+++ app/analyzers/anti_virus_analyzer.rb
@@ -11,11 +11,15 @@ class AntiVirusAnalyzer < ActiveStorage::Analyzer
       # if body["safe"] == false
       attachment = ActiveStorage::Attachment.find_by(blob_id: blob.id)
       user = User.find(blob.metadata["created_by"])
+      Rails.logger.info "$$$$$$$$$"
+      Rails.logger.info attachment
 
       if attachment
+        Rails.logger.info "Sending unsafe attachment email"
         NotifyMailer.unsafe_attachment(user: user, record_type: attachment.record_type, id: attachment.record_id).deliver_later
         attachment.purge_later
       else
+        Rails.logger.info "Sending unsafe blob email"
         NotifyMailer.unsafe_file(user: user, created_at: blob.created_at.to_s(:govuk)).deliver_later
         blob.purge_later
       end
