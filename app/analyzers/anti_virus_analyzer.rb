68fe8d485a92d9b32d1aacfcc28aa3c26c4dc04a
Kyle MacPherson <kaimac2010@gmail.com>
2022-05-12 15:14:39 +0100

    Test purging and email notifications if file is unsafe

--- app/analyzers/anti_virus_analyzer.rb
+++ app/analyzers/anti_virus_analyzer.rb
@@ -7,12 +7,21 @@ class AntiVirusAnalyzer < ActiveStorage::Analyzer
     download_blob_to_tempfile do |file|
       response = RestClient::Request.execute method: :post, url: Rails.application.config.antivirus_url, user: ENV["ANTIVIRUS_USERNAME"], password: ENV["ANTIVIRUS_PASSWORD"], payload: { file: }
       body = JSON.parse(response.body)
-      NotifyMailer.welcome("kyle", "macphersonkd@gmail.com").deliver_later
-      Rails.logger.info "££££££"
-      Rails.logger.info blob
-      Rails.logger.info "%%%%%%"
-      Rails.logger.info ActiveStorage::Attachment.find_by(blob_id: blob.id)
+
+      # if body["safe"] == false
+      attachment = ActiveStorage::Attachment.find_by(blob_id: blob.id)
+      user = User.find(blob.metadata["created_by"])
+
+      if attachment
+        NotifyMailer.unsafe_attachment(user: user, record_type: attachment.record_type, id: attachment.record_id).deliver_later
+        attachment.purge_later
+      else
+        NotifyMailer.unsafe_file(user: user, created_at: blob.created_at.to_s(:govuk)).deliver_later
+        blob.purge_later
+      end
+      # else
       { safe: body["safe"] }
+      # end
     end
   end
 end
