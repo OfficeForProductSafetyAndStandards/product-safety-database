d3c5815858b1b603b2c372b79d0901a1589d8465
Kyle MacPherson <kaimac2010@gmail.com>
2022-05-16 18:23:23 +0100

    Do not create indexes concurrently

--- db/migrate/20220516133518_add_index_to_active_storage_blobs.rb
+++ db/migrate/20220516133518_add_index_to_active_storage_blobs.rb
@@ -1,7 +1,5 @@
 class AddIndexToActiveStorageBlobs < ActiveRecord::Migration[6.1]
-  disable_ddl_transaction!
-
   def change
-    add_index :active_storage_blobs, :metadata, algorithm: :concurrently
+    safety_assured { add_index :active_storage_blobs, :metadata }
   end
 end
