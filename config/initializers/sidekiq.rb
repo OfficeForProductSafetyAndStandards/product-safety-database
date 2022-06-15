a8de022ce75d8963762cfc5c2b8204a9c877931f
Kyle MacPherson <kaimac2010@gmail.com>
2022-06-15 13:41:20 +0100

    Switch config for other cron jobs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,29 +1,3 @@
-def create_log_db_metrics_job
-  log_db_metrics_job = Sidekiq::Cron::Job.new(
-    name: "#{ENV['SIDEKIQ_QUEUE'] || 'psd'}: log db metrics, every 15 minutes",
-    cron: "*/15 * * * *",
-    class: "LogDbMetricsJob",
-    queue: ENV["SIDEKIQ_QUEUE"] || "psd"
-  )
-  unless log_db_metrics_job.save
-    Rails.logger.error "***** WARNING - Log DB metrics job was not saved! *****"
-    Rails.logger.error log_db_metrics_job.errors.join("; ")
-  end
-end
-
-def create_lock_inactive_users_job
-  lock_inactive_users_job = Sidekiq::Cron::Job.new(
-    name: "#{ENV['SIDEKIQ_QUEUE'] || 'psd'}: lock inactive users, every day at 1am",
-    cron: "0 1 * * *",
-    class: "LockInactiveUsersJob",
-    queue: ENV["SIDEKIQ_QUEUE"] || "psd"
-  )
-  unless lock_inactive_users_job.save
-    Rails.logger.error "***** WARNING - Lock inactive users job was not saved! *****"
-    Rails.logger.error lock_inactive_users_job.errors.join("; ")
-  end
-end
-
 Sidekiq.configure_server do |config|
   config.redis = Rails.application.config_for(:redis_store)
 end
