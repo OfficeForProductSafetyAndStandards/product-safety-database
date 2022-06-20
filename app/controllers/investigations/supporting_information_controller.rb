155eb3a52b7c07d8cbaa2903040cd753d72368db
Kyle MacPherson <kaimac2010@gmail.com>
2022-06-20 12:26:29 +0100

    Order supporting information by type, then by created_at

--- app/controllers/investigations/supporting_information_controller.rb
+++ app/controllers/investigations/supporting_information_controller.rb
@@ -2,26 +2,25 @@ module Investigations
   class SupportingInformationController < ApplicationController
     def index
       authorize investigation, :view_non_protected_details?
-
       @grouped_supporting_information = {
         "Accident or incidents" => {
-          items: investigation.unexpected_events.map(&:decorate),
+          items: investigation.unexpected_events.order(created_at: :desc).map(&:decorate),
           new_path: new_investigation_accident_or_incidents_type_path(investigation)
         },
         "Corrective actions" => {
-          items: investigation.corrective_actions.map(&:decorate),
+          items: investigation.corrective_actions.order(created_at: :desc).map(&:decorate),
           new_path: new_investigation_corrective_action_path(investigation)
         },
         "Risk assessments" => {
-          items: investigation.risk_assessments.map(&:decorate),
+          items: investigation.risk_assessments.order(created_at: :desc).map(&:decorate),
           new_path: new_investigation_risk_assessment_path(investigation)
         },
         "Correspondence" => {
-          items: investigation.correspondences.map(&:decorate),
+          items: investigation.correspondences.order(created_at: :desc).map(&:decorate),
           new_path: new_investigation_correspondence_path(investigation)
         },
         "Test results" => {
-          items: investigation.test_results.map(&:decorate),
+          items: investigation.test_results.order(created_at: :desc).map(&:decorate),
           new_path: new_investigation_test_result_path(investigation)
         }
       }
