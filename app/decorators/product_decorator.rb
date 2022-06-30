26fbb63b272e76f7850951b752c8db7398384027
stdstaff <85587097+stdstaff@users.noreply.github.com>
2022-06-30 11:12:23 +0100

    Mark PSD Ref HTML fragments as safe

--- app/decorators/product_decorator.rb
+++ app/decorators/product_decorator.rb
@@ -8,8 +8,8 @@ class ProductDecorator < ApplicationDecorator
   end
 
   def summary_list
-    psd_ref_key_html = "<abbr title='Product Safety Database'>PSD</abbr> <span title='reference'>ref</span>"
-    psd_secondary_text_html = "<span class='govuk-visually-hidden'> - </span>The <abbr>PSD</abbr> reference for this product record"
+    psd_ref_key_html = "<abbr title='Product Safety Database'>PSD</abbr> <span title='reference'>ref</span>".html_safe
+    psd_secondary_text_html = "<span class='govuk-visually-hidden'> - </span>The <abbr>PSD</abbr> reference for this product record".html_safe
     rows = [
       { key: { html: psd_ref_key_html }, value: { text: psd_ref, secondary_text: { html: psd_secondary_text_html } } },
       { key: { text: "Category" }, value: { text: category } },
