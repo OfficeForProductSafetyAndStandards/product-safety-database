2a1cf17647a3196b738018f920ed2ec337dbe898
See Through Data: Staff <85587097+stdstaff@users.noreply.github.com>
2022-08-03 12:01:02 +0100

    Allow blank case summaries; revert to longer summaries (#1971)

--- app/models/investigation.rb
+++ app/models/investigation.rb
@@ -24,10 +24,8 @@ class Investigation < ApplicationRecord
 
   validates :type, presence: true # Prevent saving instances of Investigation; must use a subclass instead
 
-  validates :description, presence: true, on: :update
-
   validates :user_title, length: { maximum: 100 }
-  validates :description, length: { maximum: 800 }
+  validates :description, length: { maximum: 10_000 }
   validates :non_compliant_reason, length: { maximum: 10_000 }
   validates :hazard_description, length: { maximum: 10_000 }
   validates :custom_risk_level, absence: true, if: -> { risk_level != "other" }
