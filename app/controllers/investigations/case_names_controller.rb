42b841273c93f76170468b669ee429f3255881db
Kyle MacPherson <kaimac2010@gmail.com>
2022-08-25 12:15:57 +0100

    Add ability for user to edit case name from case summary page

---
+++ app/controllers/investigations/case_names_controller.rb
@@ -0,0 +1,30 @@
+module Investigations
+  class CaseNamesController < ApplicationController
+    def edit
+      @investigation = Investigation.find_by(pretty_id: params["investigation_pretty_id"])
+      authorize @investigation, :update?
+
+      @case_name_form = CaseNameForm.new(user_title: @investigation.user_title, current_user: current_user)
+    end
+
+    def update
+      @investigation = Investigation.find_by(pretty_id: params["investigation_pretty_id"])
+      authorize @investigation, :update?
+
+      @case_name_form = CaseNameForm.new(reference_number_params.merge(current_user: current_user))
+
+      if @case_name_form.valid?
+        @investigation.update!(user_title: @case_name_form.user_title )
+        return redirect_to investigation_path(@investigation), flash: { success: "The case name has been updated." }
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def reference_number_params
+      params.require(:investigation).permit(:user_title)
+    end
+  end
+end
