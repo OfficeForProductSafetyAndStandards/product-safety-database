09a74c5e397cb10d01dddab80d1942b07d85bd86
stdstaff <85587097+stdstaff@users.noreply.github.com>
2022-09-02 14:53:58 +0100

    Update feature spec for editing a product

--- spec/features/edit_a_product_spec.rb
+++ spec/features/edit_a_product_spec.rb
@@ -19,7 +19,8 @@ RSpec.feature "Editing a product", :with_opensearch, :with_stubbed_mailer, :with
            country_of_origin:,
            has_markings: "markings_yes",
            customs_code: "initial, customs, code123",
-           investigations: [investigation])
+           investigations: [investigation],
+           owning_team: user.team)
   end
   let(:product_with_no_affected_units_status) do
     create(:product,
@@ -52,7 +53,7 @@ RSpec.feature "Editing a product", :with_opensearch, :with_stubbed_mailer, :with
   let(:new_when_placed_on_market) { (Product.when_placed_on_markets.keys - [product.when_placed_on_market]).sample }
   let(:new_customs_code) { "12345678, abc, 987" }
 
-  context "when user does not have edit permissions for the corresponding investigation" do
+  context "when the user is not in the team which owns the product" do
     before { sign_in other_user }
 
     it "does not allow user to edit the product" do
@@ -64,10 +65,10 @@ RSpec.feature "Editing a product", :with_opensearch, :with_stubbed_mailer, :with
     end
   end
 
-  context "when user does have edit permissions for the corresponding investigation" do
+  context "when the user is in the team which owns the product" do
     before { sign_in user }
 
-    it "allows to edit a product" do
+    it "allows the user to edit the product" do
       visit "/cases/#{investigation.pretty_id}/products"
       expect(page).not_to have_link "Change product details"
 
@@ -298,6 +299,129 @@ RSpec.feature "Editing a product", :with_opensearch, :with_stubbed_mailer, :with
 
       expect(page).to have_css("h2.govuk-heading-m", text: document_title)
       expect(page).to have_css("p", text: document_description)
+    end
+  end
+
+  context "when the product is not owned by a team" do
+    before do
+      product.update! owning_team: nil
+      sign_in user
+    end
+
+    it "allows the user to edit the product, and their team becomes the owner" do
+      visit "/cases/#{investigation.pretty_id}/products"
+      expect(page).to have_link "Change product details"
+
+      visit "/products/#{product.id}"
+
+      expect(page).to have_summary_item(key: "Product authenticity", value: "Not provided")
+
+      expect(page).to have_link "Edit details"
+      click_link "Edit details"
+
+      expect(page).to have_select("Product category", selected: product.category)
+      expect(page).to have_field("Product subcategory", with: product.subcategory)
+
+      within_fieldset "Is the product counterfeit?" do
+        expect(page).not_to have_checked_field("Yes")
+        expect(page).not_to have_checked_field("No")
+        expect(page).not_to have_checked_field("Unsure")
+      end
+
+      within_fieldset("Does the product have UKCA, UKNI, or CE marking?") do
+        expect(page).to have_checked_field("Yes")
+      end
+
+      within_fieldset("Select product marking") do
+        product.markings.each { |marking| expect(page).to have_checked_field(marking) }
+      end
+
+      expect(page).to have_field("Product brand",            with: product.brand)
+      expect(page).to have_field("Product name",             with: product.name)
+      expect(page).to have_field("Barcode number",           with: product.barcode)
+      expect(page).to have_field("Batch number",             with: product.batch_number)
+      expect(page).to have_field("Other product identifier", with: product.product_code)
+      expect(page).to have_field("Webpage",                  with: product.webpage)
+      expect(page).to have_select("Country of origin",       selected: "France")
+      expect(page).to have_field("Description of product",   with: product.description)
+
+      click_on "Save product"
+
+      expect(page).to have_error_messages
+      expect(page).to have_error_summary "You must state whether the product is a counterfeit"
+
+      select new_product_category, from: "Product category"
+      fill_in "Product subcategory", with: new_subcategory
+
+      within_fieldset "Was the product placed on the market before 1 January 2021?" do
+        choose when_placed_on_market_answer(new_when_placed_on_market)
+      end
+
+      within_fieldset "Is the product counterfeit?" do
+        choose counterfeit_answer(new_authenticity)
+      end
+
+      within_fieldset("Does the product have UKCA, UKNI, or CE marking?") do
+        page.find("input[value='#{new_has_markings}']").choose
+      end
+
+      within_fieldset("Select product marking") do
+        all("input[type=checkbox]").each(&:uncheck)
+        new_markings.each { |marking| check(marking) } if new_has_markings == "markings_yes"
+      end
+
+      within_fieldset("How many units are affected?") do
+        choose affected_units_status_answer(new_affected_units_status)
+        find("#approx_units").set(new_number_of_affected_units)
+      end
+
+      fill_in "Product brand",            with: new_brand
+      fill_in "Product name",             with: new_name
+      fill_in "Barcode number",           with: new_barcode
+      fill_in "Batch number",             with: new_batch_number
+      fill_in "Other product identifier", with: new_product_code
+      fill_in "Webpage",                  with: new_webpage
+      select new_country_of_origin,       from: "Country of origin"
+      fill_in "Description of product",   with: new_description
+      fill_in "Customs code",             with: new_customs_code
+
+      click_on "Save product"
+
+      expect_to_be_on_product_page(product_id: product.id, product_name: new_name)
+
+      expect(product.reload.owning_team).to eq(user.team)
+
+      expected_markings = case new_has_markings
+                          when "markings_yes" then new_markings.join(", ")
+                          when "markings_no" then "None"
+                          when "markings_unknown" then "Unknown"
+                          end
+
+      expect(page).to have_summary_item(key: "Category", value: new_product_category)
+      expect(page).to have_summary_item(key: "Product subcategory", value: new_subcategory)
+      expect(page).to have_summary_item(key: "Product authenticity",      value: I18n.t(new_authenticity, scope: Product.model_name.i18n_key))
+      expect(page).to have_summary_item(key: "Product marking",           value: expected_markings)
+      expect(page).to have_summary_item(key: "Units affected",            value: "something like 23")
+      expect(page).to have_summary_item(key: "Product brand",             value: new_brand)
+      expect(page).to have_summary_item(key: "Product name",              value: new_name)
+      expect(page).to have_summary_item(key: "Barcode number",            value: new_barcode)
+      expect(page).to have_summary_item(key: "Batch number",              value: new_batch_number)
+      expect(page).to have_summary_item(key: "Other product identifiers", value: new_product_code)
+      expect(page).to have_summary_item(key: "Webpage",                   value: new_webpage)
+      expect(page).to have_summary_item(key: "Country of origin",         value: new_country_of_origin)
+      expect(page).to have_summary_item(key: "Description",               value: new_description)
+      expect(page).to have_summary_item(key: "Customs code",              value: new_customs_code)
+
+      within("header") { click_on "Cases" }
+
+      click_on "All cases – Search"
+
+      fill_in "Search", with: new_name
+      # sleep in order to wait for the changes to be indexed
+      sleep 1
+      click_on "Apply"
+
+      expect(page).to have_listed_case(investigation.pretty_id)
     end
   end
 end
