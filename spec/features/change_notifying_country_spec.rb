e7a325b08c787bb01eaa516442bf2743fcc21770
Kyle MacPherson <kaimac2010@gmail.com>
2022-09-08 17:01:18 +0100

    skip notifying country steps until the notifying country work is done in a future ticket

--- spec/features/change_notifying_country_spec.rb
+++ spec/features/change_notifying_country_spec.rb
@@ -8,8 +8,8 @@ RSpec.feature "Changing the notifying country of a case", :with_stubbed_mailer, 
     before do
       user.roles.create!(name: "notifying_country_editor")
     end
-
-    it "can succesfully change pre-populated notifying_country" do
+    # skipping until the notifying country change is carried out on the new case page work.
+    xit "can succesfully change pre-populated notifying_country" do
       investigation.update!(notifying_country: "country:GB-ENG")
 
       sign_in_and_visit_change_notifying_country_page("England")
@@ -17,7 +17,7 @@ RSpec.feature "Changing the notifying country of a case", :with_stubbed_mailer, 
       select "Scotland", from: "Notifying country"
       click_button "Change"
       expect(page).to have_current_path("/cases/#{investigation.pretty_id}")
-      # expect(page.find("dt", text: "Notifying country")).to have_sibling("dd", text: "Scotland")
+      expect(page.find("dt", text: "Notifying country")).to have_sibling("dd", text: "Scotland")
 
       click_link "Activity"
       expect(page).to have_css("h3", text: "Notifying country changed")
@@ -25,15 +25,15 @@ RSpec.feature "Changing the notifying country of a case", :with_stubbed_mailer, 
     end
   end
 
-  # context "when user is not a notifying_country_editor" do
-  #   it "does not allow user to change country" do
-  #     sign_in user
-  #     visit "/cases/#{investigation.pretty_id}"
-  #     expect(page.find("dt", text: "Notifying country")).to have_sibling("dd", text: "England")
+  context "when user is not a notifying_country_editor" do
+    xit "does not allow user to change country" do
+      sign_in user
+      visit "/cases/#{investigation.pretty_id}"
+      expect(page.find("dt", text: "Notifying country")).to have_sibling("dd", text: "England")
 
-  #     expect(page).not_to have_css("h1", text: "Change notifying country")
-  #   end
-  # end
+      expect(page).not_to have_css("h1", text: "Change notifying country")
+    end
+  end
 
   def sign_in_and_visit_change_notifying_country_page(country)
     sign_in user
