0d920277ce8a1bb484ff6282f37a516b1d4049b2
See Through Data: Staff <85587097+stdstaff@users.noreply.github.com>
2022-09-22 14:49:26 +0100

    Move the redacted export declarations into the model files (#2050)

--- app/models/investigation_business.rb
+++ app/models/investigation_business.rb
@@ -3,5 +3,6 @@ class InvestigationBusiness < ApplicationRecord
   belongs_to :business
   default_scope { order(created_at: :desc) }
 
-  redacted_export_with :id, :business_id, :created_at, :investigation_id, :relationship, :updated_at
+  redacted_export_with :id, :business_id, :created_at, :investigation_id,
+                       :relationship, :updated_at
 end
