9f485ca2211d03acbba6c0d985203b516c70e97b
Kyle MacPherson <kaimac2010@gmail.com>
2022-09-29 10:52:05 +0100

    Add test to assert that people without investigation update permissions can't edit batch numbers

--- spec/features/case_specific_information_spec.rb
+++ spec/features/case_specific_information_spec.rb
@@ -2,7 +2,9 @@ require "rails_helper"
 
 RSpec.describe "Case specific information spec", :with_stubbed_opensearch, :with_stubbed_mailer do
   let(:team) { create :team }
+  let(:other_team) { create :team }
   let(:user) { create :user, :activated, has_viewed_introduction: true, team: }
+  let(:other_user) { create :user, :activated, has_viewed_introduction: true, team: other_team}
   let(:investigation) { create :allegation, creator: user }
   let(:product_1) { create :product }
   let(:product_2) { create :product }
@@ -16,12 +18,11 @@ RSpec.describe "Case specific information spec", :with_stubbed_opensearch, :with
       InvestigationProduct.create!(investigation_id: investigation.id, product_id: product_2.id, customs_code: "XYZ987", batch_number: "2", affected_units_status: "exact", number_of_affected_units: "91")
       InvestigationProduct.create!(investigation_id: investigation.id, product_id: product_3.id, customs_code: "ZZZ999", batch_number: "3", affected_units_status: "approx", number_of_affected_units: "10000")
       InvestigationProduct.create!(investigation_id: investigation.id, product_id: product_4.id, customs_code: "BBB222", batch_number: "1000", affected_units_status: "not_relevant")
-
-      sign_in user
-      visit investigation_path(investigation)
     end
 
     it "shows all info on case specific info section of case page" do
+      sign_in user
+      visit investigation_path(investigation)
       expect_investigation_products_to_be_listed_with_oldest_first
 
       within("dl#product-0") do
@@ -51,33 +52,51 @@ RSpec.describe "Case specific information spec", :with_stubbed_opensearch, :with
       end
     end
 
-    it "allows editing of batch numbers" do
-      within("dl#product-0") do
-        click_link "Edit the batch numbers for #{product_1.name}"
+    context 'when user has permission to update the investigation' do
+      before do
+        sign_in user
+        visit investigation_path(investigation)
       end
 
-      expect_to_be_on_edit_batch_numbers_page(product_id: product_1.id)
+      it "allows editing of batch numbers" do
+        within("dl#product-0") do
+          click_link "Edit the batch numbers for #{product_1.name}"
+        end
 
-      expect(page).to have_field("batch_number",   with: product_1.investigation_products.first.batch_number)
+        expect_to_be_on_edit_batch_numbers_page(product_id: product_1.id)
 
-      fill_in "batch_number", with: new_batch_numbers
+        expect(page).to have_field("batch_number",   with: product_1.investigation_products.first.batch_number)
 
-      click_button "Save"
+        fill_in "batch_number", with: new_batch_numbers
 
-      expect_to_be_on_case_page(case_id: investigation.pretty_id)
+        click_button "Save"
 
-      expect(page).to have_content("The case information was updated")
+        expect_to_be_on_case_page(case_id: investigation.pretty_id)
 
-      within("dl#product-0") do
-        expect(page).to have_css("dt.govuk-summary-list__key", text: "Batch numbers")
-        expect(page).to have_css("dd.govuk-summary-list__value", text: new_batch_numbers)
+        expect(page).to have_content("The case information was updated")
+
+        within("dl#product-0") do
+          expect(page).to have_css("dt.govuk-summary-list__key", text: "Batch numbers")
+          expect(page).to have_css("dd.govuk-summary-list__value", text: new_batch_numbers)
+        end
+
+        click_link "Activity"
+
+        expect_to_be_on_case_activity_page(case_id: investigation.pretty_id)
+        expect(page).to have_css("h3", text: "Batch number updated")
+        expect(page).to have_content("Batch number: #{new_batch_numbers}")
+      end
+    end
+
+    context 'when user does not have permissions to update the investigation' do
+      before do
+        sign_in other_user
+        visit investigation_path(investigation)
       end
 
-      click_link "Activity"
-
-      expect_to_be_on_case_activity_page(case_id: investigation.pretty_id)
-      expect(page).to have_css("h3", text: "Batch number updated")
-      expect(page).to have_content("Batch number: #{new_batch_numbers}")
+      it 'does not allow editing of the batch numbers' do
+        expect(page).to_not have_link("Edit the batch numbers for #{product_1.name}")
+      end
     end
   end
 
