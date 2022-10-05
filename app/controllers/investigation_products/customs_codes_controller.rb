d56603935f9251cccc251cd080081efd467c1e5a
Kyle MacPherson <kaimac2010@gmail.com>
2022-10-05 14:27:55 +0100

    Allow editing of customs code

--- app/controllers/investigation_products/customs_codes_controller.rb
+++ app/controllers/investigation_products/customs_codes_controller.rb
@@ -5,20 +5,20 @@ module InvestigationProducts
       authorize @investigation_product.investigation, :update?
     end
 
-    # def update
-    #   @investigation_product = InvestigationProduct.find(params[:investigation_product_id])
-    #   authorize @investigation_product.investigation, :update?
-    #
-    #   if @investigation_product.batch_number == batch_number_params[:batch_number]
-    #     return redirect_to investigation_path(@investigation_product.investigation)
-    #   end
-    #
-    #   ChangeBatchNumber.call!(investigation_product: @investigation_product, batch_number: batch_number_params[:batch_number], user: current_user)
-    #   redirect_to investigation_path(@investigation_product.investigation), flash: { success: "The case information was updated" }
-    # end
+    def update
+      @investigation_product = InvestigationProduct.find(params[:investigation_product_id])
+      authorize @investigation_product.investigation, :update?
 
-    # def batch_number_params
-    #   params.permit(:batch_number)
-    # end
+      if @investigation_product.customs_code.to_s == customs_code_params[:customs_code]
+        return redirect_to investigation_path(@investigation_product.investigation)
+      end
+
+      ChangeCustomsCode.call!(investigation_product: @investigation_product, customs_code: customs_code_params[:customs_code], user: current_user)
+      redirect_to investigation_path(@investigation_product.investigation), flash: { success: "The case information was updated" }
+    end
+
+    def customs_code_params
+      params.permit(:customs_code)
+    end
   end
 end
