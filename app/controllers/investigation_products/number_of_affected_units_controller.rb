5fda23f7db23236897ececac66333d63abef2df8
Kyle MacPherson <kaimac2010@gmail.com>
2022-10-12 14:20:33 +0100

    Remove redundant code

--- app/controllers/investigation_products/number_of_affected_units_controller.rb
+++ app/controllers/investigation_products/number_of_affected_units_controller.rb
@@ -21,12 +21,10 @@ module InvestigationProducts
       redirect_to investigation_path(@investigation_product.investigation), flash: result.changed ? { success: "The case information was updated" } : nil
     end
 
+  private
+
     def number_of_affected_units_params
       params.require(:number_of_affected_units_form).permit(:affected_units_status, :exact_units, :approx_units)
-    end
-
-    def investigation_product_has_no_changes?
-      @investigation_product.affected_units_status == @number_of_affected_units_form.affected_units_status && @investigation_product.number_of_affected_units == @number_of_affected_units_form.number_of_affected_units
     end
   end
 end
