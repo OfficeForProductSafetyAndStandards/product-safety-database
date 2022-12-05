7e68bec72476731a5fe7a18e7da147c479054d4e
Kyle MacPherson <kaimac2010@gmail.com>
2022-12-05 14:21:26 +0000

    Remove default scope from MoveOwnersToCollaboration migration

--- db/migrate/20200615121613_move_owners_to_collaboration.rb
+++ db/migrate/20200615121613_move_owners_to_collaboration.rb
@@ -1,7 +1,7 @@
 class MoveOwnersToCollaboration < ActiveRecord::Migration[5.2]
   def change
     safety_assured do
-      Investigation.all.each do |i|
+      Investigation.unscoped.all.each do |i|
         owner = i[:owner_type].constantize.find(i[:owner_id])
         i.owner = owner
         i.save!
