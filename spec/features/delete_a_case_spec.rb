35dabd2b9d7e8236fe646aacb5a8741ebd1427c8
Kyle MacPherson <kaimac2010@gmail.com>
2022-12-05 16:37:20 +0000

    Fix delete a case spec to properly test feature

--- spec/features/delete_a_case_spec.rb
+++ spec/features/delete_a_case_spec.rb
@@ -4,22 +4,22 @@ RSpec.describe "Deleting a case", :with_stubbed_opensearch, :with_stubbed_mailer
   let(:user) { create(:user, :activated, :opss_user, name: "Jane Jones") }
 
   context "when case has products associated with it" do
-    let!(:investigation) { create(:allegation, creator: user, is_closed: false) }
+    let!(:investigation) { create(:allegation, :with_products, creator: user, is_closed: false) }
 
-    it "does not allow user to delete case" do
+    it "allows user to close the case" do
       sign_in user
       visit "/cases/#{investigation.pretty_id}"
 
       click_link "Close case"
 
-      expect_to_be_on_cannot_close_case_page(case_id: investigation.pretty_id)
+      expect_to_be_on_close_case_page(case_id: investigation.pretty_id)
     end
   end
 
   context "when case does not have products associated with it" do
     let!(:investigation) { create(:allegation, creator: user, is_closed: false) }
 
-    it "allows user to delete case" do
+    it "does not allow user to close case, redirects to a delete case page" do
       sign_in user
       visit "/cases/#{investigation.pretty_id}"
 
