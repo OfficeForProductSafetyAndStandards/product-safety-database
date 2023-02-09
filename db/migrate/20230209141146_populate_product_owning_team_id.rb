0b2a15de270da070b7f994dc94fc3696a64a0d11
Kyle MacPherson <kaimac2010@gmail.com>
2023-02-09 15:36:03 +0000

    rubocop linting

--- db/migrate/20230209141146_populate_product_owning_team_id.rb
+++ db/migrate/20230209141146_populate_product_owning_team_id.rb
@@ -1,12 +1,12 @@
 class PopulateProductOwningTeamId < ActiveRecord::Migration[7.0]
   def up
-    sql = "UPDATE products 
-    SET owning_team_id = c.collaborator_id 
-    FROM collaborations c 
-    INNER JOIN investigation_products ON c.investigation_id = investigation_products.investigation_id 
-    INNER JOIN investigations ON investigation_products.investigation_id = investigations.id 
-    WHERE c.type = 'Collaboration::Access::OwnerTeam' 
-    AND investigations.is_closed = false 
+    sql = "UPDATE products
+    SET owning_team_id = c.collaborator_id
+    FROM collaborations c
+    INNER JOIN investigation_products ON c.investigation_id = investigation_products.investigation_id
+    INNER JOIN investigations ON investigation_products.investigation_id = investigations.id
+    WHERE c.type = 'Collaboration::Access::OwnerTeam'
+    AND investigations.is_closed = false
     AND products.owning_team_id IS NULL;"
 
     ApplicationRecord.connection.exec_update(sql)
