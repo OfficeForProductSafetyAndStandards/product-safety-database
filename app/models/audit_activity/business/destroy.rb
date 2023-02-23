6497e8e521994b20785c9f3e7dbdd5caed8a33a7
Andy Callaghan <github@andycallaghan.com>
2023-02-23 16:34:36 +0000

    Remove metadata migration methods
    This migration code is old, we need check to make sure this has all been migrated prior to deploy

--- app/models/audit_activity/business/destroy.rb
+++ app/models/audit_activity/business/destroy.rb
@@ -5,25 +5,7 @@ class AuditActivity::Business::Destroy < AuditActivity::Base
     { reason:, business: business.attributes }
   end
 
-  def metadata
-    migrate_metadata_structure
-  end
-
-private
-
   def subtitle_slug
     "Business removed"
-  end
-
-  def migrate_metadata_structure
-    metadata = self[:metadata]
-
-    return metadata if already_in_new_format?
-
-    JSON.parse({ business: business.attributes }.to_json)
-  end
-
-  def already_in_new_format?
-    self[:metadata]&.key?("business")
   end
 end
