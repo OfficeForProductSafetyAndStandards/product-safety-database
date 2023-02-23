902edd54b746db13356eb90d5d6bc67a133dd886
Andy Callaghan <github@andycallaghan.com>
2023-02-23 16:34:36 +0000

    Remove metadata migration methods
    This migration code is old, we need check to make sure this has all been migrated prior to deploy

--- app/models/audit_activity/document/add.rb
+++ app/models/audit_activity/document/add.rb
@@ -5,10 +5,6 @@ class AuditActivity::Document::Add < AuditActivity::Document::Base
     blob.metadata.merge(blob_id: blob.id)
   end
 
-  def metadata
-    migrate_metadata_structure
-  end
-
   def title(_user)
     metadata["title"]
   end
@@ -19,23 +15,5 @@ class AuditActivity::Document::Add < AuditActivity::Document::Base
 
   def restricted_title(_user)
     "Document added"
-  end
-
-private
-
-  def migrate_metadata_structure
-    metadata = self[:metadata]
-
-    return metadata if metadata
-
-    new_metadata = self.class.build_metadata(attachment.blob)
-    new_metadata["title"] = self[:title]
-    new_metadata["description"] = self[:body]
-
-    JSON.parse(new_metadata.to_json)
-  end
-
-  def subtitle_slug
-    "#{attachment_type} added"
   end
 end
