e660236c1dc7a28f8e31e23e4e69b44646bbf838
Andy Callaghan <github@andycallaghan.com>
2023-03-10 14:03:07 +0000

    Remove product links on risk assessment, test result activity pages (#2305)
    * remove product links on risk assessment, test result pages
    
    * remove product links in risk assessment, tests pages

--- app/helpers/investigations/accident_or_incidents_helper.rb
+++ app/helpers/investigations/accident_or_incidents_helper.rb
@@ -1,15 +1,9 @@
 module Investigations
   module AccidentOrIncidentsHelper
     def accident_or_incident_summary_list_rows(accident_or_incident)
-      product_value = if accident_or_incident.investigation_product.investigation_closed_at
-                        { text: "#{accident_or_incident.investigation_product.name} (#{accident_or_incident.investigation_product.psd_ref})" }
-                      else
-                        { html: link_to("#{accident_or_incident.investigation_product.name} (#{accident_or_incident.investigation_product.psd_ref})", product_path(accident_or_incident.investigation_product.product)) }
-                      end
-
       rows = [
         { key: { text: "Date of #{accident_or_incident.type.downcase}" }, value: { text: accident_or_incident.date_of_activity } },
-        { key: { text: "Product" },          value: product_value },
+        { key: { text: "Product" },          value: { text: "#{accident_or_incident.investigation_product.name} (#{accident_or_incident.investigation_product.psd_ref})" } },
         { key: { text: "Severity" },         value: { text: accident_or_incident.severity } },
         { key: { text: "Product usage" },    value: { html: accident_or_incident.usage } }
       ]
