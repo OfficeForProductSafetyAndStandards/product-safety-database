32b321800438e32517936ae66bb62284225aba9a
Chris Gannon <cjgannon@gmail.com>
2023-03-30 16:32:03 +0100

    tmp

--- spec/factories/organisations.rb
+++ spec/factories/organisations.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :organisation do
     name { "test organisation" }
+
+    trait :internal_opss_team do
+      internal_opss_team { true }
+    end
+
+    trait :external_regulator do
+      external_regulator { true }
+      regulator
+    end
+
+    trait :local_authority do
+      local_authority { true }
+      ts_region
+    end
   end
 end
