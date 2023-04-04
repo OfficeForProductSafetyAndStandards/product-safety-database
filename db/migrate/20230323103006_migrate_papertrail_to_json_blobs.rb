877cba33e874b88d048778ecd7f77c40e5170f61
Andy Callaghan <github@andycallaghan.com>
2023-04-04 12:03:49 +0100

    add aliases:true keyword for yaml loading, error given in staging

--- db/migrate/20230323103006_migrate_papertrail_to_json_blobs.rb
+++ db/migrate/20230323103006_migrate_papertrail_to_json_blobs.rb
@@ -6,11 +6,11 @@ class MigratePapertrailToJsonBlobs < ActiveRecord::Migration[7.0]
     add_column :versions, :new_object_changes, :jsonb, if_not_exists: true
 
     PaperTrail::Version.where.not(object: nil).find_each do |version|
-      new_object = Psych.safe_load(version.object, permitted_classes: [Date, Time])
+      new_object = Psych.safe_load(version.object, aliases: true, permitted_classes: [Date, Time])
       version.update_column(:new_object, new_object)
 
       if version.object_changes
-        new_object_changes = Psych.safe_load(version.object_changes, permitted_classes: [Date, Time])
+        new_object_changes = Psych.safe_load(version.object_changes, aliases: true, permitted_classes: [Date, Time])
         version.update_column(:new_object_changes, new_object_changes)
       end
     end
