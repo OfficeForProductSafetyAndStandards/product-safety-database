18f62b0cd78a95f6dfb5942a646c874ceb8472bf
Andy Callaghan <github@andycallaghan.com>
2023-04-17 16:19:28 +0100

    mock duplicate check page, controller setup

--- app/controllers/product_duplicate_checks_controller.rb
+++ app/controllers/product_duplicate_checks_controller.rb
@@ -10,25 +10,26 @@ class ProductDuplicateChecksController < ApplicationController
 
   def create
     @product_duplicate_check_form = ProductDuplicateCheckForm.new(product_duplicate_check_form_params)
+    return render :new unless @product_duplicate_check_form.valid?
 
-    if @product_duplicate_check_form.valid?
-      # TODO: if we find a duplicate, we should ask if they want to use the existing product
-      if @product_duplicate_check_form.has_barcode
-        # TODO: get product form to prefill barcode here
-        redirect_to new_product_path(barcode: @product_duplicate_check_form.barcode)
-      else
-        # TODO: do we need a flash message here?
-        redirect_to new_product_path
-      end
+    @product_duplicates = FindProductDuplicates.call(barcode: @product_duplicate_check_form.barcode)
+    if @product_duplicates.duplicates.any?
+      first_duplicate_product = @product_duplicates.duplicates.first
+      return redirect_to product_duplicate_check_path(id: first_duplicate_product.id)
+    end
+
+    if @product_duplicate_check_form.has_barcode
+      redirect_to new_product_path(barcode: @product_duplicate_check_form.barcode)
     else
-      render :new
+      # TODO: do we need a flash message here?
+      redirect_to new_product_path
     end
   end
 
 private
 
   def find_product
-    @product = Product.find_by!(pretty_id: params[:id])
+    @product = Product.find(params[:id])
   end
 
   def product_duplicate_check_form_params
