de7ea236fd41041e75c9702a73fc24b5d722836f
Andy Callaghan <github@andycallaghan.com>
2023-04-19 14:55:12 +0100

    added product duplicate form, added spec

--- app/forms/product_duplicate_check_form.rb
+++ app/forms/product_duplicate_check_form.rb
@@ -4,10 +4,11 @@ class ProductDuplicateCheckForm
   include ActiveModel::Validations::Callbacks
 
   attribute :barcode
-  attribute :has_barcode, :boolean, default: false
+  attribute :has_barcode, :boolean
 
-  validates :barcode, allow_nil: true, numericality: { only_integer: true }, if: -> { has_barcode }
-  validates :barcode, allow_nil: true, length: { minimum: 5, maximum: 15 }, if: -> { has_barcode && barcode =~ /\A\d+\z/ }
+  validates :has_barcode, inclusion: { in: [true, false] }
+  validates :barcode, numericality: { only_integer: true }, if: -> { has_barcode }
+  validates :barcode, length: { minimum: 5, maximum: 15 }, if: -> { has_barcode && barcode =~ /\A\d+\z/ }
 
   before_validation :strip_barcode
 
