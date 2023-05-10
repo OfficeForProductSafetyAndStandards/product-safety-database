8d9cd4baa3d3650136e63f8477fd18a53708bb37
Andy Callaghan <github@andycallaghan.com>
2023-05-10 11:10:21 +0100

    wait for av container to boot before proceeding

--- docker/start.sh
+++ docker/start.sh
@@ -2,6 +2,7 @@
 set -xeuo pipefail
 ./docker/wait-for-tcp.sh db 5432
 ./docker/wait-for-tcp.sh redis 6379
+./docker/wait-for-tcp.sh antivirus 3006
 
 if [[ -f ./tmp/pids/server.pid ]]; then
   rm ./tmp/pids/server.pid
