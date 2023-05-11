cc7bc86bdc9434eb097afdd0b67ed376640ff1b0
Andy Callaghan <github@andycallaghan.com>
2023-05-11 15:03:56 +0100

    refactor test results controller, move lookup to before action

--- app/controllers/investigations/test_results_controller.rb
+++ app/controllers/investigations/test_results_controller.rb
@@ -1,18 +1,17 @@
 class Investigations::TestResultsController < ApplicationController
+  before_action :set_investigation
+
   def new
-    investigation = Investigation.find_by(pretty_id: params[:investigation_pretty_id])
-    authorize investigation, :update?
+    authorize @investigation_object, :update?
     @test_result_form = TestResultForm.new
-    @investigation = investigation.decorate
   end
 
   def create
     investigation = Investigation.find_by(pretty_id: params[:investigation_pretty_id])
-    authorize investigation, :update?
+    authorize @investigation_object, :update?
     @test_result_form = TestResultForm.new(test_result_params)
     @test_result_form.load_document_file
 
-    @investigation = investigation.decorate
     return render :new if @test_result_form.invalid?(:create_with_investigation_product)
 
     service_attributes = @test_result_form
@@ -25,37 +24,31 @@ class Investigations::TestResultsController < ApplicationController
       return redirect_to investigation_supporting_information_index_path(investigation), flash: { success: "The supporting information was updated" }
     end
 
-    @investigation = investigation.decorate
     render :new
   end
 
   def show
-    investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
-    authorize investigation, :view_non_protected_details?
-    @test_result = investigation.test_results.find(params[:id]).decorate
-    @investigation = investigation.decorate
+    authorize @investigation_object, :view_non_protected_details?
+    @test_result = @investigation_object.test_results.find(params[:id]).decorate
   end
 
   def edit
-    investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
-    authorize investigation, :update?
-    @test_result_form = TestResultForm.from(investigation.test_results.find(params[:id]))
+    authorize @investigation_object, :update?
+    @test_result_form = TestResultForm.from(@investigation_object.test_results.find(params[:id]))
     @test_result_form.load_document_file
-    @investigation = investigation.decorate
   end
 
   def update
-    investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
     authorize investigation, :update?
 
-    test_result = investigation.test_results.find(params[:id])
+    test_result = @investigation_object.test_results.find(params[:id])
 
     @test_result_form = TestResultForm.from(test_result)
     @test_result_form.assign_attributes(test_result_params)
     @test_result_form.load_document_file
 
     if @test_result_form.invalid?(:create_with_investigation_product)
-      @investigation = investigation.decorate
+      @investigation = @investigation_object.decorate
       return render :edit
     end
 
@@ -71,6 +64,11 @@ class Investigations::TestResultsController < ApplicationController
   end
 
 private
+
+  def set_investigation
+    @investigation_object = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
+    @investigation = @investigation_object.decorate
+  end
 
   def test_params
     test_session_params.merge(test_result_request_params)
