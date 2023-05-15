450bb00f5e09047dad2519ab725928949f705694
Ruben Arakelyan <ruben@arakelyan.uk>
2023-05-15 09:32:12 +0100

    Remember cookie consent (#2417)
    Changes the cookie consent cookie to be remembered for 1 year rather than the duration of the browser session.
    
    This means users that respond to the cookie banner will not see it again for 1 year or until they delete all cookies.

--- spec/requests/cookies_spec.rb
+++ spec/requests/cookies_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
-describe "user declarations", type: :request do
+describe "cookies", type: :request do
   before do
     cookies["essential"] = "psd"
     cookies["_ga_NSLSMEMX9S"] = "foo"
     cookies["_gid"] = "bar"
     cookies["_gat_gtag_UA_126364208_2"] = "baz"
 
-    cookies["accept_analytics_cookies"] = accept_analytics_cookies
+    cookies["accept_analytics_cookies"] = { value: accept_analytics_cookies, expires: 1.year.from_now }
 
     get "/help/terms-and-conditions"
   end
@@ -17,8 +17,7 @@ describe "user declarations", type: :request do
 
     it "has analytics cookies" do
       # response is not returning any cookies to delete
-      # returned cookie is the one that stores cookie options
-      expect(response.cookies.to_hash.size).to eq(1)
+      expect(cookies.to_hash.size).to eq(6)
     end
   end
 
