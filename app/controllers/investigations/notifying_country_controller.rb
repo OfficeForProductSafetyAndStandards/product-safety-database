782357ca3ce84247beae177e3beca6f81f28ed2c
Chris Gannon <cjgannon@gmail.com>
2023-05-17 11:53:32 +0100

    cosmetic changes

--- app/controllers/investigations/notifying_country_controller.rb
+++ app/controllers/investigations/notifying_country_controller.rb
@@ -21,7 +21,7 @@ module Investigations
         )
 
         @investigation = investigation.decorate
-        redirect_to investigation_path(@investigation), flash: { success: "The notifying country was updated." }
+        redirect_to investigation_path(@investigation), flash: { success: "The notifying country was updated" }
       else
         @investigation = investigation.decorate
         render :edit
