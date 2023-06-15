2267bdf4c0e1065ae241ee1559b3cb646dec5f39
Andy Callaghan <github@andycallaghan.com>
2023-06-15 15:08:46 +0100

    don't render a breadcrumb on devise auth pages

--- app/helpers/breadcrumb_helper.rb
+++ app/helpers/breadcrumb_helper.rb
@@ -1,5 +1,8 @@
 module BreadcrumbHelper
   def display_breadcrumbs?
+    devise_controllers = ["users/sessions", "users/unlocks", "users/passwords", "users/check_your_email"]
+    return false if devise_controllers.include?(params[:controller])
+
     request.path != authenticated_root_path
   end
 end
