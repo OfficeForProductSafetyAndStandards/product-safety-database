eea7e939e3680c316fb417f0bad9ed945c3419d7
Andy Callaghan <github@andycallaghan.com>
2023-06-15 17:48:25 +0100

    refactor controllers, breadcrumbs need @investigation var

--- app/controllers/investigations/notifying_country_controller.rb
+++ app/controllers/investigations/notifying_country_controller.rb
@@ -1,34 +1,38 @@
 module Investigations
   class NotifyingCountryController < ApplicationController
+    before_action :set_investigation
+    before_action :set_case_breadcrumbs
+
     def edit
-      investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
-      authorize investigation, :change_notifying_country?
-      @notifying_country_form = NotifyingCountryForm.from(investigation)
-      @investigation = investigation.decorate
+      @notifying_country_form = NotifyingCountryForm.from(@investigation_object)
     end
 
     def update
-      investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
-      authorize investigation, :change_notifying_country?
       @notifying_country_form = NotifyingCountryForm.new(notifying_country_params)
 
       if @notifying_country_form.valid?
         ChangeNotifyingCountry.call!(
           @notifying_country_form.serializable_hash.merge({
-            investigation:,
+            investigation: @investigation_object,
             user: current_user
           })
         )
 
-        @investigation = investigation.decorate
+        @investigation = @investigation_object.decorate
         redirect_to investigation_path(@investigation), flash: { success: "The notifying country was updated" }
       else
-        @investigation = investigation.decorate
+        @investigation = @investigation_object.decorate
         render :edit
       end
     end
 
   private
+
+    def set_investigation
+      @investigation_object = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
+      @investigation = @investigation_object.decorate
+      authorize @investigation_object, :change_notifying_country?
+    end
 
     def notifying_country_params
       params.require(:investigation).permit(:notifying_country_uk, :notifying_country_overseas, :overseas_or_uk)
--- app/controllers/investigations/overseas_regulator_controller.rb
+++ app/controllers/investigations/overseas_regulator_controller.rb
@@ -1,34 +1,38 @@
 module Investigations
   class OverseasRegulatorController < ApplicationController
+    before_action :set_investigation
+    before_action :set_case_breadcrumbs
+
     def edit
-      investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
-      authorize investigation, :change_overseas_regulator?
-      @overseas_regulator_form = OverseasRegulatorForm.from(investigation)
-      @investigation = investigation.decorate
+      @overseas_regulator_form = OverseasRegulatorForm.from(@investigation_object)
     end
 
     def update
-      investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
-      authorize investigation, :change_overseas_regulator?
       @overseas_regulator_form = OverseasRegulatorForm.new(overseas_regulator_params)
 
       if @overseas_regulator_form.valid?
         ChangeOverseasRegulator.call!(
           @overseas_regulator_form.serializable_hash.merge({
-            investigation:,
+            investigation: @investigation_object,
             user: current_user
           })
         )
 
-        @investigation = investigation.decorate
+        @investigation = @investigation_object.decorate
         redirect_to investigation_path(@investigation), flash: { success: "The overseas regulator was updated." }
       else
-        @investigation = investigation.decorate
+        @investigation = @investigation_object.decorate
         render :edit
       end
     end
 
   private
+
+    def set_investigation
+      @investigation_object = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
+      @investigation = @investigation_object.decorate
+      authorize @investigation_object, :change_overseas_regulator?
+    end
 
     def overseas_regulator_params
       params.require(:investigation).permit(:is_from_overseas_regulator, :overseas_regulator_country)
