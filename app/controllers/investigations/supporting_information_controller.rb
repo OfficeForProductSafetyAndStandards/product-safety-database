e7e34b3f57b66d70f14eaf576f0603ef05eefba5
Andy Callaghan <github@andycallaghan.com>
2023-06-16 12:19:55 +0100

    supporting info tab breadcrumbs

--- app/controllers/investigations/supporting_information_controller.rb
+++ app/controllers/investigations/supporting_information_controller.rb
@@ -1,48 +1,49 @@
 module Investigations
-  class SupportingInformationController < ApplicationController
+  class SupportingInformationController < Investigations::BaseController
+    before_action :set_investigation
+    before_action :authorize_investigation_updates, only: %i[new add_to_case create]
+    before_action :set_case_breadcrumbs
+
     def index
-      authorize investigation, :view_non_protected_details?
+      authorize @investigation, :view_non_protected_details?
       @grouped_supporting_information = {
         "Accident or incidents" => {
-          items: investigation.unexpected_events.order(created_at: :desc).map(&:decorate),
-          new_path: new_investigation_accident_or_incidents_type_path(investigation)
+          items: @investigation.unexpected_events.order(created_at: :desc).map(&:decorate),
+          new_path: new_investigation_accident_or_incidents_type_path(@investigation)
         },
         "Corrective actions" => {
-          items: investigation.corrective_actions.order(created_at: :desc).map(&:decorate),
-          new_path: new_investigation_corrective_action_path(investigation)
+          items: @investigation.corrective_actions.order(created_at: :desc).map(&:decorate),
+          new_path: new_investigation_corrective_action_path(@investigation)
         },
         "Risk assessments" => {
-          items: investigation.risk_assessments.order(created_at: :desc).map(&:decorate),
-          new_path: new_investigation_risk_assessment_path(investigation)
+          items: @investigation.risk_assessments.order(created_at: :desc).map(&:decorate),
+          new_path: new_investigation_risk_assessment_path(@investigation)
         },
         "Correspondence" => {
-          items: investigation.correspondences.order(created_at: :desc).map(&:decorate),
-          new_path: new_investigation_correspondence_path(investigation)
+          items: @investigation.correspondences.order(created_at: :desc).map(&:decorate),
+          new_path: new_investigation_correspondence_path(@investigation)
         },
         "Test results" => {
-          items: investigation.test_results.order(created_at: :desc).map(&:decorate),
-          new_path: new_investigation_funding_source_path(investigation)
+          items: @investigation.test_results.order(created_at: :desc).map(&:decorate),
+          new_path: new_investigation_funding_source_path(@investigation)
         },
         "Other" => {
-          items: investigation.generic_supporting_information_attachments.order(created_at: :desc).map(&:decorate),
-          new_path: new_investigation_document_path(investigation)
+          items: @investigation.generic_supporting_information_attachments.order(created_at: :desc).map(&:decorate),
+          new_path: new_investigation_document_path(@investigation)
         }
       }
     end
 
     def new
-      authorize investigation, :update?
       supporting_information_type_form
     end
 
     def add_to_case
-      authorize investigation, :update?
       @add_to_case_action = true
       supporting_information_type_form
     end
 
     def create
-      authorize investigation, :update?
       @add_to_case_action = params["add_to_case_action"].present?
       return render(:new) if supporting_information_type_form.invalid?
 
@@ -62,19 +63,13 @@ module Investigations
       when "risk_assessment"
         redirect_to new_investigation_risk_assessment_path(@investigation)
       when "product"
-        redirect_to new_investigation_product_path(investigation)
+        redirect_to new_investigation_product_path(@investigation)
       when "business"
-        redirect_to new_investigation_business_types_path(investigation)
+        redirect_to new_investigation_business_types_path(@investigation)
       end
     end
 
   private
-
-    def investigation
-      @investigation ||= Investigation
-                        .find_by!(pretty_id: params[:investigation_pretty_id])
-                        .decorate
-    end
 
     def supporting_information_type_form
       options = if @add_to_case_action
