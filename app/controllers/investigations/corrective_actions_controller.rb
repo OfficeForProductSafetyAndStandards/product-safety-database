de0e01a942a7027ce9b1af9f59ce1b8296ba20b7
Andy Callaghan <github@andycallaghan.com>
2023-06-16 10:34:35 +0100

    corrective actions breadcrumbs

--- app/controllers/investigations/corrective_actions_controller.rb
+++ app/controllers/investigations/corrective_actions_controller.rb
@@ -1,28 +1,23 @@
 module Investigations
-  class CorrectiveActionsController < ApplicationController
+  class CorrectiveActionsController < Investigations::BaseController
     include CorrectiveActionsConcern
+    before_action :set_investigation
+    before_action :authorize_investigation_updates, except: %i[show]
+    before_action :set_case_breadcrumbs
 
     def new
-      investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
-      authorize investigation, :update?
       @corrective_action_form = CorrectiveActionForm.new
-      @investigation = investigation.decorate
     end
 
     def create
-      investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
-      authorize investigation, :update?
-
       @corrective_action_form = CorrectiveActionForm.new(corrective_action_params)
-
-      @investigation = investigation.decorate
       @file_blob = @corrective_action_form.document
       return render :new if @corrective_action_form.invalid?(:add_corrective_action)
 
       result = AddCorrectiveActionToCase.call(
         @corrective_action_form
           .serializable_hash(except: :further_corrective_action)
-          .merge(user: current_user, investigation:)
+          .merge(user: current_user, investigation: @investigation)
       )
 
       if result.success?
@@ -33,32 +28,23 @@ module Investigations
     end
 
     def show
-      @investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id]).decorate
       authorize @investigation, :view_non_protected_details?
       @corrective_action = @investigation.corrective_actions.find(params[:id]).decorate
     end
 
     def edit
-      investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
-      authorize investigation, :update?
-      corrective_action = investigation.corrective_actions.find(params[:id])
+      corrective_action = @investigation.corrective_actions.find(params[:id])
       @corrective_action_form = CorrectiveActionForm.from(corrective_action)
 
       @file_blob = corrective_action.document_blob
-      @investigation = investigation.decorate
     end
 
     def update
-      investigation = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
-      authorize investigation, :update?
-
-      corrective_action       = investigation.corrective_actions.find(params[:id])
+      corrective_action       = @investigation.corrective_actions.find(params[:id])
       @corrective_action_form = CorrectiveActionForm.from(corrective_action)
       @file_blob              = corrective_action.document_blob
-      @investigation          = investigation.decorate
 
       @corrective_action_form.assign_attributes(corrective_action_params)
-
       return render :edit if @corrective_action_form.invalid?(:edit_corrective_action)
 
       UpdateCorrectiveAction.call!(
@@ -71,7 +57,7 @@ module Investigations
           )
       )
 
-      redirect_to investigation_supporting_information_index_path(investigation), flash: { success: "The supporting information was updated" }
+      redirect_to investigation_supporting_information_index_path(@investigation), flash: { success: "The supporting information was updated" }
     end
   end
 end
