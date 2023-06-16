e08fd40850ac77804d29aad603f8218ab0759cc8
Andy Callaghan <github@andycallaghan.com>
2023-06-16 12:53:03 +0100

    fix notifying country spec, add breadcrumbs

--- app/controllers/investigations/notifying_country_controller.rb
+++ app/controllers/investigations/notifying_country_controller.rb
@@ -1,10 +1,11 @@
 module Investigations
-  class NotifyingCountryController < ApplicationController
+  class NotifyingCountryController < Investigations::BaseController
     before_action :set_investigation
+    before_action :authorize_change_notifying_country
     before_action :set_case_breadcrumbs
 
     def edit
-      @notifying_country_form = NotifyingCountryForm.from(@investigation_object)
+      @notifying_country_form = NotifyingCountryForm.from(@investigation)
     end
 
     def update
@@ -13,25 +14,21 @@ module Investigations
       if @notifying_country_form.valid?
         ChangeNotifyingCountry.call!(
           @notifying_country_form.serializable_hash.merge({
-            investigation: @investigation_object,
+            investigation: @investigation,
             user: current_user
           })
         )
 
-        @investigation = @investigation_object.decorate
         redirect_to investigation_path(@investigation), flash: { success: "The notifying country was updated" }
       else
-        @investigation = @investigation_object.decorate
         render :edit
       end
     end
 
   private
 
-    def set_investigation
-      @investigation_object = Investigation.find_by!(pretty_id: params[:investigation_pretty_id])
-      @investigation = @investigation_object.decorate
-      authorize @investigation_object, :change_notifying_country?
+    def authorize_change_notifying_country
+      authorize @investigation, :change_notifying_country?
     end
 
     def notifying_country_params
