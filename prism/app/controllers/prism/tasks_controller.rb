7a9c1e821ceb37357f0c8ca94d37a21c0d034181
Ruben Arakelyan <ruben@arakelyan.uk>
2023-06-22 13:58:55 +0100

    PRISM: initial triage and authentication
    Adds the initial triage question pages and PSD authentication.
    
    Authentication pages currently use the PSD template but this will be changed later to use the PRISM template.

---
+++ prism/app/controllers/prism/tasks_controller.rb
@@ -0,0 +1,6 @@
+module Prism
+  class TasksController < ApplicationController
+    def index; end
+    def index_serious_risk; end
+  end
+end
--- prism/config/routes.rb
+++ prism/config/routes.rb
@@ -1,3 +1,19 @@
 Prism::Engine.routes.draw do
-  get "/", to: ->(_) { [200, {}, %w[OK]] }
+  root "triage#index"
+
+  scope "/triage" do
+    get "full-risk-assessment-required", to: "triage#full_risk_assessment_required"
+    patch "full-risk-assessment-required", to: "triage#full_risk_assessment_required_choose"
+    put "full-risk-assessment-required", to: "triage#full_risk_assessment_required_choose"
+    get "serious-risk", to: "triage#serious_risk"
+    patch "serious-risk", to: "triage#serious_risk_choose"
+    put "serious-risk", to: "triage#serious_risk_choose"
+    get "serious-risk-rebuttable", to: "triage#serious_risk_rebuttable"
+    patch "serious-risk-rebuttable", to: "triage#serious_risk_rebuttable_choose"
+    put "serious-risk-rebuttable", to: "triage#serious_risk_rebuttable_choose"
+    get "perform-risk-triage", to: "triage#perform_risk_triage"
+  end
+
+  get "tasks", to: "tasks#index"
+  get "tasks-serious-risk", to: "tasks#index_serious_risk"
 end
