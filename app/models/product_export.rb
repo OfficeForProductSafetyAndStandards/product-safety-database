0ee117578225668890e631a8916fa3bfcfcf20a4
Ruben Arakelyan <ruben@arakelyan.uk>
2023-07-12 10:01:40 +0100

    Move product search from OpenSearch to ActiveRecord
    Changes the product search to use normal ActiveRecord queries rather than OpenSearch.

--- app/models/product_export.rb
+++ app/models/product_export.rb
@@ -1,5 +1,5 @@
 class ProductExport < ApplicationRecord
-  include ProductSearchHelper
+  include SearchHelper
 
   # Helps to manage the database query execution time within the PaaS imposed limits
   FIND_IN_BATCH_SIZE = 1000
@@ -210,5 +210,43 @@ private
     return if code.blank?
 
     code.split(":")&.last
+  end
+
+  def filter_params(user)
+    must_match_filters = [
+      get_category_filter,
+      get_status_filter,
+      get_retired_filter
+    ].compact
+
+    should_match_filters = [
+      get_owner_filter(user)
+    ].compact.flatten
+
+    { must: must_match_filters, should: should_match_filters }
+  end
+
+  def get_category_filter
+    if params[:category].present?
+      { match_phrase: { "category" => @search.category } }
+    end
+  end
+
+  def get_status_filter
+    if @search.case_status == "open_only"
+      { term: { "investigations.is_closed" => "false" } }
+    end
+  end
+
+  def get_retired_filter
+    return if @search.retired_status == "all"
+
+    if @search.retired_status == "active" || @search.retired_status.blank?
+      return { term: { "retired?" => false } }
+    end
+
+    if @search.retired_status == "retired"
+      { term: { "retired?" => true } }
+    end
   end
 end
