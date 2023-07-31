0515ffa61f163fd6b757b9e08f100fac179f144f
Andy Callaghan <github@andycallaghan.com>
2023-07-31 11:29:32 +0100

    added more case information, ready to assert its output

--- spec/models/case_export_spec.rb
+++ spec/models/case_export_spec.rb
@@ -6,7 +6,15 @@ RSpec.describe CaseExport, :with_opensearch, :with_stubbed_notify, :with_stubbed
   let!(:other_team) { create(:team, organisation:) }
   let!(:user) { create(:user, :activated, :opss_user, organisation:, team:, has_viewed_introduction: true) }
   let!(:other_user_other_team) { create(:user, :activated, :opss_user, name: "other user same team", organisation:, team: other_team) }
-  let!(:investigation) { create(:allegation, creator: user).decorate }
+  let!(:investigation) do
+    create(:allegation,
+           creator: user,
+           reported_reason: "unsafe",
+           hazard_type: "Electromagnetic disturbance",
+           hazard_description: "Much fire",
+           non_compliant_reason: "On fire, lots of fire",
+           risk_level: "serious").decorate
+  end
   let!(:other_team_investigation) { create(:allegation, creator: other_user_other_team, is_private: true).decorate }
   let(:params) { { case_type: "all", sort_by: "recent", created_by: "all", case_status: "open", teams_with_access: "all" } }
   let(:case_export) { described_class.create!(user:, params:) }
