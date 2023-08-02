804e289dc9dba1eedb1c27c67b581b2b3ba49f98
Andy Callaghan <github@andycallaghan.com>
2023-08-02 12:45:29 +0100

    replace searchable & custom elasticsearch code with searchkick module

--- config/initializers/elasticsearch.rb
+++
@@ -1,3 +0,0 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new(Rails.application.config_for(:opensearch))
-Elasticsearch::Model.client.instance_variable_set("@verified", true)
-# .merge(trace: true, logger: Rails.logger, log_level: Logger::DEBUG)
