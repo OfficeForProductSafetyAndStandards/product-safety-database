67e0db73807bea47faa3eaab3990b08b1a974832
Andy Callaghan <github@andycallaghan.com>
2023-08-25 14:28:56 +0100

    content security policy
    http header based instructions to user's browser, detailing what scripts can be loaded client side

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,26 +1,65 @@
-# Be sure to restart your server when you modify this file.
+govuk_domains = [
+  "*.london.cloudapps.digital", # For preview apps
+  "*.service.gov.uk",
+].freeze
+google_analytics_domains = %w[www.google-analytics.com
+                              ssl.google-analytics.com
+                              stats.g.doubleclick.net
+                              www.googletagmanager.com
+                              www.region1.google-analytics.com
+                              region1.google-analytics.com]
+google_static_domains = %w[www.gstatic.com]
 
-# Define an application-wide content security policy
-# For further information see the following documentation
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap and inline scripts
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src)
-#
-#   # Report CSP violations to a specified URI. See:
-#   # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-#   # config.content_security_policy_report_only = true
-# end
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/base-uri
+    policy.base_uri :none
+
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/img-src
+    # Note: we purposely don't include `data:` here because it produces a security risk.
+    policy.img_src :self,
+                   *govuk_domains,
+                   *google_analytics_domains # Tracking pixels
+
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/script-src
+    # Note: we purposely don't include `data:`, `unsafe-inline` or `unsafe-eval` because
+    # they are security risks, if you need them for a legacy app please only apply them at
+    # an app level.
+    policy.script_src :self,
+                      *google_analytics_domains,
+                      *google_static_domains
+
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/style-src
+    # Note: we purposely don't include `data:`, `unsafe-inline` or `unsafe-eval` because
+    # they are security risks, if you need them for a legacy app please only apply them at
+    # an app level.
+    policy.style_src :self, *google_static_domains
+
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/font-src
+    # Note: we purposely don't include data here because it produces a security risk.
+    policy.font_src :self
+
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/connect-src
+    policy.connect_src :self,
+                       *govuk_domains,
+                       *google_analytics_domains
+
+    # Disallow all <object>, <embed>, and <applet> elements
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/object-src
+    policy.object_src :none
+
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/frame-src
+    policy.frame_src :self, *govuk_domains
+
+    policy.report_uri ENV.fetch("SENTRY_CSP_REPORT_URI") if ENV["SENTRY_CSP_REPORT_URI"].present?
+  end
+
+  # Generate session nonces for permitted importmap and inline scripts
+  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  config.content_security_policy_nonce_directives = %w[script-src]
+
+  # For now, only report to any reports to Sentry in production
+  config.content_security_policy_report_only = true
+end
