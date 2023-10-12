d6abb116b41e1a2e334e894bf5b8b9a142dc72f2
Andy Callaghan <github@andycallaghan.com>
2023-10-12 10:44:14 +0100

    add notification case type string

--- spec/models/investigation/case_spec.rb
+++ spec/models/investigation/case_spec.rb
@@ -1,9 +1,17 @@
 require "rails_helper"
 
 RSpec.describe Investigation::Case do
-  subject(:case) { build(:case, date_received:).build_owner_collaborations_from(create(:user)) }
+  subject(:case_object) { build(:case) }
+
+  describe "#case_type" do
+    it "returns 'notification'" do
+      expect(case_object.case_type).to eq("notification")
+    end
+  end
 
   describe "#valid?" do
+    subject(:case) { build(:case, date_received:).build_owner_collaborations_from(create(:user)) }
+
     context "with valid date_received" do
       let(:date_received) { 1.day.ago }
 
