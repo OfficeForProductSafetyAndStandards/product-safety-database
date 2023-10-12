40d3f7dc0dbb11b69c3bd0e5b0e268b306d92854
Andy Callaghan <github@andycallaghan.com>
2023-10-12 12:12:06 +0100

    fixes to specs with class name changes

--- spec/models/audit_activity/investigation/add_case_spec.rb
+++ spec/models/audit_activity/investigation/add_case_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe AuditActivity::Investigation::AddCase, :with_stubbed_mailer do
-  let(:factory) { :case }
+  let(:factory) { :notification }
 
   it_behaves_like "an audit activity for investigation added"
 end
