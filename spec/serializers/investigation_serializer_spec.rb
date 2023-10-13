47707fcc66990d8ffdaa4be9a9f6a4cc8b12c167
Andy Callaghan <github@andycallaghan.com>
2023-10-13 16:37:11 +0100

    assert pretty_id is indexed for case

--- spec/serializers/investigation_serializer_spec.rb
+++ spec/serializers/investigation_serializer_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe InvestigationSerializer, :with_stubbed_mailer, :with_test_queue_a
       expect(hash[:type]).to eq(investigation.type)
       expect(hash[:hazard_type]).to eq(investigation.hazard_type)
       expect(hash[:user_title]).to eq(investigation.user_title)
+      expect(hash[:pretty_id]).to eq(investigation.pretty_id)
     end
   end
 end
