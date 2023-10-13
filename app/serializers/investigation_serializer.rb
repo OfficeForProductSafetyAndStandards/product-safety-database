05a53ccbb660cb69ff1a0ddd0e1e431b6e9441b4
Andy Callaghan <github@andycallaghan.com>
2023-10-13 16:35:43 +0100

    assert user title is indexed for case

--- app/serializers/investigation_serializer.rb
+++ app/serializers/investigation_serializer.rb
@@ -2,7 +2,7 @@ class InvestigationSerializer < ActiveModel::Serializer
   attributes :type, :owner_id, :creator_id, :hazard_type,
              :description, :product_category, :is_closed, :updated_at, :created_at,
              :pretty_id, :hazard_description, :non_compliant_reason,
-             :complainant_reference, :risk_level, :title
+             :complainant_reference, :risk_level, :title, :user_title
 
   attribute :creator_user do
     object.creator_user&.id
--- spec/serializers/investigation_serializer_spec.rb
+++ spec/serializers/investigation_serializer_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe InvestigationSerializer, :with_stubbed_mailer, :with_test_queue_a
     it "serializes the investigation object", :aggregate_failures do
       expect(hash[:type]).to eq(investigation.type)
       expect(hash[:hazard_type]).to eq(investigation.hazard_type)
+      expect(hash[:user_title]).to eq(investigation.user_title)
     end
   end
 end
