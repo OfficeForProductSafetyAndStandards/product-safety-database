7f8beb977c5b9f80ffa7ee6539e34ddd9d047a3c
Andy Callaghan <github@andycallaghan.com>
2023-10-18 16:04:13 +0100

    add business link to online marketplaces

--- app/models/online_marketplace.rb
+++ app/models/online_marketplace.rb
@@ -1,4 +1,6 @@
 class OnlineMarketplace < ApplicationRecord
+  belongs_to :business, optional: true
+
   validates :name, presence: true, uniqueness: true
   scope :approved, -> { where(approved_by_opss: true) }
 end
