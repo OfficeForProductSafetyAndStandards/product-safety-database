60207c244da8f98fb8e6373993af14bba0abc958
Andrew Cowle <acowle@byondsoftware.co.uk>
2023-11-02 14:27:24 +0000

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -387,12 +387,7 @@ Rails.application.routes.draw do
   # API routes
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resource :auth
-      resource :me, controller: :me
-      resource :password
-      resources :accounts
-      resources :users
-      resources :notification_tokens, only: :create
+      resource :auth, only: [:create]
     end
   end
 
