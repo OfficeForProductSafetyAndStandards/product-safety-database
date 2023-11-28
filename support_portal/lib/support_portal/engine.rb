fa06a31bead4ad3307648aca35efcc869c632fa8
Ruben Arakelyan <ruben@arakelyan.uk>
2023-11-28 11:19:44 +0000

    Fix redirecting from PSD to support portal
    Fixes an issue where editing records in PSD attempts to redirect to the support portal host.

--- support_portal/lib/support_portal/engine.rb
+++ support_portal/lib/support_portal/engine.rb
@@ -9,16 +9,5 @@ module SupportPortal
     initializer "support_portal.assets.precompile" do |app|
       app.config.assets.precompile << "support_portal_manifest.js"
     end
-
-    if Rails.env.production?
-      initializer "action_controller" do |app|
-        # Override `default_url_options` in the parent app
-        # so that Devise redirects correctly
-        app.config.action_controller.default_url_options = {
-          host: ENV["PSD_HOST_SUPPORT"],
-          protocol: "https"
-        }
-      end
-    end
   end
 end
