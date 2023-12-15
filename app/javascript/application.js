cf02e020db9011f1d555c46ae2517eae6b54784a
Ruben Arakelyan <ruben@arakelyan.uk>
2023-12-15 14:02:05 +0000

    Remove unused menu JavaScript and CSS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,11 +5,9 @@ import './i18n'
 
 import './location_picker'
 import './autocomplete'
-import './menu'
 
 import './investigations/attachment_description'
 import './investigations/sort_dropdown'
-import './investigations/ts_investigations/which_businesses'
 import './variant_select'
 import './dynamic_nested_step_fields'
 import './clear_radios_on_other_input'
