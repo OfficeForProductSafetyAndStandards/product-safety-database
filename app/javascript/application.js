195c9b8355776655e70638a2d82db02503341d1c
Ruben Arakelyan <ruben@arakelyan.uk>
2023-12-15 14:22:50 +0000

    Re-order JavaScript imports

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,11 +4,13 @@ import './controllers'
 
 import './autocomplete'
 
+import './clear_radios_on_other_input'
+import './dynamic_nested_step_fields'
+import './variant_select'
+
 import './investigations/attachment_description'
 import './investigations/sort_dropdown'
-import './variant_select'
-import './dynamic_nested_step_fields'
-import './clear_radios_on_other_input'
+
 import './govuk_frontend'
 
 window.GOVUKFrontend = GOVUKFrontend
