da9b8fce9826265585f9d89a529d37b443786e26
Andy Callaghan <github@andycallaghan.com>
2024-01-04 14:53:29 +0000

    refactor remaining code for ChangeCaseReferenceNumber

--- app/controllers/investigations/reference_numbers_controller.rb
+++ app/controllers/investigations/reference_numbers_controller.rb
@@ -1,18 +1,18 @@
 module Investigations
   class ReferenceNumbersController < Investigations::BaseController
-    before_action :set_investigation
-    before_action :authorize_investigation_updates
-    before_action :set_investigation_breadcrumbs
+    before_action :set_notification
+    before_action :authorize_notification_updates
+    before_action :set_notification_breadcrumbs
 
     def edit; end
 
     def update
-      if @investigation.complainant_reference == reference_number_params[:complainant_reference]
-        return redirect_to investigation_path(@investigation)
+      if @notification.complainant_reference == reference_number_params[:complainant_reference]
+        return redirect_to investigation_path(@notification)
       end
 
-      ChangeCaseReferenceNumber.call!(investigation: @investigation, reference_number: reference_number_params[:complainant_reference], user: current_user)
-      redirect_to investigation_path(@investigation), flash: { success: "The reference number was updated" }
+      ChangeNotificationReferenceNumber.call!(notification: @notification, reference_number: reference_number_params[:complainant_reference], user: current_user)
+      redirect_to investigation_path(@notification), flash: { success: "The reference number was updated" }
     end
 
   private
