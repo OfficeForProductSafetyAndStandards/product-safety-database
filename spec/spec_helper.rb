d1322459e053abbfb4283b80aedc53f290598997
Andy Callaghan <github@andycallaghan.com>
2024-01-16 15:42:20 +0000

    move all rspec config to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,58 @@
-# frozen_string_literal: true
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../config/environment", __dir__)
+require "rspec/rails"
 
 require "webmock/rspec"
+require "simplecov"
+
+# Output coverage in LCOV format for CodeCov in CI environment
+if ENV["CI"]
+  require "simplecov-lcov"
+  SimpleCov::Formatter::LcovFormatter.config do |c|
+    c.report_with_single_file = true
+    c.single_report_path = "coverage/lcov.info"
+  end
+  SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+
+end
+unless ENV["COVERAGE"] == "false"
+  SimpleCov.start "rails" do
+    enable_coverage :branch
+  end
+end
+
+require "domain_helpers"
+require 'sidekiq/testing'
+require "super_diff/rspec-rails"
+
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
+
+# Checks for pending migrations and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove these lines.
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
 
 RSpec.configure do |config|
+  # config.fixture_path = Rails.root.join("test/fixtures")
+  config.include Rails.application.routes.url_helpers
+
+  config.include ActiveSupport::Testing::TimeHelpers
+  config.include ActiveSupport::Testing::Assertions
+
+  config.include PageExpectations, type: :feature
+  config.include EmailExpectations, type: :feature
+
+  config.include DomainHelpers
+
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -24,6 +74,8 @@ RSpec.configure do |config|
       example.run
     end
   end
+
+  config.disable_monkey_patching!
 
   config.example_status_persistence_file_path = "examples.txt"
 
