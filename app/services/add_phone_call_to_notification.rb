968349123817b612f1b6f025975f552d8f370faa
Gilead <nashar101@gmail.com>
2024-01-17 15:39:50 +0000

    fixing CodeStyle error

--- app/services/add_phone_call_to_notification.rb
+++ app/services/add_phone_call_to_notification.rb
@@ -40,7 +40,8 @@ private
         entity.name,
         entity.email,
         email_update_text(entity),
-        email_subject).deliver_later
+        email_subject
+      ).deliver_later
     end
   end
 
