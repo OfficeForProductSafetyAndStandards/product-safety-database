8a7e1d000b0a44dfa70703bcb1c9f22f5a35d435
Andy Callaghan <github@andycallaghan.com>
2024-01-18 13:05:15 +0000

    working placeholder dashboard setup for osu reports

--- report_portal/lib/report_portal/engine.rb
+++ report_portal/lib/report_portal/engine.rb
@@ -1,5 +1,12 @@
+require "govuk/components"
+require "govuk_design_system_formbuilder"
+
 module ReportPortal
   class Engine < ::Rails::Engine
     isolate_namespace ReportPortal
+
+    initializer "report_portal.assets.precompile" do |app|
+      app.config.assets.precompile << "report_portal_manifest.js"
+    end
   end
 end
---
+++ spec/features/report_portal/dashboard_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.feature "Reporting dashboard", type: :feature do
+  let(:user) { create(:user, roles: %w[opss]) }
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user) }
+  let(:user3) { create(:user, name: user1.name) }
+
+  before do
+    configure_requests_for_report_domain
+
+    user1
+    user2
+    user3
+
+    sign_in user
+  end
+
+  after do
+    reset_domain_request_mocking
+  end
+
+  scenario "Searching for an account that exists" do
+    expect(page).to have_h1("Dashboard")
+
+  end
+end
