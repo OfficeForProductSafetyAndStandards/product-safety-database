416109777b15e12bb79191b5f35f0284454e7f72
Ruben Arakelyan <ruben@arakelyan.uk>
2024-01-26 09:50:58 +0000

    Fix step validation

--- app/helpers/notifications/create_helper.rb
+++ app/helpers/notifications/create_helper.rb
@@ -11,7 +11,8 @@ module Notifications
     end
 
     def task_status(task)
-      previous_task = previous_task(task)
+      optional_tasks = Notifications::CreateController::TASK_LIST_SECTIONS.slice(*Notifications::CreateController::TASK_LIST_SECTIONS_OPTIONAL).values.flatten
+      previous_task = TaskListService.previous_task(task:, all_tasks: wizard_steps, optional_tasks:)
 
       if %w[in_progress completed].include?(@notification.tasks_status[task.to_s])
         @notification.tasks_status[task.to_s]
@@ -90,39 +91,6 @@ module Notifications
         Team.all.order(:name).map do |team|
           OpenStruct.new(id: team.id, name: team.name)
         end
-    end
-
-  private
-
-    def previous_task(task)
-      return if first_task?(task)
-
-      all_tasks = wizard_steps
-      optional_tasks = Notifications::CreateController::TASK_LIST_SECTIONS.slice(*Notifications::CreateController::TASK_LIST_SECTIONS_OPTIONAL).values.flatten
-      mandatory_tasks = all_tasks - optional_tasks
-
-      # Return the previous mandatory task or `nil` if this is the first mandatory task
-      index = mandatory_tasks.index(task)
-      unless index.nil?
-        return if index.zero?
-
-        return mandatory_tasks.at(index - 1)
-      end
-
-      # This is an optional task - find and return the last mandatory task or `nil` if we get to the first task without a match
-      previous_index = all_tasks.index(task) - 1
-
-      loop do
-        previous_task = all_tasks.at(previous_index)
-        return previous_task if mandatory_tasks.include?(previous_task)
-        return if previous_index.zero?
-
-        previous_index -= 1
-      end
-    end
-
-    def first_task?(task)
-      wizard_steps.index(task).zero?
     end
   end
 end
