b1fa308b7b747f7e19de46f3fafbfb5597c0ee2e
Ruben Arakelyan <ruben@arakelyan.uk>
2024-01-26 15:09:37 +0000

    Apply restrictions to mutually-exclusive geographic scope checkboxes

---
+++ app/javascript/controllers/dynamic_checkbox_states_controller.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  update (event) {
+    event.preventDefault()
+
+    // Get a list of all the checkboxes that need to be disabled by referencing all the checkboxes that are currently checked
+    const targets = [].concat(...Array.from(this.element.querySelectorAll('input[type=checkbox]:checked')).map((el) => el.dataset.dynamicCheckboxStatesDisableTargets.split(',')))
+    const uniqueTargets = new Set(targets)
+
+    this.element.querySelectorAll('input[type=checkbox]').forEach((el) => {
+      if (uniqueTargets.has(el.value)) {
+        el.disabled = true
+        el.checked = false
+      } else {
+        el.disabled = false
+      }
+    })
+  }
+}
