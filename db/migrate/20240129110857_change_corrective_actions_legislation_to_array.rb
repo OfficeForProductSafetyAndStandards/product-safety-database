aa8e93c6fb9b20b494f13b50af9a30645b7a25c2
Ruben Arakelyan <ruben@arakelyan.uk>
2024-01-29 14:05:36 +0000

    Fix rubocop

--- db/migrate/20240129110857_change_corrective_actions_legislation_to_array.rb
+++ db/migrate/20240129110857_change_corrective_actions_legislation_to_array.rb
@@ -1,4 +1,5 @@
 class ChangeCorrectiveActionsLegislationToArray < ActiveRecord::Migration[7.0]
+  # rubocop:disable Rails/BulkChangeTable
   def up
     change_column :corrective_actions, :legislation, :string, array: true, using: "(string_to_array(legislation, ''))"
     change_column_default :corrective_actions, :legislation, []
@@ -8,4 +9,5 @@ class ChangeCorrectiveActionsLegislationToArray < ActiveRecord::Migration[7.0]
     change_column :corrective_actions, :legislation, :string, using: "(array_to_string(legislation, ','))"
     change_column_default :corrective_actions, :legislation, nil
   end
+  # rubocop:enable Rails/BulkChangeTable
 end
