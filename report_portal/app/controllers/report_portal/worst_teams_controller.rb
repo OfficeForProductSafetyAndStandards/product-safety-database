b075244b9d46c193f3e56aef2e795f05addb5a97
Andy Callaghan <github@andycallaghan.com>
2024-01-30 10:12:18 +0000

    best/worst teams views, finished queries

--- report_portal/app/controllers/report_portal/worst_teams_controller.rb
+++ report_portal/app/controllers/report_portal/worst_teams_controller.rb
@@ -1,7 +1,16 @@
 module ReportPortal
   class WorstTeamsController < ApplicationController
     def index
-      @by_notifications = Team.not_deleted.joins(:owned_notifications).select("teams.name, count(collaborations.*) as notifications_count").group("teams.name").order("notifications_count DESC")
+      @all_teams = Team.not_deleted.order(:name)
+
+      # Teams with no investigations
+      @by_notifications = @all_teams.left_outer_joins(:owned_notifications).where(collaborations: { id: nil })
+
+      # Teams with no products
+      @by_products = @all_teams.left_outer_joins(:owned_products).where(products: { id: nil })
+
+      # Teams with no active users
+      @by_users = @all_teams.left_outer_joins(:users).where('users.last_sign_in_at < ?', 3.months.ago).select('teams.*, count(users.*) as users_count').group('teams.id')
     end
   end
 end
