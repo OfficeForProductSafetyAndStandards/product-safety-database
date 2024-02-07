a55034a86007501ec784370e867ae28ffde34e14
Ruben Arakelyan <ruben@arakelyan.uk>
2024-02-07 15:13:11 +0000

    Fix

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
-  Raise exceptions instead of rendering exception templates.
+  # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment.
