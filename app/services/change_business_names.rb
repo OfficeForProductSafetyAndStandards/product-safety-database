6ef77ceea63c2eeceb54b172f5d72b37191b3090
Chris Gannon <cjgannon@gmail.com>
2024-02-27 19:03:58 +0000

    confirm business details for existing businesses

--- app/services/change_business_names.rb
+++ app/services/change_business_names.rb
@@ -1,13 +1,13 @@
 class ChangeBusinessNames
   include Interactor
 
-  delegate :trading_name, :legal_name, :notification, :business, :user, to: :context
+  delegate :trading_name, :legal_name, :company_number, :business, :user, to: :context
 
   def call
     context.fail!(error: "No trading name supplied") unless trading_name.is_a?(String)
     context.fail!(error: "No user supplied") unless user.is_a?(User)
 
-    business.assign_attributes(legal_name:, trading_name:, added_by_user_id: user.id)
+    business.assign_attributes(legal_name:, trading_name:, company_number:, added_by_user_id: user.id)
 
     business.save!
   end
