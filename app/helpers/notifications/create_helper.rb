496f3cdac635273fe24651ef74fb3ecfe056a408
Gilead <nashar101@gmail.com>
2024-04-09 13:15:08 +0100

    Changed text in Summary lists from labels to anchor links, need to find a way to position them right

--- app/helpers/notifications/create_helper.rb
+++ app/helpers/notifications/create_helper.rb
@@ -138,6 +138,14 @@ module Notifications
       end
     end
 
+    def formatted_notification_pretty_id(notification_pretty_id)
+      "/notifications/#{notification_pretty_id}"
+    end
+
+    def formatted_product(product_id)
+      "/products/#{product_id}"
+    end
+
     def formatted_uploads(uploads)
       uploads.map { |upload| link_to "#{upload.blob.filename} (opens in new tab)", upload.blob, class: "govuk-link", target: "_blank", rel: "noreferrer noopener" if upload.blob.present? }.join("<br>")
     end
