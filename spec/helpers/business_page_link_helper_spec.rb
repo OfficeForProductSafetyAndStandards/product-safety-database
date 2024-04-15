7341bf6a9a6fd75c122a3d2ad27a8ccbe7287926
Gilead <nashar101@gmail.com>
2024-04-15 12:35:13 +0100

    CodeStyle fixes

--- spec/helpers/business_page_link_helper_spec.rb
+++ spec/helpers/business_page_link_helper_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe Notifications::CreateHelper, :with_stubbed_mailer do
     let(:existing_product) { create(:product) }
     let(:team_with_access) { create(:team, name: "Team with access", team_recipient_email: nil) }
     let(:user) { create(:user, :activated, has_viewed_introduction: true, team: team_with_access) }
-    let(:notification)     { create(:notification, creator: user) }
-                
+    let(:notification) { create(:notification, creator: user) }
+
     it "outputs notification anchor link" do
-        expect(formatted_notification_pretty_id(notification.pretty_id)).to eq("<a class=\"govuk-link\" href=\"/notifications/#{notification.pretty_id}\">#{notification.pretty_id}</a>")
+      expect(formatted_notification_pretty_id(notification.pretty_id)).to eq("<a class=\"govuk-link\" href=\"/notifications/#{notification.pretty_id}\">#{notification.pretty_id}</a>")
     end
 
     it "outputs product anchor link" do
-        expect(formatted_product(existing_product.id)).to eq("<a class=\"govuk-link\" href=\"/products/#{existing_product.id}\">psd-#{existing_product.id}</a>")
+      expect(formatted_product(existing_product.id)).to eq("<a class=\"govuk-link\" href=\"/products/#{existing_product.id}\">psd-#{existing_product.id}</a>")
     end
   end
 end
