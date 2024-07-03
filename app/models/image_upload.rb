187073c7c4b8d9c469a4e506d2fa25ca82e0f765
Gilead <nashar101@gmail.com>
2024-07-03 14:51:15 +0100

    Refactored Image form to use GOVUKDesignSystemFormBuilder, made changes to the form validations so that the same file missing error does not appear 3 times.

--- app/models/image_upload.rb
+++ app/models/image_upload.rb
@@ -7,10 +7,11 @@ class ImageUpload < ApplicationRecord
 
   attribute :existing_file_upload_file_id, :string
 
-  validates :file_upload, attached: true, size: { less_than: 100.megabytes, message: "Image must be smaller than 100MB" }
-  validates :file_upload, attached: true, size: { greater_than: 1.bytes, message: "Image must be larger than 0MB" }
+  validates :file_upload, attached: true
+  validates :file_upload, size: { less_than: 100.megabytes, message: "Image must be smaller than 100MB" }
+  validates :file_upload, size: { greater_than: 1.bytes, message: "Image must be larger than 0MB" }
   # Allow standard GIF/JPEG/PNG files as well as WEBP (if downloaded from the web) or HEIC/HEIF (if taken by a smartphone)
-  validates :file_upload, attached: true, content_type: { in: ["image/gif", "image/jpeg", "image/png", "image/heic", "image/heif", "image/webp"], message: "Image must be a GIF, JPEG, PNG, WEBP or HEIC/HEIF file" }
+  validates :file_upload, content_type: { in: ["image/gif", "image/jpeg", "image/png", "image/heic", "image/heif", "image/webp"], message: "Image must be a GIF, JPEG, PNG, WEBP or HEIC/HEIF file" }
   validate :file_is_free_of_viruses
 
 private
