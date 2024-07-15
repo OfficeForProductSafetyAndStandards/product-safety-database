a5dab62e7b89a15c51d09a5072452561e1b44c6e
Alan Reid <alan.reid@triad.co.uk>
2024-07-15 18:25:36 +0100

    Adding in the missing rspec

--- app/services/generate_product_recall_pdf.rb
+++ app/services/generate_product_recall_pdf.rb
@@ -5,6 +5,7 @@ class GenerateProductRecallPdf
     @params = params
     @product = product
     @file = file
+    preload_fonts
   end
 
   def self.generate_pdf(params, product, file)
@@ -21,26 +22,22 @@ class GenerateProductRecallPdf
       CreationDate: Time.zone.now
     }
     pdf = Prawn::Document.new(page_size: "A4", font_size: 11, info: metadata)
-    # rubocop:disable Rails/SaveBang
-    pdf.font_families.update(
-      "Arial" => {
-        normal: { file: Rails.root.join("app/assets/fonts/arial.ttf"), font: "Arial" },
-        bold: { file: Rails.root.join("app/assets/fonts/arial-bold.ttf"), font: "Arial-Bold" },
-      }
-    )
-    # rubocop:enable Rails/SaveBang
+
+    update_font_families(pdf)
+
     pdf.font("Arial")
+
+    # Header
     pdf.table([
-      [{ image: File.open(Rails.root.join("app/assets/images/opss-logo.jpg")), fit: [140, 140] }, { content: title, text_color: "FF0000", font_style: :bold, size: 20, align: :right, valign: :bottom }],
+      [{ image: File.open(Rails.root.join("app/assets/images/opss-logo.jpg")), fit: [140, 140] },
+       { content: title, text_color: "FF0000", font_style: :bold, size: 20, align: :right, valign: :bottom }]
     ], width: pdf.bounds.width, cell_style: { borders: [] })
 
+    # Main Content
     pdf.table([
       [{ content: params["pdf_title"], colspan: 2, font_style: :bold, size: 14 }],
       [{ content: "Aspect", font_style: :bold }, { content: "Details", font_style: :bold }],
-      [
-        { content: "Images", font_style: :bold },
-        build_sub_table(pdf)
-      ],
+      [{ content: "Images", font_style: :bold }, build_sub_table(pdf)],
       [{ content: "Alert Number", font_style: :bold }, params["alert_number"]],
       [{ content: "Product Type", font_style: :bold }, [params["product_type"], params["subcategory"]].compact.join(" - ")],
       [{ content: "Product Identifiers", font_style: :bold }, params["product_identifiers"]],
@@ -52,18 +49,34 @@ class GenerateProductRecallPdf
       [{ content: "Risk Description", font_style: :bold }, params["risk_description"]],
       [{ content: "Corrective Measures", font_style: :bold }, params["corrective_actions"]],
       [{ content: "Online Marketplace", font_style: :bold }, online_marketplace],
-      [{ content: "Notifier", font_style: :bold }, params["notified_by"]],
+      [{ content: "Notifier", font_style: :bold }, params["notified_by"]]
     ].compact, width: pdf.bounds.width, column_widths: { 0 => 120 })
 
+    # Footer
     pdf.repeat :all do
       pdf.bounding_box [pdf.bounds.left, pdf.bounds.bottom + 25], width: pdf.bounds.width do
         pdf.text_box 'The OPSS Product Safety Alerts, Reports and Recalls Site can be accessed at the following link: <color rgb="#0000FF"><u><link href="https://www.gov.uk/guidance/product-recalls-and-alerts">https://www.gov.uk/guidance/product-recalls-and-alerts</link></u></color>', inline_format: true
       end
     end
+
     pdf.render(file)
   end
 
 private
+
+  def preload_fonts
+    @arial_font = Rails.root.join("app/assets/fonts/arial.ttf")
+    @arial_bold_font = Rails.root.join("app/assets/fonts/arial-bold.ttf")
+  end
+
+  def update_font_families(pdf)
+    pdf.font_families.update(
+      "Arial" => {
+        normal: { file: @arial_font, font: "Arial" },
+        bold: { file: @arial_bold_font, font: "Arial-Bold" }
+      }
+    )
+  end
 
   def product_safety_report?
     params["type"] == "product_safety_report"
@@ -75,25 +88,18 @@ private
 
   def build_sub_table(pdf)
     rows = image_rows
-    if rows.present?
+    return if rows.blank?
 
-      flattened_array = rows.flatten(1)
-      new_array = flattened_array.each_slice(3).to_a
+    flattened_array = rows.flatten(1)
+    new_array = flattened_array.each_slice(3).to_a
 
-      pdf.make_cell(new_array, width: pdf.bounds.width - 120)
-    end
+    pdf.make_cell(new_array, width: pdf.bounds.width - 120)
   end
 
   def image_rows
-    if params["product_image_ids"].present?
+    return if params["product_image_ids"].blank?
 
-      rows = []
-      image_ids = params["product_image_ids"].reject(&:blank?)
-      image_ids.each do |image_id|
-        rows << [image_cell(image_id)]
-      end
-      rows
-    end
+    params["product_image_ids"].reject(&:blank?).map { |image_id| [image_cell(image_id)] }
   end
 
   def image_cell(id)
@@ -110,20 +116,20 @@ private
   end
 
   def country_from_code(code)
-    country = Country.all.find { |c| c[1] == code }
-    (country && country[0]) || code
+    Country.all.find { |c| c[1] == code }&.first || code
   end
 
   def counterfeit
-    return "Unknown" if params["counterfeit"].nil?
-
-    { "counterfeit" => "Yes", "genuine" => "No", "unsure" => "Unsure" }[params["counterfeit"]]
+    { "counterfeit" => "Yes", "genuine" => "No", "unsure" => "Unsure" }[params["counterfeit"]] || "Unknown"
   end
 
   def online_marketplace
-    return "N/A" if params["online_marketplace"].nil?
-    return "No" unless params["online_marketplace"]
-
-    params["other_marketplace_name"].presence || params["online_marketplace_id"]
+    if params["online_marketplace"].nil?
+      "N/A"
+    elsif params["online_marketplace"]
+      params["other_marketplace_name"].presence || params["online_marketplace_id"] || "Yes"
+    else
+      "No"
+    end
   end
 end
