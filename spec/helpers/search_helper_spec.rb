7cfba01cacd98ec4207d6dacaac51be5aa899951
tahir-khalid <tahir.khalid@triad.co.uk>
2024-07-29 16:58:53 +0100

    Initial commit

---
+++ spec/helpers/search_helper_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe SearchHelper do
+  describe "#" do
+  end
+end
+
+# set_search_params
+# search_params
+# search_query(user)
+# query_params
+# sorting_params
+# filter_params(user)
+# page_number
+# get_owner_filter(user)
+# compute_excluded_terms(user)
+# format_owner_terms(owner_array)
