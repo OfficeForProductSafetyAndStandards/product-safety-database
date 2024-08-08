b06d9a71c830119a7bf0ff9714897c8c175cc03d
tahir-khalid <tahir.khalid@triad.co.uk>
2024-08-08 11:44:22 +0100

    restored unless conditional

--- app/services/create_notification.rb
+++ app/services/create_notification.rb
@@ -12,7 +12,7 @@ class CreateNotification
     notification.creator_user = user
     notification.creator_team = team
     notification.notifying_country = team.country
-    notification.state ||= "draft"
+    notification.state ||= "draft" unless from_task_list
 
     ActiveRecord::Base.transaction do
       # This ensures no other pretty_id generation is happening concurrently.
