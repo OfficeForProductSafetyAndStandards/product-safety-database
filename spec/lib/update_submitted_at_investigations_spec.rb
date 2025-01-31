e7b5ebab0bdc045e88c0b34df5ef48667695d100
meeravnagesh <meera.nagesh@triad.co.uk>
2025-01-31 14:23:22 +0000

    PSD-4457 - data change , rake task to update notifications

---
+++ spec/lib/update_submitted_at_investigations_spec.rb
@@ -0,0 +1,29 @@
+# rubocop:disable RSpec/DescribeClass
+require "rails_helper"
+require "rake"
+Rails.application.load_tasks
+
+RSpec.describe "investigations:update_submitted_date", :with_stubbed_antivirus, :with_stubbed_mailer do
+  let(:user) { create(:user, :opss_user, :activated, has_viewed_introduction: true, roles: %w[notification_task_list_user]) }
+  let!(:investigation_with_nil_submitted_at) { create(:notification, submitted_at: nil, created_at: 1.day.ago) }
+  let!(:investigation_with_submitted_at) { create(:notification, submitted_at: Time.zone.now, created_at: 2.days.ago) }
+
+  before do
+    # Clear the task before each test to ensure it can be re-invoked
+    Rake.application["investigations:update_submitted_date"].reenable
+  end
+
+  it "updates submitted_at with created_at for investigations where submitted_at is nil" do
+    # Run the task
+    Rake::Task["investigations:update_submitted_date"].invoke
+
+    # Reload the objects to get the updated values
+    investigation_with_nil_submitted_at.reload
+    investigation_with_submitted_at.reload
+
+    # Expectations
+    expect(investigation_with_nil_submitted_at.submitted_at).to eq(investigation_with_nil_submitted_at.created_at)
+    expect(investigation_with_submitted_at.submitted_at).not_to eq(investigation_with_submitted_at.created_at)
+  end
+end
+# rubocop:enable RSpec/DescribeClass
