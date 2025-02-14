6a2fdff2bbd841d054494e61afc09b49e6b3d763
Miro Palka <miroslav.palka@businessandtrade.gov.uk>
2025-02-14 15:06:05 +0000

    fix/PSD-3911-improve_forbidden_access_handling - Remove out-of-scope validation message changes from document_upload.rb

--- app/models/document_upload.rb
+++ app/models/document_upload.rb
@@ -7,10 +7,9 @@ class DocumentUpload < ApplicationRecord
 
   attribute :existing_file_upload_file_id, :string
 
-  validates :file_upload, attached: { message: "Select a file" }
-  validates :file_upload, size: { less_than: 100.megabytes, message: "File must be smaller than 100MB" }, if: -> { file_upload.attached? }
-  validates :file_upload, size: { greater_than: 1.byte, message: "File must be larger than 0MB" }, if: -> { file_upload.attached? }
-  validates :title, presence: { message: "Enter a document title" }
+  validates :file_upload, attached: true, size: { less_than: 100.megabytes, message: "File must be smaller than 100MB" }
+  validates :file_upload, attached: true, size: { greater_than: 1.byte, message: "File must be larger than 0MB" }
+  validates :title, presence: true
   validates :description, length: { maximum: 10_000 }
   validate :file_is_free_of_viruses
 
