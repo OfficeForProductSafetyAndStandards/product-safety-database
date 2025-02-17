07a78e26b77b2ee6c00a55db49f53df7199b697c
Miro Palka <miroslav.palka@businessandtrade.gov.uk>
2025-02-17 15:29:41 +0000

    fix/PSD-3911-cleanup_document_handling - Remove unnecessary document_upload_ids changes - Remove migrations that were only added in this branch - Remove document_upload_ids from ignored_columns

--- db/migrate/20250217150315_remove_document_upload_ids_from_investigations.rb
+++
@@ -1,7 +0,0 @@
-class RemoveDocumentUploadIdsFromInvestigations < ActiveRecord::Migration[7.0]
-  disable_ddl_transaction!
-
-  def change
-    safety_assured { remove_column :investigations, :document_upload_ids, :bigint, array: true, default: [], if_exists: true }
-  end
-end
