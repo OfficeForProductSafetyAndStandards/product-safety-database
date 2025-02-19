b2878d3b9083def03a900e287ba8b38648511879
Miro Palka <miroslav.palka@businessandtrade.gov.uk>
2025-02-19 17:32:52 +0000

    feat(PSD-3911): Add with_stubbed_mailer tag to add_supporting_documents_controller_spec and updated factories and specs.

--- spec/factories/notifications.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :supporting_document_notification, class: "Investigation::Notification" do
-    sequence(:pretty_id) { |n| "#{Time.zone.now.strftime('%y%m')}-#{n.to_s.rjust(4, '0')}" }
-    association :creator_user, factory: :user
-    association :owner_team, factory: :team
-    association :creator_team, factory: :team
-  end
-end
