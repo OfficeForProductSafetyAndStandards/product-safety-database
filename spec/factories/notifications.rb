2a2c6d61ac76d849194a18b76637679ebcca29a6
Miro Palka <miroslav.palka@businessandtrade.gov.uk>
2025-02-19 15:42:27 +0000

    Add supporting document notification factory for testing

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :supporting_document_notification, class: "Investigation::Notification" do
-    sequence(:pretty_id) { |n| "2502-#{n.to_s.rjust(4, '0')}" }
+    sequence(:pretty_id) { |n| "#{Time.zone.now.strftime('%y%m')}-#{n.to_s.rjust(4, '0')}" }
     association :creator_user, factory: :user
     association :owner_team, factory: :team
     association :creator_team, factory: :team
