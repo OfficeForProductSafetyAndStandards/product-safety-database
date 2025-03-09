108cfb9d88f771d5192eeab0c27922888f57546c
“Meera_Nagesh” <meera.nagesh@triad.co.uk>
2025-03-09 15:37:34 +0000

    PSD-4770 test updates

--- spec/factories/image_uploads.rb
+++ spec/factories/image_uploads.rb
@@ -42,4 +42,35 @@ FactoryBot.define do
       model.update!(image_upload_ids: [image_upload.id])
     end
   end
+
+  # This trait is used to create an image upload independently of a model
+  trait :with_antivirus_safe_image_upload do
+    with_image_upload_info
+    after(:create) do |image_upload|
+      file = ActiveStorage::Blob.create_and_upload!(
+        io: File.open(Rails.root.join("test/fixtures/files/testImage.png")),
+        filename: "testImage.png",
+        content_type: "image/png"
+      )
+
+      image_upload.file_upload.attach(file)
+      image_upload.file_upload.metadata = { "safe" => true }
+      image_upload.file_upload.save!
+    end
+  end
+
+  trait :with_virus_image_upload do
+    with_image_upload_info
+    after(:create) do |image_upload|
+      file = ActiveStorage::Blob.create_and_upload!(
+        io: File.open(Rails.root.join("test/fixtures/files/testImage.png")),
+        filename: "testImage.png",
+        content_type: "image/png"
+      )
+
+      image_upload.file_upload.attach(file)
+      image_upload.file_upload.metadata = { "safe" => false }
+      image_upload.file_upload.save!
+    end
+  end
 end
