7e62b28b367bb771ada31f6c14252fdee04c3b7f
aamircodes <aamir.patel@digital.trade.gov.uk>
2025-03-20 10:30:08 +0000

    fix: add mkdir pids

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,5 @@
+require "fileutils"
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -20,6 +22,8 @@ port ENV.fetch("PORT", 3000)
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
+
+FileUtils.mkdir_p("tmp/pids")
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
