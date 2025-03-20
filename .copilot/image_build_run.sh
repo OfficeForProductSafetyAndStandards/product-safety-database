971eb87a75cafb76ed826692bc93caaf9f3666e1
aamircodes <aamir.patel@digital.trade.gov.uk>
2025-03-20 13:08:18 +0000

    remove public workspace from image build script

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -6,11 +6,8 @@ set -e
 # Add commands below to run inside the container after all the other buildpacks have been applied
 
 cp -r /workspace/db/ /workspace/db-copy/
-cp -r /workspace/public/ /workspace/public-copy/
 
 rm -rf /workspace/tmp
 rm -rf /workspace/db
-rm -rf /workspace/public
 rm -rf /workspace/log
 
-export TMPDIR=/workspace/tmp
