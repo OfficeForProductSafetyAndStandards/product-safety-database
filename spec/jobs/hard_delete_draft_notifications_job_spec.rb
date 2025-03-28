5fa967fe9ebd9bc12d53ff8b2ba6fc01feeee066
Miroslav Palka <miroslav.palka@businessandtrade.gov.uk>
2025-03-28 09:54:08 +0000

    PSD-4832: Add hard delete for draft notifications over 90 days old (#3423)

--- spec/jobs/hard_delete_draft_notifications_job_spec.rb
+++ spec/jobs/hard_delete_draft_notifications_job_spec.rb
@@ -1,30 +1,30 @@
 require "rails_helper"
 
-RSpec.describe SoftDeleteDraftNotificationsJob, type: :job do
+RSpec.describe HardDeleteDraftNotificationsJob, type: :job do
   describe "#perform" do
     subject(:job) { described_class.new }
 
     context "when the feature flag is disabled" do
       before do
         allow(Flipper).to receive(:enabled?).with(:submit_notification_reminder).and_return(false)
-        allow(Investigation::Notification).to receive(:soft_delete_old_drafts!)
+        allow(Investigation::Notification).to receive(:hard_delete_old_drafts!)
       end
 
-      it "does not call soft_delete_old_drafts!" do
+      it "does not call hard_delete_old_drafts!" do
         job.perform
-        expect(Investigation::Notification).not_to have_received(:soft_delete_old_drafts!)
+        expect(Investigation::Notification).not_to have_received(:hard_delete_old_drafts!)
       end
     end
 
     context "when the feature flag is enabled" do
       before do
         allow(Flipper).to receive(:enabled?).with(:submit_notification_reminder).and_return(true)
-        allow(Investigation::Notification).to receive(:soft_delete_old_drafts!)
+        allow(Investigation::Notification).to receive(:hard_delete_old_drafts!)
       end
 
-      it "calls soft_delete_old_drafts! on Investigation::Notification" do
+      it "calls hard_delete_old_drafts! on Investigation::Notification" do
         job.perform
-        expect(Investigation::Notification).to have_received(:soft_delete_old_drafts!)
+        expect(Investigation::Notification).to have_received(:hard_delete_old_drafts!)
       end
     end
   end
