15d8cb44167b6c3cbaaa93ead91b4609d70506c7
Ruben Arakelyan <ruben.arakelyan2@businessandtrade.gov.uk>
2025-04-30 10:46:35 +0100

    feat/psd-5260-update-how-to-use-psd-page

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -8,7 +8,9 @@ class HelpController < ApplicationController
 
   def privacy_notice; end
 
-  def about; end
+  def about
+    @taxonomy_export_file_path = taxonomy_export_file_path
+  end
 
   def accessibility; end
 
@@ -16,5 +18,18 @@ class HelpController < ApplicationController
 
   def hide_nav?
     !(current_user.present? && current_user.has_accepted_declaration)
+  end
+
+private
+
+  def taxonomy_export_file_path
+    latest_product_taxonomy_import = ProductTaxonomyImport.completed.last
+    latest_file = latest_product_taxonomy_import&.export_file
+
+    if latest_file.present?
+      Rails.application.routes.url_helpers.rails_storage_proxy_path(latest_file, only_path: true)
+    else
+      "#"
+    end
   end
 end
