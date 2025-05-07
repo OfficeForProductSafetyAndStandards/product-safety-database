b21ec39137ea08e2245e61c6acd0fe9e96f9af6e
Miroslav Palka <miroslav.palka@businessandtrade.gov.uk>
2025-05-07 14:22:46 +0100

    fix: Remove request ID prefix from ASIM logs (#3482)

--- lib/formatters/json_formatter.rb
+++ lib/formatters/json_formatter.rb
@@ -1,8 +1,9 @@
 module Formatters
   class JsonFormatter < ::Logger::Formatter
     def call(_severity, _time, _progname, msg)
+      # Remove any request ID prefix from the message
+      msg = msg.gsub(/^\[[^\]]+\]\s*/, "")
       # Output only the message (which should be JSON) without Rails logger prefix
-      # This removes the "I, [2025-04-30T13:55:43.349479 #1] INFO -- : [905c0821-f6ac-431e-8e46-0a55d862e2b4]" prefix
       "#{msg}\n"
     end
   end
